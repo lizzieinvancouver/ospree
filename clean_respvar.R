5daa2be1941767d26eaba1b05b8ed6a0aee9cc52
jsamaha <jsamaha@fas.harvard.edu>
2016-01-21 09:16:00 -0500

    Starting clean_respvar.R
    Jehane’s first commit of this new R script.

---
+++ clean_respvar.R
@@ -0,0 +1,4 @@
+#Started Thursday January 21 by Jehane
+#An R script to clean the "respvar" column of the bud burst data (currently growthchambers_litreview_2016-01-15.csv, with most updated version in github> tsavas>budreview
+
+
