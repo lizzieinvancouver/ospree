c08edcb70176d492ea401c2e5fc7dcf32b870b57
jsamaha <jsamaha@fas.harvard.edu>
2016-01-21 17:02:56 -0500

    Most respvars now fixed
    Stopping for now— remaining problems are too sticky/ may be necessary
    to convert respvars to other units, etc.

--- clean_respvar.R
+++ clean_respvar.R
@@ -13,11 +13,12 @@ scrapedata <- read.csv ("growthchambers_litreview_2016-01-15.csv")
 
 names(table(scrapedata$respvar))
 
+#Fixing obvious typos and synonmyms:
 scrapedata$respvar[scrapedata$respvar == "days to budbreak (on  50% of plants)"] <- "daysto50%budburst"
 scrapedata$respvar[scrapedata$respvar == "daysto50perbudburst"] <- "daysto50%budburst"
-
 scrapedata$respvar[scrapedata$respvar == "daystodudburst"] <- "daystobudburst"
 scrapedata$respvar[scrapedata$respvar == "daystobudbust"] <- "daystobudburst"
+scrapedata$respvar[scrapedata$respvar == "daystobudset"] <- "daystobudburst"
 
 scrapedata$respvar[scrapedata$respvar == "percentbudsburst"] <- "percentbudburst"
 scrapedata$respvar[scrapedata$respvar == "percent_apicalbudburst"] <- "percentbudburst"
@@ -28,7 +29,7 @@ scrapedata$respvar[scrapedata$respvar == "percentlateralbudburst"] <- "percentbu
 scrapedata$respvar[scrapedata$respvar == "percentapicalbudburst"] <- "percentbudburst"
 scrapedata$respvar[scrapedata$respvar == "percentbloom"] <- "percentbudburst"
 scrapedata$respvar[scrapedata$respvar == "percentflowering"] <- "percentbudburst"
-
+scrapedata$respvar[scrapedata$respvar == "mean percent budbreak at end of study"] <- "percentbudburst"
 
 scrapedata$respvar[scrapedata$respvar == "growth rate 1/days to 25 pct budburst"] <- "1/daysto25%budburst"
 scrapedata$respvar[scrapedata$respvar == "cumulative growth increment"] <- "cumulativegrowthincrement"
@@ -36,9 +37,20 @@ scrapedata$respvar[scrapedata$respvar == "cumulative leaf no. increment"] <- "cu
 scrapedata$respvar[scrapedata$respvar == "cumulative leaf increment"] <- "cumulativeleafincrement"
 scrapedata$respvar[scrapedata$respvar == "numofnewleaves"] <- "numberofleaves"
 
+#responses reported as scores (for examples, scores on the BBCH scale) should be reported as "budstage"
 scrapedata$respvar[scrapedata$respvar == "leafemergencescore"] <- "budstage"
+#"budphenology" seems to be integers from 1 through 8, so included in "budstage"
+scrapedata$respvar[scrapedata$respvar == "budphenology"] <- "budstage"
 scrapedata$respvar[scrapedata$respvar == "daystoleafunfolding"] <- "daystoleafout"
-scrapedata$respvar[scrapedata$respvar == ""] <- ""
+
+#"elongation_heightgrowth" has values from 20-95 for populus seedlings, "elongation_height" has values from 0-9 for betula seedlings. Can we assume all elongation is in centimeters, and combine them? Unclear.
+scrapedata$respvar[scrapedata$respvar == "elongation (cm)"] <- "elongation"
+scrapedata$respvar[scrapedata$respvar == "elongation_height"] <- "elongation"
+scrapedata$respvar[scrapedata$respvar == "elongation_heightgrowth"] <- "elongation"
+scrapedata$respvar[scrapedata$respvar == "shootelongation"] <- "elongation"
+scrapedata$respvar[scrapedata$respvar == "shootlength"] <- "elongation"
+scrapedata$respvar[scrapedata$respvar == "shootlengthcm"] <- "elongation"
+
 scrapedata$respvar[scrapedata$respvar == ""] <- ""
 scrapedata$respvar[scrapedata$respvar == ""] <- ""
 scrapedata$respvar[scrapedata$respvar == ""] <- ""
