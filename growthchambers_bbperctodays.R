1ad1d6484102b0d169964d25e3b73682b725a58b
AileneKane <aettinger@fasmail.harvard.edu>
2016-01-21 15:39:01 -0500

    further updates
    still a few things to work out!

--- growthchambers_bbperctodays.R
+++ growthchambers_bbperctodays.R
@@ -5,22 +5,22 @@
 
 rm(list=ls()) 
 options(stringsAsFactors=FALSE)
-
-#setwd("~/Dropbox/Documents/Work/Wolkovich/retreat")
-#get_dbb_est <- function(dat,percentbb){
-#  dat_final <- data.frame(matrix(data=NA,nrow=0,ncol=0))
-dat<-read.csv("growthchambers_litreview_2016-01-15.csv")
-head(dat)
-unique(dat$respvar)
-dat$dbb<-NA
+get_dbb_est <- function(dat){
+dat <- read.csv(dat, header=TRUE)
+#dat<-read.csv("growthchambers_litreview_2016-01-15.csv", header=T)
+#head(dat)
+#unique(dat$respvar)
+#dat$dbb<-NA
+dat_final <- data.frame(matrix(data=NA,nrow=0,ncol=6))
+colnames(dat_final) <- c("datasetID","study","treatment","dbb","maxperc_bb","minperc_bb")
 ##Sites that use percent budburst
-percbbsites<-dat[which(dat$respvar=="percentbudburst"),]
+percbbsites<-dat[which(dat$respvar=="percentbudburst"),]#could add others here...
 dim(percbbsites)
 dataset<-unique(percbbsites$datasetID)
 for(i in 1:length(dataset)){
 dat1<-percbbsites[which(percbbsites$datasetID==dataset[i]),]
 study<-unique(dat1$study)
-##make new column that ombines all treatments (columns 1:33) into one columns
+##make new column that combines all treatments (columns 1:33) into one columns
 dat1$treatment <- paste(dat1$genus,dat1$species, dat1$varetc,dat1$population,dat1$other.treatment,dat1$dormancy_induction_temp,dat1$dormancy_induction_days,dat1$dormancy_induction_photoperiod_day,dat1$dormancy_induction_photoperiod_night,dat1$freeze.treatment.time,dat1$freeze.treatment.photoperiod_day,dat1$freeze.treatment.photoperiod_night,dat1$freeze.treatment.temp_day,dat1$freeze.treatment.temp_night,dat1$fieldchill,dat1$chilltemp,dat1$chillphotoperiod,dat1$chilldays,dat1$number.longdays,dat1$photoperiod_day,dat1$photoperiod_night,dat1$forcetemp,dat1$forcetemp_night, dat1$irradiance,dat1$humidity,sep = ".")
 for(j in 1:length(study)){
   dat2<-dat1[which(dat1$study==study[j]),]
@@ -28,14 +28,30 @@ for(j in 1:length(study)){
   for(k in 1:length(treat))
   dat3<-dat2[which(dat2$treatment==treat[k]),]
   maxperc_bb<-max(dat2$response)#maxpercent budburst
-  dbb<-dat3[which(dat3$response==50),]$response.time#days to 50%bb
-  if(length(dbb)==0){
-    dbb<-dat3[which(abs(dat3$response-50)==min(abs(dat3$response-50))),]$response.time}#choose closest value to 50 for percent 
-    dbb_perc<-dat3[which(abs(dat3$response-50)==min(abs(dat3$response-50))),]$response}#percemt
-  if(dbb_perc>55|dbb_perc<45){ ###need to fix this so that it uses the latest 0 and the earliest max percent as start and end points for percent                      
-    mod<-lm(as.numeric(dat3$response.time)~as.numeric(dat3$response))  #only use days to bb estimate if the percent is >45 or <55. If not, then use regression to get estimate for days to 50 percent
-    dbb<-coef(mod)[1]+coef(mod)[2]*50
-  }
+  minperc_bb<-min(dat2$response)#minpercent budburst- hopefully zero
+  if(length(dat3[which(dat3$response==50),]$response.time)==1){dbb<-dat3[which(dat3$response==50),]$response.time}else#days to 50%bb if there is exactly one measurement at 50% bb
+  if(length(dat3[which(dat3$response==50),]$response.time)==0){
+    dbb<-dat3[which(abs(dat3$response-50)==min(abs(dat3$response-50))),]$response.time#choose closest value to 50 for percent 
+    dbb_perc<-dat3[which(abs(dat3$response-50)==min(abs(dat3$response-50))),]$response
+    }#percemt
+  if(dbb_perc>55|dbb_perc<45 & length(dat3$response)>1){ ###need to fix this so that it uses the latest 0 and the earliest max percent as start and end points for percent                      
+    dat3$response=round(dat3$response, digits=0)
+    dat4<-dat3[which(dat3$response==min(dat3$response, na.rm=TRUE)):min(which(dat3$response==max(dat3$response,na.rm=TRUE)),na.rm=TRUE),]
+    mod<-lm(as.numeric(dat4$response.time)~as.numeric(dat4$response))  #only use days to bb estimate if the percent is >45 or <55. If not, then use regression to get estimate for days to 50 percent
+    dbb<-coef(mod)[1]+coef(mod)[2]*50}
+  
+  if(dbb_perc>55|dbb_perc<45 & length(na.omit(dat3$response))<2){ ###need to fix this so that it uses the latest 0 and the earliest max percent as start and end points for percent                      
+    dat3$response=round(dat3$response, digits=0)
+    dat4<-dat3[which(dat3$response==min(dat3$response, na.rm=TRUE)):min(which(dat3$response==max(dat3$response,na.rm=TRUE)),na.rm=TRUE),]
+    mod<-lm(as.numeric(dat4$response.time)~as.numeric(dat4$response))  #only use days to bb estimate if the percent is >45 or <55. If not, then use regression to get estimate for days to 50 percent
+    dbb<-coef(mod)[1]+coef(mod)[2]*50}
+    sum_dbb<-cbind(dataset[i],study[j],treat[k],dbb,maxperc_bb,minperc_bb)
+    colnames(sum_dbb) <- c("datasetID","study","treatment","dbb","maxperc_bb","minperc_bb")
 }
+dat_final<- rbind(dat_final,sum_dbb)
 }
+return(dat_final)
+
+}
+
 ##
