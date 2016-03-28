aab9cde83948302bef2a83a4527fc2ac3c68f16c
Dan Flynn <danfbflynn@gmail.com>
2016-03-28 11:18:49 -0400

    Small edits for summarizing
    merge issues cleaned up, again

--- fixing merge issues.R
+++ fixing merge issues.R
@@ -2,7 +2,7 @@
 
 setwd("~/Documents/git/budreview")
 
-d <- read.csv("growthchambers_litreview-CLEANING.csv") 
+d <- read.csv("growthchambers_litreview_CLEANING.csv") 
 
 # Look at each cleaning block. Make unique identifiers for rows. Make master list, and put only unique rows in it.
 
@@ -14,8 +14,10 @@ names(d)
 d$uniqueid = 
   apply(d[,c("datasetID","study","genus","species","population","population.detail",
               "forcetemp","photoperiod_day","chilltemp","chilldays",
-                "respvar","response","response.time","n","response..pre.treatment.",
-                    "error.type","resp_error", "number.longdays","dormancy_induction_temp","freeze.treatment.time"
+                "respvar","response","response.time"
+             #, "n","response..pre.treatment."
+             #  , "error.type","resp_error", "number.longdays","dormancy_induction_temp_day","freeze.treatment.time"
+             
                       )], 1,
         function(x)
           paste(x
@@ -28,6 +30,8 @@ summary(duplicated(d$uniqueid)) # should be >10k, would rather still have some d
 # Start with the last block. Add any from previous blocks which are unique
 nrow(d) # with many duplicates
 
+d$CleanBlock = d$Group
+
 a <- d[d$CleanBlock == max(d$CleanBlock,na.rm = T),]
 
 nrow(a)
@@ -36,20 +40,20 @@ nrow(a)
 summary(duplicated(a$uniqueid)) # 
 
 a1 <- d[d$CleanBlock == 1,]
-a2 <- d[d$CleanBlock == 2,]
-a3 <- d[d$CleanBlock == 3,]
-a4 <- d[d$CleanBlock == 4,]
+#a2 <- d[d$CleanBlock == 2,]
+#a3 <- d[d$CleanBlock == 3,]
+#a4 <- d[d$CleanBlock == 4,]
 
 # look at duplicates  
-summary(!a4$uniqueid %in% a$uniqueid)
-summary(!a3$uniqueid %in% a$uniqueid)
-summary(!a2$uniqueid %in% a$uniqueid)
+#summary(!a4$uniqueid %in% a$uniqueid)
+#summary(!a3$uniqueid %in% a$uniqueid)
+#summary(!a2$uniqueid %in% a$uniqueid)
 summary(!a1$uniqueid %in% a$uniqueid)
 
 a <- rbind(a, a1[!a1$uniqueid %in% a$uniqueid,])
-a <- rbind(a, a2[!a2$uniqueid %in% a$uniqueid,])
-a <- rbind(a, a3[!a3$uniqueid %in% a$uniqueid,])
-a <- rbind(a, a4[!a4$uniqueid %in% a$uniqueid,])
+#a <- rbind(a, a2[!a2$uniqueid %in% a$uniqueid,])
+#a <- rbind(a, a3[!a3$uniqueid %in% a$uniqueid,])
+#a <- rbind(a, a4[!a4$uniqueid %in% a$uniqueid,])
 
 dim(a)
 
