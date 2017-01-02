0d548dcafe5a7bef25d0f5488894e3f4ba7e3b03
cchambe12 <CatherineChamberlain@Catherines-MacBook-Pro-2.local>
2017-01-02 15:06:57 -0500

    updated study type table to include Zohner data

--- source/Zohner.studytype.R
+++ source/Zohner.studytype.R
@@ -1,9 +1,6 @@
-## Started 7 July 2016 ##
-## By Cat and Lizzie##
-
-## An R script to organize the studies by experiment type for the bud burst data. ##
-## Studies were organized by number of field sampling dates, photoperiods, ##
-## forcing temperatures, and experimental chilling hours. ##
+## Using Dan's Zohner Data to integrate into Study Type Table ##
+## Started January 2 2017 - Cat ##
+## Using same script as Weinberger ##
 
 # Clear workspace
 rm(list=ls()) # remove everything currently held in the R memory
@@ -17,33 +14,31 @@ library(ggplot2)
 library(lubridate)
 
 setwd("~/Documents/git/ospree/analyses/input")
-ospree <- read.csv("ospree_clean_respvar.csv", header=TRUE)
+ospree <- read.csv("zohner_formated.csv", header=TRUE, fileEncoding="latin1")
 
 ## Variation in Field Sample Dates
 woody<- ospree %>%
-  dplyr::select(datasetID,study,genus,species,woody,ID_fieldsample.date,fieldsample.date,
-         Exp_Chilling_Hours,photoperiod_day,photoperiod_night,
-         Total_Chilling_Hours,respvar.simple,response.time,forcetemp) %>%
+  dplyr::select(datasetID,study,genus,species,woody,fieldsample.date,
+                photoperiod_day,photoperiod_night,
+                response.time,forcetemp) %>%
   filter(woody=="yes")%>%
   unite(genus.species, genus, species, sep=".") 
 woody <- woody %>%
-  group_by(datasetID, study, genus.species, respvar.simple) %>%
+  group_by(datasetID, study, genus.species) %>%
   mutate(fieldsample.date = replace(fieldsample.date,
                                     fieldsample.date<0, NA))%>%
   mutate(photoperiod_day = replace(photoperiod_day,
-                                    photoperiod_day<0, NA))%>%
+                                   photoperiod_day<0, NA))%>%
   mutate(forcetemp = replace(forcetemp,forcetemp<0, NA))
 
-respvar<-as.data.frame(table(woody$respvar.simple))
-
 field<-woody %>% ## studies with field sampling dates
-  dplyr::select(fieldsample.date,datasetID,study,genus.species, respvar.simple)%>%
+  dplyr::select(fieldsample.date,datasetID,study,genus.species)%>%
   group_by(fieldsample.date,datasetID)%>%
   filter(!is.na(fieldsample.date))%>%
   filter(row_number()==1)
 
 none.field<-woody%>% ## studies without field sampling dates
-  dplyr::select(datasetID,study, genus.species,fieldsample.date, respvar.simple)%>%
+  dplyr::select(datasetID,study, genus.species,fieldsample.date)%>%
   filter(is.na(fieldsample.date))%>%
   group_by(datasetID)%>%
   arrange(datasetID)%>%
@@ -65,13 +60,13 @@ weinberger<-full_join(woody,fieldsample,by="datasetID")%>%
 
 ## Number of Species
 species<-woody %>% 
-  dplyr::select(genus.species,datasetID,study, respvar.simple)%>%
+  dplyr::select(genus.species,datasetID,study)%>%
   group_by(genus.species,datasetID)%>%
   filter(!is.na(genus.species))%>%
   filter(row_number()==1)
 
 none.species<-woody%>%
-  dplyr::select(datasetID,study,genus.species,respvar.simple)%>%
+  dplyr::select(datasetID,study,genus.species)%>%
   filter(is.na(genus.species))%>%
   group_by(datasetID)%>%
   arrange(datasetID)%>%
@@ -91,13 +86,13 @@ weinberger<-full_join(weinberger,genus.species,by="datasetID")%>%
 
 ## Variation in Photoperiod
 photo<-woody %>% 
-  dplyr::select(photoperiod_day,datasetID,study,genus.species, respvar.simple)%>%
+  dplyr::select(photoperiod_day,datasetID,study,genus.species)%>%
   group_by(photoperiod_day,datasetID)%>%
   filter(!is.na(photoperiod_day))%>%
   filter(row_number()==1)
 
 none.photo<-woody%>%
-  dplyr::select(datasetID,study,genus.species,photoperiod_day, respvar.simple)%>%
+  dplyr::select(datasetID,study,genus.species,photoperiod_day)%>%
   filter(is.na(photoperiod_day))%>%
   group_by(datasetID)%>%
   arrange(datasetID)%>%
@@ -114,16 +109,16 @@ photo.day<-full_join(photoperiods,none.photo,by="datasetID")%>%
 weinberger<-full_join(weinberger,photo.day,by="datasetID")%>%
   arrange(datasetID)%>%
   filter(row_number()==1)
-  
+
 ## Variation in Forcing
 forcing<-woody %>% 
-  dplyr::select(forcetemp,datasetID,study,genus.species, respvar.simple)%>%
+  dplyr::select(forcetemp,datasetID,study,genus.species)%>%
   group_by(forcetemp,datasetID)%>%
   filter(!is.na(forcetemp))%>%
   filter(row_number()==1)
 
 none.force<-woody%>%
-  dplyr::select(datasetID,study,genus.species,forcetemp, respvar.simple)%>%
+  dplyr::select(datasetID,study,genus.species,forcetemp)%>%
   filter(is.na(forcetemp))%>%
   group_by(datasetID)%>%
   arrange(datasetID)%>%
@@ -141,40 +136,14 @@ weinberger<-full_join(weinberger,forcetemp.day,by="datasetID")%>%
   arrange(datasetID)%>%
   filter(row_number()==1)
 
-## Variation in Experimental Chilling
-expchill<-woody %>% 
-  dplyr::select(Exp_Chilling_Hours,datasetID,study,genus.species, respvar.simple)%>%
-  group_by(Exp_Chilling_Hours,datasetID)%>%
-  filter(!is.na(Exp_Chilling_Hours))%>%
-  filter(row_number()==1)
-
-none.expchill<-woody%>%
-  dplyr::select(datasetID,study,genus.species,Exp_Chilling_Hours, respvar.simple)%>%
-  filter(is.na(Exp_Chilling_Hours))%>%
-  group_by(datasetID)%>%
-  arrange(datasetID)%>%
-  filter(row_number()==1)
-
-expchilling<-as.data.frame(table(expchill$datasetID)) %>%
-  rename("datasetID"=Var1)%>%
-  rename("expchill.count"=Freq)
-
-expchill.hours<-full_join(expchilling,none.expchill,by="datasetID")%>%
-  dplyr::select(datasetID,expchill.count)%>%
-  arrange(datasetID)
-
-weinberger<-full_join(weinberger,expchill.hours,by="datasetID")%>%
-  arrange(datasetID) %>%
-  filter(row_number()==1)
-
 write.csv(weinberger, file="~/Documents/git/ospree/analyses/output/studytype.csv",
           row.names=FALSE)
 
 # Create a more concise table
 studytype<- weinberger %>%
-  group_by(study, genus.species, respvar.simple, datasetID)%>%
+  group_by(study, genus.species, datasetID)%>%
   ungroup()%>%
-  dplyr::select(datasetID, samplingdates.count, species.count, photoperiods.count, forcetemps.count, expchill.count) %>%
+  dplyr::select(datasetID, samplingdates.count, species.count, photoperiods.count, forcetemps.count) %>%
   group_by(datasetID) %>%
   arrange(datasetID) %>%
   filter(row_number()==1) 
