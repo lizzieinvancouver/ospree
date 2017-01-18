e058aceb877a92589dba600ef1a1b1c94d03aca2
cchambe12 <CatherineChamberlain@Catherines-MacBook-Pro-2.local>
2017-01-18 12:21:26 -0500

    fixed guerriero90 and jones12 respvar.simple

--- analyses/cleaning/cleanup_all.R
+++ analyses/cleaning/cleanup_all.R
@@ -13,10 +13,31 @@ library(tidyr)
 setwd("~/Documents/git/ospree/analyses/output")
 d<- read.csv("ospree_master_clean.csv")
 
-# ghelardini10 issues
+# ghelardini10 issues - removed 8 rows not affiliated with study
 for(i in d){
-    y <- d[!(d$datasetID == "ghelardini10" & d$material == "root cuttings") &
+    df <- d[!(d$datasetID == "ghelardini10" & d$material == "root cuttings") &
              !(d$datasetID == "ghelardini10" & d$Entered.By == "DF"),]
-  }
+}
 
-write.csv(y, "~/Documents/git/ospree/analyses/output/ospree_master_clean.csv", row.names = FALSE)
+# guerriero90 issues - changed respvar.simple from 'phenstageper.probonestudy' to 'leaves'
+df <- within(df, respvar[datasetID== 'guerriero90' & respvar == 'percentstage06'] <- 'leaves')
+df$respvar.simple[df$respvar == "leaves"] <- "othernums"
+
+# jones12 issues
+# phenstage01 - percentbudburst
+# phenstage02 - leaves to othernums
+# phenstage03 - fruits to othernums
+# phenstage04 - flowers to percentflower
+df <- within(df, respvar[datasetID== 'jones12' & respvar == 'percentstage01'] <- 'percentbudburst')
+df$respvar.simple[df$respvar == "percentbudburst"] <- "percentbudburst"
+
+df <- within(df, respvar[datasetID== 'jones12' & respvar == 'percentstage02'] <- 'leaves')
+df$respvar.simple[df$respvar == "leaves"] <- "othernums"
+
+df <- within(df, respvar[datasetID== 'jones12' & respvar == 'percentstage03'] <- 'fruits')
+df$respvar.simple[df$respvar == "fruits"] <- "othernums"
+
+df <- within(df, respvar[datasetID== 'jones12' & respvar == 'percentstage04'] <- 'flowers')
+df$respvar.simple[df$respvar == "flowers"] <- "percentflower"
+
+write.csv(df, "~/Documents/git/ospree/analyses/output/ospree_master_clean.csv", row.names = FALSE)
