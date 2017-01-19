ca6df859f09fc71b344cd6b9728fb7eb97cdf579
cchambe12 <CatherineChamberlain@Catherines-MacBook-Pro-2.local>
2017-01-19 14:26:57 -0500

    finished updating forcetemp column

--- analyses/cleaning/cleanup_all.R
+++ analyses/cleaning/cleanup_all.R
@@ -52,11 +52,17 @@ df<- read.csv("ospree_master_clean.csv")
 df <- within(df, forcetemp[datasetID== 'charrier11' & forcetemp == 'ambient'] <- 25)
 # cronje03 - change blank to 25
 df <- within(df, forcetemp[datasetID== 'cronje03' & forcetemp == ''] <- 25)
-## REVISIT FALUSI96 PAPER! ##
-
-
-
-
-
+# falusi96 - change exp2 from blank to 16
+df <- within(df, forcetemp[datasetID== 'falusi96' & study == 'exp2'] <- 16)
+df <- within(df, forcetemp[datasetID== 'falusi96' & study == 'exp3'] <- 24)
+# gomory15 - change from ambient to 4.9 (Svarin - 49 deg lat) or 9.2 (Hladomer - 48 deg lat)
+df <- within(df, forcetemp[datasetID== 'gomory15' & growing.lat == 48.448201] <- 9.2)
+df <- within(df, forcetemp[datasetID== 'gomory15' & growing.lat == 49.017914] <- 4.9)
+# guerriero90 - changed blanks to 23
+df <- within(df, forcetemp[datasetID== 'guerriero90' & forcetemp == ''] <- 23)
+# schnabel87 - change ambient to 24
+df <- within(df, forcetemp[datasetID== 'schnabel87' & forcetemp == 'ambient'] <- 24)
+# yazdaniha64 - change ambient to ambient to 17
+df <- within(df, forcetemp[datasetID== 'yazdaniha64' & forcetemp == 'ambient'] <- 17)
 
 write.csv(df, "~/Documents/git/ospree/analyses/output/ospree_master_clean.csv", row.names = FALSE)
