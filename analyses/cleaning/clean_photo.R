262b64ac4589e08b8abbce777bbf01118be791e1
cchambe12 <cchamberlain@g.harvard.edu>
2017-02-02 17:20:43 -0500

    finished cleaning photoperiod_day

--- analyses/cleaning/clean_photo.R
+++ analyses/cleaning/clean_photo.R
@@ -17,11 +17,23 @@ d$photoperiod_night[d$photoperiod_night == "longday"] <- 13
 # cannell83 - does not specify for Figure 3, can assume ambient
 # chavarria09 - doesn't specify, can't assume ambient
 # falusi96 - doesn't specify for exp2 but does specify for exp3 - SD is 9, LD is 13
+# gansert02 - change "" to "ambient"
+d<- within(d, photoperiod_day[datasetID == 'gansert02'] <- 'ambient')
 # gianfagna85 - doesn't specify, can't assume ambient
 # hawerroth13 - "in fitotrons without light.." - changed to 0
-d <- within(d, photoperiod_day[datasetID== 'hawerroth13'] <- 0)
+d <- within(d, photoperiod_day[datasetID == 'hawerroth13'] <- 0)
+# hawkins12 - changed "" to "ambient"
+d<- within(d, photoperiod_day[datasetID == 'hawkins12'] <- 'ambient')
+# linkosalo06 - changed "" to "ambient"
+d<- within(d, photoperiod_day[datasetID == 'linkosalo06'] <- 'ambient')
 # manson91 - doesn't specify, can't assume ambient
+# morin10 - changed "" to "ambient"
+d<- within(d, photoperiod_day[datasetID == 'morin10'] <- 'ambient')
 # nishimoto95 - doesn't specify, can't assume ambient
+# guerriero90 has missing photoperiod data for some rows, change to 12
+d <- within(d, photoperiod_day[datasetID == 'guerriero90'] <- 12)
+d <- within(d, photoperiod_night[datasetID == 'guerriero90']<- 12)
+
 
 ## Should we change 'constant' to 24 h?
 # cronje03 - fixed to constant - irradiance 215 umol m-2 s2
@@ -33,10 +45,62 @@ d$photoperiod_day[d$photoperiod_day == "constant"] <- 24
 d$photoperiod_night[d$photoperiod_day==24]<-0
 
 ## Ambient
-# gansert02
 # hawkins12 - ambient for exp1
+
+# heide15 - remove duplicate rows and adjust photoperiod_day accordingly!!
+d <- d[!(d$datasetID == "heide15" & d$photoperiod_day == "10, 15"), ]
+d <- d[!(d$datasetID == "heide15" & d$photoperiod_day == "15, 10"), ]
+d <- within(d, photoperiod_day[datasetID == 'heide15' & respvar == 'shootgrowthcm'
+                               & response.time == 14 & response == 22.2] <- 11)
+d <- within(d, photoperiod_day[datasetID == 'heide15' & respvar == 'shootgrowthcm'
+                               & response.time == 21 & response == 22.6] <- 12)
+d <- within(d, photoperiod_day[datasetID == 'heide15' & respvar == 'shootgrowthcm'
+                               & response.time == 28 & response == 22.6] <- 13)
+d <- within(d, photoperiod_day[datasetID == 'heide15' & respvar == 'shootgrowthcm'
+                               & response.time == 35 & response == 22.6] <- 14)
+d <- within(d, photoperiod_day[datasetID == 'heide15' & respvar == 'shootgrowthcm'
+                               & response.time == 42 & response == 22.6] <- 15)
+d <- within(d, photoperiod_day[datasetID == 'heide15' & respvar == 'shootgrowthcm'
+                               & response.time == 14 & response == 30.7] <- 14)
+d <- within(d, photoperiod_day[datasetID == 'heide15' & respvar == 'shootgrowthcm'
+                               & response.time == 21 & response == 33.3] <- 13)
+d <- within(d, photoperiod_day[datasetID == 'heide15' & respvar == 'shootgrowthcm'
+                               & response.time == 28 & response == 33.3] <- 12)
+d <- within(d, photoperiod_day[datasetID == 'heide15' & respvar == 'shootgrowthcm'
+                               & response.time == 35 & response == 33.3] <- 11)
+d <- within(d, photoperiod_day[datasetID == 'heide15' & respvar == 'shootgrowthcm'
+                               & response.time == 42  & response == 33.3] <- 10)
+d <- within(d, photoperiod_day[datasetID == 'heide15' & respvar == 'nodes'
+                               & response.time == 14 & response == 4.6] <- 11)
+d <- within(d, photoperiod_day[datasetID == 'heide15' & respvar == 'nodes'
+                               & response.time == 21 & response == 4.6] <- 12)
+d <- within(d, photoperiod_day[datasetID == 'heide15' & respvar == 'nodes'
+                               & response.time == 28 & response == 4.6] <- 13)
+d <- within(d, photoperiod_day[datasetID == 'heide15' & respvar == 'nodes'
+                               & response.time == 35 & response == 4.6] <- 14)
+d <- within(d, photoperiod_day[datasetID == 'heide15' & respvar == 'nodes'
+                               & response.time == 42  & response == 4.6] <- 15)
+d <- within(d, photoperiod_day[datasetID == 'heide15' & respvar == 'nodes'
+                               & response.time == 14 & response == 6.2] <- 14)
+d <- within(d, photoperiod_day[datasetID == 'heide15' & respvar == 'nodes'
+                               & response.time == 21 & response == 6.7] <- 13)
+d <- within(d, photoperiod_day[datasetID == 'heide15' & respvar == 'nodes'
+                               & response.time == 28 & response == 7.1] <- 12)
+d <- within(d, photoperiod_day[datasetID == 'heide15' & respvar == 'nodes'
+                               & response.time == 35 & response == 7.1] <- 11)
+d <- within(d, photoperiod_day[datasetID == 'heide15' & respvar == 'nodes'
+                               & response.time == 42  & response == 7.1] <-10)
+
+# thielges75 - fix one row
+d <- within(d, photoperiod_day[datasetID == 'thielges75' & photoperiod_day == 
+                                 'half in 16, half ambient(9.5-10.5)'] <- 16)
+
+
+# Remove extra lines for ghelardini10 added by Dan that do not correlate to paper
+d[-which(d$datasetID=='ghelardini10' & d$Entered.By == 'DF'), ]
 
 stop("Not an error, just stopping here to say we're now done cleaning photo. The d item in your workspace is now all cleaned up for its photoperiod_day and photoperiod_night columns. Zippitydoodah ...")
 
 # Make new data sheet
 # write.csv(d, file = "ospree_clean_photo.csv", row.names=FALSE)
+
