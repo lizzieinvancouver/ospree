1ec80fbb81e6cda4676013e1a9f2b3b60b53941a
Elizabeth Wolkovich <wolkovich@biodiversity.edu>
2017-02-03 21:29:40 -0500

    Some progress ... not done

--- analyses/graphics_nmore.R
+++ analyses/graphics_nmore.R
@@ -4,19 +4,59 @@
 ## Some quick code to look at data ##
 ## And some model stuff ##
 
+## Updated on 3 February 20017 ##
 
 ## To do! ##
 # Next up, extract lab group from my gephi figures and see if that helps ... 
 
 ## housekeeping
 rm(list=ls()) 
-options(stringsAsFactors=FALSE)
+options(stringsAsFactors = FALSE)
 
-## get set up
+# libraries
+library(rstanarm)
 library(ggplot2)
 
-setwd("~/Documents/git/projects/treegarden/budreview/ospree/")
+# Setting working directory. Add in your own path in an if statement for your file structure
+if(length(grep("danflynn", getwd())>0)) { 
+  setwd("~/Documents/git/ospree") 
+  } else setwd("~/Documents/git/projects/treegarden/budreview/ospree/analyses")
 
+# get the data
+
+# make sure this is the correct file (we're still cleaning as I write this!) 
+bb <- read.csv("output/ospree_clean_withchill_BB.csv", header=TRUE)
+labgroups <- read.csv("output/labgroups.csv", header=TRUE)
+
+# merge in labgroup (we could do this elsewhere someday
+bb.wlab <- merge(bb, labgroups, by="datasetID", all.x=TRUE)
+
+# how much data?
+dim(bb.wlab)
+
+columnstokeep <- c("datasetID", "genus", "species", "varetc", "woody", "forcetemp",
+    "photoperiod_day", "respvar", "respvar.simple", "response", "response.time", 
+    "Total_Chilling_Hours","Total_Utah_Model", "Total_Chill_portions",
+    "Exp_Chilling_Hours",  "Exp_Utah_Model","Exp_Chill_portions", "cat")
+    
+bb.wlab.sm <- subset(bb.wlab, select=columnstokeep)
+
+# make a bunch of things numeric (eek!)
+bb.wlab.sm$force <- as.numeric(bb.wlab.sm$forcetemp)
+bb.wlab.sm$photo <- as.numeric(bb.wlab.sm$photoperiod_day)
+bb.wlab.sm$resp <- as.numeric(bb.wlab.sm$response.time)
+bb.wlab.sm$chillhrs <- as.numeric(bb.wlab.sm$Total_Chilling_Hours)
+bb.wlab.sm$chillpor <- as.numeric(bb.wlab.sm$Total_Chill_portions)
+bb.wlab.sm$utah <- as.numeric(bb.wlab.sm$Total_Utah_Model)
+bb.wlab.sm$expchillhrs <- as.numeric(bb.wlab.sm$Exp_Chilling_Hours)
+bb.wlab.sm$expchillpor <- as.numeric(bb.wlab.sm$Exp_Chill_portions)
+bb.wlab.sm$exputah <- as.numeric(bb.wlab.sm$Exp_Utah_Model)
+
+# where do we lose data
+dim(subset(bb.wlab.sm, is.na(force)==FALSE))
+dim(subset(bb.wlab.sm, is.na(photo)==FALSE))
+dim(subset(bb.wlab.sm, is.na(chillhrs)==FALSE))
+dim(subset(bb.wlab.sm, is.na(chillhrs)==FALSE & is.na(photo)==FALSE & is.na(force)==FALSE))
 
 ## grab the data and set it up as though about to run Stan model on BB days
 # see also: Ospree_Analysis.R
