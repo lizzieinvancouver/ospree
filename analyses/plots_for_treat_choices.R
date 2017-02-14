462e8ed8fb69fd96dd26c156308a019745628db3
dbuona <dbuonaiuto@g.harvard.edu>
2017-02-14 12:15:44 -0500

    draft plots for experimental choice
    mostly complete script to show latitudinal correlations between choice
    for max and min photoperiods, forcing and chilling temperature.

--- analyses/plots_for_treat_choices.R
+++ analyses/plots_for_treat_choices.R
@@ -8,49 +8,51 @@ library(plyr)
 library(dplyr)
 library(tidyr)
 library(ggplot2)
-
+library(gridExtra)
 
 c<-read.csv("ospree_clean.csv",header = TRUE)
 c$photoperiod_day<-as.numeric(c$photoperiod_day)
 
 treatsum<-ddply(c,c("datasetID"), summarise,
 mean=mean(photoperiod_day), max=max(photoperiod_day), min=min(photoperiod_day),meangrow=mean(growing.lat), meanprov=mean(provenance.lat))
-
 treatsum<-filter(treatsum, meanprov>0)
 ##seperate which studies use same max and min photoperoioda
 treatsum$yesphoto<-ifelse(treatsum$max==treatsum$min,"no-manipulation","manipulation")
 
-##run 1m to see if there is a trend in photoperiod over space
+###plot for photoperiod
+plottingtreatsum<-gather(treatsum, maxmin,value, max:min)
+q<-ggplot(plottingtreatsum, aes(x=meanprov, y=value, color=maxmin)) 
+q+geom_point()+labs(title="Photoperiod", x=" Latititude", y="photoperiod")+ stat_smooth(method = "lm", formula = y ~ x, se = FALSE)
+
+##linear models for photoperiod
 Modmax<-lm(max~meanprov,data=treatsum)
 summary(Modmax)
-#plotthem
-q<-ggplot(treatsum, aes(x=max, y=meanprov, color=yesphoto))
-q+geom_point()+labs(title="Maximum experimental photoperiod", x="Max photoperiod", y="Latitude")
-##for minimum photoperiod
 Modmin<-lm(min~meanprov,data=treatsum)
 summary(Modmin)
-r<-ggplot(treatsum, aes(x=min, y=meanprov, color=yesphoto))
-r+geom_point()+labs(title="Minimum experimental photoperiod", x="Min photoperiod", y="Latitude")
-# next step could be to subset to include only studies where photoperiod was manipulated and run lms again
-#########################################forcing temperature###############################
+
+###Forcing
+
 c$forcetemp<-as.numeric(c$forcetemp)
 treatsum2<-ddply(c,c("datasetID"), summarise,
      mean=mean(forcetemp), max=max(forcetemp), min=min(forcetemp),meangrow=mean(growing.lat), meanprov=mean(provenance.lat))
 treatsum2$yesforce<-ifelse(treatsum2$max==treatsum2$min,"no-manipulation","manipulation")
 treatsum2<-filter(treatsum2, meanprov>0)
-q<-ggplot(treatsum2, aes(x=max, y=meanprov, color=yesforce))
-q+geom_point()+labs(title="Maximum experimental forcing", x="Max forcing", y="Latitude")
-r<-ggplot(treatsum2, aes(x=min, y=meanprov, color=yesforce))
-r+geom_point()+labs(title="Minimum experimental forcing", x="Min forcing", y="Latitude")
+
+##Plot for forcing
+plottingtreatsum2<-gather(treatsum2, maxmin,value, max:min)
+q<-ggplot(plottingtreatsum2, aes(x=meanprov, y=value, color=maxmin)) 
+q+geom_point()+labs(title="Forcing", x=" Latititude", y="Forcing level")+ stat_smooth(method = "lm", formula = y ~ x, se = FALSE)
+
+###Linear model
 Modmax2<-lm(max~meanprov,data=treatsum2)
 summary(Modmax2)
 Modmin2<-lm(min~meanprov,data=treatsum2)
 summary(Modmin2)
 
-
 ### let look just where thing were manipulated
 treatsum<-filter(treatsum,yesphoto=="manipulation")
 treatsum2<-filter(treatsum2,yesforce=="manipulation")
+
 #lms with only manipulated value
 Modmax<-lm(max~meanprov,data=treatsum)
 summary(Modmax)
@@ -60,18 +62,17 @@ Modmax2<-lm(max~meanprov,data=treatsum2)
 summary(Modmax2)
 Modmin2<-lm(min~meanprov,data=treatsum2)
 summary(Modmin2)
-##max min photo for manipulated only
-q<-ggplot(treatsum, aes(x=max, y=meanprov, color=yesphoto))
-q+geom_point()+labs(title="Maximum experimental photoperiod", x="Max photoperiod", y="Latitude")
-r<-ggplot(treatsum, aes(x=min, y=meanprov, color=yesphoto))
-r+geom_point()+labs(title="Minimum experimental photoperiod", x="Min photoperiod", y="Latitude")
-##max and min forcing for manipulated only
-q<-ggplot(treatsum2, aes(x=max, y=meanprov, color=yesforce))
-q+geom_point()+labs(title="Maximum experimental forcing", x="Max forcing", y="Latitude")
-r<-ggplot(treatsum2, aes(x=min, y=meanprov, color=yesforce))
-r+geom_point()+labs(title="Minimum experimental forcing", x="Min forcing", y="Latitude")
+
+##plots for manipulated only
+plottingtreatsum<-gather(treatsum, maxmin,value, max:min)
+q<-ggplot(plottingtreatsum, aes(x=meanprov, y=value, color=maxmin)) 
+q+geom_point()+labs(title="Manipulated Photoperiod", x=" Latititude", y="photoperiod")+ stat_smooth(method = "lm", formula = y ~ x, se = FALSE)
+plottingtreatsum2<-gather(treatsum2, maxmin,value, max:min)
+q<-ggplot(plottingtreatsum2, aes(x=meanprov, y=value, color=maxmin)) 
+q+geom_point()+labs(title="Manipulated Forcing", x=" Latititude", y="Forcing level")+ stat_smooth(method = "lm", formula = y ~ x, se = FALSE)
 
 #### LETS LOOK AT CHILLING
+##make all numeric
 chill<-read.csv("ospree_clean_withchill.csv",header = TRUE)
 chill$Field_Utah_Model<-as.numeric(chill$Field_Utah_Model)
 chill$Exp_Utah_Model<-as.numeric(chill$Exp_Utah_Model)
@@ -79,16 +80,45 @@ chill$Exp_Chilling_Hours<-as.numeric(chill$Exp_Chilling_Hours)
 chill$Field_Chilling_Hours<-as.numeric(chill$Field_Chilling_Hours)
 chill$Exp_Chill_portions<-as.numeric(chill$Exp_Chill_portions)
 chill$Field_Chill_portions<-as.numeric(chill$Field_Chill_portions)
-###gather all of these so they can be compared on the same graph...this is incomplete
-treat<-gather(chill,ex.calculation,ex.chillunits,Exp_Chilling_Hours:Exp_Chill_portions)
-treat<-gather()
-###not neccisarily using this part below
-treat<-treatsum<-ddply(chill,c("datasetID"), summarise,
-           maxFU=max(Field_Utah_Model), minFU=min(Field_Utah_Model),
-         maxEU=max(Exp_Utah_Model), minEU=min(Exp_Utah_Model),
-         maxECH=max(Exp_Chilling_Hours), minECH=min(Exp_Chilling_Hours),
-         maxFCH=max(Field_Chilling_Hours),minFCH=min(Field_Chilling_Hours),
-          maxECP=max(Exp_Chill_portions), minECP=min(Exp_Chill_portions),
-         maxFCP=max(Field_Chill_portions),minFCP=min(Field_Chill_portions),
-         meanprov=mean(provenance.lat))
 
+treat<-ddply(chill,c("datasetID"), summarise,
+           maxFU=max(Field_Utah_Model), minFU=min(Field_Utah_Model),
+           maxEU=max(Exp_Utah_Model), minEU=min(Exp_Utah_Model),
+           maxTU=max(Total_Utah_Model), minTU=min(Total_Utah_Model),
+        maxECH=max(Exp_Chilling_Hours), minECH=min(Exp_Chilling_Hours),
+        maxFCH=max(Field_Chilling_Hours),minFCH=min(Field_Chilling_Hours),
+        maxTCH=max(Total_Chilling_Hours),minTCH=min(Total_Chilling_Hours), 
+        maxECP=max(Exp_Chill_portions), minECP=min(Exp_Chill_portions),
+        maxFCP=max(Field_Chill_portions),minFCP=min(Field_Chill_portions),
+        maxTCP=max(Total_Chill_portions), minTCP=min(Total_Chill_portions),
+        meanprov=mean(provenance.lat))
+treat<-filter(treat, meanprov>0)
+###########################################
+####plotting total chilling
+plottingUtah<-gather(treat, maxmin,value, maxTU:minTU)
+q<-ggplot(plottingUtah, aes(x=meanprov, y=value, color=maxmin)) 
+q+geom_point()+labs(title="Utah Chilling", x=" Latititude", y="Chilling")+ stat_smooth(method = "lm", formula = y ~ x, se = FALSE)
+
+plottingCP<-gather(treat, maxmin,value, maxTCP:minTCP)
+q<-ggplot(plottingCP, aes(x=meanprov, y=value, color=maxmin)) 
+q+geom_point()+labs(title="Chilling Portions Chilling", x=" Latititude", y="Chilling")+ stat_smooth(method = "lm", formula = y ~ x, se = FALSE)
+
+plottingCH<-gather(treat, maxmin,value, maxTCH:minTCH)
+q<-ggplot(plottingCH, aes(x=meanprov, y=value, color=maxmin)) 
+q+geom_point()+labs(title="Chilling Hours Chilling", x=" Latititude", y="Chilling")+ stat_smooth(method = "lm", formula = y ~ x, se = FALSE)
+
+#################just for experimental
+plottingUtah<-gather(treat, maxmin,value, maxEU:minEU)
+q<-ggplot(plottingUtah, aes(x=meanprov, y=value, color=maxmin)) 
+q+geom_point()+labs(title="Utah Exp. Chilling", x=" Latititude", y="Chilling")+ stat_smooth(method = "lm", formula = y ~ x, se = FALSE)
+
+plottingCP<-gather(treat, maxmin,value, maxECP:minECP)
+q<-ggplot(plottingCP, aes(x=meanprov, y=value, color=maxmin)) 
+q+geom_point()+labs(title="Chilling Portions Exp. Chilling", x=" Latititude", y="Chilling")+ stat_smooth(method = "lm", formula = y ~ x, se = FALSE)
+
+plottingCH<-gather(treat, maxmin,value, maxECH:minECH)
+q<-ggplot(plottingCH, aes(x=meanprov, y=value, color=maxmin)) 
+q+geom_point()+labs(title="Chilling Hours Exp. Chilling", x=" Latititude", y="Chilling")+ stat_smooth(method = "lm", formula = y ~ x, se = FALSE)
+
+###Next step lms for chilling
+###instead of removing southern hemisphere I could, make absolute value
