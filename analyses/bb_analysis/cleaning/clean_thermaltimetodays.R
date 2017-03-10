c2e930e9321a569568fa3f504a994d229a6fc031
Elizabeth Wolkovich <wolkovich@biodiversity.edu>
2017-03-10 10:40:28 -0500

    Some work on issue #72
    Sorry! Needed to re-open this.

--- analyses/bb_analysis/cleaning/clean_thermaltimetodays.R
+++ analyses/bb_analysis/cleaning/clean_thermaltimetodays.R
@@ -10,8 +10,6 @@ options(stringsAsFactors=FALSE)
 # Load libraries
 library(dplyr)
 library(tidyr)
-library(ggplot2)
-library(arm)
 
 ## read data
 #setwd("C:/Users/Ignacio/Documents/GitHub/ospree/analyses/output")
@@ -23,10 +21,15 @@ d<-read.csv("output/ospree_clean_withchill.csv",as.is=TRUE)
 ## Can change ghelardini10 & heide93 from thermaltime to daystobudburst
 d<-within(d, respvar.simple[datasetID=="ghelardini10" & respvar.simple=="thermaltime"]<-"daystobudburst")
 d<-within(d, respvar.simple[datasetID=="heide93"]<-"daystobudburst")
+
 d<-within(d, respvar.simple[datasetID=="karlsson03" & respvar.simple=="thermaltime"]<-"daystobudburst")
+
 dbb<-subset(d,respvar.simple=="daystobudburst")
-# attempting to convert response.time to daystobudburst using two methods, both have same error
+
+# converting response.time to daystobudburst 
 dbb$response.time[which(dbb$datasetID=="ghelardini10")] <-
-  as.numeric(dbb$response[which(dbb$datasetID=="ghelardini10")])/as.numeric(dbb$forcetemp[which(dbb$datasetID=="ghelardini10")])
+    as.numeric(dbb$response[which(dbb$datasetID=="ghelardini10")])/
+    as.numeric(dbb$forcetemp[which(dbb$datasetID=="ghelardini10")])
 dbb$response.time[which(dbb$datasetID=="heide93")] <-
-  as.numeric(dbb$response[which(dbb$datasetID=="heide93")])/as.numeric(dbb$forcetemp[which(dbb$datasetID=="heide93")])
+    as.numeric(dbb$response[which(dbb$datasetID=="heide93")])/
+    as.numeric(dbb$forcetemp[which(dbb$datasetID=="heide93")])
