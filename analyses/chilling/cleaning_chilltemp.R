d75c4cc5cd607a95f0ac27a9e1e63714010c8a5d
cchambe12 <cchamberlain@g.harvard.edu>
2017-04-05 16:51:35 -0400

    final edits to cleaning_chilling.R

--- analyses/chilling/cleaning_chilltemp.R
+++ analyses/chilling/cleaning_chilltemp.R
@@ -14,13 +14,12 @@ d[which(d$chilltemp=="ambient plus days at 4C"),]$chilltemp<-4#okie11 exp2
 d[which(d$chilltemp=="neg 3,2"),]$chilltemp<--0.5 #man10 - can we change to -0.5? seedlings were chilled for one month at -3, and one month at 2, took the average
 d[which(d$chilltemp=="6,.5"),]$chilltemp<-3.25 #li05 - can we change to 3.25? seedlings were chilled for 3 weeks at 6, and 3 weeks at 0.5, took the average
 d[which(d$chilltemp=="-3, 3"),]$chilltemp<-1 #lamb37 - 26.6F for 8 hr and 37.4 for 16 hr - take average (33.8F) converted - 1degC
-#d[which(d$chilltemp=="8, 8, -4"),]#jones12
-#d[which(d$chilltemp=="8, 4, 0"),]#jones12
-#d[which(d$chilltemp=="4, 0, -4"),]#jones12
-#d[which(d$chilltemp=="0, 4, 8"),]#jones12
-#d[which(d$chilltemp=="-4, 8, 8"),]#jones12
-#d[which(d$chilltemp=="-4, 0, 4"),]#jones12
-#d[which(d$chilltemp=="-4, 0, 4"),]#jones12
+d[which(d$chilltemp=="8, 8, -4"),]$chilltemp <- 4 #jones12 Cuttings were exposed to either a 6-week or a 12-week chillingperiod, with each period split into three equal parts at one of the treatment temperatures (-4, 0, 4 or 8C).
+d[which(d$chilltemp=="8, 4, 0"),]$chilltemp <- 4 #jones12
+d[which(d$chilltemp=="4, 0, -4"),]$chilltemp <- 0 #jones12
+d[which(d$chilltemp=="0, 4, 8"),]$chilltemp <- 4 #jones12
+d[which(d$chilltemp=="-4, 8, 8"),]$chilltemp <- 4 #jones12
+d[which(d$chilltemp=="-4, 0, 4"),]$chilltemp <- 0 #jones12
 #d[which(d$chilltemp=="Chilling treatment at 0.7 \xb1 0.7 C interrupted by mild spells of 14 days duration at a constant temperature of 8 or 12 C"),]#granhus09
 #d[which(d$chilltemp=="<16"),]#falusi90
 
