e4c2d19334695823d2156f9c95a29174a9d7c43a
AileneKane <aettinger@fasmail.harvard.edu>
2017-06-14 21:58:51 -0400

    code to pull daily climate data for PMP

---
+++ analyses/pmp/pullclimate_nam_pmp.R
@@ -0,0 +1,91 @@
+# Read in netCDF files to pull climate data for North America
+# This requires you to work off of external hard drive
+# This code pulls data for the calendar yar before and after the field sample date
+# because PNP requires full year of data to run
+# Ailene got a start on this: June 13, 2017
+
+nafiles <- dir(climatedrive)[grep("livneh", dir(climatedrive))]
+#loop through each lat/long for which we want to calculate chilling and pull the climate data for that lat/long
+#the climate data that we are pulling is daily min and max temperature
+
+for(i in 1:nrow(nam)){ # i = 1
+  
+  # find this location
+  lo <- nam[i,"chill.long"]
+  la <- nam[i,"chill.lat"]
+  
+  # make sure longitudes are negative, need to be for North America
+  if(lo > 0) { lo = lo*-1 }
+  
+  yr <- as.numeric(nam[i,"year"])
+  # start and end days of the climate data we need to calculate chilling, for the focal lat/long. This is in days since baseline date (sept 1) Set to GMT to avoid daylight savings insanity
+  # using d$fieldsample.date2 (this is the same as fieldsampledate, but formatted as  "%Y-%m-%d")
+  if(nam[i,"fieldsample.date2"]!=""){endday <- strptime(nam[i,"fieldsample.date2"],"%Y-%m-%d", tz = "GMT")}
+  if(nam[i,"fieldsample.date2"]==""){endday <- strptime(paste(yr, "12-31", sep="-"),"%Y-%m-%d", tz = "GMT")} #if no sampling date given, use december 31 of same year
+  
+  if(substr(endday,1,4)==yr & as.numeric(substr(endday,6,7))<=9){#when sampling occurred in same year as study and when collection occurred before that year's sept chilling,
+    stday <- strptime(paste(yr-1, "09-01", sep="-"),"%Y-%m-%d", tz="GMT")
+    prevmo <- paste(yr-1, formatC(9:12, width=2, flag="0"), sep="");# use previous year's fall months of chilling (Sept-Dec)
+    endmo<-substr(endday,6,7);#month of sampling date
+    thismo <- paste(yr, formatC(1:endmo, width=2, flag="0"), sep="")#months from current year of chilling, through sampling date (Jan-whenever sampled)
+    chillmo<-c(prevmo, thismo)
+  }
+  
+  if(substr(endday,1,4)==yr-1 & as.numeric(substr(endday,6,7))<=12  & as.numeric(substr(endday,6,7))>=9){#when sampling occurred in previous year as study only
+    stday <- strptime(paste(yr-1, "01-01", sep="-"),"%Y-%m-%d", tz="GMT")
+    prevmo <- paste(yr-1, formatC(9:substr(endday,6,7), width=2, flag="0"), sep="");# use previous year's fall months of chilling (Sept-whenever collection occured)}
+    chillmo<-prevmo
+  }
+  
+  if(substr(endday,1,4)==yr & as.numeric(substr(endday,6,7))>=9){#when sampling occurred in same year as study and after chilling started that year
+    stday <- strptime(paste(yr, "09-01", sep="-"),"%Y-%m-%d", tz="GMT")
+    prevmo <- paste(yr, formatC(9:substr(endday,6,7), width=2, flag="0"), sep="");# use previous year's fall months of chilling (Sept-whenever collection occured)}
+    chillmo<-prevmo
+  }
+  
+  if(substr(endday,1,4)==yr-1 & as.numeric(substr(endday,6,7))<=12  & as.numeric(substr(endday,6,7))>=9){#when sampling occurred in previous year as study between sept and dec
+    stday <- strptime(paste(yr-1, "01-01", sep="-"),"%Y-%m-%d", tz="GMT")
+    prevmo <- paste(yr-1, formatC(9:substr(endday,6,7), width=2, flag="0"), sep="");# use previous year's fall months of chilling (Sept-whenever collection occured)}
+    chillmo<-prevmo
+  }
+  
+  if(substr(endday,1,4)==yr-1 & as.numeric(substr(endday,6,7))<=12  & as.numeric(substr(endday,6,7))<9){#when sampling occurred in previous year as study, NOT during the fall
+    stday <- strptime(paste(as.numeric(substr(endday,1,4))-1, "09-01", sep="-"),"%Y-%m-%d", tz="GMT")
+    prevmo <- paste(as.numeric(substr(endday,1,4))-1, formatC(9:12, width=2, flag="0"), sep="");# use previous year's fall months of chilling (Sept-Dec)
+    endmo<-substr(endday,6,7);#month of sampling date
+    thismo <- paste(as.numeric(substr(endday,1,4)), formatC(1:endmo, width=2, flag="0"), sep="")#months from current year of chilling, through sampling date (Jan-whenever sampled)
+    chillmo<-c(prevmo, thismo)
+  }
+  # now loop over these year-month combo files and get temperature values for this date range.
+
+  mins <- maxs <- vector()
+  
+  for(j in c(chillmo)){ # j = "200009"
+    file <- file.path(climatedrive,nafiles[grep(j, nafiles)])
+    jx <- nc_open(file)
+    
+    diff.long.cell <- abs(jx$dim$lon$vals-as.numeric(lo))
+    diff.lat.cell <- abs(jx$dim$lat$vals-as.numeric(la))
+    long.cell <- which(diff.long.cell==min(diff.long.cell))[1] 
+    lat.cell <- which(diff.lat.cell==min(diff.lat.cell))[1]
+    
+    mins <- c(mins, ncvar_get(jx,'Tmin',start=c(long.cell,lat.cell,1),count=c(1,1,-1)))
+    maxs <- c(maxs, ncvar_get(jx,'Tmax',start=c(long.cell,lat.cell,1),count=c(1,1,-1)))
+    nc_close(jx)
+    }
+  
+  tempval[[as.character(nam[i,"ID_fieldsample.date2"])]] <- data.frame(Lat = la,Long = lo,Date = as.character(seq(stday, endday, by = "day")),
+                                                                       Tmin = mins[1:length(seq(stday, endday, by = "day"))], Tmax =maxs[1:length(seq(stday, endday, by = "day"))])#
+}
+
+# If you want to (as Lizzie does) you can write out tempval, which is all the climate pulled in a list form
+save(tempval, file="output/fieldclimate_pmp.RData")
+dailytemp <- do.call("rbind", tempval)
+dailytemp<-as.data.frame(cbind(row.names(dailytemp),dailytemp))
+colnames(dailytemp)[1]<-"ID_fieldsample.date2"
+dailytemp2<-separate(data = dailytemp, col = ID_fieldsample.date2, into = c("datasetID", "lat","long","fieldsample.date2"), sep = "\\_")
+row.names(dailytemp2)<-NULL
+dailytemp3<-subset(dailytemp2,select=c(datasetID,lat,long,fieldsample.date2,Date,Tmin,Tmax))
+
+stop("Not an error, just stopping here to say we're now done pulling daily climate data for North America!")
+
---
+++ analyses/pmp/pulldailyclim.R
@@ -0,0 +1,53 @@
+## Pull climate data for use in PMP by Lizzie 
+# By Ailene Ettinger
+# June 14, 2017
+## This code is based on the chillmerge_all.R code, and the first 4 steps are sourced to the chilling folder where cleaning occurs
+
+
+## housekeeping
+rm(list=ls()) 
+options(stringsAsFactors = FALSE)
+
+# Set working directory: 
+if(length(grep("Lizzie", getwd())>0)) {setwd("~/Documents/git/projects/treegarden/budreview/ospree/analyses") 
+} else if
+    (length(grep("ailene", getwd()))>0) {setwd("/Users/aileneettinger/git/ospree/analyses")
+}else 
+setwd("~/Documents/git/ospree/analyses")
+
+# Load libraries
+library(dplyr)
+library(tidyr)
+library(plyr)
+library(ncdf4)
+
+
+# 1. Get the data
+d <- read.csv("output/ospree_clean.csv")
+
+# 2. Clean the chilltemp column
+
+source("chilling/cleaning_chilltemp.R")
+
+#3. Clean the provenance.latitude and provenance.longitude columns, to get appropriate locations
+source("chilling/cleaning_provlatlong.R")
+
+# 4. Pull daily min and max temperature for all sites with fieldsampledates (using growing or provenance lat/long to pull climate data)- REQUIRES EXTERNAL HARD DRIVE FOR THIS
+
+# 4a: summarize lat/longs needed to pull climate data from europe and north america
+source("chilling/fieldchillcalc_latlong.R")
+
+# 4b: Set the location of the external hard drive, then pull daily climate data for Europe and North America
+#Skip head to 4e if you do not have the climate data drive
+climatedrive = "/Volumes/Ospree Climate" # (Ospree Climate is name of the external drive, change with new device)
+# climatedrive =  "/Volumes/BackYouEvilFiend/ospreeclimate" # Lizzie's backup drive (at WeldHill currently)
+# climatedrive = "/Volumes/My Book for Mac/ospreeclimate" # Lizzie's backup drive (at HUH currently)
+
+# 4c. pull climate data from europe
+source("pmp/pullclimate_eur_pmp.R")
+
+# 4d: pull climate data from north america
+source("pmp/pullclimate_nam_pmp.R")
+
+#write a csv file of the daily Tmin and Tmax data
+write.csv(dailytemp3, "output/dailytemp.csv", row.names=FALSE, eol="\r\n")
