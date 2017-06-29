5230c5503f381a7f86c6d6eeaa5139532290df05
MoralesCastilla <moralescastilla@g.harvard.edu>
2017-06-29 16:25:23 -0400

    Nacho's updates

--- analyses/bb_analysis/cleaning/bb_cleanmergeall.R
+++ analyses/bb_analysis/cleaning/bb_cleanmergeall.R
@@ -25,11 +25,11 @@ library(tidyr)
 # 1. Get the data (that has already been cleaned for respvar and chilling)
 d <- read.csv("output/ospree_clean_withchill.csv")
 
-# 2. Select out the highest percentage of budburst only, and remove studies that contain duplicate data in two forms
-source("bb_analysis/cleaning/multiresp.R")
-
-# 3. Need to deal with thermal time to days
+# 2. Need to deal with thermal time to days
 source("bb_analysis/cleaning/clean_thermaltimetodays.R")
+
+# 3. Select out the highest percentage of budburst only, and remove studies that contain duplicate data in two forms
+source("bb_analysis/cleaning/multiresp.R")
 
 # 4. Clean ambient forcing
 source("bb_analysis/cleaning/clean_ambientforcing.R")
@@ -38,7 +38,7 @@ source("bb_analysis/cleaning/clean_ambientforcing.R")
 source("bb_analysis/cleaning/clean_bbperctodays.R")
 
 # 6. Clean duplicate responses across treatments/categories (see bbcleaning_README.txt)
-# source("bb_analysis/cleaning/clean_moreduplicates.R")
+#source("bb_analysis/cleaning/clean_moreduplicates.R")
 
 # 7. Write out the final file! 
 write.csv(d, "output/ospree_clean_withchill_BB.csv", row.names=FALSE) ##
