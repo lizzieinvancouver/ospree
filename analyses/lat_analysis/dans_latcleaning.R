1cba46ad084055969568ba33119d4bde1cd8fdbf
dbuona <dbuonaiuto@g.harvard.edu>
2017-06-29 12:07:19 -0400

    checked my side of the lat studies

--- analyses/lat_analysis/dans_latcleaning.R
+++ analyses/lat_analysis/dans_latcleaning.R
@@ -1,17 +1,16 @@
 ## Started 28 June 2017 ##
 ## By Dan
  ### Run Latitude_Studies.R
-
-osp2<-filter(osp, respvar=="daystobudburst")
-unique(osp2$datasetID)
-View(filter(osp2, datasetID=="heide12"))
+View(filter(d, count>1))
 
 ##Dan should clean:
-#"ashby62"-not multi lat
-#"basler12"- more longitude and altitude manipulated
-#"biasi12" not multi lat    
-#"calme94" multi lats are actually just different species
-# ****"campbell75" yes multi lat and multi alt
-#"charrier11" slight lat difference in exp 1, mostly multi alt 
- # "gunderson12" different lats are for different species
-#***"heide12" yes multi lat
+
+#"basler12"- more longitude and altitude manipulated, but yes different lats
+# "campbell75" heck yes multi lat (and multi alt)
+#"charrier11" slight lat difference in exp 1, mostly multi-alt 
+#cook00b- I think there are different varieties from each latitidue
+#cook05-yes
+#falusi96- heck yes
+#hawkins12- heck yes
+#heide12" heck yes
+#junttila12 heck yes, but not sure where the "Betula pendula,pubescens" entries come from (2 rows)
