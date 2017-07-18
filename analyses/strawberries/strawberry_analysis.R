efc5d18facb87b703b9b005fed4c68b0de50d260
dbuona <dbuonaiuto@g.harvard.edu>
2017-07-18 13:47:54 -0400

    berry mods

--- analyses/strawberries/strawberry_analysis.R
+++ analyses/strawberries/strawberry_analysis.R
@@ -9,66 +9,50 @@ berries<-read.csv("output/strawberries.csv")
 table(berries$respvar.simple)
 berries$forcetemp <- as.numeric(berries$forcetemp)
 berries$photoperiod_day <- as.numeric(berries$photoperiod_day)
-berries$chilldays<-as.numeric(berries$chilldays)
+berries$Total_Utah_Model<-as.numeric(berries$Total_Utah_Model)
+berries$Total_Chilling_Hours<-as.numeric(berries$Total_Chilling_Hours)
+berries$Total_Chill_portions<-as.numeric(berries$Total_Chill_portions)
 berries$responsedays <- as.numeric(berries$response.time)
 berries$response <- as.numeric(berries$response)
 
 condition1<-c("percentbudburst","percentflower")
 straw <- filter(berries, respvar.simple %in% condition1)
 
-
+mixed<-lmer(response~responsedays+responsedays:forcetemp+responsedays:photoperiod_day+responsedays:Total_Utah_Model+(1+respvar|respvar), data=straw)
+summary(mixed)
+coef(mixed)
+###i Think this is wrong, but why
 
 
 ###filtering and cleaning
 
-bud<-filter(berries, respvar.simple=="percentbudburst")
+bud<-filter(straw, respvar.simple=="percentbudburst")
 buddy<-filter(bud, response.time!="")
 
-flo<-filter(berries, respvar=="inflorescences") ##what is this variable? there are 200 percent value and respvar simpl eis just flowers. Its actually "number not percent"
+flo<-filter(straw, respvar=="inflorescences") ##what is this variable? there are 200 percent value and respvar simpl eis just flowers. Its actually "number not percent"
 
 
 floy<-filter(flo, response.time!="")
 unique(floy$datasetID)
 unique(buddy$datasetID)
 
-
-##make prediucotrs
-buddy$responsedays <- as.numeric(buddy$response.time)
-buddy$forcetemp <- as.numeric(buddy$forcetemp)
-buddy$photoperiod_day <- as.numeric(buddy$photoperiod_day)
-buddy$chilldays<-as.numeric(buddy$chilldays)
-buddy$response <- as.numeric(buddy$response)
-
-floy$responsedays<-as.numeric(floy$response.time)
-floy$forcetemp <- as.numeric(floy$forcetemp)
-floy$photoperiod_day <- as.numeric(floy$photoperiod_day)
-floy$chilldays<-as.numeric(floy$chilldays)
-
-###are there enought
-#someplots
-plot(buddy$photoperiod_day,buddy$response.time)
-plot(floy$photoperiod_day,floy$response.time)
-plot(buddy$forcetemp,buddy$response.time)
-plot(floy$forcetemp,floy$response.time)
-
 ### a few fun exploratory models with just forcing and photoperoid
 #####%budburst
-mod <- lm(response~responsedays+responsedays:forcetemp+responsedays:photoperiod_day, data=buddy)
+mod <- lm(response~responsedays+responsedays:forcetemp+responsedays:photoperiod_day+responsedays:Total_Utah_Model, data=buddy)
 summary(mod)
-table(buddy$photoperiod_day)
-table(buddy$chilldays)
+
 ###percent flower
-mod2 <- lm(response~responsedays+responsedays:forcetemp+responsedays:photoperiod_day, data=floy)
+mod2 <- lm(response~responsedays+responsedays:forcetemp+responsedays:photoperiod_day+responsedays:Total_Utah_Model, data=floy)
 summary(mod2)
 table(floy$forcetemp) #Why is floy rank deficient in force temp, they dont work as a mixed model either
 
+#### investigate mod and mod2
 
 ###look at jsut phenology ###i think there isnt enough levels for each factor
 #flower
 flo.phen<-filter(berries, respvar.simple=="daystoflower")
-flo.phen$forcetemp <- as.numeric(flo.phen$forcetemp)
-flo.phen$photoperiod_day <- as.numeric(flo.phen$photoperiod_day)
-flowers<-lm(response.time~forcetemp+photoperiod_day+chilldays, data=flo.phen)
+
+flowers<-lm(response.time~forcetemp+photoperiod_day+Total_Utah_Model, data=flo.phen)
 summary(flowers)
 plot(flowers)
 
@@ -80,12 +64,9 @@ fix <- which(bud.phen$figure.table..if.applicable.=="fig 3")
 bud.phen$response.time[fix] <- as.numeric(bud.phen$response[fix])
 bud.phen<-filter(bud.phen, figure.table..if.applicable.!= "fig 4")
 
-bud.phen$forcetemp <- as.numeric(bud.phen$forcetemp)
-bud.phen$photoperiod_day <- as.numeric(bud.phen$photoperiod_day)
-bud.phen$response.time <- as.numeric(bud.phen$response.time)
 
-leaves<-lm(response.time~forcetemp+photoperiod_day, data=bud.phen)
-summary(leaves)
+leaves<-lm(response.time~forcetemp+photoperiod_day+Total_Utah_Model, data=bud.phen)
+summary(leaves)  ###cant really run because all force temps are the same
 
 
 
