4ce32ae5473776fce47a39cc45d90ea6c211d129
dbuona <dbuonaiuto@g.harvard.edu>
2017-07-25 17:45:01 -0400

    comprehensive list of species complexed
    This will be our grouping factors
    see complex_levels.csv

--- analyses/misc/species_manipulation_levels.R
+++ analyses/misc/species_manipulation_levels.R
@@ -1,6 +1,5 @@
 ###how to divide
 
-
 rm(list=ls()) 
 options(stringsAsFactors = FALSE)
 library(ggplot2)
@@ -28,12 +27,56 @@ xx <- within(xx, { spp <- ave(species, name, FUN=function(x) length(unique(x)))}
 xx <- within(xx, { prov.long <- ave(provenance.long,name, species, FUN=function(x) length(unique(x)))}) # multiple provenance.longs
 xx <- within(xx, { datasets <- ave(datasetID, name, species, FUN=function(x) length(unique(x)))}) 
 
-xx<-dplyr::select(xx,name, datasets, force, photo, chill,field.sample,prov.lat)
+xx<-dplyr::select(xx,name,genus, datasets, force, photo, chill,field.sample,prov.lat)
 xx<-xx[!duplicated(xx),]
-View(xx)
-multi.dat<-filter(xx,datasets>=3)
-datforce<-filter(multi.dat, force>=3)
-multiforcepho<-filter(datforce,photo>=3)
-multiforcephochill<-filter(multiforcepho,chill>=3)
 
-write.csv(xx, file="~/Documents/git/ospree/analyses/output/species_manipulation_levels.csv", row.names = FALSE)
+#write.csv(xx, file="~/Documents/git/ospree/analyses/output/species_manipulation_levels.csv", row.names = FALSE)
+
+###make object with all acceptable (<1 data set species)
+accept<-filter(xx,datasets>1)
+species4taxon<-c(accept$name)
+spec.tax<-filter(d, name %in% species4taxon)
+spec.tax$complex<-spec.tax$name
+spec.tax$use<-"Y"
+
+
+###making complexes
+
+comp<-filter(xx,datasets==1)
+complex4taxon<-c(comp$name)
+###building complexes
+complexdata<- filter(d, name %in% complex4taxon)
+goober<-dplyr::select(complexdata,name,genus, datasetID)
+goober<-goober[!duplicated(goober),]
+#write.csv(goober, file="~/Documents/git/ospree/analyses/bb_analysis/taxon/one_ataset_list.csv", row.names = FALSE)
+complexdata$complex<-NA
+complexdata$complex[complexdata$genus == "Acer"] <- "Acer_complex"
+complexdata$complex[complexdata$genus == "Betula"] <- "Betula_complex"
+complexdata$complex[complexdata$genus == "Fraxinus"] <- "Fraxinus_complex"
+complexdata$complex[complexdata$genus == "Juglans"] <- "Juglans_complex"
+complexdata$complex[complexdata$genus == "Picea"] <- "Picea_complex"
+complexdata$complex[complexdata$genus == "Pinus"] <- "Pinus_complex"
+complexdata$complex[complexdata$genus == "Populus"] <- "Populus_complex"
+complexdata$complex[complexdata$genus == "Prunus"] <- "Prunus_complex"
+complexdata$complex[complexdata$genus == "Pyrus"] <- "Pyrus_complex"
+complexdata$complex[complexdata$genus == "Quercus"] <- "Quercus_complex"
+complexdata$complex[complexdata$genus == "Rhododendron"] <- "Rhododendron_complex"
+complexdata$complex[complexdata$genus == "Salix"] <- "Salix_complex"
+complexdata$complex[complexdata$genus == "Sorbus"] <- "Sorbus_complex"
+complexdata$complex[complexdata$genus == "Ulmus"] <- "Ulmus_complex"
+complexdata$complex[complexdata$genus == "Vaccinium"] <- "Vaccinium_complex"
+complexdata$complex[complexdata$genus == "Tilia"] <- "Tilia_complex"
+
+complexdata$use<-NA
+complexdata$use[!is.na(complexdata$complex)] <-"Y"
+complexdata$use[is.na(complexdata$complex)] <-"N"
+
+good<-select(spec.tax,genus,species,complex,use)
+good<-good[!duplicated(good),]
+
+bad<-select(complexdata,genus,species,complex,use)
+bad<-bad[!duplicated(bad),]
+complex.full<-rbind(good,bad)
+
+write.csv(complex.full, file="~/Documents/git/ospree/analyses/bb_analysis/taxon/complex_levels.csv", row.names = FALSE)
+
