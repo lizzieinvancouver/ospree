c18cd37b3566cd531345d8c2c8624a80528650db
dbuona <dbuonaiuto@g.harvard.edu>
2017-08-04 10:30:30 -0400

    fixed taxon is in bbdataplease.R
    I do believe everything included now fits our 2 datasetID criteria

--- analyses/bb_analysis/source/bbdataplease.R
+++ analyses/bb_analysis/source/bbdataplease.R
@@ -20,12 +20,38 @@ respvars.thatwewant <- c("daystobudburst", "percentbudburst")
 bb.wtaxa.resp <- bb.wtaxa[which(bb.wtaxa$respvar.simple %in% respvars.thatwewant),]
 bb.wtaxa.resp <- subset(bb.wtaxa.resp, respvar != "thermaltime") # doesn't remove anything
 
+###we have complexes with 1 only dataset id. This happened because we
+#filtered them out before reducing the response variables, code belowshould fix.
+huh <- within(bb.wtaxa.resp, { datasets <- ave(datasetID, complex, complex, FUN=function(x) length(unique(x)))}) 
+huh2<-select(huh,datasets, complex)
+huh2<-huh2[!duplicated(huh2),]
+#remerg<-filter(huh,datasets==1)
+remerg<-select(huh,datasetID, complex)
+remerg<-remerg[!duplicated(remerg),]
+
+
+### making new 
+huh<-within(huh,complex[complex=="Cornus_alba"]<-"Cornus_complex")
+huh<-within(huh,complex[complex=="Cornus_mas"]<-"Cornus_complex")
+huh<-within(huh,complex[complex=="Fraxinus_excelsior"]<-"Fraxinus_complex")
+huh<-within(huh,complex[complex=="Prunus_avium"]<-"Prunus_complex")
+huh <- within(huh, { datasets <- ave(datasetID, complex, complex, FUN=function(x) length(unique(x)))}) 
+
+huh$use2<-NA
+huh<- within(huh, use2[datasets<=1 ]<-"N")
+huh<- within(huh, use2[datasets>1 ]<-"Y")
+
+
+amended<-filter(huh,use2=="Y")
+
 ## subsetting for experimental chilling
 #bb<-subset(bb,!is.na(as.numeric(chilltemp)))
 
+table(amended$datasets)
 # slim down our columns
 columnstokeep <- c("datasetID", "genus", "species", "varetc", "woody", "forcetemp", "forcetemp_night",
                    "photoperiod_day", "response", "response.time", "Total_Chilling_Hours", 
                    "complex", "type","provenance.lat")
-bb <- subset(bb.wtaxa.resp, select=columnstokeep)
+bb <- subset(amended, select=columnstokeep)
+
 
