a3ade7e06bdd6b5a7c0774db31cfeda5e3a2aa6c
MoralesCastilla <moralescastilla@g.harvard.edu>
2017-09-05 16:10:48 +0200

    Updates from Nacho

--- analyses/bb_analysis/GDD_plotting.R
+++ analyses/bb_analysis/GDD_plotting.R
@@ -155,3 +155,99 @@ ggdraw(plot_grid(plot_grid(gdds[[4]],gdds.nomalus[[4]],gdds.partial[[4]], ncol=3
                  rel_widths=c(0.8, 0.5)))
 
 
+## replicate plotting accounting for photoperiod treatments
+gdds.photo<-list()
+gdds.nomalus.photo<-list()
+gdds.partial.photo<-list()
+gdds.legends.photo<-list()
+photos<-sort(as.numeric(unique(bb.wstart$photoperiod_day)))
+photo.i<-c(1,2,3,4)
+for (i in chills){#i=70
+  
+  chills.i<-names(bb.wstart)[i]  
+  bb.wstart[,chills.i]<-as.numeric(bb.wstart[,chills.i])
+  bb.plot<-subset(bb.wstart,!is.na(bb.wstart[,chills.i]))
+  bb.plot$chillmetric<-bb.plot[,chills.i]
+  gdds.photo[[which(chills==i)]]<-list(NULL,NULL,NULL,NULL)
+  gdds.nomalus.photo[[which(chills==i)]]<-list(NULL,NULL,NULL,NULL)
+  gdds.partial.photo[[which(chills==i)]]<-list(NULL,NULL,NULL,NULL)
+  gdds.legends.photo[[which(chills==i)]]<-list(NULL,NULL,NULL,NULL)
+  
+  for (j in photo.i){
+    print(paste(i,j))
+    
+  if(j==1){
+    bb.plot.photo<-subset(bb.plot,photoperiod_day%in%as.character(photos[1:7]))
+  }
+  if(j==2){
+      bb.plot.photo<-subset(bb.plot,photoperiod_day%in%as.character(photos[8:16]))
+  }
+  if(j==3){
+      bb.plot.photo<-subset(bb.plot,photoperiod_day%in%as.character(photos[7:25]))
+  }
+  if(j==4){
+      bb.plot.photo<-subset(bb.plot,photoperiod_day%in%as.character(photos[26:34]))
+    }
+    
+      
+  # Scatterplot all
+    gdds.photo[[which(chills==i)]][[j]] <- ggplot(bb.plot.photo, aes(x=chillmetric, y=gdd)) + 
+    geom_point(aes(col=factor(genus)),alpha=0.25,size=3) + 
+    geom_smooth(method="lm", se=T) + 
+    #xlim(c(0, 200)) + 
+    #ylim(c(0, 7500)) + 
+    theme(legend.position="none")+
+    labs( 
+      y="GDD", 
+      x=chills.i
+      #title="Scatterplot", 
+    )
+  
+  # Scatterplot without malus 
+  gdds.nomalus.photo[[which(chills==i)]][[j]] <- ggplot(bb.plot.photo, aes(x=chillmetric, y=gdd)) + 
+    geom_point(aes(col=factor(genus)),alpha=0.25,size=3) + 
+    geom_smooth(method="lm", se=T) + 
+    #xlim(c(0, 200)) + 
+    ylim(c(0, 3500)) + 
+    theme(legend.position="none")+
+    labs( 
+      y="GDD", 
+      x=chills.i
+      #title="Scatterplot", 
+    )
+  
+  # Scatterplot without malus and each generic fitted line
+  gdds.partial.photo[[which(chills==i)]][[j]] <- ggplot(bb.plot.photo, aes(x=chillmetric, y=gdd)) + 
+    geom_point(aes(col=factor(genus)),alpha=0.25,size=3) + 
+    geom_smooth(aes(col=factor(genus)),method="lm", se=F) + 
+    #xlim(c(0, 200)) + 
+    ylim(c(0, 3500)) + 
+    theme(legend.position="none")+
+    labs( 
+      y="GDD", 
+      x=chills.i
+      #title="Scatterplot", 
+    )
+  
+  # legends
+  if(nrow(bb.plot.photo)>2){
+  gdds.legends.photo[[which(chills==i)]][[j]]<-get_legend( ggplot(bb.plot.photo, aes(x=chillmetric, y=gdd)) + 
+                                                  geom_point(aes(col=factor(genus))))
+  }
+}
+
+}
+
+i=3;j=1
+ggdraw(plot_grid(plot_grid(plot_grid(gdds.photo[[i]][[j]],gdds.nomalus.photo[[i]][[j]],gdds.partial.photo[[i]][[j]], ncol=3, align='h'),
+                            plot_grid(gdds.legends.photo[[1]][[j]], ncol=1,axis="t"),
+                            rel_widths=c(1, 0.2)),
+                 plot_grid(plot_grid(gdds.photo[[i]][[j+1]],gdds.nomalus.photo[[i]][[j+1]],gdds.partial.photo[[i]][[j+1]], ncol=3, align='h'),
+                           plot_grid(gdds.legends.photo[[1]][[j+1]], ncol=1,axis="t"),
+                           rel_widths=c(1, 0.2)),
+                 plot_grid(plot_grid(gdds.photo[[i]][[j+2]],gdds.nomalus.photo[[i]][[j+2]],gdds.partial.photo[[i]][[j+2]], ncol=3, align='h'),
+                           plot_grid(gdds.legends.photo[[1]][[j+2]], ncol=1,axis="t"),
+                           rel_widths=c(1, 0.2)),
+                 plot_grid(plot_grid(gdds.photo[[i]][[j+3]],gdds.nomalus.photo[[i]][[j+3]],gdds.partial.photo[[i]][[j+3]], ncol=3, align='h'),
+                           plot_grid(gdds.legends.photo[[i]][[j+3]], ncol=1,axis="t"),
+                           rel_widths=c(1, 0.2))))
---
+++ analyses/bb_analysis/get_meantemp_ambient.R
@@ -0,0 +1,106 @@
+
+##############################################################################################################
+# Script and functions to:
+#' * Extract GDD from last day of chilling until day of BB - potential response variable  
+#'
+#'  Ospree
+#'  started 21 July 2017
+#'  
+#'  This file is unfinished -- need to explicitly incorporate ambient and ambient +1, +4, etc.
+##############################################################################################################
+
+
+## to start
+rm(list=ls())
+options(stringsAsFactors=FALSE)
+
+# Set working directory: 
+if(length(grep("Lizzie", getwd())>0)) { setwd("~/Documents/git/projects/treegarden/budreview/ospree/analyses") 
+} else if
+(length(grep("Ignacio", getwd()))>0) { setwd("~/GitHub/ospree/analyses") 
+} else if
+(length(grep("ailene", getwd()))>0) {setwd("/Users/aileneettinger/git/ospree/analyses")
+} else 
+  setwd("~/Documents/git/ospree/analyses")
+
+## read in last day of chilling and format a little
+chill.day <- read.csv("output/daily_expchill.csv")
+chill.unique.exptreat<-unique(chill.day$uniqueID)
+
+chillneeded <- subset(chill.day, select=c("uniqueID", "lastchilldate"))
+chilly <- chillneeded[!duplicated(chillneeded), ]
+#head(chilly)
+
+## read in data for pmp containing climate each day each site
+#load("output/fieldclimate_pmp.RData")
+#pmp.data <- tempval
+#rm(tempval)
+#studiesnames <- names(pmp.data)
+
+clima <- read.csv("output/pmp/percbb_clim_pmpA.csv", header=TRUE)
+climb <- read.csv("output/pmp/percbb_clim_pmpB.csv", header=TRUE)
+climc <- read.csv("output/pmp/percbb_clim_pmpC.csv", header=TRUE)
+climd <- read.csv("output/pmp/percbb_clim_pmpD.csv", header=TRUE)
+
+climdatab <- rbind(clima,climb,climc,climd)
+climdat <- climdatab[!duplicated(climdatab), ] 
+rm(clima,climb,climc,climd)
+
+## get all the BB data and format a little
+dat.all <- read.csv("output/ospree_clean_withchill_BB.csv", header=TRUE)
+dat.some <- subset(dat.all, respvar.simple=="daystobudburst"|respvar.simple=="percentbudburst")
+bbdat <- subset(dat.some, response.time!="")
+
+
+# add a column for when the experiment starts to bb data
+# fill it in with either last date of experimental chilling or (if not present) field sample date
+bbdat$uniqueID <- paste(bbdat$datasetID, bbdat$fieldsample.date2, bbdat$forcetemp, bbdat$chilltemp, bbdat$chilldays,
+                        bbdat$chillphotoperiod, bbdat$photoperiod_day) # this is what becomes stn in the climate data 
+
+
+bb <- merge(chilly, bbdat, by="uniqueID", all.y=TRUE)
+bb$expstartdate <- bb$lastchilldate # subset(bb, is.na(bb$expstartdate)==FALSE)
+bb$expstartdate[which(is.na(bb$expstartdate)==TRUE)] <- bb$fieldsample.date2[which(is.na(bb$expstartdate)==TRUE)] 
+#dim(subset(bb, is.na(bb$expstartdate)==TRUE))
+
+## okay, need to get response.time and expstartdate into date formats...
+bb$expstartdate <- as.Date(bb$expstartdate, format="%Y-%m-%d")
+bb$response.time.integer <- as.integer(bb$response.time)
+bb$bbdate <- as.Date(bb$response.time.integer, origin=bb$expstartdate, format="%Y-%m-%d")
+
+
+
+# generate an empty variable to store mean temps
+bb$avg_bbtemp<-NA
+
+## Loop to add mean temp to each line in bb
+for(i in 1:nrow(bb)){#i=319
+  lon.i<-bb[i,"chill.long"]
+  lat.i<-bb[i,"chill.lat"]
+  start.i<-bb[i,"expstartdate"]
+  end.i<-bb[i,"bbdate"]
+  year.i<-as.numeric(format(start.i,"%Y"))
+  year.end.i<-as.numeric(format(end.i,"%Y"))
+  ID.i<-bb[i,'uniqueID']
+  doy.i<-as.numeric(format(start.i,"%j"))
+  doy.end.i<-as.numeric(format(end.i,"%j"))
+  
+  clim.i<-subset(climdat,stn==ID.i)
+  
+  #clim.i$Tmean
+  if(nrow(clim.i)>0 & sum(!is.na(clim.i$Tmean))>0){
+    
+    print(i)
+  bb$avg_bbtemp[i]<-mean(clim.i[which(clim.i$year==year.i & clim.i$doy2==doy.i):
+           which(clim.i$year==year.end.i & clim.i$doy2==doy.end.i),"Tmean"],na.rm=T)
+  #clim.i<-subset(clim.i,year==year.i | year==year.end.i)
+  }
+  
+}
+
+
+## saving results to output - Still in need to be added to clean_ambientforcing.R
+write.csv(bb,"output/bbdata_avgTemp.csv")
+
+
+
