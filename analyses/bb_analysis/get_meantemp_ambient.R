175547a147b16a08b9ca8ca4b44e016687c94e4c
Elizabeth Wolkovich <wolkovich@biodiversity.edu>
2017-09-11 18:56:21 -0400

    Just small edits to commented text
    See also issue 130.

--- analyses/bb_analysis/get_meantemp_ambient.R
+++ analyses/bb_analysis/get_meantemp_ambient.R
@@ -31,12 +31,7 @@ chillneeded <- subset(chill.day, select=c("uniqueID", "lastchilldate"))
 chilly <- chillneeded[!duplicated(chillneeded), ]
 #head(chilly)
 
-## read in data for pmp containing climate each day each site
-#load("output/fieldclimate_pmp.RData")
-#pmp.data <- tempval
-#rm(tempval)
-#studiesnames <- names(pmp.data)
-
+## read in data containing climate each day each site (it's big so it is in pieces)
 clima <- read.csv("output/pmp/percbb_clim_pmpA.csv", header=TRUE)
 climb <- read.csv("output/pmp/percbb_clim_pmpB.csv", header=TRUE)
 climc <- read.csv("output/pmp/percbb_clim_pmpC.csv", header=TRUE)
@@ -51,14 +46,13 @@ dat.all <- read.csv("output/ospree_clean_withchill_BB.csv", header=TRUE)
 dat.some <- subset(dat.all, respvar.simple=="daystobudburst"|respvar.simple=="percentbudburst")
 bbdat <- subset(dat.some, response.time!="")
 
-
-# add a column for when the experiment starts to bb data
-# fill it in with either last date of experimental chilling or (if not present) field sample date
-bbdat$uniqueID <- paste(bbdat$datasetID, bbdat$fieldsample.date2, bbdat$forcetemp, bbdat$chilltemp, bbdat$chilldays,
-                        bbdat$chillphotoperiod, bbdat$photoperiod_day) # this is what becomes stn in the climate data 
-
-
+# format: make a column to match to climate data, and merge the BB and climate data
+bbdat$uniqueID <- paste(bbdat$datasetID, bbdat$fieldsample.date2, bbdat$forcetemp, bbdat$chilltemp, 
+    bbdat$chilldays,bbdat$chillphotoperiod, bbdat$photoperiod_day)  
 bb <- merge(chilly, bbdat, by="uniqueID", all.y=TRUE)
+
+## add a column for when the experiment starts to bb data
+# fill it in with either last date of experimental chilling or (if not present) field sample date
 bb$expstartdate <- bb$lastchilldate # subset(bb, is.na(bb$expstartdate)==FALSE)
 bb$expstartdate[which(is.na(bb$expstartdate)==TRUE)] <- bb$fieldsample.date2[which(is.na(bb$expstartdate)==TRUE)] 
 #dim(subset(bb, is.na(bb$expstartdate)==TRUE))
@@ -67,8 +61,6 @@ bb$expstartdate[which(is.na(bb$expstartdate)==TRUE)] <- bb$fieldsample.date2[whi
 bb$expstartdate <- as.Date(bb$expstartdate, format="%Y-%m-%d")
 bb$response.time.integer <- as.integer(bb$response.time)
 bb$bbdate <- as.Date(bb$response.time.integer, origin=bb$expstartdate, format="%Y-%m-%d")
-
-
 
 # generate an empty variable to store mean temps
 bb$avg_bbtemp<-NA
@@ -97,7 +89,6 @@ for(i in 1:nrow(bb)){#i=319
   }
   
 }
-
 
 ## saving results to output - Still in need to be added to clean_ambientforcing.R
 write.csv(bb,"output/bbdata_avgTemp.csv")
