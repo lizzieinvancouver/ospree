f0ef0ae840bb7b8034d8b4a92f9baba922ea058c
cchambe12 <cchamberlain@g.harvard.edu>
2017-09-28 11:57:43 -0400

    added climate script

---
+++ analyses/limitingcues/ClimatePEP_data.R
@@ -0,0 +1,62 @@
+# Extract Climate Data for Limiting Cues Paper
+# 28 September 2017 - Cat 
+
+# Clear workspace
+rm(list=ls()) # remove everything currently held in the R memory
+options(stringsAsFactors=FALSE)
+graphics.off()
+
+# Load libraries
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(lubridate)
+library(ncdf4)
+library(Interpol.T)
+library(chillR)
+library(raster)
+library(reshape2)
+library(data.table)
+library(arm)
+
+# Set working directory: 
+if(length(grep("Lizzie", getwd())>0)) {
+  setwd("~/Documents/git/projects/treegarden/budreview/ospree/analyses") 
+} else
+  setwd("~/Documents/git/ospree/analyses/limitingcues")
+
+
+r<-brick("tn_0.25deg_reg_v15.0.nc", sep="")  ## from climate drive or W drive
+betula<-read.csv("input/PEP_betula.csv", header=TRUE)
+fagus<-read.csv("input/PEP_fagus.csv", header=TRUE)
+
+df<- full_join(betula, fagus)
+df$lat.long<-paste(df$LAT, df$LONG, sep=",")
+df<-df[!duplicated(df$lat.long),]
+
+lats <- df$LAT
+lons <- df$LON
+
+coords <- data.frame(x=lons,y=lats)
+coords<-na.omit(coords)
+
+points <- SpatialPoints(coords, proj4string = r@crs)
+
+values <- extract(r,points)
+
+dx <- cbind.data.frame(coordinates(points),values)
+
+dx<-melt(df, id.vars=c("x","y"))
+
+dx<-dx%>%
+  rename(long=x)%>%
+  rename(lat=y)%>%
+  rename(date=variable)%>%
+  rename(Tmin=value)
+
+dx$date<-substr(dx$date, 2,11)
+dx$year<-as.numeric(substr(dx$date, 0,4))
+dx$month<-as.numeric(substr(dx$date, 6, 7))
+dx$day<-as.numeric(substr(dx$date, 9,10))
+dx$date<-as.Date(paste(dx$year, dx$month, dx$day, sep="-"))
+
