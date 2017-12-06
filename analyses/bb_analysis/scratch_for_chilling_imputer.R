11ec87368a82b34ab78161b6f80619b98c41ce73
dbuona <dbuonaiuto@g.harvard.edu>
2017-12-06 15:47:17 -0500

    Went through all missing chilling and diagnosed the problem
    see chillmeout_check.csv for my erratics notes
    scratch_for_chilling_imputter.R for a slightly more organized overview

---
+++ analyses/bb_analysis/scratch_for_chilling_imputer.R
@@ -0,0 +1,63 @@
+###scartch for dan to think about chilling imputation 12 Dec 2017
+### Here is my assesmenst
+
+#Field Chill Units listed than maybe Ailene can calculate
+  #biasi12
+  #cook00b
+  #nishimoto95
+
+#entries with NA in chil calcs when should be 0
+  #sogaard08	exp1<- 0 chilldays become NA in chillin calcs
+  #sogaard08	exp2 <- 0 chilldays become NA in chillin calcs
+  #sogaard08	exp3a<- 0 chilldays become NA in chillin calcs
+  #sogaard08	exp1b<- 0 chilldays become NA in chillin calcs
+  #thielges75 <- 0 chilldays become NA in chillin calcs
+  #caffarra11b exp3 fig1 <- 0 chilldays become NA in chillin calcs
+  #falusi96 exp2 field.chill==no
+  #falusi96 exp3 field.chill==no
+  #falusi90 exp1 field.chill==no
+
+###same as above but need 0 chill days and 0 chill temp imputed
+  #li05 other.treatment="short day controls"
+  #junttila12 figure table if applicable==table2
+
+
+#Entrie where chill temp could be added
+  #sogaard08	exp3b <- chill temp 4
+
+###we have lat and long and field sample data but chilling wasnt calculated
+  #heide93 exp 3 just for corylus avenulla for some reason
+  #caffarra11a exp1 <- database says no field sample date but paper says: 28 February 2004.
+  #gheraldini10 exp1- need to imput field sample day from figure 2
+  #gheraldini10 exp2-need to imput field sample day from figure 4
+
+###studies we could impute chill temp and duration
+  #heide12 exp3 2c for 10-12 week. Idea. 11 weeks?
+
+###re-clean chilltemp column
+  #granhus09 it "Chilling treatment at 0.7 \xb1 0.7 C interrupted by mild spells of 14 days duration at a constant temperature of 8 or 12 C"
+    ###change to just 0.7
+
+#Chilling hours until endodormancy release in table-not sre about this one
+  #charrier11<-exp 1
+  #charrier11<-exp 2 could be the same chilling requirement as above. 
+
+#############Harder to fix##################
+
+## multistep: 1) Lat long and field sample date 2) freeze treatment 3) exp chilling: figure 1
+  #cook05: exp1 fig1: <- would need to re-extract from figure
+##multistep: 1)Freeze treatment and 2)exp chilling
+  #cook05: exp1 fig2,-would need to reextract chilling from figure 2
+
+#Possible Impuation of field chilling dates
+  #falusi96 exp1 fall 1987-?
+  #falusi96 exp2 field.chill==yes begining october 1989
+  #falusi96 exp3 field.chill==yes begining october 1990
+  #falusi90 exp1 chilldays=winter  ? to first week of Febrary
+
+###cant/wont fix
+  #gansert02: only 4 rows would require climate data from multiple Mt Fugi Weather stations
+
+##############ask Lizze about###############:
+#Charier11
+
