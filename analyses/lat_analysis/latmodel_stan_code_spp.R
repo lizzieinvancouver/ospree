c71f901296ebdf7ee44a6b0260c2af78f936ef95
cchambe12 <cchamberlain@g.harvard.edu>
2018-03-05 17:59:45 -0500

    working on latitude analysis

--- analyses/lat_analysis/latmodel_stan_code_spp.R
+++ analyses/lat_analysis/latmodel_stan_code_spp.R
@@ -18,6 +18,8 @@ library(ggplot2)
 library(shinystan)
 library(bayesplot)
 library(rstanarm)
+library(brms)
+library(ggstance)
 
 # Setting working directory. Add in your own path in an if statement for your file structure
 if(length(grep("danflynn", getwd())>0)) { 
@@ -53,11 +55,11 @@ taxon <- read.csv("output/bb_analysis/taxon/complex_levels.csv", header=TRUE)
 # merge in labgroup (we could do this elsewhere someday)
 bb.wlab <- merge(bb, taxon, by=c("genus","species"), all.x=TRUE)
 myspp<-c("Betula_pendula", "Betula_pubscens", "Fagus_sylvatica", "Picea_abies",
-         "Ribes_nigrum", "Corylus_avellana", "Quercus_robur", "Larix_decidua")
+         "Ribes_nigrum")
 bb.wlab<-dplyr::filter(bb.wlab, complex%in%myspp)
 
 columnstokeep <- c("datasetID", "genus", "species", "varetc", "woody", "forcetemp",
-                   "photoperiod_day", "response", "response.time", "Total_Chilling_Hours",
+                   "photoperiod_day", "response", "response.time", "Total_Chill_portions",
                    "complex", "provenance.lat")
 
 bb.wlab.sm <- subset(bb.wlab, select=columnstokeep)
@@ -66,15 +68,9 @@ bb.wlab.sm <- subset(bb.wlab, select=columnstokeep)
 ## make a bunch of things numeric (eek!)
 bb.wlab.sm$force <- as.numeric(bb.wlab.sm$forcetemp)
 bb.wlab.sm$photo <- as.numeric(bb.wlab.sm$photoperiod_day)
-bb.wlab.sm$chill <- as.numeric(bb.wlab.sm$Total_Chilling_Hours)
+bb.wlab.sm$chill <- as.numeric(bb.wlab.sm$Total_Chill_portions)
 bb.wlab.sm$resp <- as.numeric(bb.wlab.sm$response.time)
 bb.wlab.sm$lat<-as.numeric(bb.wlab.sm$provenance.lat)
-
-
-## In case we decide to center data, not doing it for now
-#bb.wlab.sm$photo.cen <- scale(bb.wlab.sm$photo, center=TRUE, scale=TRUE)
-#bb.wlab.sm$force.cen <- scale(bb.wlab.sm$force, center=TRUE, scale=TRUE)
-#bb.wlab.sm$chill.cen <- scale(bb.wlab.sm$chill, center=TRUE, scale=TRUE)
 
 
 ## subsetting data, preparing genus variable, removing NAs
@@ -82,7 +78,104 @@ ospr.prepdata <- subset(bb.wlab.sm, select=c("resp", "chill", "photo", "force", 
 dim(subset(bb.wlab.sm, is.na(chill)==FALSE & is.na(photo)==FALSE & is.na(force)==FALSE 
            & is.na(lat)==FALSE))
 ospr.stan <- ospr.prepdata[complete.cases(ospr.prepdata),]
-ospr.stan$complex <- as.numeric(as.factor(ospr.stan$complex))
+ospr.stan$sp <- as.numeric(as.factor(ospr.stan$complex))
+
+
+lat.stan<-stan_glmer(resp~chill+photo+force+lat+(1|complex), data=ospr.stan)
+
+## Woohoo!!!
+#stan_glmer
+#family:  gaussian [identity]
+#formula: resp ~ chill + photo + force + lat + (1 | complex)
+#------
+  
+#  Estimates:
+#  Median MAD_SD
+#(Intercept) 164.5   12.9 
+#chill        -0.2    0.0 
+#photo        -1.4    0.2 
+#force        -0.6    0.2 
+#lat          -1.3    0.2 
+#sigma        40.3    0.7 
+
+#Error terms:
+#  Groups   Name        Std.Dev.
+#complex  (Intercept) 18      
+#Residual             40      
+#Num. levels: complex 7 
+
+#Sample avg. posterior predictive 
+#distribution of y (X = xbar):
+#  Median MAD_SD
+#mean_PPD 46.6    1.3  
+
+#------
+#  For info on the priors used see help('prior_summary.stanreg').
+
+#lat.brm<-brm(resp~ force + photo + chill + lat + force:photo + force:chill + photo:chill + force:lat + photo:lat + chill:lat + (1|sp) + (force-1|sp) + (photo-1|sp)
+#             + (chill-1|sp) + (force:photo-1|sp) +
+#               (force:chill-1|sp) + (photo:chill-1|sp) + (force:lat-1|sp) + (photo:lat-1|sp) + (chill:lat-1|sp), data=ospr.stan)
+
+lat.brm<-brm(resp~ force + photo + chill + lat + (1|sp) + (force-1|sp) + (photo-1|sp)
+             + (chill-1|sp) + (lat-1|sp), data=ospr.stan)
+m<-lat.brm
+m.int<-posterior_interval(m)
+sum.m<-summary(m)
+cri.f<-as.data.frame(sum.m$fixed[,c("Estimate", "l-95% CI", "u-95% CI")])
+cri.f<-cri.f[-1,] #removing the intercept 
+fdf1<-as.data.frame(rbind(as.vector(cri.f[,1]), as.vector(cri.f[,2]), as.vector(cri.f[,3])))
+fdf2<-cbind(fdf1, c(0, 0, 0) , c("Estimate", "2.5%", "95%"))
+names(fdf2)<-c(rownames(cri.f), "sp", "perc")
+
+cri.r<-(ranef(m, summary = TRUE, robust = FALSE,
+              probs = c(0.025, 0.975)))$sp
+cri.r2<-cri.r[, ,-1]
+cri.r2<-cri.r2[,-2,]
+dims<-dim(cri.r2)
+twoDimMat <- matrix(cri.r2, prod(dims[1:2]), dims[3])
+mat2<-cbind(twoDimMat, c(rep(1:7, length.out=21)), rep(c("Estimate", "2.5%", "95%"), each=7))
+df<-as.data.frame(mat2)
+names(df)<-c(rownames(cri.f), "sp", "perc")
+dftot<-rbind(fdf2, df)
+dflong<- tidyr::gather(dftot, var, value, force:lat, factor_key=TRUE)
+
+#adding the coef estiamtes to the random effect values 
+for (i in seq(from=1,to=nrow(dflong), by=24)) {
+  for (j in seq(from=7, to=23, by=1)) {
+    dflong$value[i+j]<- as.numeric(dflong$value[i+j]) + as.numeric(dflong$value[i])
+  }
+}
+dflong$rndm<-ifelse(dftot$sp>0, 2, 1)
+dfwide<-tidyr::spread(dflong, perc, value)
+dfwide[,4:6] <- as.data.frame(lapply(c(dfwide[,4:6]), as.numeric ))
+dfwide$sp<-as.factor(dfwide$sp)
+## plotting
+
+pd <- position_dodgev(height = -0.5)
+
+estimates<-c("Forcing", "Photoperiod", "Chill Portions", "Forcing x Photoperiod", 
+             "Forcing x Chill Portions", "Photoperiod x Chill Portions")
+estimates<-c("Forcing", "Photoperiod", "Chill Portions", "Latitude")
+dfwide$legend<-factor(dfwide$sp,
+                      labels=c("Overall Effects","1","2","3","4","5","6","7"))
+estimates<-rev(estimates)
+#write.csv(dfwide, file="~/Documents/git/springfreeze/output/df_modforplot.csv", row.names=FALSE)
+fig1 <-ggplot(dfwide, aes(x=Estimate, y=var, color=legend, size=factor(rndm), alpha=factor(rndm)))+
+  geom_point(position =pd)+
+  geom_errorbarh(aes(xmin=(`2.5%`), xmax=(`95%`)), position=pd, size=.5, height =0, width=0)+
+  geom_vline(xintercept=0)+
+  scale_colour_manual(values=c("blue", "firebrick3", "orangered1","orange3","sienna2", "green4", "green3", "purple2"),
+                      breaks=c("Overall Effects"))+
+  scale_size_manual(values=c(3, 2, 2, 2, 2, 2, 2, 2)) +
+  scale_shape_manual(labels="", values=c("1"=16,"2"=16))+
+  scale_alpha_manual(values=c(1, 0.5)) +
+  guides(size=FALSE, alpha=FALSE) + 
+  scale_y_discrete(limits = rev(unique(sort(dfwide$var))), labels=estimates) + ylab("") + 
+  labs(col="Effects") + theme(legend.position = "none", legend.box.background = element_rect(), 
+                              legend.title=element_blank(), legend.key.size = unit(0.05, "cm")) +
+  xlab(expression(atop("Model Estimate of Change ", paste("in Duration of Vegetative Risk (days)"))))
+fig1
+
 
 
 # Fairly strict rules of inclusion in this analysis: manipulation of forcing temperature, 
@@ -90,12 +183,13 @@ ospr.stan$complex <- as.numeric(as.factor(ospr.stan$complex))
 #of these columns, including labgroup!
 
 ## remove NAs individually .... (not needed currently)
-ospr.stan$resp<-ospr.stan[which(is.na(ospr.stan$resp)==FALSE),]
-ospr.stan$chill<-ospr.stan[which(is.na(ospr.stan$chill)==FALSE),]
-ospr.stan$force<-ospr.stan[which(is.na(ospr.stan$force)==FALSE),]
-ospr.stan$photo<-ospr.stan[which(is.na(ospr.stan$photo)==FALSE),]
-ospr.stan$lat<-ospr.stan[which(is.na(ospr.stan$lat)==FALSE),]
-ospr.stan$complex<-ospr.stan[which(is.na(ospr.stan$complex)==FALSE),]
+#ospr.stan$resp<-ospr.stan[which(is.na(ospr.stan$resp)==FALSE),]
+#ospr.stan$chill<-ospr.stan[which(is.na(ospr.stan$chill)==FALSE),]
+#ospr.stan$force<-ospr.stan[which(is.na(ospr.stan$force)==FALSE),]
+#ospr.stan$photo<-ospr.stan[which(is.na(ospr.stan$photo)==FALSE),]
+#ospr.stan$lat<-ospr.stan[which(is.na(ospr.stan$lat)==FALSE),]
+#ospr.stan$complex<-ospr.stan[which(is.na(ospr.stan$complex)==FALSE),]
+
 
 
 y = ospr.stan$resp
