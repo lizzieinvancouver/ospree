fb4d83ac41ba3716ff419f099ba0bc0172fbb29d
cchambe12 <cchamberlain@g.harvard.edu>
2018-03-07 15:47:23 -0500

    playing with latitude analysis more...

--- analyses/lat_analysis/latmodel_stan_code_spp.R
+++ analyses/lat_analysis/latmodel_stan_code_spp.R
@@ -54,8 +54,12 @@ taxon <- read.csv("output/bb_analysis/taxon/complex_levels.csv", header=TRUE)
 
 # merge in labgroup (we could do this elsewhere someday)
 bb.wlab <- merge(bb, taxon, by=c("genus","species"), all.x=TRUE)
-myspp<-c("Betula_pendula", "Betula_pubscens", "Fagus_sylvatica", "Picea_abies",
-         "Ribes_nigrum")
+tt <- table(bb.wlab$complex)
+bb.wlab <- subset(bb.wlab, complex %in% names(tt[tt > 200])) ### testing 
+  # [1] "Betula_pendula"       "Betula_pubescens"     "Fagus_sylvatica"      "Malus_domestica"     
+  # [5] "Picea_abies"          "Prunus_complex"       "Pyrus_complex"        "Rhododendron_complex"
+  # [9] "Ribes_nigrum"         "Sorbus_complex" 
+myspp<-c("Betula_pendula", "Betula_pubescens", "Fagus_sylvatica", "Picea_abies")
 bb.wlab<-dplyr::filter(bb.wlab, complex%in%myspp)
 
 columnstokeep <- c("datasetID", "genus", "species", "varetc", "woody", "forcetemp",
@@ -80,44 +84,188 @@ dim(subset(bb.wlab.sm, is.na(chill)==FALSE & is.na(photo)==FALSE & is.na(force)=
 ospr.stan <- ospr.prepdata[complete.cases(ospr.prepdata),]
 ospr.stan$sp <- as.numeric(as.factor(ospr.stan$complex))
 
+#ospr.stan$resp<-ifelse(ospr.stan$resp==0, 0.01, ospr.stan$resp) ## if Gamma is necessary!
+lat.stan<-stan_glmer(resp~chill+photo+force+lat+(1|sp) + (force-1|sp) + (photo-1|sp)
+                     + (chill-1|sp) + (lat-1|sp), data=ospr.stan)
+lat.stan.inter<-stan_glmer(resp~chill+photo+force+lat+(1|sp) + chill:photo +
+                             chill:force + chill:lat + photo:force + photo:lat +
+                             force:lat + (force-1|sp) + (photo-1|sp)
+                           + (chill-1|sp) + (lat-1|sp) + (force:photo-1|sp) +
+                             (force:chill-1|sp) + (photo:chill-1|sp) + 
+                             (force:lat-1|sp) + (photo:lat-1|sp) + (chill:lat-1|sp), data=ospr.stan)
 
-lat.stan<-stan_glmer(resp~chill+photo+force+lat+(1|complex), data=ospr.stan)
+m<-lat.stan
+# This is an Rstanarm object, and so behaves differently than th previous brms object. Thus the coeffs have to be extracted 
+#differently.  There's probably a better way to do this, but I already had this code written up for something else, so used it again.
+sum.m <-
+  summary(
+    m,
+    pars = c(
+      "(Intercept)",
+      "chill",
+      "force",
+      "photo",
+      "lat"
+      ,
+      "b[chill sp:1]",
+      "b[force sp:1]",
+      "b[photo sp:1]",
+      "b[lat sp:1]"
+      ,
+      "b[chill sp:2]",
+      "b[force sp:2]",
+      "b[photo sp:2]",
+      "b[lat sp:2]"
+      ,
+      "b[chill sp:3]",
+      "b[force sp:3]",
+      "b[photo sp:3]",
+      "b[lat sp:3]"
+      ,
+      "b[chill sp:4]",
+      "b[force sp:4]",
+      "b[photo sp:4]",
+      "b[lat sp:4]"
+    )
+  )
+
+sum.m <-
+  summary(
+    m,
+    pars = c(
+      "(Intercept)",
+      "chill",
+      "force",
+      "photo",
+      "lat",
+      "chill:force",
+      "chill:photo",
+      "chill:lat",
+      "force:photo",
+      "force:lat",
+      "photo:lat"
+      ,
+      "b[chill sp:1]",
+      "b[force sp:1]",
+      "b[photo sp:1]",
+      "b[lat sp:1]",
+      "b[chill:force sp:1]",
+      "b[chill:photo sp:1]",
+      "b[chill:lat sp:1]",
+      "b[force:photo sp:1]",
+      "b[force:lat sp:1]",
+      "b[photo:lat sp:1]"
+      ,
+      "b[chill sp:2]",
+      "b[force sp:2]",
+      "b[photo sp:2]",
+      "b[lat sp:2]",
+      "b[chill:force sp:2]",
+      "b[chill:photo sp:2]",
+      "b[chill:lat sp:2]",
+      "b[force:photo sp:2]",
+      "b[force:lat sp:2]",
+      "b[photo:lat sp:2]"
+      ,
+      "b[chill sp:3]",
+      "b[force sp:3]",
+      "b[photo sp:3]",
+      "b[lat sp:3]",
+      "b[chill:force sp:3]",
+      "b[chill:photo sp:3]",
+      "b[chill:lat sp:3]",
+      "b[force:photo sp:3]",
+      "b[force:lat sp:3]",
+      "b[photo:lat sp:3]"
+      ,
+      "b[chill sp:4]",
+      "b[force sp:4]",
+      "b[photo sp:4]",
+      "b[lat sp:4]",
+      "b[chill:force sp:4]",
+      "b[chill:photo sp:4]",
+      "b[chill:lat sp:4]",
+      "b[force:photo sp:4]",
+      "b[force:lat sp:4]",
+      "b[photo:lat sp:4]"
+    )
+  )
+cri.f<-sum.m[c(2:21),c(1,4,8)] #just selecting the mean and 95% CI. Removing the intercept 
+fdf<-data.frame(cri.f)
+#binding 
+fdf2<-as.data.frame(
+  cbind(
+    (c(rownames(fdf)[1:20], rep(rev(rownames(fdf)[1:20]), each=4))), #stdarzing the parameter  names 
+    as.numeric(as.character(fdf$mean)),  # the estimate 
+    as.numeric(as.character(fdf$X2.5.)), #lower bound, 95% CI
+    as.numeric(as.character(fdf$X97.5.)),  #upper bound, 95% CI
+    as.numeric(c(rep(1, 20), rep(2, 5))),  # A variable to signify if the corresponding row is a fixed  or random effect. 1=global, 2=rndm
+    as.numeric( c(rep(0,20), rep(seq(1:4),20 ))))) #sp variable. Zero when a factor 
+names(fdf2)<-c("var", "Estimate", colnames(cri.f)[c(2,3)], "rndm", "sp") #renaming. 
+fdf2$Estimate<-as.numeric(fdf2$Estimate)      
+fdf2$`2.5%`<-as.numeric(fdf2$`2.5%`)
+fdf2$`97.5%`<-as.numeric(fdf2$`97.5%`)      
+#Fixed effect estimates:
+fixed<-c(rep(0, 100), rep(as.numeric(rev(fdf2[c(1:100),2])), each=2))
+dff<-fdf2
+#adding the fixef estiamtes to the random effect values:
+dff$Estimate<-fdf2$Estimate+fixed
+dff$`2.5%`<-fdf2$`2.5%`+fixed
+dff$`97.5%`<-fdf2$`97.5%`+fixed
+dff$var <- fct_inorder(dff$var) #so that the categorical variables plot in the right order 
+## plotting
+pd <- position_dodgev(height = -0.5)
+fig2<-ggplot(dff, aes(x=Estimate, y=var, color=factor(sp), size=factor(rndm), alpha=factor(rndm)))+
+  geom_point(position =pd, size=4)+
+  geom_errorbarh(aes(xmin=(`2.5%`), xmax=(`97.5%`)), position=pd, size=.5, height =0)+
+  geom_vline(xintercept=0)+
+  scale_colour_manual(labels = c("Fixed effects", "CAPBUR", "CHEMAJ", "DACGLO", "PLALAN", "PLAMAJ", "RUMCRI", "TAROFF"),
+                      values=c("blue", "red", "orangered1", "sienna4", "green4", "green1", "purple2", "magenta2"))+  scale_alpha_manual(values=c(1, 0.5))+
+  scale_shape_manual(labels="", values=c("1"=16,"2"=16))+
+  scale_alpha_manual(values=c(1, 0.5))+
+  guides(alpha=FALSE) + #removes the legend 
+  ggtitle(label = "Germination rate")+
+  scale_y_discrete(limits = rev(unique(sort(dff$var))))
+#pdf(file.path(figpath, "Fig2.pdf"), width = 7, height = 8)
+fig2
+
 
 ## Woohoo!!!
 #stan_glmer
-#family:  gaussian [identity]
-#formula: resp ~ chill + photo + force + lat + (1 | complex)
+#family:       gaussian [identity]
+#formula:      resp ~ chill + photo + force + lat + (1 | sp)
+#observations: 1889
 #------
-  
-#  Estimates:
 #  Median MAD_SD
-#(Intercept) 164.5   12.9 
-#chill        -0.2    0.0 
-#photo        -1.4    0.2 
-#force        -0.6    0.2 
-#lat          -1.3    0.2 
-#sigma        40.3    0.7 
+#(Intercept) 98.1   15.3  
+#chill       -0.2    0.0  
+#photo       -1.7    0.2  
+#force       -0.3    0.2  
+#lat         -0.3    0.1  
+#sigma       38.7    0.6  
 
 #Error terms:
 #  Groups   Name        Std.Dev.
-#complex  (Intercept) 18      
-#Residual             40      
-#Num. levels: complex 7 
+#sp       (Intercept) 35      
+#Residual             39      
+#Num. levels: sp 5 
 
-#Sample avg. posterior predictive 
-#distribution of y (X = xbar):
+#Sample avg. posterior predictive distribution of y:
 #  Median MAD_SD
-#mean_PPD 46.6    1.3  
+#mean_PPD 43.6    1.2  
 
-#------
-#  For info on the priors used see help('prior_summary.stanreg').
 
-#lat.brm<-brm(resp~ force + photo + chill + lat + force:photo + force:chill + photo:chill + force:lat + photo:lat + chill:lat + (1|sp) + (force-1|sp) + (photo-1|sp)
-#             + (chill-1|sp) + (force:photo-1|sp) +
-#               (force:chill-1|sp) + (photo:chill-1|sp) + (force:lat-1|sp) + (photo:lat-1|sp) + (chill:lat-1|sp), data=ospr.stan)
+lat.brm<-brm(resp~ force + photo + chill + lat + force:photo + force:chill + photo:chill + force:lat + photo:lat + chill:lat + (1|sp) + (force-1|sp) + (photo-1|sp)
+             + (chill-1|sp) + (lat-1|sp) + (force:photo-1|sp) +
+               (force:chill-1|sp) + (photo:chill-1|sp) + (force:lat-1|sp) + (photo:lat-1|sp) + (chill:lat-1|sp), data=ospr.stan)
 
-lat.brm<-brm(resp~ force + photo + chill + lat + (1|sp) + (force-1|sp) + (photo-1|sp)
-             + (chill-1|sp) + (lat-1|sp), data=ospr.stan)
+#lat.brm<-brm(resp~ force + photo + chill + lat + force:photo + force:chill + photo:chill + force:lat + photo:lat + 
+#               chill:lat + (1|sp), data=ospr.stan)
+
+
+#lat.brm<-brm(resp~ force + photo + chill + lat + (1|sp) + (force-1|sp) + (photo-1|sp)
+#             + (chill-1|sp) + (lat-1|sp), data=ospr.stan)
+
 m<-lat.brm
 m.int<-posterior_interval(m)
 sum.m<-summary(m)
@@ -133,15 +281,15 @@ cri.r2<-cri.r[, ,-1]
 cri.r2<-cri.r2[,-2,]
 dims<-dim(cri.r2)
 twoDimMat <- matrix(cri.r2, prod(dims[1:2]), dims[3])
-mat2<-cbind(twoDimMat, c(rep(1:7, length.out=21)), rep(c("Estimate", "2.5%", "95%"), each=7))
+mat2<-cbind(twoDimMat, c(rep(1:4, length.out=12)), rep(c("Estimate", "2.5%", "95%"), each=4))
 df<-as.data.frame(mat2)
 names(df)<-c(rownames(cri.f), "sp", "perc")
 dftot<-rbind(fdf2, df)
 dflong<- tidyr::gather(dftot, var, value, force:lat, factor_key=TRUE)
 
 #adding the coef estiamtes to the random effect values 
-for (i in seq(from=1,to=nrow(dflong), by=24)) {
-  for (j in seq(from=7, to=23, by=1)) {
+for (i in seq(from=1,to=nrow(dflong), by=18)) {
+  for (j in seq(from=3, to=17, by=1)) {
     dflong$value[i+j]<- as.numeric(dflong$value[i+j]) + as.numeric(dflong$value[i])
   }
 }
@@ -157,7 +305,7 @@ estimates<-c("Forcing", "Photoperiod", "Chill Portions", "Forcing x Photoperiod"
              "Forcing x Chill Portions", "Photoperiod x Chill Portions")
 estimates<-c("Forcing", "Photoperiod", "Chill Portions", "Latitude")
 dfwide$legend<-factor(dfwide$sp,
-                      labels=c("Overall Effects","1","2","3","4","5","6","7"))
+                      labels=c("Overall Effects","1","2","3","4","5"))
 estimates<-rev(estimates)
 #write.csv(dfwide, file="~/Documents/git/springfreeze/output/df_modforplot.csv", row.names=FALSE)
 fig1 <-ggplot(dfwide, aes(x=Estimate, y=var, color=legend, size=factor(rndm), alpha=factor(rndm)))+
