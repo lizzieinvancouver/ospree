5c5b540b737342cbbb2c9d649a910932193783a7
Elizabeth Wolkovich <wolkovich@biodiversity.edu>
2018-03-19 17:06:22 -0700

    Updated brms code to run with new structure
    Just udpated the first lines, also added code to show stan code

--- analyses/bb_analysis/models_brms.R
+++ analyses/bb_analysis/models_brms.R
@@ -4,65 +4,25 @@
 # housekeeping
 rm(list=ls()) 
 options(stringsAsFactors = FALSE)
-library(rstan)
-library(ggplot2)
-library(shinystan)
-library(bayesplot)
-library(brms)
 
 # Setting working directory. Add in your own path in an if statement for your file structure
-if(length(grep("danflynn", getwd())>0)) { 
-  setwd("~/Documents/git/ospree") 
+if(length(grep("lizzie", getwd())>0)) { 
+  setwd("~/Documents/git/treegarden/budreview/ospree/bb_analysis") 
 } else if (length(grep("ailene", getwd()))>0) {setwd("/Users/aileneettinger/git/ospree/analyses")
 }else if(length(grep("Ignacio", getwd()))>0) { 
-  setwd("~/GitHub/ospree/analyses") 
+  setwd("~/GitHub/ospree/analyses/bb_analysis") 
 } else if(length(grep("catchamberlain", getwd()))>0) { 
-  setwd("~/Documents/git/ospree/analyses") 
+  setwd("~/Documents/git/ospree/analyses/bb_analysis") 
 } else if(length(grep("danielbuonaiuto", getwd()))>0) { 
-  setwd("~/Documents/git/ospree/analyses") 
-}else setwd("~/Documents/git/projects/treegarden/budreview/ospree/analyses")
+  setwd("~/Documents/git/ospree/analyses/bb_analysis") 
+  }else setwd("~/Documents/git/projects/treegarden/budreview/ospree/analyses/bb_analysis")
 
-source('stan/savestan.R')
+library(brms)
 
 rstan_options(auto_write = TRUE)
 options(mc.cores = parallel::detectCores())
 
-## 3 steps to major cleaning: Get the data, merge in taxa info, subset down to what we want for:
-## Be sure to keep an eye on this part of the code and the files it sources, they will need updating!
-## (1) Get the data and slim down to correct response and no NAs ...
-source("bb_analysis/bbmodels_saturna/bbdataplease copy.R")
-## (2) Deal with species
-dim(bb.noNA)
-d <- bb.noNA
-source("bb_analysis/source/speciescomplex.R")
-bb.noNA.wtaxa <- d
-dim(bb.noNA.wtaxa)
-unique(bb.noNA.wtaxa$complex)
-# (3) Get fewer columns for sanity
-#source("bb_analysis/source/commoncols.R")
-bb <- subset(bb.noNA.wtaxa)#, select=columnstokeep)
-
-## For centering data, not doing it for now
-#bb$photo.cen <- scale(bb$photo, center=TRUE, scale=TRUE)
-#bb$force.cen <- scale(bb$force, center=TRUE, scale=TRUE)
-#bb$chill.cen <- scale(bb$chill, center=TRUE, scale=TRUE)
-
-## subsetting data, preparing genus variable, removing NAs (err, again
-# remove crops?
-# bb <- subset(bb, type!="crop")
-bb.stan <- subset(bb, select=c("datasetID", "resp", "chill", "photo", "force","chill.cent","photo.cent","force.cent", "complex", "type"))
-bb.stan$complex <- as.numeric(as.factor(bb.stan$complex))
-
-# remove the two values above 600
-bb.stan <- subset(bb.stan, resp<600)
-colnames(bb.stan)
-
-# adjust chilling (if needed)
-# here we are transforming chilling to have it in a scale more similar to the rest of variables and so that 
-# it can be interpreted as 10 days (so the coefficient will tell us change in BB every 10 days of chilling)
-bb.stan$chill <- bb.stan$chill/240
-
-length(unique(bb.stan$datasetID))
+source("source/bbstanleadin.R")
 
 # Fairly strict rules of inclusion in this analysis: manipulation of forcing temperature, 
 # photoperiod, and where we have a response in days and total chilling. 
@@ -70,12 +30,14 @@ length(unique(bb.stan$datasetID))
 #Fit model with no interactions and a species random slopes effect
 ########## m2l.ni: a(sp) + f(sp) + p(sp) + c(sp)
 
-m2l.ni.brms <- brm(resp ~ chill.cent+force.cent+photo.cent+#fixed effects
-                     (chill.cent+force.cent+photo.cent|complex), #random effects
-                   data = bb.stan, prior = c(set_prior("normal(0,50)", class = "b",coef="force.cent"),#priors
-                             set_prior("normal(0,10)", class = "b",coef="photo.cent"),
-                             set_prior("normal(0,50)", class = "b",coef="chill.cent")),
-                   chains = 2, cores = 2)
+m2l.ni.brms <- brm(resp ~ chill.cen+force.cen+photo.cen+#fixed effects
+                     (chill.cen+force.cen+photo.cen|complex), #random effects
+                   data = bb.stan, prior = c(set_prior("normal(0,50)", class = "b",coef="force.cen"),#priors
+                             set_prior("normal(0,10)", class = "b",coef="photo.cen"),
+                             set_prior("normal(0,50)", class = "b",coef="chill.cen")),
+                   chains = 2) 
+
+stancode(m2l.ni.brms)
 
 #Stan model says: a: 71.6, f=-22.4, p=-7.1, c=-9.5, sigma: 23.33
 summary(m2l.ni.brms)
