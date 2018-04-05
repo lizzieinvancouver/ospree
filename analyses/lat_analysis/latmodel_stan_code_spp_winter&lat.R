9082a1955fb4955ed3b88aad5413adf913c19824
cchambe12 <cchamberlain@g.harvard.edu>
2018-04-05 16:45:12 -0400

    working on lat analysis some more

--- analyses/lat_analysis/latmodel_stan_code_spp_winter&lat.R
+++ analyses/lat_analysis/latmodel_stan_code_spp_winter&lat.R
@@ -18,6 +18,7 @@ library(ggplot2)
 library(shinystan)
 library(bayesplot)
 library(rstanarm)
+library(brms)
 
 # Setting working directory. Add in your own path in an if statement for your file structure
 if(length(grep("danflynn", getwd())>0)) { 
@@ -50,7 +51,7 @@ options(mc.cores = parallel::detectCores())
 #bb.wlab <- merge(bb, taxon, by=c("genus","species"), all.x=TRUE)
 
 ######Above is all old code. Below is what formats the data properly in accordance with other models
-source("bb_analysis/source/bbdataplease.R")
+source("lat_analysis/source/bbdataplease.R")
 ## (2) Deal with species
 dim(bb.noNA)
 d <- bb.noNA
@@ -58,20 +59,26 @@ source("bb_analysis/source/speciescomplex.R")
 bb.noNA.wtaxa <- d
 dim(bb.noNA.wtaxa)
 unique(bb.noNA.wtaxa$complex)
+bb.wlab<-bb.noNA.wtaxa
 
 ###filter for species of interest
-myspp<-c("Betula_pendula", "Betula_pubscens", "Fagus_sylvatica", "Picea_abies",
-         "Ribes_nigrum", "Corylus_avellana", "Quercus_robur", "Larix_decidua")
-bb.wlab<-dplyr::filter(bb.noNA.wtaxa, complex%in%myspp)
+tt <- table(bb.wlab$complex)
+bb.wlab <- subset(bb.wlab, complex %in% names(tt[tt > 100])) 
+#myspp<-c("Betula_pendula", "Betula_pubscens", "Fagus_sylvatica", "Picea_abies",
+         #"Ribes_nigrum", "Corylus_avellana", "Quercus_robur", "Larix_decidua")
+#bb.wlab<-dplyr::filter(bb.noNA.wtaxa, complex%in%myspp)
 
 ####below is handled in source i think
-#columnstokeep <- c("datasetID", "genus", "species", "varetc", "woody", "forcetemp",
-                  # "photoperiod_day", "response", "response.time", "Total_Chilling_Hours",
-                   #"complex", "provenance.lat")
+columnstokeep <- c("datasetID", "genus", "species", "varetc", "woody", "forcetemp",
+                   "photoperiod_day", "response", "response.time", "Total_Chilling_Hours",
+                   "complex", "provenance.lat")
 
 bb.wlab.sm <- bb.wlab
 unique(bb.wlab.sm$complex)
 table(bb.wlab.sm$complex)
+myspp<-c("Betula_pendula", "Betula_pubescens", "Fagus_sylvatica", "Picea_abies", "Picea_glauca",
+         "Pseudotsuga_menziesii", "Ribes_nigrum")
+bb.wlab.sm<-dplyr::filter(bb.wlab.sm, complex%in%myspp)
 
 ## make a bunch of things numeric (eek!)
 bb.wlab.sm$force <- as.numeric(bb.wlab.sm$forcetemp)
@@ -80,12 +87,11 @@ bb.wlab.sm$chill <- as.numeric(bb.wlab.sm$Total_Chilling_Hours)
 bb.wlab.sm$resp <- as.numeric(bb.wlab.sm$response.time)
 bb.wlab.sm$lat<-as.numeric(bb.wlab.sm$provenance.lat)
 
-
 ## In case we decide to center data, not doing it for now
-bb.wlab.sm$photo <- scale(bb.wlab.sm$photo, center=TRUE, scale=TRUE)
-bb.wlab.sm$force <- scale(bb.wlab.sm$force, center=TRUE, scale=TRUE)
-bb.wlab.sm$chill <- scale(bb.wlab.sm$chill, center=TRUE, scale=TRUE)
-bb.wlab.sm$lat<- scale(bb.wlab.sm$lat, center=TRUE, scale=TRUE)
+#bb.wlab.sm$cphoto <- scale(bb.wlab.sm$photo, center=TRUE, scale=FALSE)
+#bb.wlab.sm$cforce <- scale(bb.wlab.sm$force, center=TRUE, scale=FALSE)
+#bb.wlab.sm$cchill <- scale(bb.wlab.sm$chill, center=TRUE, scale=FALSE)
+#bb.wlab.sm$clat<- scale(bb.wlab.sm$lat, center=TRUE, scale=FALSE)
 
 
 ## subsetting data, preparing genus variable, removing NAs
@@ -95,6 +101,215 @@ dim(subset(bb.wlab.sm, is.na(chill)==FALSE & is.na(photo)==FALSE & is.na(force)=
 ospr.stan <- ospr.prepdata[complete.cases(ospr.prepdata),]
 ospr.stan$complex <- as.numeric(as.factor(ospr.stan$complex))
 
+#lat.stan<-stan_glmer(resp~chill+photo+force+lat+(1|complex), data=ospr.stan) - ugly pp_checks
+ospr.stan$resp<-as.integer(ospr.stan$resp+1)
+ospr.stan<-ospr.stan[(ospr.stan$resp!=1000),]
+
+lat.pois<-stan_glmer(resp~chill+photo+force+lat+(1|complex), data=ospr.stan, family=poisson)
+
+ospr.stan$sm.chill<-ospr.stan$chill/240
+
+lat.pois<-stan_glmer(resp~sm.chill+photo+force+lat+(1|complex), data=ospr.stan, family=poisson)
+lat.pois.oneinter<-stan_glmer(resp~sm.chill+photo+force+lat+photo:lat+(1|complex), data=ospr.stan, family=poisson)
+
+lat.brm.inter<-brm(resp~sm.chill+photo+force+lat+photo:lat+(1|complex), data=ospr.stan, family=poisson)
+
+lat.pois.oneinter<-stan_glmer(resp~sm.chill+photo+force+lat+photo:lat+(1|complex)+
+                                (sm.chill-1|complex)+(photo-1|complex)+
+                                (force-1|complex)+(lat-1|complex)+
+                                (photo:lat-1|complex), data=ospr.stan, family=neg_binomial_2)
+
+lat.brm<-brm(resp~sm.chill+photo+force+lat+photo:lat+(1|complex)+
+                                (sm.chill-1|complex)+(photo-1|complex)+
+                                (force-1|complex)+(lat-1|complex)+
+                                (photo:lat-1|complex), data=ospr.stan, family=negbinomial, chains=2)
+
+m<-mod_rate 
+# This is an Rstanarm object, and so behaves differently than th previous brms object. Thus the coeffs have to be extracted 
+#differently.  There's probably a better way to do this, but I already had this code written up for something else, so used it again.
+sum.m <-
+  summary(
+    m,
+    pars = c(
+      "(Intercept)",
+      "origin",
+      "strat",
+      "temp1",
+      "temp2",
+      "temp3",
+      "origin:strat",
+      "origin:temp1",
+      "origin:temp2",
+      "origin:temp3",
+      "strat:temp1",
+      "strat:temp2",
+      "strat:temp3",
+      "origin:strat:temp1",
+      "origin:strat:temp2",
+      "origin:strat:temp3"
+      ,
+      "b[origin sp:1]",
+      "b[strat sp:1]",
+      "b[temp1 sp:1]",
+      "b[temp2 sp:1]",
+      "b[temp3 sp:1]",
+      "b[origin:strat sp:1]",
+      "b[origin:temp1 sp:1]",
+      "b[origin:temp2 sp:1]",
+      "b[origin:temp3 sp:1]",
+      "b[strat:temp1 sp:1]",
+      "b[strat:temp2 sp:1]",
+      "b[strat:temp3 sp:1]",
+      "b[origin:strat:temp1 sp:1]",
+      "b[origin:strat:temp2 sp:1]",
+      "b[origin:strat:temp3 sp:1]"
+      ,
+      "b[origin sp:2]",
+      "b[strat sp:2]",
+      "b[temp1 sp:2]",
+      "b[temp2 sp:2]",
+      "b[temp3 sp:2]",
+      "b[origin:strat sp:2]",
+      "b[origin:temp1 sp:2]",
+      "b[origin:temp2 sp:2]",
+      "b[origin:temp3 sp:2]",
+      "b[strat:temp1 sp:2]",
+      "b[strat:temp2 sp:2]",
+      "b[strat:temp3 sp:2]",
+      "b[origin:strat:temp1 sp:2]",
+      "b[origin:strat:temp2 sp:2]",
+      "b[origin:strat:temp3 sp:2]"
+      ,
+      "b[origin sp:3]",
+      "b[strat sp:3]",
+      "b[temp1 sp:3]",
+      "b[temp2 sp:3]",
+      "b[temp3 sp:3]",
+      "b[origin:strat sp:3]",
+      "b[origin:temp1 sp:3]",
+      "b[origin:temp2 sp:3]",
+      "b[origin:temp3 sp:3]",
+      "b[strat:temp1 sp:3]",
+      "b[strat:temp2 sp:3]",
+      "b[strat:temp3 sp:3]",
+      "b[origin:strat:temp1 sp:3]",
+      "b[origin:strat:temp2 sp:3]",
+      "b[origin:strat:temp3 sp:3]"
+      ,
+      "b[origin sp:4]",
+      "b[strat sp:4]",
+      "b[temp1 sp:4]",
+      "b[temp2 sp:4]",
+      "b[temp3 sp:4]",
+      "b[origin:strat sp:4]",
+      "b[origin:temp1 sp:4]",
+      "b[origin:temp2 sp:4]",
+      "b[origin:temp3 sp:4]",
+      "b[strat:temp1 sp:4]",
+      "b[strat:temp2 sp:4]",
+      "b[strat:temp3 sp:4]",
+      "b[origin:strat:temp1 sp:4]",
+      "b[origin:strat:temp2 sp:4]",
+      "b[origin:strat:temp3 sp:4]"
+      ,
+      "b[origin sp:5]",
+      "b[strat sp:5]",
+      "b[temp1 sp:5]",
+      "b[temp2 sp:5]",
+      "b[temp3 sp:5]",
+      "b[origin:strat sp:5]",
+      "b[origin:temp1 sp:5]",
+      "b[origin:temp2 sp:5]",
+      "b[origin:temp3 sp:5]",
+      "b[strat:temp1 sp:5]",
+      "b[strat:temp2 sp:5]",
+      "b[strat:temp3 sp:5]",
+      "b[origin:strat:temp1 sp:5]",
+      "b[origin:strat:temp2 sp:5]",
+      "b[origin:strat:temp3 sp:5]"
+      ,
+      "b[origin sp:6]",
+      "b[strat sp:6]",
+      "b[temp1 sp:6]",
+      "b[temp2 sp:6]",
+      "b[temp3 sp:6]",
+      "b[origin:strat sp:6]",
+      "b[origin:temp1 sp:6]",
+      "b[origin:temp2 sp:6]",
+      "b[origin:temp3 sp:6]",
+      "b[strat:temp1 sp:6]",
+      "b[strat:temp2 sp:6]",
+      "b[strat:temp3 sp:6]",
+      "b[origin:strat:temp1 sp:6]",
+      "b[origin:strat:temp2 sp:6]",
+      "b[origin:strat:temp3 sp:6]"
+      ,
+      "b[origin sp:7]",
+      "b[strat sp:7]",
+      "b[temp1 sp:7]",
+      "b[temp2 sp:7]",
+      "b[temp3 sp:7]",
+      "b[origin:strat sp:7]",
+      "b[origin:temp1 sp:7]",
+      "b[origin:temp2 sp:7]",
+      "b[origin:temp3 sp:7]",
+      "b[strat:temp1 sp:7]",
+      "b[strat:temp2 sp:7]",
+      "b[strat:temp3 sp:7]",
+      "b[origin:strat:temp1 sp:7]",
+      "b[origin:strat:temp2 sp:7]",
+      "b[origin:strat:temp3 sp:7]"
+    )
+  )
+
+cri.f<-sum.m[c(2:121),c(1,4,8)] #just selecting the mean and 95% CI. Removing the intercept 
+fdf<-data.frame(cri.f)
+#binding 
+fdf2<-as.data.frame(
+  cbind(
+    (c(rownames(fdf)[1:15], rep(rev(rownames(fdf)[1:15]), each=7))), #stdarzing the parameter  names 
+    as.numeric(as.character(fdf$mean)),  # the estimate 
+    as.numeric(as.character(fdf$X2.5.)), #lower bound, 95% CI
+    as.numeric(as.character(fdf$X97.5.)),  #upper bound, 95% CI
+    as.numeric(c(rep(1, 15), rep(2, 105))),  # A variable to signify if the corresponding row is a fixed  or random effect. 1=global, 2=rndm
+    as.numeric( c(rep(0,15), rep(seq(1:7),15 ))))) #sp variable. Zero when a factor 
+
+names(fdf2)<-c("var", "Estimate", colnames(cri.f)[c(2,3)], "rndm", "sp") #renaming. 
+
+fdf2$Estimate<-as.numeric(fdf2$Estimate)      
+fdf2$`2.5%`<-as.numeric(fdf2$`2.5%`)
+fdf2$`97.5%`<-as.numeric(fdf2$`97.5%`)      
+
+
+#Fixed effect estimates:
+fixed<-c(rep(0, 15), rep(as.numeric(rev(fdf2[c(1:15),2])), each=7))
+dff<-fdf2
+
+#adding the fixef estiamtes to the random effect values:
+dff$Estimate<-fdf2$Estimate+fixed
+dff$`2.5%`<-fdf2$`2.5%`+fixed
+dff$`97.5%`<-fdf2$`97.5%`+fixed
+
+dff$var <- fct_inorder(dff$var) #so that the categorical variables plot in the right order 
+
+## plotting
+
+pd <- position_dodgev(height = -0.5)
+
+
+fig2<-ggplot(dff, aes(x=Estimate, y=var, color=factor(sp), size=factor(rndm), alpha=factor(rndm)))+
+  geom_point(position =pd, size=4)+
+  geom_errorbarh(aes(xmin=(`2.5%`), xmax=(`97.5%`)), position=pd, size=.5, height =0)+
+  geom_vline(xintercept=0)+
+  scale_colour_manual(labels = c("Fixed effects", "CAPBUR", "CHEMAJ", "DACGLO", "PLALAN", "PLAMAJ", "RUMCRI", "TAROFF"),
+                      values=c("blue", "red", "orangered1", "sienna4", "green4", "green1", "purple2", "magenta2"))+  scale_alpha_manual(values=c(1, 0.5))+
+  scale_shape_manual(labels="", values=c("1"=16,"2"=16))+
+  scale_alpha_manual(values=c(1, 0.5))+
+  guides(alpha=FALSE) + #removes the legend 
+  ggtitle(label = "Germination rate")+
+  scale_y_discrete(limits = rev(unique(sort(dff$var))))
+#pdf(file.path(figpath, "Fig2.pdf"), width = 7, height = 8)
+fig2
 
 # Fairly strict rules of inclusion in this analysis: manipulation of forcing temperature, 
 #photoperiod, and where we have a response in days and total chilling. There are NA in each 
---
+++ analyses/lat_analysis/source/bbdataplease.R
@@ -0,0 +1,84 @@
+## Started 30 July 2017 ##
+## Lizzie took this from bbmodels_stan.R ##
+
+## Source file for reading in the data and
+## doing some cleaning we should ALL do
+
+## But! Keep an eye on this file! We probably need to edit it
+checkdataforNAs <- FALSE # Set to TRUE for looking at missing data rows
+
+# below file is cleaned, had chilling added and some BB cleaning done also
+bb.all <- read.csv("output/ospree_clean_withchill_BB.csv", header=TRUE)
+
+# file to adjust species into species or species complexes ... 
+taxon <- read.csv("output/bb_analysis/taxon/complex_levels.csv", header=TRUE)
+
+## read taxon data to get the 'type' category, then delete what we don't need
+bb.all.wtaxa <- merge(bb.all, taxon, by=c("genus","species"), all.x=TRUE)
+bb.all.wtaxa$complex <- NULL
+bb.all.wtaxa$use <- NULL
+
+## just the bb data ...
+respvars.thatwewant <- c("daystobudburst", "percentbudburst")
+bb.resp <- bb.all.wtaxa[which(bb.all.wtaxa$respvar.simple %in% respvars.thatwewant),]
+bb.resp <- subset(bb.resp, respvar != "thermaltime") # doesn't remove anything
+
+## make a bunch of things numeric (eek!)
+bb.resp$forceday <- as.numeric(bb.resp$forcetemp)
+bb.resp$forcenight <- as.numeric(bb.resp$forcetemp_night)
+bb.resp$photonight <- as.numeric(bb.resp$photoperiod_night)
+
+bb.resp$photo <- as.numeric(bb.resp$photoperiod_day)
+bb.resp$force <- bb.resp$forceday
+bb.resp$force[is.na(bb.resp$forcenight)==FALSE & is.na(bb.resp$photo)==FALSE &
+    is.na(bb.resp$photonight)==FALSE] <-
+    (bb.resp$forceday[is.na(bb.resp$forcenight)==FALSE & is.na(bb.resp$photo)==FALSE &
+    is.na(bb.resp$photonight)==FALSE]*
+    bb.resp$photo[is.na(bb.resp$forcenight)==FALSE & is.na(bb.resp$photo)==FALSE &
+    is.na(bb.resp$photonight)==FALSE] +
+    bb.resp$forcenight[is.na(bb.resp$forcenight)==FALSE & is.na(bb.resp$photo)==FALSE &
+    is.na(bb.resp$photonight)==FALSE]*
+    bb.resp$photonight[is.na(bb.resp$forcenight)==FALSE & is.na(bb.resp$photo)==FALSE &
+    is.na(bb.resp$photonight)==FALSE])/24
+
+bb.resp$chill <- as.numeric(bb.resp$Total_Utah_Model) # before 12 March 2018: Total_Chilling_Hours, Total_Chill_portions
+bb.resp$resp <- as.numeric(bb.resp$response.time)
+
+## center the predictors:
+bb.resp$force.cen <- bb.resp$force/mean(bb.resp$force,na.rm=TRUE)
+bb.resp$photo.cen <- bb.resp$photo/mean(bb.resp$photo,na.rm=TRUE)
+bb.resp$chill.cen <- bb.resp$chill/mean(bb.resp$chill,na.rm=TRUE)
+
+## remove the NAs (must do this before you can deal with taxon issues)
+bb.noNA <- subset(bb.resp, is.na(force)==FALSE & is.na(photo)==FALSE &
+    is.na(chill)==FALSE & is.na(resp)==FALSE)
+# bb.noNA<-subset(bb.noNA, field.sample<=1)
+
+# Vector needed to identify weinberger-design studies
+if(FALSE){
+weinberg<-c("falusi03", "falusi97", "heide93", "jones12", "partanen05", "ramos99",
+            "ashby62","basler14","biasi12","boyer","calme94","charrier11","cook00b",
+             "ganset02"  ,"gianfagna85","guerriero90","gunderson12")
+}
+       
+if(checkdataforNAs){
+forceissues <- subset(bb.resp, is.na(force)==TRUE)
+table(forceissues$datasetID)
+photoissues <- subset(bb.resp, is.na(photo)==TRUE)
+table(photoissues$datasetID)
+chillissues <- subset(bb.resp, is.na(chill)==TRUE)
+table(chillissues$datasetID)
+respissues <- subset(bb.resp, is.na(resp)==TRUE)
+table(respissues$datasetID)
+    }
+
+# See imputechilling.R for notes (and issue # 147)
+
+if(FALSE){
+## what is lost due to NAs?
+forceNA <- bb.resp[which(is.na(bb.resp$force)==TRUE),]
+forceNA$forcetemp
+subset(forceNA, forcetemp=="")  # what is up with the no entry ones? hawkins12 and gansert02
+photoNA <- bb.resp[which(is.na(bb.resp$photo)==TRUE),]
+photoNA$photoperiod_day # these datasetIDs are all mentioned as not possible to fix in clean_photoperiod.R
+    }
