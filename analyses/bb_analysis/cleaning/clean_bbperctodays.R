cc13db4681fe70ddf428e07dea9b3d343416d61f
Elizabeth Wolkovich <wolkovich@biodiversity.edu>
2018-04-07 17:55:04 -0700

    Just organizing code, see issue 168

--- analyses/bb_analysis/cleaning/clean_bbperctodays.R
+++ analyses/bb_analysis/cleaning/clean_bbperctodays.R
@@ -5,10 +5,8 @@
 
 # Edited by Dan Flynn, then lots of edits in early 2017 by Nacho Morales-Castilla #
 
-## read data
-#setwd("C:/Users/Ignacio/Documents/GitHub/ospree/analyses/output")
-# setwd("~/Documents/git/projects/treegarden/budreview/ospree/analyses/output")
-#d<-read.csv("ospree_clean_withchill.csv",as.is=TRUE)
+# b <- d
+# d <- subset(b, datasetID=="falusi96")
 
 targetvalue <- 80 # we want value closest to this
 acceptablerange <- 0.50 # meaning as low as 40% allowed (e.g., 80-80*0.5=40)
@@ -39,34 +37,36 @@ for(i in 1:length(dataset)){ # i = 2
   
   study<-unique(dat1$study)
   ##make new column that combines all treatments (columns 1:33) into one columns
-  dat1$treatment <- paste(dat1$genus,dat1$species, dat1$varetc,dat1$population,dat1$other.treatment,
-                          dat1$dormancy_induction_temp,dat1$dormancy_induction_days,
-                          dat1$dormancy_induction_photoperiod_day,dat1$dormancy_induction_photoperiod_night,
-                          dat1$freeze.treatment.time,dat1$freeze.treatment.photoperiod_day,
-                          dat1$freeze.treatment.photoperiod_night,dat1$freeze.treatment.temp_day,
-                          dat1$freeze.treatment.temp_night,dat1$fieldchill,dat1$chilltemp,dat1$chillphotoperiod,
-                          dat1$chilldays,dat1$number.longdays,dat1$photoperiod_day,dat1$photoperiod_night,dat1$forcetemp,
-                          dat1$forcetemp_night, dat1$irradiance,dat1$humidity, dat1$fieldsample.date,sep = ".")
+  dat1$treatment <- paste(dat1$genus,dat1$species, dat1$varetc,dat1$population,
+      dat1$other.treatment, dat1$dormancy_induction_temp, 
+      dat1$dormancy_induction_days, dat1$dormancy_induction_photoperiod_day,
+      dat1$dormancy_induction_photoperiod_night,
+      dat1$freeze.treatment.time, dat1$freeze.treatment.photoperiod_day,
+      dat1$freeze.treatment.photoperiod_night,
+      dat1$freeze.treatment.temp_day, dat1$freeze.treatment.temp_night,
+      dat1$fieldchill,dat1$chilltemp, dat1$chillphotoperiod, dat1$chilldays,
+      dat1$number.longdays, dat1$photoperiod_day,dat1$photoperiod_night,dat1$forcetemp,
+      dat1$forcetemp_night, dat1$irradiance,dat1$humidity,
+      dat1$fieldsample.date,sep = ".")
 
   for(j in 1:length(study)){ # j = 1
-  dat2<-dat1[which(dat1$study==study[j]),]
-  treat=unique(dat2$treatment)
-	dat2$response <- as.numeric(as.character(dat2$response))
-  
-	for(k in 1:length(treat)) { # k = 6 # This for loop cleans each treatment within studies with more than one treatment
-  #print(paste(i,j,k))
+      dat2<-dat1[which(dat1$study==study[j]),]
+      treat=unique(dat2$treatment)
+      dat2$response <- as.numeric(as.character(dat2$response))
+    for(k in 1:length(treat)) { # k = 6
+    # This for loop cleans each treatment within studies with more than one treatment
+    # print(paste(i,j,k))
     dat3 <- dat2[which(dat2$treatment==treat[k]),]
-    
     maxperc_bb<-max(as.numeric(dat3$response)) # maxpercent budburst
     minperc_bb<-min(as.numeric(dat3$response)) # minpercent budburst - hopefully zero
     #dists.to.target<-dist(dat3$response,upper=F)
     #dat3$response-target.percent
                    
     # Check if this can be estimated at all.
-    if(length(dat3$response[!is.na(dat3$response)])>1 & is.numeric(dat3$response.time)) {
-    
-      # Check how many values are within 35% of target percent, if at least one we proceed
-      values.in.target<-which(dat3$response>(target.percent-target.percent*acceptablerange)&dat3$response<(target.percent+target.percent*acceptablerange))
+    if(length(dat3$response[!is.na(dat3$response)])>1 & is.numeric(dat3$response.time))
+      {
+       # Check how many values are within 35% of target percent, if at least one we proceed
+          values.in.target<-which(dat3$response>(target.percent-target.percent*acceptablerange) & dat3$response<(target.percent+target.percent*acceptablerange))
       if(length(values.in.target)==1){
       index<-rownames(dat3[values.in.target,])  
       d[index,"dbb"]<-dat3[values.in.target,"response.time"]
@@ -80,12 +80,14 @@ for(i in 1:length(dataset)){ # i = 2
       if(length(values.in.target)>1 & length(which(dat3$response==target.percent))==0){  
         index<-rownames(dat3[values.in.target,])  
         d[index,"dbb"]<-dat3[values.in.target,"response.time"]
-        d[index,"maxperc_bb"]<-rep(max(dat3[values.in.target,"response"]),length(values.in.target))
-        d[index,"minperc_bb"]<-rep(min(dat3[values.in.target,"response"]),length(values.in.target))
+        d[index,"maxperc_bb"]<-rep(max(dat3[values.in.target,"response"]),
+            length(values.in.target))
+        d[index,"minperc_bb"]<-rep(min(dat3[values.in.target,"response"]),
+            length(values.in.target))
         d[index,"dist.50bb"]<-abs(dat3[values.in.target,"response"]-target.percent)
         
       }
-      # If there are more than 1 rows with values within within the acceptable range of target percent and at least one value in the 
+      # If there are more than 1 rows with values within the acceptable range of target percent and at least one value in the 
       # response variable is equal to the targetted % in the function we proceed
       if(length(values.in.target)>1 & length(which(dat3$response==target.percent))>0){  
         index<-rownames(dat3[which(dat3$response==target.percent),])  
@@ -110,7 +112,8 @@ if(type=="BB_analysis"){
   #d.subset.2<-subset(d,!is.na(dbb))
   d.subset.2<-subset(d,respvar.simple!="percentbudburst")
   d.subsetted<-rbind(d.subset.1,d.subset.2)
-  d.subsetted$response.time[which(!is.na(d.subsetted$dbb))]<-d.subsetted$dbb[which(!is.na(d.subsetted$dbb))]
+  d.subsetted$response.time[which(!is.na(d.subsetted$dbb))] <-
+      d.subsetted$dbb[which(!is.na(d.subsetted$dbb))]
   }
 
 if(type=="add.columns"){
