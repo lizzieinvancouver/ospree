924ffdaa4c5e9af0f013ac8b47a4af02026630e2
AileneKane <aettinger@fasmail.harvard.edu>
2018-08-22 13:10:06 -0400

    a start on imputation with arms

--- analyses/bb_analysis/models_brms.R
+++ analyses/bb_analysis/models_brms.R
@@ -1,6 +1,6 @@
 # Started 15 March 2018 
 # By Ailene 
-#Updated a tiny bit by Dan 19 June 2018
+# Updated a tiny bit by Dan 19 June 2018
 # Fitting bb models with brms to compare coefficients with stan models
 # housekeeping
 rm(list=ls()) 
@@ -32,24 +32,24 @@ source("source/bbstanleadin.R")
 #Fit model with no interactions and a species random slopes effect
 ########## m2l.ni: a(sp) + f(sp) + p(sp) + c(sp)
 
-m2l.ni.brms <- brm(y ~ chill+force+photo+#fixed effects
-                     ((chill+force+photo)|sp), #random effects
-                   data = datalist.bb, 
-                      chains = 2) 
+#m2l.ni.brms <- brm(y ~ chill+force+photo+#fixed effects
+#                     ((chill+force+photo)|sp), #random effects
+#                   data = datalist.bb, 
+#                      chains = 2) 
 
-stancode(m2l.ni.brms)
+#stancode(m2l.ni.brms)
 
-summary(m2l.ni.brms)
+#summary(m2l.ni.brms)
 #brms model says a: 71.19, f=-22.10, p=--4.87 , c=-12.93; species sigma: 15.81 
 #priors don't seem to affect much
 #marginal_effects(m2l.ni.brms, surface = TRUE)
-stanplot(m2l.ni.brms)
-stanplot(m2l.ni.brms, pars = "^b_")
+#stanplot(m2l.ni.brms)
+#stanplot(m2l.ni.brms, pars = "^b_")
 #zscored:
-m2l.nin.brms.z <- brm(resp ~ force.z + photo.z + chill.z +#main effects
+#m2l.nin.brms.z <- brm(resp ~ force.z + photo.z + chill.z +#main effects
                     ((force.z + photo.z + chill.z)|complex.wname), data = bb.stan,
                     chains = 2, cores = 2)
-summary(m2l.nin.brms.z)
+#summary(m2l.nin.brms.z)
 #brms model says a:28.41, f= -4.55 , p=--3.83, c= -8.81, 
 
 #Fit model with interactions and a species random slopes effect
@@ -61,7 +61,7 @@ summary(m2l.nin.brms.z)
 #uncentered:
 m2l.winsp.brms <- brm(resp ~ (force + photo + chill +#main effects
                              force*photo + force*chill + photo*chill)+ #interactions
-                        ((force + photo + chill)|complex.wname),warmup=2500,iter=4000, data = bb.stan,
+                        ((force + photo + chill + force*photo + force*chill + photo*chil)|complex.wname),warmup=2500,iter=4000, data = bb.stan,
                    chains = 2, cores = 2,control = list(max_treedepth = 12,adapt_delta = 0.99))
 summary(m2l.winsp.brms)
 stancode(m2l.winsp.brms)
@@ -85,13 +85,13 @@ launch_shinystan(m2l.winsp.brms)
 #centered:
 m2l.winsp.brms.cen <- brm(resp ~ (force.cen + photo.cen + chill.cen +#main effects
                                 force.cen*photo.cen + force.cen*chill.cen + photo.cen*chill.cen)+ #interactions
-                        ((force.cen + photo.cen + chill.cen)|complex.wname), data = bb.stan,
+                        ((force.cen + photo.cen + chill.cen + force.cen*photo.cen + force.cen*chill.cen + photo.cen*chill.cen)|complex.wname), data = bb.stan,
                       chains = 2, cores = 2,control = list(max_treedepth = 12,adapt_delta = 0.99))
 summary(m2l.winsp.brms.cen)
 #zscored:
 m2l.winsp.brms.z <- brm(resp ~ (force.z + photo.z + chill.z +#main effects
                                     force.z*photo.z + force.z*chill.z + photo.z*chill.z)+ #interactions
-                            ((force.z + photo.z + chill.z)|complex.wname), data = bb.stan,
+                            ((force.z + photo.z + chill.z+force.z*photo.z + force.z*chill.z + photo.z*chill.z)|complex.wname), data = bb.stan,
                           chains = 2, cores = 2,control = list(max_treedepth = 12,adapt_delta = 0.99))
 summary(m2l.winsp.brms.z)
 #brms model says a: 28.90, f= -4.63, p=-4.43, c= -9.64, 
@@ -101,91 +101,96 @@ stanplot(m2l.wisp.brms, pars = "^b_")
 launch_shinystan(m2l.wisp.brms)
 
 
-#Fit model with no interactions, a species random slopes effect, and a datasetID random intercept effect
-########## a(sp)+ a(id) + f(sp) + p(sp) + c(sp) + 
-#################################################################################
-# real data on 2 level model (sp) with interactions and study ID on intercept
-# Note the notation: M1_daysBBnointer_2level_studyint.stan: m2l.nistudy
-#################################################################################
-#Add study to model
-datalist.bb.study.cen <- with(bb, 
-                              list(y = resp, 
-                                   chill = chill.cen, 
-                                   force = force.cen, 
-                                   photo = photo.cen,
-                                   study = as.numeric(as.factor(bb.stan$datasetID)),
-                                   n_study = length(unique(bb.stan$datasetID)), 
-                                   sp = complex.wname,
-                                   N = nrow(bb.stan),
-                                   n_sp = length(unique(bb.stan$complex))
-                              )
-)
-datalist.bb.study <- with(bb, 
-                          list(y = resp, 
-                               chill = chill, 
-                               force = force, 
-                               photo = photo,
-                               study = as.numeric(as.factor(bb.stan$datasetID)),
-                               n_study = length(unique(bb.stan$datasetID)), 
-                               sp = complex.wname,
-                               N = nrow(bb.stan),
-                               n_sp = length(unique(bb.stan$complex))
-                          )
-)
+########################
+######Imputation########
+########################
+#same model with imputation, using brms imputation during model fitting
 
-
-#Fit model with NO interactions, a species random slopes effect, and a datasetID random intercept effect
-########## a(sp) + f(sp) + p(sp) + c(sp)  + (datasetid)
-m2l.nistudy.brms <- brm(y ~ chill+force +photo+
-                       + ((chill+force+photo)|sp)+(1|study), 
-                        data = datalist.bb.study,
-                        chains = 2, cores = 2,control = list(max_treedepth = 12))
-summary(m2l.nistudy.brms)
-
-m2l.nistudy.brms.z <- brm(resp ~ force.z + photo.z + chill.z +#main effects
-                        ((force.z + photo.z + chill.z)|complex.wname)+(1|datasetID), data = bb.stan,
-                      chains = 2, cores = 2)
-summary(m2l.nistudy.brms.z)
-
-#Fit model with interactions, a species random slopes effect, and a datasetID random intercept effect
-########## a(sp) + f(sp) + p(sp) + c(sp) + cf(sp) + cp(sp) + fp(sp) + (datasetid)
-m2l.wistudy.brms <- brm(y ~ chill+force +photo+
-                    chill:force+chill:photo+force:photo + 
-                    ((chill+force+photo)|sp)+(1|study), 
-                    data = datalist.bb.study.cen,
-                   chains = 2, cores = 2,control = list(max_treedepth = 12))
-summary(m2l.wistudy.brms)
-#brms model says a: 220.94, f= 104.09, p=455.34, c= -24.04, 
-#f*c: -155.17, c*p: 153.17,  f*p: 150.58; sigma sp: 15.37
-
-stanplot(m2l.wistudy.brms)
-stanplot(m2l.wistudy.brms, pars = "^b_")
-ranef(m2l.wistudy.brms)
-launch_shinystan(m2l.wistudy.brms)
-
-#zscored
-m2l.wistudy.brms.z <- brm(resp ~ chill.z+force.z +photo.z+
-                          chill.z:force.z+chill.z:photo.z+force.z:photo.z + 
-                          ((chill.z+force.z+photo.z)|complex.wname)+(1|datasetID), 
-                        data = bb.stan,
-                        chains = 2, cores = 2)
-summary(m2l.wistudy.brms)
-#brms model says a: 220.94, f= 104.09, p=455.34, c= -24.04, 
-#f*c: -155.17, c*p: 153.17,  f*p: 150.58; sigma sp: 15.37
-
-
-#same model with imputation
-
-bb.imp <- mice(bb.stan, m = 5, print = FALSE)#this is not quite right! need to select the datafile that HAS NAs
-m2l.wistudy.brms.zimp <- brm(resp ~ chill.z+force.z +photo.z+
-                            chill.z:force.z+chill.z:photo.z+force.z:photo.z + 
-                            ((chill.z+force.z+photo.z)|complex.wname)+(1|datasetID), 
-                          data = bb.imp,
-                          chains = 2, cores = 2)
+bform <- bf(bmi | mi() ~ age * mi(chl)) +
+  bf(chl | mi() ~ age) + set_rescor(FALSE)
 summary(m2l.wistudy.brms.zimp)
 
 #test for picea 
 picea<-bb.stan[bb.stan$complex.wname=="Picea_abies",]
 pcmod<-lm(resp ~ force.cen + photo.cen + chill.cen +force.cen:photo.cen + force.cen*chill.cen + photo.cen*chill.cen, data=picea)
  summary(pcmod)         
+ 
+#####Old Code that includes study
+ ####We decided not to include study in model because it was making estimates unstable. Thus the below code is not necessary.
+ #Fit model with no interactions, a species random slopes effect, and a datasetID random intercept effect
+ ########## a(sp)+ a(id) + f(sp) + p(sp) + c(sp) + 
+ #################################################################################
+ # real data on 2 level model (sp) with interactions and study ID on intercept
+ # Note the notation: M1_daysBBnointer_2level_studyint.stan: m2l.nistudy
+ #################################################################################
+ #Add study to model
+ #datalist.bb.study.cen <- with(bb, 
+ #                              list(y = resp, 
+ #                                   chill = chill.cen, 
+ #                                   force = force.cen, 
+ #                                   photo = photo.cen,
+ #                                   study = as.numeric(as.factor(bb.stan$datasetID)),
+ #                                   n_study = length(unique(bb.stan$datasetID)), 
+ #                                  sp = complex,
+ #                                   N = nrow(bb.stan),
+ #                                   n_sp = length(unique(bb.stan$complex))
+ #                              )
+ #)
+ #datalist.bb.study <- with(bb, 
+ #                          list(y = resp, 
+ #                               chill = chill, 
+ #                               force = force, 
+ #                               photo = photo,
+ #                               study = as.numeric(as.factor(bb.stan$datasetID)),
+ #                               n_study = length(unique(bb.stan$datasetID)), 
+ #                               sp = complex,
+ #                               N = nrow(bb.stan),
+ #                               n_sp = length(unique(bb.stan$complex))
+ #                          )
+ #)
+ 
+ 
+ #Fit model with NO interactions, a species random slopes effect, and a datasetID random intercept effect
+ ########## a(sp) + f(sp) + p(sp) + c(sp)  + (datasetid)
+ m2l.nistudy.brms <- brm(y ~ chill+force +photo+
+                           + ((chill+force+photo)|sp)+(1|study), 
+                         data = datalist.bb.study,
+                         chains = 2, cores = 2,control = list(max_treedepth = 12))
+ summary(m2l.nistudy.brms)
+ 
+ m2l.nistudy.brms.z <- brm(resp ~ force.z + photo.z + chill.z +#main effects
+                             ((force.z + photo.z + chill.z)|complex.wname)+(1|datasetID), data = bb.stan,
+                           chains = 2, cores = 2)
+ summary(m2l.nistudy.brms.z)
+ 
+ #Fit model with interactions, a species random slopes effect, and a datasetID random intercept effect
+ ########## a(sp) + f(sp) + p(sp) + c(sp) + cf(sp) + cp(sp) + fp(sp) + (datasetid)
+ m2l.wistudy.brms <- brm(y ~ chill+force +photo+
+                           chill:force+chill:photo+force:photo + 
+                           ((chill+force+photo)|sp)+(1|study), 
+                         data = datalist.bb.study.cen,
+                         chains = 2, cores = 2,control = list(max_treedepth = 12))
+ summary(m2l.wistudy.brms)
+ #brms model says a: 220.94, f= 104.09, p=455.34, c= -24.04, 
+ #f*c: -155.17, c*p: 153.17,  f*p: 150.58; sigma sp: 15.37
+ 
+ stanplot(m2l.wistudy.brms)
+ stanplot(m2l.wistudy.brms, pars = "^b_")
+ ranef(m2l.wistudy.brms)
+ launch_shinystan(m2l.wistudy.brms)
+ 
+ #zscored
+ m2l.wistudy.brms.z <- brm(resp ~ chill.z+force.z +photo.z+
+                             chill.z:force.z+chill.z:photo.z+force.z:photo.z + 
+                             ((chill.z+force.z+photo.z)|complex)+(1|datasetID), 
+                           data = bb.stan,
+                           chains = 2, cores = 2)
+ summary(m2l.wistudy.brms.z)
+ ranef(m2l.wistudy.brms.z)
+ stanplot(m2l.wistudy.brms.z, pars = "^b_")
+ stanplot(m2l.wistudy.brms.z)
+ stanplot(m2l.wistudy.brms.z, type = "hist")
+ 
+ #brms model says a: 220.94, f= 104.09, p=455.34, c= -24.04, 
+ #f*c: -155.17, c*p: 153.17,  f*p: 150.58; sigma sp: 15.37
  
---
+++ analyses/bb_analysis/testdata_impute.R
@@ -0,0 +1,187 @@
+## Started 10 March 2017 ##
+## By Lizzie to start ##
+## Modified by Ailene 17 August 2018 to try out imputation in brms
+
+## This file builds tests fake data for testing Stan models for OSPREE budburst analysis ##
+## Then it adds in some NAs
+## This files has versions with simple linear model with no interactions:
+# bb ~ force + photo + chill
+# and only random intercepts for species!
+# And also with interactions ... 
+
+library(lme4)
+library(rstan)
+library(shinystan)
+library(bayesplot)
+
+#setwd("~/Documents/git/projects/treegarden/budreview/ospree/analyses/bb_analysis")
+# Set working directory: 
+if(length(grep("Lizzie", getwd())>0)) {setwd("~/Documents/git/projects/treegarden/budreview/ospree/analyses/bb_analysis") 
+} else if
+(length(grep("ailene", getwd()))>0) {setwd("/Users/aileneettinger/git/ospree/analyses/bb_analysis")
+}else 
+  setwd("~/Documents/git/ospree/analyses/bb_analysis")
+
+# source('stan/savestan.R')
+rstan_options(auto_write = TRUE)
+# options(mc.cores = parallel::detectCores())
+
+source("testdata_generate.R")
+
+#now add some NAs to the force column
+#our forcing data has 4.9%; chilling has 5.7% missing data
+#Replace 5% of each of these with NAs
+propmiss<-0.05
+numiss<-propmiss*dim(testdat)[1]
+#dim(testdat)
+missing.chill<-as.integer(runif(numiss,min=1, max=1000))
+missing.force<-as.integer(runif(numiss,min=1, max=1000))
+testdat.wNA<-testdat
+testdat.wNA$chill[missing.chill]<-NA
+##########################################################
+# Model with no interactions; intercept only for grouping 
+##########################################################
+
+# lme version
+summary(lme1 <- lmer(bb ~ chill+force+photo + (1|sp), data = testdat)) 
+ranef(lme1)
+fixef(lme1)
+
+##
+# try the model (intercept only)
+datalist.td <- with(testdat, 
+    list(y = bb, 
+         chill = as.numeric(chill), 
+         force = as.numeric(force), 
+         photo = as.numeric(photo),
+         sp = as.numeric(sp),
+         N = nrow(testdat),
+         n_sp = length(unique(sp))
+         )
+)
+
+#osp.td <- stan('..//stan/bb/M1_daysBBnointer_2level_interceptonly.stan', data = datalist.td, 
+ #                iter = 100
+  #                ) 
+#with no missing data
+osp.td.brms<-brm(bb ~ chill+force+photo+#fixed effects
+                    (1|sp), #random effects
+                    data = testdat ,
+                    chains = 2) 
+                      
+summary(osp.td.brms)
+fixef(ops.td.brms)
+ranef(ops.td.brms)
+
+#with missing data
+ops.tdNA.brms<-brm(bb ~ chill+force+photo+#fixed effects
+                   (1|sp), #random effects
+                 data = testdat.wNA ,
+                 chains = 2) 
+
+summary(ops.tdNA.brms)
+fixef(ops.tdNA.brms)
+
+##with missing data AND imputation.
+## first with just chilling missing
+bform <- bf(bb ~ mi(chill)+force+photo+#fixed effects
+              (1|sp))+ #random effect
+            bf(chill | mi() ~ bb)#how to mode missing data)
+ops.tdNAimp.brms<-brm(bform, 
+                   data = testdat.wNA) 
+
+summary(ops.tdNAimp.brms)
+
+print(osp.td, pars = c("b_force", "b_photo", "b_chill", "mu_a_sp", "sigma_a_sp", "sigma_y"))
+
+betas <- as.matrix(osp.td, pars = c("b_force", "b_photo", "b_chill"))
+mcmc_intervals(betas)
+
+spinters <- as.matrix(osp.td, pars=c(colnames(as.matrix(osp.td))[grep("a_sp",
+    colnames(as.matrix(osp.td)))]))
+mcmc_intervals(spinters)
+
+siga_draws <- as.matrix(osp.td, pars = "sigma_a_sp")
+
+siga_and_prior <- cbind(
+  prior = rnorm(nrow(siga_draws), 0, 10), # draw from prior distribution
+  posterior = siga_draws[, 1]
+)
+mcmc_areas(siga_and_prior) 
+
+
+## Alternative! Try map2stan
+library(rethinking)
+
+# I think map2stan recognizes parameters because they have priors, so you always have to give them priors ...
+# Note that the default iterations are very low!
+goober <- map2stan(
+    alist(
+        y ~ dnorm(yhat, sigma_y),
+        yhat ~ a[sp] + b_force*force + b_photo*photo + b_chill*chill,
+        a[sp] ~ dnorm(mu_a_sp, sigma_a_sp),
+        sigma_y ~ dnorm(0, 30),
+        mu_a_sp ~ dnorm(0, 50),
+        sigma_a_sp ~ dnorm(0, 10),
+        b_force~ dnorm(0, 10),
+        b_photo~ dnorm(0, 10),
+        b_chill~ dnorm(0, 10)
+    ) ,
+    data=datalist.td )
+
+# estimates
+precis(goober)
+## magic! See the underlying Stan code
+stancode(goober)
+
+
+##########################################################
+# Model with interactions; stan model has grouping on intercepts and all slopes
+##########################################################
+
+# lme version (just grouping on intercepts)
+summary(lme2 <- lmer(bb ~ chill+force+photo+chill*force+chill*photo+ force*photo + (1|sp), data = testdat2)) 
+ranef(lme2)
+fixef(lme2)
+
+testdatX<-testdat2 # for centered data
+#testdatX<-testdat3 # for uncentered data, gives treedepth issues but returns the correct information
+testdatX<-testdat2.smintxn # for centered data with small intxns, seems to return the generally correct values but does not perfectly estimate the intxns
+testdatX<-testdat2.smintxn2 # for centered data with small intxns and larger sigmas
+  
+datalist.td2 <- with(testdatX, 
+    list(y = bb, 
+         chill = as.numeric(chill), 
+         force = as.numeric(force), 
+         photo = as.numeric(photo),
+         sp = as.numeric(sp),
+         N = nrow(testdat2),
+         n_sp = length(unique(sp))
+         )
+)
+
+osp.td2 <- stan('..//stan/bb/M1_daysBBwinter_2level.stan', data = datalist.td2, 
+                 iter = 2000
+                  ) 
+
+sumer.td2 <- summary(osp.td2)$summary
+sumer.td2[grep("mu_", rownames(sumer.td2)),] # the means look good! need to check some more ...
+
+
+print(osp.td2, pars = c("b_force", "b_photo", "b_chill", "b_cf","b_cp","b_fp", "mu_a_sp", "sigma_a_sp", "sigma_y"))
+
+betas <- as.matrix(osp.td2, pars = c("b_force", "b_photo", "b_chill", "b_cf","b_cp","b_fp"))
+mcmc_intervals(betas)
+
+spinters <- as.matrix(osp.td2, pars=c(colnames(as.matrix(osp.td2))[grep("a_sp",
+    colnames(as.matrix(osp.td2)))]))
+mcmc_intervals(spinters)
+
+siga_draws <- as.matrix(osp.td2, pars = "sigma_a_sp")
+
+siga_and_prior <- cbind(
+  prior = rnorm(nrow(siga_draws), 0, 10), # draw from prior distribution
+  posterior = siga_draws[, 1]
+)
+mcmc_areas(siga_and_prior) 
+
