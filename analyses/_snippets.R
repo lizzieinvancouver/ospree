16628027cfaddc487c32f2560e4d5bee464b54a0
Elizabeth Wolkovich <wolkovich@biodiversity.edu>
2018-10-12 11:55:47 -0700

    Some code for making a couple graphs for a talk next week
    ... sort of cool, will eventually try to merge into our full code

--- analyses/_snippets.R
+++ analyses/_snippets.R
@@ -1,5 +1,87 @@
 ## Potentially useful code snippets ##
 
+## Just for dealing with data issues in Oct 2018 (delete later)
+
+## To look at models before we imputed so much photoperiod ...
+
+# CHANGE bbdataplease
+d<-read.csv("Oct2018compare/olderBBdata/ospree_clean_withchill_BB8Jun2018.csv", header=TRUE)
+# CHANGE stan plotting (this is just before m1.bb <- m2l.winsp) 
+load("stan/output/Oct2018_check/M1_daysBBwinter_2level_8Jun2018.Rda")
+
+
+## Plotting cues against each other ...
+
+# plotlet from D Flynn
+plotlet <- function(x, y, xlab=NULL, ylab=NULL, data, xtimes, ytimes, groups = NULL, ...){
+  if(is.null(xlab)) xlab = x; if(is.null(ylab)) ylab = y
+  if(is.null(groups)) { col.pch = "black"; col.lines = "grey50" }
+    else {
+      colz = c("brown", "blue3")
+      ccolz = rep(colz[1], length(groups))
+      ccolz[groups == 2] = colz[2]
+      col.pch = ccolz
+      col.lines = alpha(ccolz, 0.4)
+    }
+  
+  plot(
+  data[grep(paste(x,"\\[",sep=""), rownames(data)),1]*xtimes,
+  data[grep(paste(y,"\\[",sep=""), rownames(data)),1]*ytimes,
+  pch = "+",
+  ylab = ylab,
+  xlab = xlab,
+  col = col.pch,
+  ...
+  )
+
+  abline(h=0, lty = 3, col = "grey60")
+  abline(v=0, lty = 3, col = "grey60")
+  
+  arrows(
+    data[grep(paste(x,"\\[",sep=""), rownames(data)),"mean"]*xtimes,
+    data[grep(paste(y,"\\[",sep=""), rownames(data)),"25%"]*ytimes,
+    data[grep(paste(x,"\\[",sep=""), rownames(data)),"mean"]*xtimes,
+    data[grep(paste(y,"\\[",sep=""), rownames(data)),"75%"]*ytimes,
+    length = 0, col = col.lines)
+  
+  arrows(
+    data[grep(paste(x,"\\[",sep=""), rownames(data)),"25%"]*xtimes,
+    data[grep(paste(y,"\\[",sep=""), rownames(data)),"mean"]*ytimes,
+    data[grep(paste(x,"\\[",sep=""), rownames(data)),"75%"]*xtimes,
+    data[grep(paste(y,"\\[",sep=""), rownames(data)),"mean"]*ytimes,
+    length = 0, col = col.lines)
+  
+  # match with species names
+  text( data[grep(paste(x,"\\[",sep=""), rownames(data)),1]*xtimes,
+        data[grep(paste(y,"\\[",sep=""), rownames(data)),1]*ytimes,
+        sort(unique(bb.stan$complex.wname)), # ALERT! This is data species, we should change!!
+        cex = 0.5, 
+        pos = 3,
+        col = col.pch)
+}
+
+plotlet( "b_photo", "b_force",
+         ylab = "Advance due to 5° warming (OSPREE)", 
+         xlab = "Advance due to 4 hr longer photoperiod (OSPREE)", 
+         ylim = c(-27, 0.5),
+         xlim = c(-16, 0.5),
+         #  xaxt="n", 
+         xtimes=4,
+         ytimes=5,
+         group = NULL,
+         data = sumer.wi)
+
+plotlet( "b_photo", "b_force",
+         ylab = "Advance due to 5° warming (OSPREE)", 
+         xlab = "Advance due to 4 hr longer photoperiod (OSPREE)", 
+         ylim = c(-27, 5),
+         xlim = c(-16, 5),
+         #  xaxt="n", 
+         xtimes=4,
+         ytimes=5,
+         group = NULL,
+         data = sumer.wi)
+
 ## Clear workspace
 rm(list=ls()) # remove everything currently held in the R memory
 options(stringsAsFactors=FALSE)
