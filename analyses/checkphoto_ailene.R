3d9a4d4dc389f2e008ccda5d5f48985319f299f5
AileneKane <aettinger@fasmail.harvard.edu>
2018-10-19 16:00:02 -0700

    done checking photoperiods and other treatments!

--- analyses/checkphoto_ailene.R
+++ analyses/checkphoto_ailene.R
@@ -32,15 +32,75 @@ cbind(osp$fieldchill[osp$datasetID=="hawerroth13"],ospch$fieldchill[ospch$datase
 ospch$Field_Chilling_Hours[ospch$datasetID=="hawerroth13"]
 
 #heide12
-osp$chilltemp[osp$datasetID=="heide12"]
-ospbb$chilltemp[ospbb$datasetID=="heide12"]
-ospcl$chilltemp[ospcl$datasetID=="heide12"]
+osp$chilltemp[osp$datasetID=="heide12"]#no chill dat
+ospbb$chilltemp[ospbb$datasetID=="heide12"]#no chill dat
+ospcl$chilltemp[ospcl$datasetID=="heide12"]#no chill dat
 cbind(osp$response[osp$datasetID=="heide12"],osp$forcetemp[osp$datasetID=="heide12"],osp$photoperiod_day[osp$datasetID=="heide12"],osp$figure.table..if.applicable.[osp$datasetID=="heide12"])
 cbind(osp$forcetemp[osp$datasetID=="heide12"],ospbb$forcetemp[ospbb$datasetID=="heide12"])
 
 cbind(ospbb$other.treatment[ospbb$datasetID=="heide12" & ospbb$figure.table..if.applicable.=="table 1"],ospbb$response[ospbb$datasetID=="heide12" & ospbb$figure.table..if.applicable.=="table 1"])
+#response
+cbind( osp$respvar[osp$datasetID=="heide12" & osp$figure.table..if.applicable.=="table 1"],
+    osp$response[osp$datasetID=="heide12" & osp$figure.table..if.applicable.=="table 1"],
+      osp$forcetemp[osp$datasetID=="heide12"& osp$figure.table..if.applicable.=="table 1"],
+      osp$photoperiod_day[osp$datasetID=="heide12"& osp$figure.table..if.applicable.=="table 1"],
+      osp$figure.table..if.applicable.[osp$datasetID=="heide12"& osp$figure.table..if.applicable.=="table 1"])
+plot(as.numeric(ospcl$response[ospcl$datasetID=="heide12"]),as.numeric(ospch$response[ospch$datasetID=="heide12"]))
+cbind( ospcl$respvar.simple[ospcl$datasetID=="heide12" & ospcl$figure.table..if.applicable.=="fig 2"],
+       ospcl$response[ospcl$datasetID=="heide12" & ospcl$figure.table..if.applicable.=="fig 2"],
+       ospcl$forcetemp[ospcl$datasetID=="heide12"& ospcl$figure.table..if.applicable.=="fig 2"],
+       ospcl$photoperiod_day[ospcl$datasetID=="heide12"& ospcl$figure.table..if.applicable.=="fig 2"])
 
-cbind(osp$figure.table..if.applicable.[osp$datasetID=="hawerroth13"],ospcl$figure.table..if.applicable.[ospcl$datasetID=="hawerroth13"])
-#no table/figure listed. Add?
-cbind(osp$fieldchill[osp$datasetID=="hawerroth13"],osp$fieldchill[ospch$datasetID=="hawerroth13"])
-ospch$Field_Chilling_Hours[ospch$datasetID=="hawerroth13"]
+cbind(ospbb$respvar.simple[ospbb$datasetID=="heide12"],
+      ospbb$response[ospbb$datasetID=="heide12"],
+      ospbb$forcetemp[ospbb$datasetID=="heide12"],
+      ospbb$photoperiod_day[ospbb$datasetID=="heide12"],
+      ospbb$figure.table..if.applicable.[ospbb$datasetID=="heide12"])
+
+cbind(osp$respvar.simple[osp$datasetID=="heide12"],
+      osp$response[osp$datasetID=="heide12"],
+      osp$forcetemp[osp$datasetID=="heide12"],
+      osp$photoperiod_day[osp$datasetID=="heide12"],
+      osp$figure.table..if.applicable.[osp$datasetID=="heide12"])
+
+#howe95
+osp$chilltemp[osp$datasetID=="howe95"]#no chill dat
+ospbb$chilltemp[ospbb$datasetID=="howe95"]#no chill dat
+ospcl$chilltemp[ospcl$datasetID=="howe95"]#no chill dat
+cbind(osp$response[osp$datasetID=="howe95"],osp$forcetemp[osp$datasetID=="howe95"],osp$photoperiod_day[osp$datasetID=="howe95"],osp$figure.table..if.applicable.[osp$datasetID=="howe95"])
+cbind(osp$forcetemp[osp$datasetID=="howe95"],ospbb$forcetemp[ospbb$datasetID=="howe95"])
+cbind(osp$respvar[osp$datasetID=="howe95"],osp$response.time[osp$datasetID=="howe95"],osp$forcetemp[osp$datasetID=="howe95"],osp$photoperiod_day[osp$datasetID=="howe95"],osp$figure.table..if.applicable.[osp$datasetID=="howe95"])
+cbind(ospbb$respvar[ospbb$datasetID=="howe95"],ospbb$response.time[ospbb$datasetID=="howe95"],ospbb$forcetemp[ospbb$datasetID=="howe95"],ospbb$photoperiod_day[ospbb$datasetID=="howe95"],ospbb$figure.table..if.applicable.[ospbb$datasetID=="howe95"])
+
+
+#myking97
+osp$chilltemp[osp$datasetID=="myking97"]#0 degrees
+ospbb$chilltemp[ospbb$datasetID=="myking97"]#0 degrees
+ospcl$chilltemp[ospcl$datasetID=="myking97"]#0 degrees
+osp$chilldays[osp$datasetID=="myking97"]#120 days
+ospbb$chilldays[ospbb$datasetID=="myking97"]#no chill dat
+ospcl$chilldays[ospcl$datasetID=="myking97"]#no chill dat
+
+cbind(osp$response[osp$datasetID=="myking97"],osp$forcetemp[osp$datasetID=="myking97"],osp$photoperiod_day[osp$datasetID=="myking97"],osp$figure.table..if.applicable.[osp$datasetID=="myking97"])
+cbind(osp$forcetemp[osp$datasetID=="myking97"],ospbb$forcetemp[ospbb$datasetID=="myking97"])
+cbind(osp$respvar[osp$datasetID=="myking97"],osp$response.time[osp$datasetID=="myking97"],osp$forcetemp[osp$datasetID=="myking97"],osp$photoperiod_day[osp$datasetID=="myking97"],osp$figure.table..if.applicable.[osp$datasetID=="myking97"])
+cbind(ospbb$respvar[ospbb$datasetID=="myking97"],ospbb$response.time[ospbb$datasetID=="myking97"],ospbb$forcetemp[ospbb$datasetID=="myking97"],ospbb$photoperiod_day[ospbb$datasetID=="myking97"],ospbb$figure.table..if.applicable.[ospbb$datasetID=="myking97"])
+cbind(ospbb$forcetemp_night[ospbb$datasetID=="myking97"],ospbb$forcetemp[ospbb$datasetID=="myking97"],ospbb$response.time[ospbb$datasetID=="myking97"],ospbb$photoperiod_day[ospbb$datasetID=="myking97"],ospbb$figure.table..if.applicable.[ospbb$datasetID=="myking97"])
+
+
+#partanen98
+osp$chilltemp[osp$datasetID=="partanen98"]#no chilltemp data
+ospbb$chilltemp[ospbb$datasetID=="partanen98"]#no chilltemp data
+ospcl$chilltemp[ospcl$datasetID=="partanen98"]#no chilltemp data
+osp$chilldays[osp$datasetID=="partanen98"]#chilldays are there though..
+ospbb$chilldays[ospbb$datasetID=="partanen98"]#no chill dat
+ospcl$chilldays[ospcl$datasetID=="partanen98"]#no chill dat
+
+cbind(osp$response.time[osp$datasetID=="partanen98"],osp$forcetemp[osp$datasetID=="partanen98"],osp$photoperiod_day[osp$datasetID=="partanen98"],osp$figure.table..if.applicable.[osp$datasetID=="partanen98"])
+cbind(osp$respvar[osp$datasetID=="partanen98"],osp$response.time[osp$datasetID=="partanen98"],osp$forcetemp[osp$datasetID=="partanen98"],osp$photoperiod_day[osp$datasetID=="partanen98"],osp$figure.table..if.applicable.[osp$datasetID=="partanen98"])
+cbind(ospbb$respvar[ospbb$datasetID=="partanen98"],ospbb$response.time[ospbb$datasetID=="partanen98"],ospbb$forcetemp[ospbb$datasetID=="partanen98"],ospbb$photoperiod_day[ospbb$datasetID=="partanen98"],ospbb$figure.table..if.applicable.[ospbb$datasetID=="partanen98"])
+cbind(ospbb$forcetemp_night[ospbb$datasetID=="partanen98"],ospbb$forcetemp[ospbb$datasetID=="partanen98"],ospbb$response.time[ospbb$datasetID=="partanen98"],ospbb$photoperiod_day[ospbb$datasetID=="partanen98"],ospbb$figure.table..if.applicable.[ospbb$datasetID=="partanen98"])
+
+osp$photoperiod_day[osp$datasetID=="partanen98"]
+ospch$photoperiod_day[ospch$datasetID=="partanen98"]
+ospbb$photoperiod_day[ospbb$datasetID=="partanen98"]
