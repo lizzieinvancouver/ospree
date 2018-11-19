6320ee119614cc3f5cfe54b8e91d53bf7f80da94
cchambe12 <cchamberlain@g.harvard.edu>
2018-11-19 12:59:22 -0500

    Update models_gamma_brms.R

--- analyses/bb_analysis/models_gamma_brms.R
+++ analyses/bb_analysis/models_gamma_brms.R
@@ -19,6 +19,7 @@ if(length(grep("lizzie", getwd())>0)) {
 
 library(brms)
 library(rstan)
+library(rstanarm)
 rstan_options(auto_write = TRUE)
 options(mc.cores = parallel::detectCores())
 
@@ -32,11 +33,19 @@ source("source/bbstanleadin.R")
 #range(bb.stan$resp)
 bb.stan.onecue$respg<-bb.stan.onecue$resp+0.001
 
+gamma.arm <- stan_glmer(respg ~ (force + photo + chill +#main effects
+                                 force*photo + force*chill + photo*chill)+ #interactions
+                        ((force + photo + chill + force*photo + force*chill + photo*chill)|complex.wname),warmup=2500,iter=4000, data = bb.stan.onecue,
+                      chains = 2, cores = 2,control = list(max_treedepth = 12,adapt_delta = 0.99), family=Gamma(link="inverse"))
+
+#save(gamma.arm, file="~/Documents/git/regionalrisk/gammaoutput.Rdata")
+
+
 #uncentered gamma:
 m2l.winsp.brms <- brm(respg ~ (force + photo + chill +#main effects
                                 force*photo + force*chill + photo*chill)+ #interactions
                         ((force + photo + chill + force*photo + force*chill + photo*chill)|complex.wname),warmup=2500,iter=4000, data = bb.stan.onecue,
-                      chains = 2, cores = 2,control = list(max_treedepth = 12,adapt_delta = 0.99), family=Gamma("log"))
+                      chains = 2, cores = 2,control = list(max_treedepth = 12,adapt_delta = 0.99), family=Gamma("identity"))
 #summary(m2l.winsp.brms)
 #stancode(m2l.winsp.brms)
 #str(brms::make_standata(resp ~ (force + photo + chill +#main effects
@@ -44,33 +53,31 @@ m2l.winsp.brms <- brm(respg ~ (force + photo + chill +#main effects
  #                         ((force + photo + chill)|complex.wname),data=bb.stan))
 
 
-#brms model says a:94.77, f= -1.85, p=-1.41, c= -6.94, 
-#f*c: 0.15, c*p: 0.10  f*p: 0.01; sigma sp: 15.37
-
 #stancode(m2l.winsp.brms)
 #marginal_effects(m2l.wi.brms, surface = TRUE)
 #stanplot(m2l.winsp.brms, pars = "^b_")
 
-#zscored gamma:
-m2l.winsp.brms.cen<- brm(respg ~ (force.cen + photo.cen + chill.cen +#main effects
-                                   force.cen*photo.cen + force.cen*chill.cen + photo.cen*chill.cen)+ #interactions
-                          ((force.cen + photo.cen + chill.cen+force.cen*photo.cen + force.cen*chill.cen + photo.cen*chill.cen)|complex.wname), data = bb.stan.onecue,
-                        chains = 2, cores = 2,control = list(max_treedepth = 12,adapt_delta = 0.99), family=Gamma("log"))
-summary(m2l.winsp.brms.z)
-#brms model says a: 28.90, f= -4.63, p=-4.43, c= -9.64, 
-#f*c: 2.43, c*p: 2.16  f*p: -0.26; sigma sp: 14.60 
-#marginal_effects(m2l.wi.brms, surface = TRUE)
-stanplot(m2l.winsp.brms.z, pars = "^b_")
+#centered gamma:
+#m2l.winsp.brms.cen<- brm(respg ~ (force.cen + photo.cen + chill.cen +#main effects
+ #                                  force.cen*photo.cen + force.cen*chill.cen + photo.cen*chill.cen)+ #interactions
+  #                        ((force.cen + photo.cen + chill.cen+force.cen*photo.cen + force.cen*chill.cen + photo.cen*chill.cen)|complex.wname), data = bb.stan.onecue,
+   #                     chains = 2, cores = 2,control = list(max_treedepth = 12,adapt_delta = 0.99), family=Gamma("log"))
+#summary(m2l.winsp.brms.cen)
+#stanplot(m2l.winsp.brms.cen, pars = "^b_")
 
 #zscored gamma:
 m2l.winsp.brms.z <- brm(respg ~ (force.z + photo.z + chill.z +#main effects
                                   force.z*photo.z + force.z*chill.z + photo.z*chill.z)+ #interactions
-                          ((force.z + photo.z + chill.z+force.z*photo.z + force.z*chill.z + photo.z*chill.z)|complex.wname), data = bb.stan,
+                          ((force.z + photo.z + chill.z+force.z*photo.z + force.z*chill.z + photo.z*chill.z)|complex.wname), data = bb.stan.onecue,
                         chains = 2, cores = 2,control = list(max_treedepth = 12,adapt_delta = 0.99), family=Gamma("log"))
 summary(m2l.winsp.brms.z)
-#brms model says a: 28.90, f= -4.63, p=-4.43, c= -9.64, 
-#f*c: 2.43, c*p: 2.16  f*p: -0.26; sigma sp: 14.60 
-#marginal_effects(m2l.wi.brms, surface = TRUE)
+#brms model says a: 3.31, f= -0.32, p=-0.13, c= -0.32, 
+#f*c: -0.15, c*p: 0.10  f*p: -0.12; sigma sp: 2.93
+## Converted:
+# a: 27.385, f= 0.726, p=0.878, c= 0.726, 
+#f*c: -0.15, c*p: 0.10  f*p: -0.12; sigma sp: 2.93
+
+marginal_effects(m2l.winsp.brms.z, surface = TRUE)
 stanplot(m2l.winsp.brms.z, pars = "^b_")
 
 
