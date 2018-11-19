158cb98ac3a7be589e0b3062291b931dc9c2f11d
cchambe12 <cchamberlain@g.harvard.edu>
2018-11-19 08:48:19 -0500

    Update models_gamma_brms.R

--- analyses/bb_analysis/models_gamma_brms.R
+++ analyses/bb_analysis/models_gamma_brms.R
@@ -22,19 +22,21 @@ library(rstan)
 rstan_options(auto_write = TRUE)
 options(mc.cores = parallel::detectCores())
 
+use.chillunits = FALSE # change to true for testing chill units
+use.allspp = FALSE
 source("source/bbstanleadin.R")
 
 # Fairly strict rules of inclusion in this analysis: manipulation of forcing temperature, 
 # photoperiod, and where we have a response in days and total chilling. 
 
 #range(bb.stan$resp)
-bb.stan$respg<-bb.stan$resp+0.001
+bb.stan.onecue$respg<-bb.stan.onecue$resp+0.001
 
 #uncentered gamma:
-#m2l.winsp.brms <- brm(respg ~ (force + photo + chill +#main effects
- #                               force*photo + force*chill + photo*chill)+ #interactions
- #                       ((force + photo + chill + force*photo + force*chill + photo*chill)|complex.wname),warmup=2500,iter=4000, data = bb.stan,
-  #                    chains = 2, cores = 2,control = list(max_treedepth = 12,adapt_delta = 0.99), family=Gamma("log"))
+m2l.winsp.brms <- brm(respg ~ (force + photo + chill +#main effects
+                                force*photo + force*chill + photo*chill)+ #interactions
+                        ((force + photo + chill + force*photo + force*chill + photo*chill)|complex.wname),warmup=2500,iter=4000, data = bb.stan.onecue,
+                      chains = 2, cores = 2,control = list(max_treedepth = 12,adapt_delta = 0.99), family=Gamma("log"))
 #summary(m2l.winsp.brms)
 #stancode(m2l.winsp.brms)
 #str(brms::make_standata(resp ~ (force + photo + chill +#main effects
@@ -48,6 +50,17 @@ bb.stan$respg<-bb.stan$resp+0.001
 #stancode(m2l.winsp.brms)
 #marginal_effects(m2l.wi.brms, surface = TRUE)
 #stanplot(m2l.winsp.brms, pars = "^b_")
+
+#zscored gamma:
+m2l.winsp.brms.cen<- brm(respg ~ (force.cen + photo.cen + chill.cen +#main effects
+                                   force.cen*photo.cen + force.cen*chill.cen + photo.cen*chill.cen)+ #interactions
+                          ((force.cen + photo.cen + chill.cen+force.cen*photo.cen + force.cen*chill.cen + photo.cen*chill.cen)|complex.wname), data = bb.stan.onecue,
+                        chains = 2, cores = 2,control = list(max_treedepth = 12,adapt_delta = 0.99), family=Gamma("log"))
+summary(m2l.winsp.brms.z)
+#brms model says a: 28.90, f= -4.63, p=-4.43, c= -9.64, 
+#f*c: 2.43, c*p: 2.16  f*p: -0.26; sigma sp: 14.60 
+#marginal_effects(m2l.wi.brms, surface = TRUE)
+stanplot(m2l.winsp.brms.z, pars = "^b_")
 
 #zscored gamma:
 m2l.winsp.brms.z <- brm(respg ~ (force.z + photo.z + chill.z +#main effects
