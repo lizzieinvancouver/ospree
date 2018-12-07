5bbd2abc69b1554e28d3d47f8e9c7b6102702b5a
MoralesCastilla <moralescastilla@g.harvard.edu>
2018-12-07 13:51:07 +0100

    updated funciton to extract climate in range

--- analyses/ranges/extract_climate_inrange_shapes.R
+++ analyses/ranges/extract_climate_inrange_shapes.R
@@ -23,13 +23,12 @@ setwd("~/GitHub/ospree/")
 ## from 1950 to 2017 / you need to download/unzip the climate from:
 ##https://eca.knmi.nl/download/ensembles/data/Grid_0.25deg_reg/
 
-tavg<-brick("~/Data_Harvard/EU trees/tg_0.25deg_reg_v17.0.nc", varname="tg", sep="")
-tavg<-brick("~/tg_0.25deg_reg_v17.0.nc", varname="tg", sep="")
+#tavg<-brick("~/Data_Harvard/EU trees/tg_0.25deg_reg_v17.0.nc", varname="tg", sep="")
+#tavg<-brick("~/tg_0.25deg_reg_v17.0.nc", varname="tg", sep="")
 
 
 ## read in distribution data
 trees<-read.csv("data/distributiondata/EU trees/Trees_EU_116sps.csv")
-unique()
 
 ## example to extract climate for one species "Betula Pendula" (can be
 ## replicated or looped to include any other species)
@@ -62,10 +61,12 @@ extractchillforce<-function(spslist,fullnames,trees,tavg,period){
   ## define array to store results
   nsps<-length(spslist)
   nyears<-length(period)
-  chillforcespsyears<-array(NA,dim=c(nsps,4,nyears))
-  row.names(chillforcespsyears)<-spslist
+  chillforcespsyears<-array(NA,dim=c(nyears,4,nsps))
+  row.names(chillforcespsyears)<-period
   colnames(chillforcespsyears)<-c("Mean.Chill","SDev.Chill","Mean.GDD",
                                   "SDev.GDD.sites")
+  #dimnames(chillforcespsyears)<-spslist
+  
   ## subset climate years
   yearsinclim<-as.numeric(format(as.Date(names(tavg),format="X%Y.%m.%d"),"%Y"))
   yearsinperiod<-which(yearsinclim%in%period)
@@ -80,7 +81,6 @@ extractchillforce<-function(spslist,fullnames,trees,tavg,period){
   chillsub<-subset(climsub,monthsinchill)
   forcesub<-subset(climsub,monthsinforce)
   
-  
   ## commence loop  
   for (i in 1:nsps){#i=1
     print(i)
@@ -90,7 +90,7 @@ extractchillforce<-function(spslist,fullnames,trees,tavg,period){
     ## load shape
     direct<-dir(paste("data/distributiondata/",
                       fullnamei,"/","shapefiles/",sep=""))
-    directname<-direct[which(grepl(direct,fullnamei))]
+    #directname<-direct[which(grepl(direct,fullnamei))]
     spsshape<-shapefile(paste("data/distributiondata/",
                               fullnamei,"/","shapefiles/",
                               paste(gsub(" ","_",fullnamei),"_plg.shp",sep="")
@@ -102,7 +102,7 @@ extractchillforce<-function(spslist,fullnames,trees,tavg,period){
     
     ## loop across years to extract each years averages and stddevs
     # save first an array to store results
-    yearlyresults<-array(NA,dim=c(length(period),5))
+    yearlyresults<-array(NA,dim=c(length(period),4))
       
     for(j in period){#j=1980
       print(paste(i,j))
@@ -127,68 +127,56 @@ extractchillforce<-function(spslist,fullnames,trees,tavg,period){
       ff<-do.call("rbind",tempsforces)
       ff<-subset(ff,!is.na(rowSums(ff)))
       
+      
       ## calculate chilling (Utah) and GDD across the period
-      chillunitseachcelleachday<-t(apply(ff,1,function(x){
-        utah<-x ## need to apply the correct version of the function to obtain chilling units
-        return(utah)}))
-      utahssum<-rowSums(gddseachcelleachday)
-      
-      gddseachcelleachday<-t(apply(ff,1,function(x){return(x-10)}))
+      gddseachcelleachday<-apply(ff,2,function(x){
+        Tb<-10
+        gdd<-ifelse((x-Tb)<0,0,x-Tb)
+        return(gdd)})
       gddssum<-rowSums(gddseachcelleachday)
+      #hist(gddssum)
+      
+      chillunitseachcelleachday<-apply(ch,2,function(x){
+        tlow=-5 ## not sure about which parameteres we are using
+        thigh=5
+        minns<-ifelse((thigh-x)>(thigh-tlow),thigh-tlow,thigh-x)
+        utah<-ifelse(minns>0,minns,0)
+        return(utah)})
+      utahssum<-rowSums(chillunitseachcelleachday)
+      #hist(utahssum)
       
       
+      yearlyresults[which(period==j),1]<-mean(gddssum,na.rm=T)
+      yearlyresults[which(period==j),2]<-sd(gddssum,na.rm=T)
+      yearlyresults[which(period==j),3]<-mean(utahssum,na.rm=T)
+      yearlyresults[which(period==j),4]<-sd(utahssum,na.rm=T)
       
       }
     
+    chillforcespsyears[,,i]<-yearlyresults
     
-    ## extract data for shapefile
-    
-    aa<-chillsub[[c(1:3)]]
-    x=aa[[1]]
-    calc(aa,fun=function(x){
-      rr<-unlist(extract(x,spsshapeproj))
-      return(rr)
-     },forcefun=T)
-    calc(aa,fun=function(x){
-      function(x){x * 10}
-      },na.rm=T)
-    
-    
-    
-    
-    ?calc
-    
-    ff<-extract(chillsub[[c(1:3)]],spsshapeproj)
-  str(ff)  
-     
-  }  
+    }  
   
+  return(chillforcespsyears)
+
 }
 
-coords.FS<-subset(trees,select=c("X","Y",FaSy)) # subset to fagus 
-coords.FS.no0<-coords.FS[which(coords.FS[,3]==1),1:2] # fagus yes's only
 
-coords.PiAb<-subset(trees,select=c("X","Y",PiAb)) #subset data
-coords.PiAb.no0<-coords.PiAb[which(coords.PiAb[,3]==1),1:2]
-
-# extract climate for each day within the range of the species
-# (this can take a while)
-
-extracted.clim.spsi <- extract(tavg,coords.spsi.no0)
+## apply function
+Climate.in.range<-extractchillforce(spslist,fullnames,trees,tavg,period)
 
 
 
-library(rworldmap)
-newmap <- getMap(resolution = "low")
-plot(newmap, xlim = c(-20, 59), ylim = c(35, 71), asp = 1)
-#points(coords.spsi.no0$X, coords.spsi.no0$Y, col = "green",pch=20, cex=0.6)
-points(coords.FS.no0$X, coords.FS.no0$Y, col = "red",pch=20,cex=0.6)
-points(coords.PiAb.no0$X, coords.PiAb.no0$Y, col = "blue",pch=20,cex=0.6)
-
-##ask nacho, is it better to work with ranges as a shapefile, polygon etc?
-
-
-## to be completed if we only want a given set of dates...
+## the above function extracts forcing and chilling 
+## it takes a bit of time per species
+## the parameters to compute chilling and gdds should be reviewed:
+## for GDD I used Tb=10
+## for Utah units I used Thigh=5 ; Tlow=-5
+## Still lacking code to extract centroids in environmental space
+## The output table yields yearly mean and sd values of chilling and 
+## forcing across the range computed out of daily values within
+## the Oct-Feb period (chilling) and March-May period (forcing)
+## Do we want further results?
 
 
 
