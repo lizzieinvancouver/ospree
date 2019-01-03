5a753a427ae3b372d93776ab7135cd94e90cfb56
Elizabeth Wolkovich <wolkovich@biodiversity.edu>
2019-01-03 13:47:13 -0800

    Lizzie's first attempts at some comparisons
    These are NOT right, but just a way to get started ...

---
+++ analyses/bb_analysis/models_stan_plotting_APC.R
@@ -0,0 +1,121 @@
+## Started 3 Jan 2019 ##
+## By Lizzie (to start) ##
+
+## Marginal effects from Stan models ##
+## Based off models_stan_plotting.R ##
+
+############################################
+## housekeeping
+rm(list=ls()) 
+options(stringsAsFactors = FALSE)
+
+
+# Setting working directory. Add in your own path in an if statement for your file structure
+if(length(grep("ailene", getwd())>0)) { 
+  setwd("/Users/aileneettinger/git/ospree/analyses/bb_analysis")
+} else setwd("~/Documents/git/projects/treegarden/budreview/ospree/analyses/bb_analysis")
+if(length(grep("Ignacio", getwd()))>0) { 
+  setwd("~/GitHub/ospree/analyses") 
+} else setwd("~/Documents/git/projects/treegarden/budreview/ospree/analyses/bb_analysis")
+
+figpath <- "figures"
+
+## set up the flags
+use.chillports = TRUE
+use.zscore = FALSE
+use.allspp = FALSE
+use.multcuespp = FALSE
+use.cropspp = FALSE
+# Default is species complex use  alltypes of designs
+use.expramptypes.fp = TRUE
+use.exptypes.fp = FALSE
+use.expchillonly = FALSE
+
+##
+source("source/bbstanleadin.R")
+##
+
+# Set up colors
+if(FALSE){
+library(RColorBrewer)
+cols <- adjustcolor("indianred3", alpha.f = 0.3) 
+my.pal <- rep(brewer.pal(n = 12, name = "Paired"), 4)
+# display.brewer.all()
+my.pch <- rep(15:18, each=12)
+alphahere = 0.4
+}
+
+# non-z-scored models
+if(use.zscore==FALSE){
+load("stan/output/m2lni_spcompexprampfp_nonz.Rda") # m2l.ni
+load("stan/output/m2lnib_spcompexprampfp_nonz.Rda") # m2l.nib
+modelhere <- m2l.ni
+}
+
+hist(bb.stan$chill.ports)
+rownameshere <- c("mu_a_sp", "mu_b_force_sp", "mu_b_photo_sp", "mu_b_chill_sp")
+
+# Let's pretend that average spring temp is 5 and average chill portions is 140
+# And, let's pretend 2 deg warming decreases chilling by 30 portions (I made this up)
+sprtemp.forplot <- 5
+chillport.forplot <- 140
+photo.forplot <- 14
+warmspring <- 2
+warmwinter <- -30
+
+# CHEAP (and wrong) way to do this
+# RIGHT way would be to create a new posterior!
+getBBestimates <- function(modelvalues){
+avgBB <- modelvalues[1] + modelvalues[2]*sprtemp.forplot +
+    modelvalues[3]*photo.forplot + modelvalues[4]*chillport.forplot
+warmsprBB <- modelvalues[1] + modelvalues[2]*(sprtemp.forplot + warmspring) +
+    modelvalues[3]*photo.forplot + modelvalues[4]*chillport.forplot
+warmwinBB <- modelvalues[1] + modelvalues[2]*sprtemp.forplot +
+    modelvalues[3]*photo.forplot + modelvalues[4]*(chillport.forplot+warmwinter)
+warmsprwinBB <- modelvalues[1] + modelvalues[2]*(sprtemp.forplot + warmspring) +
+    modelvalues[3]*photo.forplot + modelvalues[4]*(chillport.forplot+warmwinter)
+yeBBest <- c(avgBB, warmsprBB, warmwinBB, warmsprwinBB)
+return(yeBBest)
+}
+
+goo <- summary(modelhere)$summary[rownameshere,"mean"]
+lowci <- summary(modelhere)$summary[rownameshere,"25%"]
+uppci <- summary(modelhere)$summary[rownameshere,"75%"]
+
+lowci.values <- getBBestimates(lowci)
+uppci.values <- getBBestimates(uppci)
+mean.values <- getBBestimates(goo)
+
+# More cheap stuff to add in photo effects
+# Assume 14 hr day, which is Apr 17 in Berlin
+mean.values[2]-mean.values[1] # 1.5 days earlier ... no real change
+mean.values[3]-mean.values[1] # 7 days later ... 0.5 longer day
+mean.values[4]-mean.values[1] # 6 days later ... 0.5 longer day
+
+modelvalues <- goo
+photo.avgBB <- modelvalues[1] + modelvalues[2]*sprtemp.forplot +
+    modelvalues[3]*photo.forplot + modelvalues[4]*chillport.forplot
+photo.warmsprBB <- modelvalues[1] + modelvalues[2]*(sprtemp.forplot + warmspring) +
+    modelvalues[3]*photo.forplot + modelvalues[4]*chillport.forplot
+photo.warmwinBB <- modelvalues[1] + modelvalues[2]*sprtemp.forplot +
+    modelvalues[3]*(photo.forplot+0.5) + modelvalues[4]*(chillport.forplot+warmwinter)
+photo.warmsprwinBB <- modelvalues[1] + modelvalues[2]*(sprtemp.forplot + warmspring) +
+    modelvalues[3]*(photo.forplot+0.5) + modelvalues[4]*(chillport.forplot+warmwinter)
+
+xlim = c(0, 5)
+ylim = c(5, 40)
+par(mar=c(8,7,3,5))
+plot(x=NULL,y=NULL, xlim=xlim, xaxt='n', ylim=ylim,
+     ylab="Days to BB", xlab="", main="14 hour photo")
+axis(1, at=1:4, labels=c("avg", "spr warming", "winter warming",
+    "both warming"), las=2)
+  pos.x <- (1:4)
+  pos.y <- mean.values
+  points(pos.x,pos.y, cex=1.5, pch=19, col="darkblue")
+  pos.y.photo <- c(photo.avgBB, photo.warmsprBB, photo.warmwinBB, photo.warmsprwinBB)
+  points(pos.x, pos.y.photo, cex=1.5, pch=19, col="purple")
+for(i in 1:4){
+  lines(c(pos.x[i], pos.x[i]), c(lowci.values[i], uppci.values[i]), 
+      col="darkblue")
+}
+
