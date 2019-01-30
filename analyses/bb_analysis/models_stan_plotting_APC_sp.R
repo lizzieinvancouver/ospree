49d7ff3fe6e66d78e609175ee365dc2d7ac88447
AileneKane <aettinger@fasmail.harvard.edu>
2019-01-30 13:42:34 -0800

    new species plots for forecasting

--- analyses/bb_analysis/models_stan_plotting_APC_sp.R
+++ analyses/bb_analysis/models_stan_plotting_APC_sp.R
@@ -35,9 +35,6 @@ use.expramptypes.fp = TRUE
 use.exptypes.fp = FALSE
 use.expchillonly = FALSE
 
-#Choose lat analysis or not:
-use.latmod = TRUE
-
 ##
 source("source/bbstanleadin.R")
 ##
@@ -51,136 +48,144 @@ my.pch <- rep(15:18, each=12)
 alphahere = 0.4
 
 # non-z-scored models
-if(use.zscore==FALSE & use.latmod==FALSE){
+if(use.zscore==FALSE){
 load("stan/output/m2lni_spcompexprampfp_nonz.Rda") # m2l.ni
 #load("stan/output/m2lnib_spcompexprampfp_nonz.Rda") # m2l.nib
   modelhere <- m2l.ni
 }
-if(use.zscore = FALSE & use.latmod==TRUE){
-  load("../lat_analysis/stan/m2l.inter_lat.nonz.Rda")
-  modelhere <- m2l.inter
-}
-hist(bb.stan$chill.ports)
-rownameshere <- c("mu_a_sp", "mu_b_force_sp", "mu_b_photo_sp", "mu_b_chill_sp")
+
+#rownameshere <- c("mu_a_sp", "mu_b_force_sp", "mu_b_photo_sp", "mu_b_chill_sp")
+
 #species to plot
 sp<-c("betpen","fagsyl")
+sp.num<-c(9,15)
+#what numbers are these? 9 and 15 I think...
 
 #For main effects of model:
-# Select the lat,long and temperature change that you want
-lat<-48.16447
-long<-11.50293
+# Select the species and temperature change that you want
 tempforecast<-c(1,2,3,4,5,6,7)#enter in the amount of warming (in degrees C) you want to forecast 
 
-# Read in observed chilling from 1950 to 2014
-chillfilename<-paste("../output/dailyclim/chill_observed_",lat,"_",long,"_1979_2014.csv",sep="")
-tempfilename<-paste("../output/dailyclim/temp_observed_",lat,"_",long,"_1979_2014.csv",sep="")
-chillall<-read.csv(chillfilename, header=TRUE) 
-tempall<-read.csv(tempfilename, header=TRUE)
-tempall$Tmean[tempall$Month>3 & tempall$Month<7 ]<-"spring"
-sprtemp <- mean(tempall$Month[tempall$Month>2 & tempall$Month<6])#March-May (4 degrees C) Should it be April-June instead (12 degrees C)?
-budburstdoy<-60#March 1
-daylengthbbdoy <- daylength(lat, budburstdoy)#$Daylength
-chillport <- mean(chillall$Chill_portions)
+#Define the function we will use to estimate budburst
 
-## Plotting
-# First, we estimate the posteriors for each thing we want to plot...
+getspest.bb <- function(fit, sprtemp, daylength, chillport, warmspring, warmwinter,
+                      daylengthwarmspr, daylengthwarmwin, daylengthwarmsprwin){
+  listofdraws <- extract(fit)
+  avgbb <- listofdraws$a_sp[,sp.num[s]] + listofdraws$b_force[,sp.num[s]]*sprtemp + 
+    listofdraws$b_photo[,sp.num[s]]*daylength + listofdraws$b_chill[,sp.num[s]]*chillport
+  
+  warmsprbb <- listofdraws$a_sp[,sp.num[s]] + listofdraws$b_force[,sp.num[s]]*(sprtemp+warmspring) + 
+    listofdraws$b_photo[,sp.num[s]]*(daylength + daylengthwarmspr) + listofdraws$b_chill[,sp.num[s]]*chillport
+  
+  warmwinbb <- listofdraws$a_sp[,sp.num[s]] + listofdraws$b_force[,sp.num[s]]*sprtemp + 
+    listofdraws$b_photo[,sp.num[s]]*(daylength + daylengthwarmwin) + listofdraws$b_chill[,sp.num[s]]*(chillport+warmwinter)
+  
+  warmsprwinbb <- listofdraws$a_sp[,sp.num[s]] + listofdraws$b_force[,sp.num[s]]*(sprtemp+warmspring) +
+    listofdraws$b_photo[,sp.num[s]]*(daylength + daylengthwarmsprwin) + listofdraws$b_chill[,sp.num[s]]*(chillport+warmwinter)
+  
+  yebbest <- list(avgbb, warmsprbb, warmwinbb, warmsprwinbb)
+  return(yebbest)
+}
+
+sp<-c("betpen","fagsyl")
 fit <-modelhere
+#s=1
 
-list_of_draws <- extract(fit)
-print(names(list_of_draws))
-str(list_of_draws$mu_a_sp)
-
-
-getest.bb <- function(fit, sprtemp, daylength, chillport, warmspring, warmwinter,
-    daylengthwarmspr, daylengthwarmwin, daylengthwarmsprwin){
-    listofdraws <- extract(fit)
-    avgbb <- listofdraws$mu_a_sp + listofdraws$mu_b_force_sp*sprtemp + 
-        listofdraws$mu_b_photo_sp*daylength + listofdraws$mu_b_chill_sp*chillport
-    warmsprbb <- listofdraws$mu_a_sp + listofdraws$mu_b_force_sp*(sprtemp+warmspring) + 
-        listofdraws$mu_b_photo_sp*(daylength + daylengthwarmspr) + listofdraws$mu_b_chill_sp*chillport
-    warmwinbb <- listofdraws$mu_a_sp + listofdraws$mu_b_force_sp*sprtemp + 
-        listofdraws$mu_b_photo_sp*(daylength + daylengthwarmwin) + listofdraws$mu_b_chill_sp*(chillport+warmwinter)
-    warmsprwinbb <- listofdraws$mu_a_sp + listofdraws$mu_b_force_sp*(sprtemp+warmspring) +
-        listofdraws$mu_b_photo_sp*(daylength + daylengthwarmsprwin) + listofdraws$mu_b_chill_sp*(chillport+warmwinter)
-    yebbest <- list(avgbb, warmsprbb, warmwinbb, warmsprwinbb)
-    return(yebbest)
-}
-
-# NOTE: I believe quantile is fine for extracting probabilities, see https://discourse.mc-stan.org/t/reporting-credible-confidence-intervals/2262
-
-#make blank dataframe to fill with estimates
-predicts <- as.data.frame(matrix(NA,ncol=5,nrow=7))
-
-predicts.25per <- as.data.frame(matrix(NA,ncol=5,nrow=7))
-predicts.75per <- as.data.frame(matrix(NA,ncol=5,nrow=7))
-colnames(predicts)<-colnames(predicts.25per) <-colnames(predicts.75per) <-
-  c("warming","nowarm","sprwarm","winwarm","bothwarm")
-
-for (i in 1:length(tempforecast)){
-  chillforfilename<-paste("../output/dailyclim/chill_forecast",tempforecast[i],"deg_",lat,"_",long,"_1979_2014.csv",sep="")
-  chillfor<-read.csv(chillforfilename, header=TRUE) 
-  photo.forplot <- daylengthbbdoy
-  warmspring <-tempforecast[i]
-  warmwinter <- mean(chillfor$Chill_portions)-chillport
-  bbposteriors <- getest.bb(fit, sprtemp, daylengthbbdoy, chillport, warmspring, warmwinter, 0, 0, 0)
-  meanz <- unlist(lapply(bbposteriors, mean))
-  quantz <- lapply(bbposteriors, function(x) quantile(x,  c(0.25, 0.5, 0.75)))
-  quant25per <- unlist(lapply(bbposteriors, function(x) quantile(x,  c(0.25))))
-  quant75per <- unlist(lapply(bbposteriors, function(x) quantile(x,  c(0.75))))
-  daychange.springwarm<-meanz[2]-meanz[1] 
-  daychange.wintwarm<-meanz[3]-meanz[1]
-  daychange.bothwarm<-meanz[4]-meanz[1] 
-  daylengthchange.springwarm<-daylength(lat,budburstdoy+daychange.springwarm)-daylengthbbdoy
-  daylengthchange.wintwarm<- daylength(lat,budburstdoy+daychange.wintwarm)-daylengthbbdoy
-  daylengthchange.bothwarm<-daylength(lat,budburstdoy+daychange.bothwarm)-daylengthbbdoy
-  
-  bbposteriors.wdaylength <- getest.bb(m2l.ni, sprtemp, daylengthbbdoy, chillport, warmspring, warmwinter, daylengthchange.springwarm, daylengthchange.wintwarm, daylengthchange.bothwarm)
-  meanz.wdaylength <- unlist(lapply(bbposteriors.wdaylength, mean))
-  quant25per.wdaylength <- unlist(lapply(bbposteriors.wdaylength, function(x) quantile(x,  c(0.25))))
-  quant75per.wdaylength <- unlist(lapply(bbposteriors.wdaylength, function(x) quantile(x,  c(0.75))))
-  predicts[i,]<-c(warmspring,meanz.wdaylength)
-  predicts.25per[i,]<-c(warmspring,quant25per.wdaylength)
-  predicts.75per[i,]<-c(warmspring,quant75per.wdaylength)
-}
-predicts<-rbind(c(0,predicts$nowarm[1:4]),predicts)
-predicts<-predicts[,-2]
-predicts.25per<-rbind(c(0,predicts.25per$nowarm[1:4]),predicts.25per)
-predicts.25per<-predicts.25per[,-2]
-predicts.75per<-rbind(c(0,predicts.75per$nowarm[1:4]),predicts.75per)
-predicts.75per<-predicts.75per[,-2]
-
-xlim = c(0, 7)
-ylim = c(15, 30)
-figname<-paste("tempforecast",lat,long,min(tempforecast),max(tempforecast),"degwarm.pdf", sep="_")
-pdf(file.path(figpath,figname), width = 9, height = 6)
-
-#pdf(paste(figpath,"/tempforecast",min(tempforecast),"-",max(tempforecast),"_deg_",lat,"_",long,".pdf",sep=""))
-#quartz()
-par(mar=c(8,7,3,5))
-plot(x=NULL,y=NULL, xlim=xlim, xlab="Amount of warming (C)", ylim=ylim,
-     ylab="Days to BB", main=paste(round(daylengthbbdoy, digits=0)," hours", sep=""), bty="l")
-  pos.x <- 0
-  pos.y <- predicts[1,2]
-  points(pos.x, pos.y, cex=1.2, pch=19, bg="gray")
-  #Add shading around line for credible intervals
-  
-  for(i in 3:5){
-  polygon(c(rev(predicts$warming), predicts$warming), c(rev(predicts.75per[,i-1]), predicts.25per[,i-1]), col = alpha(cols[i-2], 0.2), border = NA)
+for(s in 1:length(sp)){
+  #50 sites were chosen within the range of each species
+  numsites<-length(list.files(path=paste("../output/dailyclim/",sp[s],sep=""),pattern="temp_forforecast__"))
+  tempfiles<-list.files(path=paste("../output/dailyclim/",sp[s],sep=""),pattern="temp_forforecast__")
+  chillfiles<-list.files(path=paste("../output/dailyclim/",sp[s],sep=""),pattern="chill_observed_")
+  spdir<-paste("../output/dailyclim/",sp[s],sep="")
+  #for (i in 1:length(numsites)){
+    #for now do with i=1 and i=50 for each species
+    quartz(width=9,height=5)
+    par(mar=c(8,4,3,4), mfrow=c(1,2))
+    # Read in observed chilling from 1950 to 2014
+    chillall<-read.csv(paste(spdir,"/",chillfiles[i],sep=""), header=TRUE) 
+    tempall<-read.csv(paste(spdir,"/",tempfiles[i],sep=""), header=TRUE)
+    tempall$Tmean[tempall$Month>3 & tempall$Month<7 ]<-"spring"
+    sprtemp <- mean(tempall$Month[tempall$Month>2 & tempall$Month<6])#March-May (4 degrees C) Should it be April-June instead (12 degrees C)?
+    budburstdoy<-60#March 1#change this to the bbdoy observed in pep!
+    #extract the lat/long from the file name...argh!
+    lat<-as.numeric(strsplit(substr(chillfiles[i],16,nchar(chillfiles[i])-14),"_")[[1]][1])
+    long<-as.numeric(strsplit(substr(chillfiles[i],16,nchar(chillfiles[i])-14),"_")[[1]][2])
+    daylengthbbdoy <- daylength(lat, budburstdoy)#$Daylength
+    chillport <- mean(chillall$Chill_portions)
+    #make blank dataframe to fill with estimates
+    predicts <- as.data.frame(matrix(NA,ncol=5,nrow=7))
+    
+    predicts.25per <- as.data.frame(matrix(NA,ncol=5,nrow=7))
+    predicts.75per <- as.data.frame(matrix(NA,ncol=5,nrow=7))
+    colnames(predicts)<-colnames(predicts.25per) <-colnames(predicts.75per) <-
+      c("warming","nowarm","sprwarm","winwarm","bothwarm")
+    
+    for (j in 1:length(tempforecast)){
+      chillforfilename<-paste(spdir,"/","chillforecast",tempforecast[j],"deg_",lat,"_",long,"_1951_2014.csv",sep="")
+      chillfor<-read.csv(chillforfilename, header=TRUE) 
+      photo.forplot <- daylengthbbdoy
+      warmspring <-tempforecast[j]
+      warmwinter <- mean(chillfor$Chill_portions)-chillport
+      bbposteriors <- getspest.bb(fit, sprtemp, daylengthbbdoy, chillport, warmspring, warmwinter, 0, 0, 0)
+      meanz <- unlist(lapply(bbposteriors, mean))
+      quantz <- lapply(bbposteriors, function(x) quantile(x,  c(0.25, 0.5, 0.75)))
+      quant25per <- unlist(lapply(bbposteriors, function(x) quantile(x,  c(0.25))))
+      quant75per <- unlist(lapply(bbposteriors, function(x) quantile(x,  c(0.75))))
+      daychange.springwarm<-meanz[2]-meanz[1] 
+      daychange.wintwarm<-meanz[3]-meanz[1]
+      daychange.bothwarm<-meanz[4]-meanz[1] 
+      daylengthchange.springwarm<-daylength(lat,budburstdoy+daychange.springwarm)-daylengthbbdoy
+      daylengthchange.wintwarm<- daylength(lat,budburstdoy+daychange.wintwarm)-daylengthbbdoy
+      daylengthchange.bothwarm<-daylength(lat,budburstdoy+daychange.bothwarm)-daylengthbbdoy
+      
+      bbposteriors.wdaylength <- getest.bb(modelhere, sprtemp, daylengthbbdoy, chillport, warmspring, warmwinter, daylengthchange.springwarm, daylengthchange.wintwarm, daylengthchange.bothwarm)
+      meanz.wdaylength <- unlist(lapply(bbposteriors.wdaylength, mean))
+      quant25per.wdaylength <- unlist(lapply(bbposteriors.wdaylength, function(x) quantile(x,  c(0.25))))
+      quant75per.wdaylength <- unlist(lapply(bbposteriors.wdaylength, function(x) quantile(x,  c(0.75))))
+      predicts[j,]<-c(warmspring,meanz.wdaylength)
+      predicts.25per[j,]<-c(warmspring,quant25per.wdaylength)
+      predicts.75per[j,]<-c(warmspring,quant75per.wdaylength)
+    }
+    predicts<-rbind(c(0,predicts$nowarm[1:4]),predicts)
+    predicts<-predicts[,-2]
+    predicts.25per<-rbind(c(0,predicts.25per$nowarm[1:4]),predicts.25per)
+    predicts.25per<-predicts.25per[,-2]
+    predicts.75per<-rbind(c(0,predicts.75per$nowarm[1:4]),predicts.75per)
+    predicts.75per<-predicts.75per[,-2]
+    
+    xlim = c(0, 7)
+    ylim = c(10, 50)
+    #figname<-paste("tempforecast",lat,long,min(tempforecast),max(tempforecast),"degwarm.pdf", sep="_")
+    #pdf(file.path(figpath,figname), width = 9, height = 6)
+    
+    #pdf(paste(figpath,"/tempforecast",min(tempforecast),"-",max(tempforecast),"_deg_",lat,"_",long,".pdf",sep=""))
+    #quartz()
+    #par(mar=c(8,7,3,5), mfrow=c(1,2))
+    plot(x=NULL,y=NULL, xlim=xlim, xlab="Amount of warming (C)", ylim=ylim,
+         ylab="Days to BB", main=paste(sp[s],", lat=",round(lat,digits=2),", ",round(daylengthbbdoy, digits=0)," hrs", sep=""), bty="l")
+    pos.x <- 0
+    pos.y <- predicts[1,2]
+    points(pos.x, pos.y, cex=1.2, pch=19, bg="gray")
+    #Add shading around line for credible intervals
+    
+    for(t in 3:5){
+      polygon(c(rev(predicts$warming), predicts$warming), c(rev(predicts.75per[,t-1]), predicts.25per[,t-1]), col = alpha(cols[t-2], 0.2), border = NA)
+    }
+    
+    for(t in 3:5){
+      lines(predicts$warming, predicts[,t-1], 
+            col=cols[t-2], lwd=2)}
+    
+    # intervals
+    # for(i in 3:5){
+    #   lines(predicts.25per$warming, predicts.25per[,i-1], 
+    #         col=cols[i-2], lwd=1, lty=2)
+    # }
+    # for(i in 3:5){
+    #   lines(predicts.75per$warming, predicts.75per[,i-1], 
+    #         col=cols[i-2], lwd=1, lty=2)
+    # }
+    #dev.off()
   }
   
-  for(i in 3:5){
-    lines(predicts$warming, predicts[,i-1], 
-          col=cols[i-2], lwd=2)}
+  legend(0,18,legend=c("Spring warming","Winter warming","Both"),lty=1,lwd=2,col=cols,bty="n", cex=0.9)
   
-  # intervals
-  # for(i in 3:5){
-  #   lines(predicts.25per$warming, predicts.25per[,i-1], 
-  #         col=cols[i-2], lwd=1, lty=2)
-  # }
-  # for(i in 3:5){
-  #   lines(predicts.75per$warming, predicts.75per[,i-1], 
-  #         col=cols[i-2], lwd=1, lty=2)
-  # }
-legend(0,18,legend=c("Spring warming","Winter warming","Both"),lty=1,lwd=2,col=cols,bty="n", cex=0.9)
-dev.off()
-
