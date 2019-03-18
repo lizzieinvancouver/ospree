4011099984c9094890d374481f4032ac57f88d80
Elizabeth Wolkovich <wolkovich@biodiversity.edu>
2019-03-17 17:22:47 -0700

    Compared my code to Cat's ... found some mistakes!

--- analyses/limitingcues/countintxns/countintxns_lizzie.R
+++ analyses/limitingcues/countintxns/countintxns_lizzie.R
@@ -39,7 +39,8 @@ goober <- data.frame(datasetID=character(), study=character(), photo=numeric(),
 for (did in unique(testdat$datasetID)){
     subbydid <- subset(testdat, datasetID==did)
        for (studyid in unique(subbydid$study)){
-          subbydidexp <- subset(subbydid, study==studyid)
+          subbydidexp.allcols <- subset(subbydid, study==studyid)
+          subbydidexp <- subbydidexp.allcols[,c("force", "photo")]
              for (photohere in unique(subbydidexp$photo)){
                  # Finally, subset to one photoperiod in each datasetID and study combo ...
                  samephoto <- subset(subbydidexp, photo==photohere)
@@ -56,53 +57,55 @@ for (did in unique(testdat$datasetID)){
                            # otherwise, see if there are more than one forcing temps ...                      
                            uniquehereagain <- subby.diffphoto[!duplicated(subby.diffphoto),]
                            forcedistagain <- dist(uniquehereagain$force)
-                             if (length(forcedist)==0) gooberadd <- data.frame(datasetID=did,
+                             if (length(forcedistagain)==0) gooberadd <- data.frame(datasetID=did,
                                 study=studyid, intxn=0) else{
                                 gooberadd <- data.frame(datasetID=did, study=studyid, intxn=1)
                                 }
                            }
                      }
                  goober <- rbind(goober, gooberadd)
-                 goobermean <- aggregate(goober["intxn"], goober[c("datasetID", "study")], FUN=mean)
+                 goobersum <- aggregate(goober["intxn"], goober[c("datasetID", "study")], FUN=sum)
                  }
           }
     }
 
-goobermean # Hmm, this does not come out the same as when I functionalize ....
+goobersum 
 
 # Functionalize the above!
 get.treatdists <- function(df, treatcol, othertreatcol){
-    goober <- data.frame(datasetID=character(), study=character(), intxn=numeric())
+    dfbuild <- data.frame(datasetID=character(), study=character(), intxn=numeric())
     for (did in unique(df[["datasetID"]])){
      subbydid <- subset(df, datasetID==did)
        for (studyid in unique(subbydid$study)){
-          subbydidexp <- subset(subbydid, study==studyid)
+          subbydidexp.allcols <- subset(subbydid, study==studyid)
+          subbydidexp <- subbydidexp.allcols[,c(treatcol, othertreatcol)]
              for (treathere in unique(subbydidexp[[treatcol]])){
+                 # Finally, subset to one treatment (of treatcol) in each datasetID and study combo ...
                  sametreat <- subbydidexp[which(subbydidexp[[treatcol]]==treathere),]
                  uniquehere <- sametreat[!duplicated(sametreat),]
                  othertreatdist <- dist(uniquehere[[othertreatcol]])
                  # If there is not more than one other treatment, intxn=0
-                 if (length(othertreatdist)==0) gooberadd <- data.frame(datasetID=did,
+                 if (length(othertreatdist)==0) dfbuildadd <- data.frame(datasetID=did,
                      study=studyid, intxn=0) else {
                      subby.othertreat <- subbydidexp[which(subbydidexp[[othertreatcol]] %in% uniquehere[[othertreatcol]]),]
                      subby.diff <- subby.othertreat[which(!subby.othertreat[[treatcol]] %in% treathere),]
                        # If there are not any other treatments with a different treats and treathere, intxn=0
-                       if (nrow(subby.diff) <= 1) gooberadd <- data.frame(datasetID=did,
+                       if (nrow(subby.diff) <= 1) dfbuildadd <- data.frame(datasetID=did,
                        study=studyid, intxn=0) else{
                            uniquehereagain <- subby.diff[!duplicated(subby.diff),]
                            othertreatdistagain <- dist(uniquehereagain[[othertreatcol]])
-                             if (length(othertreatdistagain)==0) gooberadd <- data.frame(datasetID=did,
+                             if (length(othertreatdistagain)==0) dfbuildadd <- data.frame(datasetID=did,
                                 study=studyid, intxn=0) else{
-                                gooberadd <- data.frame(datasetID=did, study=studyid, intxn=1)
+                                dfbuildadd <- data.frame(datasetID=did, study=studyid, intxn=1)
                                 }
                            }
                      }
-                 goober <- rbind(goober, gooberadd)
-                 goobermean <- aggregate(goober["intxn"], goober[c("datasetID", "study")], FUN=mean)
+                 dfbuild <- rbind(dfbuild, dfbuildadd)
+                 dfbuildsum <- aggregate(dfbuild["intxn"], dfbuild[c("datasetID", "study")], FUN=sum)
                  }
           }
     }
-  return(goobermean)
+  return(dfbuildsum)
 }
 
 get.treatdists(testdat, "photo", "force")
@@ -121,4 +124,35 @@ datsm$photo <- as.numeric(datsm$photoperiod_day)
 datsm.noNA <- subset(datsm, is.na(force)==FALSE & is.na(photo)==FALSE)
 
 osp.fp <- get.treatdists(datsm.noNA, "photo", "force")
-osp.fpintxn <- subset(osp.fp, intxn==1)
+osp.fpintxn <- subset(osp.fp, intxn>=2)
+osp.fpintxn[order(osp.fpintxn$datasetID),]
+# Compare to what Cat got ... (taken from countinxns_Cat.R)
+fp.cat <- c("basler14", "heide05", "heide08", "heide11", "heide93", "heide93a", "okie11", "partanen98", "pettersen71", "Sanz-Perez09", "sogaard08")
+setdiff(fp.cat, unique(osp.fpintxn$datasetID))
+setdiff(unique(osp.fpintxn$datasetID), fp.cat)
+
+# f(x) to look at unique treats
+get.uniquetreats <- function(df, treatcol, othertreatcol){
+    dfbuild <- data.frame(datasetID=character(), study=character(), treat=numeric(), othertreat=numeric())
+    for (did in unique(df[["datasetID"]])){
+     subbydid <- subset(df, datasetID==did)
+       for (studyid in unique(subbydid$study)){
+          subbydidexp <- subset(subbydid, study==studyid)
+             for (treathere in unique(subbydidexp[[treatcol]])){
+                 # Finally, subset to one treatment (of treatcol) in each datasetID and study combo ...
+                 sametreat <- subbydidexp[which(subbydidexp[[treatcol]]==treathere),]
+                 uniquehere <- sametreat[!duplicated(sametreat),]
+                 dfadd <- data.frame(datasetID=rep(did, nrow(uniquehere)), study=rep(studyid, nrow(uniquehere)),
+                     treat=uniquehere[[treatcol]], othertreat=uniquehere[[othertreatcol]])
+                 dfbuild <- rbind(dfbuild, dfadd)
+                 }
+          }
+     }
+  return(dfbuild)
+}
+
+lookatunique <- get.uniquetreats(datsm.noNA, "photo", "force")
+subset(lookatunique, datasetID=="sogaard08") # According to my cheap code, sogaard08 has three forcing temperatures ... at only one temperature do they vary photoperiod ... what is that? Can you estimate intxns and main effects from it? I am at a loos. 
+subset(lookatunique, datasetID=="partanen98") # partanen98 has three photoperiods, varies temp for only one of them.
+
+# My code was wrong in taking unique rows for the OSPREE data, fixed by deleting all but the columns for the two treatments in question from df ...
