916fe394d65aa820752aa41e48836b63b4f1317c
Ailene <aettinger@fas.harvard.edu>
2019-05-08 14:43:35 -0700

    Create salixtilia.R

---
+++ notes/salixtilia.R
@@ -0,0 +1,12 @@
+#look into why salix and tilia might have weird responses
+#Tilia: "basler12"    "caffarra11a" "zohner16"  
+#Salix: "caffarra11a" "zohner16" 
+#Salix and tilia "caffarra11a": lower days  to bb and higher % bb with long photoperiod vs short photoperiod, especiall with low chilling
+#caffarra: forcetemp "18" "24" "6"  "32" "12", (mean=16), -5-32
+#caffarra: chilltemp=3 (range of whole dataset =-10  16), mean=3.4 across whole dataset
+#caffara: chilldays= "105" "16"  "30"  "55" (mean is 77)
+#caffarra: total chillports: 70.12978  9.96549 19.42954 36.32962, total Utah: 2520  384  720 1320
+#zohner forcetemp: 18, chilltemp, days=field = 61.11481 123.49099  95.34689; Utah: 1067.5 2223.5 1650.0
+
+#basler: forcetemp=5,chilltemp, days=field =1345 1297 CH, 81.80449 89.65475 CP
+#full range: CP, 0.000 173.871; UT: -1304.5  4724.0
