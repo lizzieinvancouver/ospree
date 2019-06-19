bcfc5f264372a166d288594b04c44152a004fda7
AileneKane <ailene.ettinger@nwcdm04178674.nmfs.local>
2019-06-19 12:15:11 -0700

    table of all species for supp

--- analyses/bb_analysis/maketables.forsupp/make_sp_table_forsupp.R
+++ analyses/bb_analysis/maketables.forsupp/make_sp_table_forsupp.R
@@ -1,13 +1,13 @@
-## StartedMay 2019 
-## By Ailene, modified from models_stan_ploting to match names in models_stan
-## Plotting results from Stan models ##
+## Started June 2019 
+## By Ailene, 
+## Table of all species and which studies contain them for supplement
 
 ############################################
 ## housekeeping
 rm(list=ls()) 
 options(stringsAsFactors = FALSE)
 
-library(RColorBrewer)
+library(tidyr)
 
 # Setting working directory. Add in your own path in an if statement for your file structure
 if(length(grep("ailene", getwd()))>0) { 
@@ -17,14 +17,14 @@ if(length(grep("Ignacio", getwd()))>0) {
   setwd("~/GitHub/ospree/analyses") 
 } else setwd("~/Documents/git/projects/treegarden/budreview/ospree/analyses/bb_analysis")
 
-## set up the flags
+## set up the flags to get the most species and studies included
 use.chillports = FALSE
 use.zscore = TRUE
 use.allspp = TRUE
 use.multcuespp = FALSE
 use.cropspp = TRUE
 # Default is species complex use  alltypes of designs
-use.expramptypes.fp = TRUE
+use.expramptypes.fp = FALSE
 use.exptypes.fp = FALSE
 use.expchillonly = FALSE
 
@@ -41,12 +41,35 @@ if (use.allspp==TRUE & use.multcuespp==FALSE & use.cropspp==TRUE &
 ## functions for plotting 
 source("source/bbstanleadin.R")
 #get sp names
-spnames<-sort(unique(bb.stan$complex.wname))
+bb.stan$spname<-paste(bb.stan$genus,bb.stan$species,sep=".")
+spnames<-sort(unique(bb.stan$spname))
 
 
 #get studies that go with the names
-bb.stan$ID_sp<-paste(bb.stan$datasetID,bb.stan$complex.wname)
-sp.studies <- bb.stan %>% # start with the data frame
+bb.stan$ID_sp<-paste(bb.stan$datasetID,bb.stan$spname)
+sp.st <- bb.stan %>% # start with the data frame
   distinct(ID_sp, .keep_all = TRUE) %>% # establishing grouping variables
-  #filter(continent == 'europe' & year >= 1950) %>%#select out europe#this removes all rows with  field sample date=2013-10-22, even though some are in europe
-  dplyr::select(datasetID, complex.wname)
+  dplyr::select(datasetID, spname)
+sp.st$study<-"study"
+studynum<-rowSums(table(sp.st$spname,sp.st$datasetID))
+spstudynum<-c()  
+for (i in 1:length(spnames)){
+    numstud<-studynum[i]
+    listnumstud<-seq(1,numstud,by=1)
+    spstudynum<-c(spstudynum,listnumstud)
+  }
+ 
+sp.st$numstud<-spstudynum
+sp.st$newcols<-paste(sp.st$study,sp.st$numstud,sep=".")
+sp.st2<-subset(sp.st,select=c(spname,datasetID,newcols))
+sp.st.wide <- spread(sp.st2,newcols,datasetID, fill="")
+sp.st.wide$stnum<-studynum
+sp.st.wide$studies<-paste(sp.st.wide1$study.1,sp.st.wide1$study.2,sp.st.wide1$study.3,sp.st.wide1$study.4,sp.st.wide1$study.5,sp.st.wide1$study.6,sp.st.wide1$study.7,sp.st.wide1$study.8,sp.st.wide1$study.8,sp.st.wide1$study.10,sep=",")
+
+sp.st.wide$studies<-gsub(",,,,,,,,,","", sp.st.wide$studies)
+sp.st.wide$studies<-gsub(",,,,,,,,","", sp.st.wide$studies)
+sp.st.wide$studies<-gsub(",,,,,,,","", sp.st.wide$studies)
+sp.st.wide$studies<-gsub(",,,,,,","", sp.st.wide$studies)
+sp.st.wide$studies<-gsub(",,,,","", sp.st.wide$studies)
+
+sp.st.table<-subset(sp.st.wide,select=c(spname,stnum,studies))
