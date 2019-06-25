9ba2f04c004de0b5c749ae12368ab9888cf1b027
cchambe12 <cchamberlain@g.harvard.edu>
2019-06-25 14:40:08 -0400

    working on lat table

--- analyses/bb_analysis/maketables.forsupp/mod_table_lat.R
+++ analyses/bb_analysis/maketables.forsupp/mod_table_lat.R
@@ -3,14 +3,14 @@
 #would be good to add n_studies
 
 #chill ports, z
-load("../../../analyses/lat_analysis/stan/output/m2l.inter.lat.z.Rda") # m2l.inter
+load("../../analyses/lat_analysis/stan/output/m2l.inter.lat.z.Rda") # m2l.inter
 fit.z <- summary(m2l.inter)$summary
 ztab<-as.data.frame(round(cbind(fit.z[1:13,1],fit.z[1:13,4],fit.z[1:13,8]),digits=2))
 ztab<-rbind(ztab,c(length(fit.z[grep("a_sp", rownames(fit.z)),1]),"","",""))
 rownames(ztab)[14]<-"n_sp"
 if(FALSE){
 #cp units, nonz
-load("../../../analyses/bb_analysis/stan/output/m2l.inter.lat.nonz.Rda") # m2l.inter
+load("../../analyses/bb_analysis/stan/output/m2l.inter.lat.nonz.Rda") # m2l.inter
 fitcp.nonz <- summary(m2l.inter)$summary
 nonzcptab<-as.data.frame(round(cbind(fitcp.nonz[1:13,1],fitcp.nonz[1:13,4],fitcp.nonz[1:13,8]),digits=2))
 nonzcptab<-rbind(nonzcptab,c(length(fitcp.nonz[grep("a_sp", rownames(fitcp.nonz)),1]),"","",""))
@@ -30,5 +30,5 @@ nonztab$credint<-paste(nonztab$lci,nonztab$uci, sep="-")
 
 colnames(zmodtable)<-c("parameter", "cp.est","cp.ci")
 
-write.csv(zmodtable,"../../../analyses/output/supptables/zmodetable_latcpz.csv", row.names = FALSE)
+write.csv(zmodtable,"../../analyses/output/supptables/zmodetable_latcpz.csv", row.names = FALSE)
 
