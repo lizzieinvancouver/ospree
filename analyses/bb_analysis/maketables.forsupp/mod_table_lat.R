a87bbb0b1548dd78bd1ef352f50df34f74d31c1e
cchambe12 <cchamberlain@g.harvard.edu>
2019-06-27 16:29:13 -0700

    updated code

--- analyses/bb_analysis/maketables.forsupp/mod_table_lat.R
+++ analyses/bb_analysis/maketables.forsupp/mod_table_lat.R
@@ -3,9 +3,9 @@
 #would be good to add n_studies
 
 #chill ports, z
-load("../../analyses/lat_analysis/stan/output/m2l.inter.lat.z.Rda") # m2l.inter
+load("../../analyses/lat_analysis/stan/m2l.inter.lat.z.Rda") # m2l.inter
 fit.z <- summary(m2l.inter)$summary
-ztab<-as.data.frame(round(cbind(fit.z[1:13,1],fit.z[1:13,4],fit.z[1:13,8]),digits=2))
+ztab<-as.data.frame(round(cbind(fit.z[1:13,1],fit.z[1:13,4],fit.z[1:13,5],fit.z[1:13,7],fit.z[1:13,8]),digits=2))
 ztab<-rbind(ztab,c(length(fit.z[grep("a_sp", rownames(fit.z)),1]),"","",""))
 rownames(ztab)[14]<-"n_sp"
 if(FALSE){
@@ -20,15 +20,15 @@ rownames(nonzcptab)[14]<-"n_sp"
 
 
 #add column names to all sub tables
-colnames(ztab)<-c("est","lci","uci")
+colnames(ztab)<-c("estimate","2.5%","25%", "75%", "97.5%")
 
-ztab$credint<-paste(ztab$lci,ztab$uci, sep="-")
+#ztab$credint<-paste(ztab$`2.5%`, ztab$`2.5%`,ztab$uci, sep="-")
 
-zmodtable<-cbind(c(row.names(fit.z)[1:13],"n_sp"),ztab$est,ztab$credint)
+#zmodtable<-cbind(c(row.names(fit.z)[1:13],"n_sp"),ztab$est,ztab$credint)
 
-nonztab$credint<-paste(nonztab$lci,nonztab$uci, sep="-")
+#nonztab$credint<-paste(nonztab$lci,nonztab$uci, sep="-")
 
-colnames(zmodtable)<-c("parameter", "cp.est","cp.ci")
+#colnames(zmodtable)<-c("parameter", "cp.est","cp.ci")
 
-write.csv(zmodtable,"../../analyses/output/supptables/zmodetable_latcpz.csv", row.names = FALSE)
+write.csv(ztab,"../../analyses/output/supptables/zmodetable_latcpz.csv", row.names = FALSE)
 
