dbc59d6d5cd1eb8500971691e4aabced68e85c05
AileneKane <aettinger@fasmail.harvard.edu>
2019-06-27 09:54:13 -0700

    new model tables with updated code

--- analyses/bb_analysis/maketables.forsupp/mod_table.R
+++ analyses/bb_analysis/maketables.forsupp/mod_table.R
@@ -4,41 +4,41 @@
 load("../../analyses/bb_analysis/stan/output/m2lni_spcompexprampfputah_nonz.Rda") # m2l.ni
 fit.nonz <- summary(m2l.ni)$summary
 nonztab<-as.data.frame(round(cbind(fit.nonz[1:9,1],fit.nonz[1:9,4],fit.nonz[1:9,8]),digits=2))
-
 #add n_sp
-nonztab<-rbind(nonztab,c(length(fit.nonz[grep("a_sp", rownames(fit.nonz)),1]),"","",""))
+nonztab<-rbind(nonztab,c(length(fit.nonz[grep("a_sp", rownames(fit.nonz)),1])-2,"","",""))
 rownames(nonztab)[10]<-"n_sp"
-#utah units, z
 
+#utah units, z
 load("../../analyses/bb_analysis/stan/output/m2lni_spcompexprampfputah_z.Rda") # m2l.ni
 fit.z <- summary(m2l.ni)$summary
 ztab<-as.data.frame(round(cbind(fit.z[1:9,1],fit.z[1:9,4],fit.z[1:9,8]),digits=2))
-ztab<-rbind(ztab,c(length(fit.z[grep("a_sp", rownames(fit.z)),1]),"","",""))
+ztab<-rbind(ztab,c(length(fit.z[grep("a_sp", rownames(fit.z)),1])-2,"","",""))
 rownames(ztab)[10]<-"n_sp"
+
 #cp units, nonz
 load("../../analyses/bb_analysis/stan/output/m2lni_spcompexprampfpcp_nonz.Rda") # m2l.ni
 fitcp.nonz <- summary(m2l.ni)$summary
 nonzcptab<-as.data.frame(round(cbind(fitcp.nonz[1:9,1],fitcp.nonz[1:9,4],fitcp.nonz[1:9,8]),digits=2))
-nonzcptab<-rbind(nonzcptab,c(length(fitcp.nonz[grep("a_sp", rownames(fitcp.nonz)),1]),"","",""))
+nonzcptab<-rbind(nonzcptab,c(length(fitcp.nonz[grep("a_sp", rownames(fitcp.nonz)),1])-2,"","",""))
 rownames(nonzcptab)[10]<-"n_sp"
 #cp units, z
 load("../../analyses/bb_analysis/stan/output/m2lni_spcompexprampfpcp_z.Rda") # m2l.ni
 fitcp.z <- summary(m2l.ni)$summary
 zcptab<-as.data.frame(round(cbind(fitcp.z[1:9,1],fitcp.z[1:9,4],fitcp.z[1:9,8]),digits=2))
-zcptab<-rbind(zcptab,c(length(fitcp.z[grep("a_sp", rownames(fitcp.z)),1]),"","",""))
+zcptab<-rbind(zcptab,c(length(fitcp.z[grep("a_sp", rownames(fitcp.z)),1])-2,"","",""))
 rownames(zcptab)[10]<-"n_sp"
 
 #add model with crops and all species and all treatments included, nonz
 load("../../analyses/bb_analysis/stan/output/m2lni_allsppwcrop_utah_nonz.Rda") # m2l.ni
 fitallsp.nonz <- summary(m2l.ni)$summary
 nonzallsptab<-as.data.frame(round(cbind(fitallsp.nonz[1:9,1],fitallsp.nonz[1:9,4],fitallsp.nonz[1:9,8]),digits=2))
-nonzallsptab<-rbind(nonzallsptab,c(length(fitallsp.nonz[grep("a_sp", rownames(fitallsp.nonz)),1]),"","",""))
+nonzallsptab<-rbind(nonzallsptab,c(length(fitallsp.nonz[grep("a_sp", rownames(fitallsp.nonz)),1])-2,"","",""))
 rownames(nonzallsptab)[10]<-"n_sp"
 #add model with crops and all species and all treatments included, z
 load("../../analyses/bb_analysis/stan/output/m2lni_allsppwcrop_utah_z.Rda") # m2l.ni
 fitallsp.z <- summary(m2l.ni)$summary
 zallsptab<-as.data.frame(round(cbind(fitallsp.z[1:9,1],fitallsp.z[1:9,4],fitallsp.z[1:9,8]),digits=2))
-zallsptab<-rbind(zallsptab,c(length(fitallsp.z[grep("a_sp", rownames(fitallsp.z)),1]),"","",""))
+zallsptab<-rbind(zallsptab,c(length(fitallsp.z[grep("a_sp", rownames(fitallsp.z)),1])-2,"","",""))
 rownames(zallsptab)[10]<-"n_sp"
 
 #add column names to all sub tables
