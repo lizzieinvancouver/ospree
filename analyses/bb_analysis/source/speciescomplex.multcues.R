225caaefbb999d7226d9f8cc4b4a92eb79db66c2
cchambe12 <cchamberlain@g.harvard.edu>
2019-10-17 08:45:54 -0400

    removing crops

--- analyses/bb_analysis/source/speciescomplex.multcues.R
+++ analyses/bb_analysis/source/speciescomplex.multcues.R
@@ -26,6 +26,8 @@ sppcomplexfx.multcue <- function(d){
 
 #d <- read.csv("~/Documents/git/ospree/analyses/output/ospree_clean_withchill_BB.csv")
 d$name<-paste(d$genus,d$species,sep="_") ###make  a column for genus_species
+cropspp <- c("Actinidia_deliciosa", "Malus_domestica", "Vitis_vinifera", "Ribes_nigrum")
+d<-d[!(d$name%in%cropspp),]
 
 xx<-d
 ### make a list of which studies manipulate what.
