936093a970629778d1cc6afc87117ee1c113f7e5
cchambe12 <cchamberlain@g.harvard.edu>
2019-10-29 17:27:53 -0400

    testing climate data

--- analyses/chilling/comparingclimdata.R
+++ analyses/chilling/comparingclimdata.R
@@ -16,7 +16,6 @@ if(length(grep("Lizzie", getwd())>0)) {setwd("~/Documents/git/projects/treegarde
 # Load libraries
 library(dplyr)
 library(tidyr)
-library(plyr)
 library(ncdf4)
 library(Interpol.T)
 library(chillR)
@@ -28,6 +27,9 @@ source("chilling/cleaning_chilltemp.R")
 source("chilling/fieldchillcalc_latlong.R")
 
 climatedrive = "/Volumes/climdata" # Cat's climate drive
+
+### But since Livneh only goes through 2013 need to remove some data first...
+nam <- nam[(nam$year<=2013),]
 
 nafiles <- dir(climatedrive)[grep("princetonclimdata", dir(climatedrive))]
 #loop through each lat/long for which we want to calculate chilling and pull the climate data for that lat/long
@@ -42,33 +44,12 @@ for(i in 1:nrow(nam)){ # i = 5
   # make sure longitudes are negative, need to be for North America this step is now done in "cleaning/clean_latlong" so it is no longer necessary
   #if(lo > 0) { lo = lo*-1 }
   yr<-as.numeric(substr(nam[i,"fieldsample.date2"],1,4))
-  # start and end days of the climate data we need to calculate chilling, for the focal lat/long. 
-  #This is in days since baseline date (sept 1) Set to GMT to avoid daylight savings insanity
-  # using d$fieldsample.date2 (this is the same as fieldsampledate, but formatted as  "%Y-%m-%d")
-  
-  #do everything in reference to field sample year becuase the year column is too variable
-  if(nam[i,"fieldsample.date2"]!=""){endday <- strptime(nam[i,"fieldsample.date2"],"%Y-%m-%d", tz = "GMT")
+
+  endday <- strptime(paste(yr, "08-30", sep="-"),"%Y-%m-%d", tz = "GMT")
   doyend <- yday(endday)
-  }
-  if(nam[i,"fieldsample.date2"]==""){endday <- strptime(paste(yr, "08-30", sep="-"),"%Y-%m-%d", tz = "GMT")
-  doyend <- yday(endday)
-  }#I think we have field sample dates for everything, but just in case...
-  
-  if(nam[i,"fieldsample.date2"]!="" & as.numeric(substr(nam[i,"fieldsample.date2"],6,7))>=9){
-    stday <- strptime(paste(yr, "06-01", sep="-"),"%Y-%m-%d", tz="GMT")
-    #chillmo<-paste(yr, formatC(9:substr(endday,6,7), width=2, flag="0"), sep="")
-  }#If field sample date is after september 1, then we use the chilling from the current year, since sept 1
-  
-  if(nam[i,"fieldsample.date2"]!="" & as.numeric(substr(nam[i,"fieldsample.date2"],6,7))<9){
-    stday <- strptime(paste(yr-1, "06-01", sep="-"),"%Y-%m-%d", tz="GMT")#
-    #prevmo <- paste(yr-1, formatC(9:12, width=2, flag="0"), sep="");# use previous year's fall months of chilling (Sept-Dec)
-    #endmo<-substr(endday,6,7);#month of sampling date
-    #thismo <- paste(yr, formatC(1:endmo, width=2, flag="0"), sep="")#months from current year of chilling, through sampling date (Jan-whenever sampled)
-    #chillmo<-c(prevmo, thismo)
-  }#If field sample date is before september 1, then we use the chilling from the previous year.
-  
-  # now loop over these year-month combo files and get temperature values for this date range.
-  
+  stday <- strptime(paste(yr, "06-01", sep="-"),"%Y-%m-%d", tz="GMT")
+  doyst <- yday(stday)
+ 
   mins <- maxs <- vector()
   
   ## for now exclude prevey18
@@ -81,11 +62,12 @@ for(i in 1:nrow(nam)){ # i = 5
     jx <- nc_open(tmax)
     jn <- nc_open(tmin)
     
-    yrend <- vector()
+    yrend <- j
     gddstart <- vector()
     
     jx$dim$time$vals<-seq(1, yrend, by=1)
     thisyr <- which(jx$dim$time$vals<=doyend)
+    thisyr <- which(thisyr>=doyst)
    
     diff.long.cell <- abs(jx$dim$lon$vals-as.numeric(lo))#differences between all longitudes & latitudes in the focal month's dataset and longitude[i]
     diff.lat.cell <- abs(jx$dim$lat$vals-as.numeric(la))
@@ -137,7 +119,7 @@ calibration_l = list(
 
 testcalcs_prince <- vector()
 
-for(i in names(tempval_prince)){ 
+for(i in names(tempval_prince)){ #i=2
   
   xx <- tempval_prince[[i]]
   xx$Date<-strptime(xx$Date,"%Y-%m-%d", tz="GMT")
@@ -158,7 +140,7 @@ for(i in names(tempval_prince)){
   
   hrly = vector()
   
-  for(j in 1:nrow(xx)){
+  for(j in 1:nrow(xx)){ #j=2
     
     xy <- Th_interp(Tmin, Tmax, #function that creates 24 values of hourly temperature from minimum and maximum daily values.
                     day = j,
@@ -192,8 +174,10 @@ for(i in names(tempval_prince)){
 #####################################################
 ######## I think end of Princeton test... ###########
 #####################################################
+nafiles <- dir(climatedrive)[grep("livnehclimdata", dir(climatedrive))]
+
 tempval_liv <- list() 
-for(i in 1:nrow(nam)){ # i = 5
+for(i in 1:nrow(nam)){ # i = 102
   # find this location
   lo <- nam[i,"chill.long"]
   la <- nam[i,"chill.lat"]
@@ -201,50 +185,18 @@ for(i in 1:nrow(nam)){ # i = 5
   # make sure longitudes are negative, need to be for North America this step is now done in "cleaning/clean_latlong" so it is no longer necessary
   #if(lo > 0) { lo = lo*-1 }
   yr<-as.numeric(substr(nam[i,"fieldsample.date2"],1,4))
-  # start and end days of the climate data we need to calculate chilling, for the focal lat/long. 
-  #This is in days since baseline date (sept 1) Set to GMT to avoid daylight savings insanity
-  # using d$fieldsample.date2 (this is the same as fieldsampledate, but formatted as  "%Y-%m-%d")
   
-  #do everything in reference to field sample year becuase the year column is too variable
-  if(nam[i,"fieldsample.date2"]!=""){endday <- strptime(nam[i,"fieldsample.date2"],"%Y-%m-%d", tz = "GMT")
+  endday <- strptime(paste(yr, "08-30", sep="-"),"%Y-%m-%d", tz = "GMT")
   doyend <- yday(endday)
-  }
-  if(nam[i,"fieldsample.date2"]==""){endday <- strptime(paste(yr, "08-30", sep="-"),"%Y-%m-%d", tz = "GMT")
-  doyend <- yday(endday)
-  }#I think we have field sample dates for everything, but just in case...
-  
-  if(nam[i,"fieldsample.date2"]!="" & as.numeric(substr(nam[i,"fieldsample.date2"],6,7))>=9){
-    stday <- strptime(paste(yr, "06-01", sep="-"),"%Y-%m-%d", tz="GMT")
-    #chillmo<-paste(yr, formatC(9:substr(endday,6,7), width=2, flag="0"), sep="")
-  }#If field sample date is after september 1, then we use the chilling from the current year, since sept 1
-  
-  if(nam[i,"fieldsample.date2"]!="" & as.numeric(substr(nam[i,"fieldsample.date2"],6,7))<9){
-    stday <- strptime(paste(yr-1, "06-01", sep="-"),"%Y-%m-%d", tz="GMT")#
-    #prevmo <- paste(yr-1, formatC(9:12, width=2, flag="0"), sep="");# use previous year's fall months of chilling (Sept-Dec)
-    #endmo<-substr(endday,6,7);#month of sampling date
-    #thismo <- paste(yr, formatC(1:endmo, width=2, flag="0"), sep="")#months from current year of chilling, through sampling date (Jan-whenever sampled)
-    #chillmo<-c(prevmo, thismo)
-  }#If field sample date is before september 1, then we use the chilling from the previous year.
-  
-  # now loop over these year-month combo files and get temperature values for this date range.
-  
+  stday <- strptime(paste(yr, "06-01", sep="-"),"%Y-%m-%d", tz="GMT")
+
+  chillmo<-paste(yr, formatC(6:substr(endday,6,7), width=2, flag="0"), sep="")
+ 
   mins <- maxs <- vector()
   
-  ## for now exclude prevey18
-  
-  for(j in c(yr)) { # j = yr
-    print(c(i, j))
-    
-    tmax <- list.files(path=paste(climatedrive,nafiles, sep="/"), pattern=paste0("tmax",yr), full.names = TRUE)
-    tmin <- list.files(path=paste(climatedrive,nafiles, sep="/"), pattern=paste0("tmin",yr), full.names = TRUE)
-    jx <- nc_open(tmax)
-    jn <- nc_open(tmin)
-    
-    yrend <- vector()
-    gddstart <- vector()
-    
-    jx$dim$time$vals<-seq(1, yrend, by=1)
-    thisyr <- which(jx$dim$time$vals<=doyend)
+  for(j in c(chillmo)){ # j = "198306"
+    file <- list.files(path=paste(climatedrive,nafiles, sep="/"), pattern=paste0(j), full.names = TRUE)
+    jx <- nc_open(file)
     
     diff.long.cell <- abs(jx$dim$lon$vals-as.numeric(lo))#differences between all longitudes & latitudes in the focal month's dataset and longitude[i]
     diff.lat.cell <- abs(jx$dim$lat$vals-as.numeric(la))
@@ -252,47 +204,38 @@ for(i in 1:nrow(nam)){ # i = 5
     lat.cell <- which(diff.lat.cell==min(diff.lat.cell))[1]
     long.cell <- which.min(abs(jx$dim$lon$vals-as.numeric(lo)))
     lat.cell <- which.min(abs(jx$dim$lat$vals-as.numeric(la)))
-    maxtest<-(ncvar_get(jx,start=c(long.cell,lat.cell,1),count=c(1,1,-1)))[thisyr]-273.15#check that the lat/long combinations has temperature data. 
+    mintest<-ncvar_get(jx,'Tmin',start=c(long.cell,lat.cell,1),count=c(1,1,-1))#checl that the lat/long combinations has temperature data. 
     #if no temperature data for the focal lat/long, choose the next closest one. 
-    #the below code goes up to 0.1 degrees (~10km) away from the closest lat/long)
-    if(is.na(unique(maxtest))){#if there are no temp data for the selected lat/long, choose a different one
+    #the below code cose up to 0.1 degrees (~10km) away from the closest lat/long)
+    if(is.na(unique(mintest))){#if there are no temp data for the selected lat/long, chosee a different one
       diff.long.cell[which(diff.long.cell==min(diff.long.cell,na.rm=TRUE))[1]]<-NA
       diff.lat.cell[which(diff.lat.cell==min(diff.lat.cell,na.rm=TRUE))[1]]<-NA
       long.cell <- which(diff.long.cell==min(diff.long.cell,na.rm=TRUE))[1] #select the closest longitude & latitude with climate data to longitude[i]
       lat.cell <- which(diff.lat.cell==min(diff.lat.cell,na.rm=TRUE))[1]
-      maxtest<-(ncvar_get(jx,start=c(long.cell,lat.cell,1),count=c(1,1,-1)))[thisyr]-273.15#check that the lat/long combinations has temperature data.
-      if(is.na(unique(maxtest))){
+      mintest<-ncvar_get(jx,'Tmin',start=c(long.cell,lat.cell,1),count=c(1,1,-1))
+      if(is.na(unique(mintest))){
         diff.long.cell[which(diff.long.cell==min(diff.long.cell,na.rm=TRUE))[1]]<-NA
         diff.lat.cell[which(diff.lat.cell==min(diff.lat.cell,na.rm=TRUE))[1]]<-NA
         long.cell <- which(diff.long.cell==min(diff.long.cell,na.rm=TRUE))[1] #select the closest longitude & latitude with climate data to longitude[i]
         lat.cell <- which(diff.lat.cell==min(diff.lat.cell,na.rm=TRUE))[1]
-        maxtest<-(ncvar_get(jx,start=c(long.cell,lat.cell,1),count=c(1,1,-1)))[thisyr]-273.15#check that the lat/long combinations has temperature data. 
-        if(is.na(unique(maxtest))){
+        mintest<-ncvar_get(jx,'Tmin',start=c(long.cell,lat.cell,1),count=c(1,1,-1))
+        if(is.na(unique(mintest))){
           diff.long.cell[which(diff.long.cell==min(diff.long.cell,na.rm=TRUE))[1]]<-NA
           diff.lat.cell[which(diff.lat.cell==min(diff.lat.cell,na.rm=TRUE))[1]]<-NA
           long.cell <- which(diff.long.cell==min(diff.long.cell,na.rm=TRUE))[1] #select the closest longitude & latitude with climate data to longitude[i]
           lat.cell <- which(diff.lat.cell==min(diff.lat.cell,na.rm=TRUE))[1]
         }}}
     
-    maxs<-(ncvar_get(jx,start=c(long.cell,lat.cell,1),count=c(1,1,-1)))[thisyr]-273.15
-    mins<-(ncvar_get(jn,start=c(long.cell,lat.cell,1),count=c(1,1,-1)))[thisyr]-273.15
+    mins <- c(mins, ncvar_get(jx,'Tmin',start=c(long.cell,lat.cell,1),count=c(1,1,-1)))#minimum temperatures for selected lat/long
+    maxs <- c(maxs, ncvar_get(jx,'Tmax',start=c(long.cell,lat.cell,1),count=c(1,1,-1)))#minimum temperatures for selected lat/long
     nc_close(jx)
-    nc_close(jn)
   }
   
   tempval_liv[[as.character(nam[i,"ID_fieldsample.date2"])]] <- data.frame(Lat = la,Long = lo,Date = as.character(seq(stday, endday, by = "day")),  
-                                                                              Tmin = mins[1:length(seq(stday, endday, by = "day"))], Tmax =maxs[1:length(seq(stday, endday, by = "day"))])
+                                                                       Tmin = mins[1:length(seq(stday, endday, by = "day"))], Tmax =maxs[1:length(seq(stday, endday, by = "day"))])
 }
 # If you want to (as Lizzie does) you can write out tempval, which is all the climate pulled in a list form
-#save(tempval_liv, file="output/fieldclimate_livtest.RData")
-
-calibration_l = list(
-  Average = data.frame(time_min = rep(5, 12),
-                       time_max = rep(14, 12),
-                       time_suns = rep(17, 12),
-                       C_m = rep(0.35, 12))
-)
-
+#save(tempval, file="output/fieldclimate.RData")
 
 testcalcs_liv <- vector()
 
@@ -347,4 +290,31 @@ for(i in names(tempval_liv)){
 
 #write.csv(testcalcs_prince, "output/fieldchillcalcslatlong_princetest.csv", row.names=FALSE, eol="\r\n")
 
+###### Now let's combine them all and check... 
+#prince$gdd <- testcalcs_prince$GDH/24
+#testcalcs_prince$climdata <- "prince"
+prince <- testcalcs_prince
+prince$gdd_prince <- testcalcs_prince$GDH/24
+
+#testcalcs_liv$gdd <- testcalcs_liv$GDH/24
+#testcalcs_liv$climdata <- "livneh"
+liv <- testcalcs_liv
+liv$gdd_liv <- testcalcs_liv$GDH/24
+
+testall <- cbind(prince, liv$gdd_liv)
+testall <- dplyr::select(testall, -Season, -GDH)
+names(testall) <- c("datasetIDlatlong", "year", "gdd_prince", "gdd_liv")
+
+library(ggplot2)
+compareplot <- ggplot(testall, aes(x=gdd_prince, y=gdd_liv)) + geom_point() +
+  geom_abline(intercept=0, slope=1) + xlab("Princeton") + ylab("Livneh") + 
+  theme_classic() + coord_cartesian(x=c(175, 1700), y=c(175, 1700))
+
+png("chilling/princetontest/livneh_vs_princeton.png", 
+    width=4,
+    height=4, units="in", res = 350 )
+plot(compareplot)
+dev.off()
+
+write.csv(testall, "chilling/princetontest/livneh_vs_princeton.csv", row.names=FALSE)
 
