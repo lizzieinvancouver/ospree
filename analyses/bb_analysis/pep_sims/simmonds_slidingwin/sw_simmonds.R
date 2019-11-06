3942aefa1eab59480abcaab940bf83aa99ef8480
cchambe12 <cchamberlain@g.harvard.edu>
2019-11-06 17:22:58 -0500

    tweaks to data to get results

--- analyses/bb_analysis/pep_sims/simmonds_slidingwin/sw_simmonds.R
+++ analyses/bb_analysis/pep_sims/simmonds_slidingwin/sw_simmonds.R
@@ -10,33 +10,33 @@ library(lubridate)
 
 # Setting working directory. Add in your own path in an if statement for your file structure
 if(length(grep("ailene", getwd()))>0) { 
-  setwd("~/Documents/GitHub/ospree/analyses/bb_analysis/simmonds_slidingwin")
+  setwd("~/Documents/GitHub/ospree/analyses/bb_analysis/pep_sims/simmonds_slidingwin")
 } else if(length(grep("lizzie", getwd()))>0) {
-  setwd("~/Documents/git/projects/treegarden/budreview/ospree/analyses/bb_analysis/simmonds_slidingwin")
+  setwd("~/Documents/git/projects/treegarden/budreview/ospree/analyses/bb_analysis/pep_sims/simmonds_slidingwin")
 } else if(length(grep("Ignacio", getwd()))>0) { 
-  setwd("~/GitHub/ospree/analyses/bb_analysis/simmonds_slidingwin") 
+  setwd("~/GitHub/ospree/analyses/bb_analysis/pep_sims/simmonds_slidingwin") 
 } else if(length(grep("catchamberlain", getwd()))>0) { 
-  setwd("~/Documents/git/ospree/analyses/bb_analysis/simmonds_slidingwin")
-} else setwd("~/Documents/git/projects/treegarden/budreview/ospree/analyses/bb_analysis/simmonds_slidingwin")
+  setwd("~/Documents/git/ospree/analyses/bb_analysis/pep_sims/simmonds_slidingwin")
+} else setwd("~/Documents/git/projects/treegarden/budreview/ospree/analyses/bb_analysis/pep_sims/simmonds_slidingwin")
 
-
+################# If looking at results jump to line 84 ##################
 set.seed(123)
 
 ## We'll input variables here based off of pep_sims:
 ## Adjust `postcctemp` depending on amount of warming as below
 # precctemp is the avg spring temp, postcctemp is +1deg of warming, etc, preccbb is the expected day of budburst (DOY), postccbb is after warming
-precctemp <- 10
-postcctemp <- 11
-sigmatemp <- 5
-fstar <- 400
+precctemp <- 6
+postcctemp <- 7
+sigmatemp <- 3
+fstar <- 150
 
 source("fakedata_swfx.R") ## can take some time...
 sim1 <- simgenerate(precctemp, postcctemp, sigmatemp, fstar)
-postcctemp <- 12
+postcctemp <- 8
 sim2 <- simgenerate(precctemp, postcctemp, sigmatemp, fstar)
-postcctemp <- 13
+postcctemp <- 9
 sim3 <- simgenerate(precctemp, postcctemp, sigmatemp, fstar)
-postcctemp <- 14
+postcctemp <- 10
 sim4 <- simgenerate(precctemp, postcctemp, sigmatemp, fstar)
 
 bbdata1 <- sim1[[1]]
@@ -68,18 +68,58 @@ names(datafile) <- c("Year", "bb_date", "bb_mean", "doy95")
 
 #### Now to run using differing simulations:
 refday <- c(01, 03)
-datafile <- bbdata4 #### CHANGE BASED ON SIMULATION!!! i.e., bbdataX (X = 1-4)
-climate <- climate.data4 #### CHANGE BASED ON SIMULATION!!! i.e., bbdataX (X = 1-4)
+datafile <- bbdata1 #### CHANGE BASED ON SIMULATION!!! i.e., bbdataX (X = 1-4)
+climate <- climate.data1 #### CHANGE BASED ON SIMULATION!!! i.e., bbdataX (X = 1-4)
 climate$X <- NA ### needed in order to run... 
 
-Results_SWA4 <- run_SW(absolute=TRUE, datafile, climate, refday) ## takes a while to run
-write.csv(Results_SWA4[[2]], file="output/results_swa4.csv")
+Results_SWA1 <- run_SW(absolute=TRUE, datafile, climate, refday) ## takes a while to run
+write.csv(Results_SWA1[[2]], file="output/results_swa1.csv")
+write.csv(Results_SWA1[[1]], file="output/sumstats_swa1.csv")
 
-
+if(FALSE){
 swa1 <- Results_SWA1[[2]]
 swa2 <- Results_SWA2[[2]]
 swa3 <- Results_SWA3[[2]]
 swa4 <- Results_SWA4[[2]]
+}
+
+
+##### Now let's check out the results and see how these compare to our original simulation:
+swa1 <- read.csv("output/results_swa1.csv")
+swa1$degwarm <- 1
+swa2 <- read.csv("output/results_swa2.csv")
+swa2$degwarm <- 2
+swa3 <- read.csv("output/results_swa3.csv")
+swa3$degwarm <- 3
+swa4 <- read.csv("output/results_swa4.csv")
+swa4$degwarm <- 4
+
+#Step 6: Whoop! Now we can caculate temperature sensitivities.
+varstatsfx <- function(df){
+
+  df$cc <- ifelse(df$X<=34, "precc", "postcc")
+  estprecc <- lm(yvar~climate, data=subset(df, cc=="precc"))
+  estpostcc <- lm(yvar~climate, data=subset(df, cc=="postcc"))
+
+  diffbefore.after <- coef(estprecc)[2]-coef(estpostcc)[2]
+# negative means a decline in sensitivity AFTER climate change
+  draws <- rbind(draws, diffbefore.after)
+
+# get a few more things
+  preccdf <- subset(df, cc=="precc")
+  postccdf <- subset(df, cc=="postcc")
+  precc.sens <-  rbind(precc.sens, coef(estprecc)[2])
+  postcc.sens <-  rbind(postcc.sens, coef(estpostcc)[2])
+  var.lo.precc <- rbind(var.lo.precc, var(preccdf$yvar, na.rm=TRUE))
+  var.lo.postcc <- rbind(var.lo.postcc, var(postccdf$yvar, na.rm=TRUE))
+
+
+  # Okay, now build a df with a few things we want...
+  df.return <- data.frame(cbind(draws, precc.sens, postcc.sens, var.lo.precc, var.lo.postcc), row.names = NULL) 
+  names(df.return) <- c("diffbefore.after", "precc.sens", "postcc.sens", "var.lo.precc", "var.lo.postcc")
+}
+
+
 
 
 
