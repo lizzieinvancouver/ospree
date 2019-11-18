f2412340dafef8da2dd57d2e7504ded7d306ccd2
cchambe12 <cchamberlain@g.harvard.edu>
2019-11-18 06:37:33 -0500

    removing blueberries

--- analyses/bb_analysis/source/speciescomplex.nocrops.R
+++ analyses/bb_analysis/source/speciescomplex.nocrops.R
@@ -22,7 +22,8 @@ sppcomplexfx.nocrops <- function(d){
   
   
   d$name<-paste(d$genus,d$species,sep="_") ###make  a column for genus species
-  cropspp <- c("Actinidia_deliciosa", "Malus_domestica", "Vitis_vinifera", "Ribes_nigrum")
+  cropspp <- c("Actinidia_deliciosa", "Malus_domestica", "Vitis_vinifera", "Ribes_nigrum",
+               "Vaccinium_ashei", "Vaccinium_corymbosum")
   d<-d[!(d$name%in%cropspp),]
   
   xx<-d
