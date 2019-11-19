1cf87ae5c1582100e3ce3cbd6ce7a3029b9e36ee
AileneKane <ailene.ettinger@gmail.com>
2019-11-18 21:51:25 -0800

    new material for ncc rev response

---
+++ analyses/bb_analysis/source/commoncols_newbbmods.R
@@ -0,0 +1,15 @@
+# slim down our columns
+columnstokeep <- c("datasetID","study", "genus", "species", "varetc", "woody", "forcetemp", "forcetemp_night",
+                   "photoperiod_day", "response", "response.time", "Total_Chilling_Hours", "chilltemp",
+                  "chilldays", "fieldsample.date",
+                  "provenance.lat", "force", "photo", "chill", "resp", "force_type",  "photo_type", "chill_type")
+
+#columnscentered <- c("force.z","chill.z", "photo.z") # chill.z is utah
+
+columnschillunits <- c("chill.hrs","chill.ports")
+
+
+if(use.stage == TRUE){columnstokeep <- c("datasetID","study", "genus", "species", "varetc", "woody", "forcetemp", "forcetemp_night",
+                                             "photoperiod_day", "response", "response.time", "Total_Chilling_Hours", "chilltemp",
+                                             "chilldays", "fieldsample.date",
+                                             "provenance.lat", "force", "photo", "chill", "resp", "force_type",  "photo_type", "chill_type","stage")}
