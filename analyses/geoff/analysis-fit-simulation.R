8e24059b801381b3a7175dc36521dc0ee2471e48
Geoffrey Legault <thegeoffrey@gmail.com>
2019-11-19 07:36:01 -0800

    update simulation script

--- analyses/geoff/analysis-fit-simulation.R
+++ analyses/geoff/analysis-fit-simulation.R
@@ -41,6 +41,7 @@ head(testdata1)
 # Fit models to data
 
 ## lme4 mixed effects model
+### Fit
 model1 <- lmer(Response ~ (force.env+photo.env+chill.env) ^ 2 + (1 | Species), data = testdata1)
 ### Summarize fit
 summary(model1)
@@ -48,6 +49,7 @@ summary(model1)
 t(model.parameters)
 
 ## rstanarm
+### Fit
 model2 <- stan_lmer(Response ~ (force.env+photo.env+chill.env) ^ 2 + (1|Species),
                     data = testdata1,
                     seed = 73,
@@ -73,5 +75,44 @@ testdatastan <- list(y = testdata1$Response,
 stanmodel1 <- stan("stan/interac_intpool.stan", data = testdatastan, iter = 5000, warmup = 2500, chains = 4, verbose = TRUE)
 ### Summarize fit
 summary(stanmodel1, pars = c("mu_a_sp", "b_force", "b_photo", "b_chill", "b_cf", "b_cp", "b_fp", "sigma_a_sp", "sigma_y"))$summary
+### Compare to true parameters (not necessarily same order)
+t(model.parameters)
+
+# Simulate less data
+## Default parameters; 3 species, 10 observations per species
+testdata2 <- simulate.linear(model.parameters, # parameters, already generated
+                           model.random = c("Intercept"), # Which effects are random? Entries must correspond to parameters
+                           env, # environment, already generated
+                           response.distribution = c("rnorm"), # What is the name of the function that generates the distribution of the response variable? (rnorm, rpois supported)
+                           n.species = 3, # number of species to simulate
+                           n.observations = 10) # number of observations per species
+head(testdata2)
+## Make data unbalanced and reduce sample size further
+testdata2  <- testdata2[sample(1:nrow(testdata2), size = 8, replace = FALSE), ]
+testdata2
+
+# Fit models to data
+
+## lme4 mixed effects model
+### Fit
+model2 <- lmer(Response ~ (force.env+photo.env+chill.env) ^ 2 + (1 | Species), data = testdata2)
+### Summarize fit
+summary(model2)
+### Compare to true parameters (not necessarily same order)
+t(model.parameters)
+
+## Pure stan
+### Create compatible list
+testdatastan <- list(y = testdata2$Response,
+                     chill = testdata2$chill.env,
+                     force = testdata2$force.env,
+                     photo = testdata2$photo.env,
+                     sp = testdata2$Species,
+                     N = nrow(testdata2),
+                     n_sp = length(unique(testdata2$Species)))
+### Fit
+stanmodel2 <- stan("stan/interac_intpool.stan", data = testdatastan, iter = 5000, warmup = 2500, chains = 4, verbose = TRUE, control = list(adapt_delta = 0.99))
+### Summarize fit
+summary(stanmodel2, pars = c("mu_a_sp", "b_force", "b_photo", "b_chill", "b_cf", "b_cp", "b_fp", "sigma_a_sp", "sigma_y"))$summary
 ### Compare to true parameters (not necessarily same order)
 t(model.parameters)
