4bd7a1d9fe3aaf2b6cb8daf4543eb8d1d386c3cd
cchambe12 <cchamberlain@g.harvard.edu>
2019-11-20 10:13:24 -0800

    adding in prunus and making maps

--- analyses/bb_analysis/source/speciescomplex.multcues.R
+++ analyses/bb_analysis/source/speciescomplex.multcues.R
@@ -27,7 +27,7 @@ sppcomplexfx.multcue <- function(d){
 #d <- read.csv("~/Documents/git/ospree/analyses/output/ospree_clean_withchill_BB.csv")
 d$name<-paste(d$genus,d$species,sep="_") ###make  a column for genus_species
 cropspp <- c("Actinidia_deliciosa", "Malus_domestica", "Vitis_vinifera", "Ribes_nigrum", 
-             "Vaccinium_ashei", "Vaccinium_corymbosum")
+             "Vaccinium_ashei", "Vaccinium_corymbosum", "Prunus_persica")
 d<-d[!(d$name%in%cropspp),]
 
 xx<-d
