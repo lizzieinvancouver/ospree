6db4292d0d43b4994ee8cc742c5125f49beedc72
Elizabeth Wolkovich <wolkovich@biodiversity.edu>
2020-01-05 13:15:49 -0800

    First draft of true declining sensitivities code done

--- analyses/decsens/decsensSimsMo.R
+++ analyses/decsens/decsensSimsMo.R
@@ -1,62 +1,125 @@
 ## Started 28 December 2019 ##
 ## By Lizzie ##
 
-## Simulations of winter and spring temperature ... 120 day winter + spring season
+## Simulations of winter to spring temperatures with fstar (GDD) required increasing when chilling is low ##
 
-# 6. Calculate GDD (days 31 onward) and chilling (first 90 days)
-# 7. Select some range of high chill where same GDD is needed for budburst, then set up a linear relationship between chill (at lower range) and GDD required (more GDD for lower chill)
-# 8. Predict GDD based on simulated weather and 7.
+# This code requires higher forcing (aka GDD, aka fstar) for leafout when chilling is low #
+# It does not vary when GDD accumulates though, it always just starts accumulating on a certain day (daystostart) #
 
-# Step 1: Set up years, days per year, temperatures, sampling frequency, required GDD (fstar)
-daysperyr <- 60
+## To do! ##
+# Check the code, in particular:
+# (a) Make sure daily temp is working correctly
+# (b) Look more at the intermediate output to make sure it makes sense
+
+
+# Step 1: Set up years, days per year, temperatures, required GDD (fstar), required chill (cstar) and how much higher fstar is when cstar is not met
+daysperseason <- 100
+daysperinterseason <- 25
+daystostart <- daysperseason+daysperinterseason # this defines the break between 'winter' and 'spring,' functionally we accumulate chill only in 1:daystostart and GDD only in daystostart:end(df)
 yearz <- 30
-sitez <- 45 # reps
-degreez <- c(0, 0.5, 1, 2, 4, 7)
+sitez <- 45 # aka reps
+degreez <- c(0, 0.5, 1, 2, 4, 6, 7) # warming -- applied evenly across whole period
 sigma <- 4
-basetemp <- 6
-fstar <- 150
+fstar <- 200
+cstar <- 110
+fstaradjforchill <- 3 # how much more GDD to you need based on diff from cstar at end of daystostart
 
-# Step 2: Build the data and calculate sensitivities
-df <- data.frame(degwarm=numeric(), rep=numeric(), simplelm=numeric(), loglm=numeric(), perlm=numeric())
+if(FALSE){
+# Saving some intermediate progress .... setting up the seasonal temps and looking at chill and GDD calculated from them 
+df <- data.frame(degwarm=numeric(), rep=numeric(), chill=numeric(), gdd=numeric())
 
 for (i in degreez){
    for (j in 1:sitez){
        yearly_expected_temp <- rep(6, yearz)
-       daily_temp <- sapply(yearly_expected_temp, function(x) rnorm(daysperyr, basetemp + i, sigma)) 
-       leafout_date <- sapply(1:ncol(daily_temp), function(x) min(which(cumsum(daily_temp[,x]) > fstar)))
-       yearly_temp <- colMeans(daily_temp)
-       per_leafout_date <- leafout_date/mean(leafout_date)
-       per_yearly_temp <- yearly_temp/mean(yearly_temp)
-       plot(yearly_temp, leafout_date, pch=20)
-       # yearly_temp_trunc <- sapply(1:ncol(daily_temp), function(x) mean(daily_temp[1:leafout_date[x], x]))
-       dfadd <- data.frame(degwarm=i, rep=j, simplelm=coef(lm(leafout_date~yearly_temp))[2],
-           loglm=coef(lm(log(leafout_date)~log(yearly_temp)))[2],
-           perlm=coef(lm(per_leafout_date~per_yearly_temp))[2])
+       daily_temp <- sapply(yearly_expected_temp, function(x) c(rnorm(daysperseason, 0 + i, sigma),
+           rnorm(daysperinterseason, 2 + i , sigma), rnorm(daysperinterseason, 4 + i, sigma),
+           rnorm(daysperseason, 6 + i, sigma)))
+       chill <- daily_temp
+       chill[(chill)<0] <- 0
+       chill[(chill)>5] <- 0
+       gdd <- daily_temp
+       gdd[(gdd)<0] <- 0
+       dfadd <- data.frame(degwarm=i, rep=j, chill=mean(colSums(chill[1:daystostart,], na.rm = TRUE)),
+           gdd=mean(colSums(gdd[daystostart:nrow(gdd),], na.rm = TRUE)))
        df <- rbind(df, dfadd)
     }
 }
 
-##
-##
+plot(daily_temp[,1]~c(1:nrow(daily_temp)), type="l")
+par(mfrow=c(1,2))
+plot(chill~degwarm, df)
+plot(gdd~degwarm, df)
+par(mfrow=c(1,1))
 
-# First, build up some simulated climate and leafout (after 200 GDD) data
-yearly_expected_temp <- c(rep(0,20), rep(5,20), rep(10,20))
+# Saving some more intermediate progress ... this sets up a varying fstar when chilling is below cstar
+gddreq <- c()
+leafout_date <- c()
+for (k in 1:ncol(chill)){
+    chillsum <- sapply(1:ncol(chill), function(x) (cumsum(chill[,x])))
+    gddsum <- sapply(1:ncol(gdd), function(x) (cumsum(gdd[daystostart:nrow(gdd),x])))
+    if (chillsum[daystostart,k]>cstar) {
+        gddreq[k] <- fstar
+        } else {
+        gddreq[k] <- fstar + (cstar-chillsum[daystostart,k])*fstaradjforchill
+        }
+    leafout_date[k] <- min(which(gddsum[,k] > gddreq[k]))
+    }
 
-# Temp below is 60 days with base of -5, 15 days at -2.5, 15 days at 2.5 and 30 days at 5
-daily_temp <- sapply(yearly_expected_temp, function(x) c(rnorm(60, -5 + x, 4), rnorm(15, -2.5 + x, 4), rnorm(15, 2.5 + x, 4), rnorm(30, 5 + x, 4)))
+plot(leafout_date~colMeans(daily_temp))
+plot(leafout_date~gddreq)
+}
 
-gdd <- daily_temp[30:nrow(daily_temp),]
-gdd[(gdd)<0] <- 0
+## Step 2: Now I put together the seasonal temps, varying fstar (increases when chill is low) and calculate the sensitivities
 
-leafout_date <- sapply(1:ncol(daily_temp), function(x) min(which(cumsum(daily_temp[,x]) > 200)))
-yearly_temp <- colMeans(daily_temp)
+df <- data.frame(degwarm=numeric(), rep=numeric(), chill=numeric(), fstar=numeric(), simplelm=numeric(),
+    loglm=numeric(), perlm=numeric())
 
-yearly_temp_trunc <- sapply(1:ncol(daily_temp), function(x) mean(spring_daily_temp[1:leafout_date[x], x]))
+# yearlytemp <- "postwinter"
+yearlytemp <- "alltemps"
 
-plot(yearly_temp, leafout_date, pch=20)
-points(yearly_temp_trunc, leafout_date, pch=20, col = "red")
+for (i in degreez){
+   for (j in 1:sitez){
+       yearly_expected_temp <- rep(6, yearz)
+       daily_temp <- sapply(yearly_expected_temp, function(x) c(rnorm(daysperseason, 0 + i, sigma),
+           rnorm(daysperinterseason, 2 + i , sigma), rnorm(daysperinterseason, 4 + i, sigma),
+           rnorm(daysperseason, 6 + i, sigma)))
+       chill <- daily_temp
+       chill[(chill)<0] <- 0
+       chill[(chill)>5] <- 0
+       gdd <- daily_temp
+       gdd[(gdd)<0] <- 0
+       gddreq <- c()
+       leafout_date <- c()
+       for (k in 1:ncol(chill)){
+           chillsum <- sapply(1:ncol(chill), function(x) (cumsum(chill[,x])))
+           gddsum <- sapply(1:ncol(gdd), function(x) (cumsum(gdd[daystostart:nrow(gdd),x])))
+           if (chillsum[daystostart,k]>cstar) {
+           gddreq[k] <- fstar
+           } else {
+           gddreq[k] <- fstar + (cstar-chillsum[daystostart,k])*fstaradjforchill
+           }
+           leafout_date[k] <- min(which(gddsum[,k] > gddreq[k]))
+           meanchill <- mean(chillsum[daystostart,])
+           meanfstar <- mean(gddreq)
+           }
+           yearly_tempall <- colMeans(daily_temp)
+           yearly_temppostwinter <- colMeans(daily_temp[daystostart:nrow(daily_temp),])
+           if(yearlytemp=="postwinter") {
+               yearly_temp <- yearly_temppostwinter
+               } else {
+               yearly_temp <- yearly_tempall
+               }
+           per_leafout_date <- leafout_date/mean(leafout_date)
+           per_yearly_temp <- yearly_temp/mean(yearly_temp)
+           plot(yearly_temp, leafout_date, pch=20)
+           dfadd <- data.frame(degwarm=i, rep=j, chill=meanchill, fstar=meanfstar,     
+               simplelm=coef(lm(leafout_date~yearly_temp))[2],
+               loglm=coef(lm(log(leafout_date)~log(yearly_temp)))[2],
+               perlm=coef(lm(per_leafout_date~per_yearly_temp))[2])
+           df <- rbind(df, dfadd)
+       }
+   }
 
-plot(yearly_temp_trunc, leafout_date, pch=20, col = "red")
-
-# Lizzie added below ... 
-plot(log(yearly_temp_trunc), log(leafout_date), pch=20, col = "dodgerblue")
+plot(simplelm~degwarm, data=df, pch=16, ylab="Sensitivity (days/C or log(days)/log(C)", xlab="Degree warming")
+points(loglm~degwarm, data=df, col="dodgerblue")
+plot(perlm~degwarm, data=df, col="firebrick")
