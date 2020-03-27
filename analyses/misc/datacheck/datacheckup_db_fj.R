faf6671056f656b140b61ab0aaeb262009a37928
FaithJones <faithann.jones2@gmail.com>
2020-03-27 08:42:08 -0700

    adding Faith and Dans cleaning script

---
+++ analyses/misc/datacheck/datacheckup_db_fj.R
@@ -0,0 +1,151 @@
+## Started 24 March 2020 ##
+## Let's review the new data! ##
+
+# housekeeping
+rm(list=ls()) 
+options(stringsAsFactors = FALSE)
+
+library(rstan)
+library(RColorBrewer)
+library(ggplot2)
+
+# Setting working directory. Add in your own path in an if statement for your file structure
+if(length(grep("ailene", getwd()))>0) { 
+  setwd("~/Documents/GitHub/ospree/analyses/")
+} else setwd("~/Documents/git/projects/treegarden/budreview/ospree/analyses/")
+if(length(grep("Ignacio", getwd()))>0) { 
+  setwd("~/GitHub/ospree/analyses") 
+} else setwd("~/Documents/git/projects/treegarden/budreview/ospree/analyses/")
+
+
+# Grab the data, all of it
+dall <- read.csv("output/ospree_clean.csv", header=TRUE)
+dbb <- read.csv("output/ospree_clean_withchill_BB.csv", header=TRUE)
+
+# Get your subset ... 
+sort(unique(dbb$datasetID))
+medata <- c("malyshev18", "nanninga17")
+
+mall <- dall[which(dall$datasetID %in% medata),]
+mdbb <- dbb[which(dbb$datasetID %in% medata),]
+
+# check if you lose data between all and bb (if you do, find out what is lost!)
+dim(mall)
+dim(mdbb) 
+
+## bb stuff... this code poached from:
+# models_stan.R, models_stan_plotting.R, bbstanleadin.R, and bbdataplease.R 
+## just the bb data ...
+d <- mdbb
+respvars.thatwewant <- c("daystobudburst", "percentbudburst")
+bb.resp <- d[which(d$respvar.simple %in% respvars.thatwewant),]
+bb.resp <- subset(bb.resp, respvar != "thermaltime") # doesn't remove anything
+
+## make a bunch of things numeric (eek!)
+bb.resp$forceday <- as.numeric(bb.resp$forcetemp)
+bb.resp$forcenight <- as.numeric(bb.resp$forcetemp_night)
+bb.resp$photonight <- as.numeric(bb.resp$photoperiod_night)
+
+# Correct forcing for day/night differences
+bb.resp$photo <- as.numeric(bb.resp$photoperiod_day)
+bb.resp$force <- bb.resp$forceday
+bb.resp$force[is.na(bb.resp$forcenight)==FALSE & is.na(bb.resp$photo)==FALSE &
+    is.na(bb.resp$photonight)==FALSE] <-
+    (bb.resp$forceday[is.na(bb.resp$forcenight)==FALSE & is.na(bb.resp$photo)==FALSE &
+    is.na(bb.resp$photonight)==FALSE]*
+    bb.resp$photo[is.na(bb.resp$forcenight)==FALSE & is.na(bb.resp$photo)==FALSE &
+    is.na(bb.resp$photonight)==FALSE] +
+    bb.resp$forcenight[is.na(bb.resp$forcenight)==FALSE & is.na(bb.resp$photo)==FALSE &
+    is.na(bb.resp$photonight)==FALSE]*
+    bb.resp$photonight[is.na(bb.resp$forcenight)==FALSE & is.na(bb.resp$photo)==FALSE &
+    is.na(bb.resp$photonight)==FALSE])/24
+
+bb.resp$chill <- as.numeric(bb.resp$Total_Utah_Model) 
+bb.resp$chill.hrs <- as.numeric(bb.resp$Total_Chilling_Hours)
+bb.resp$chill.ports <- as.numeric(bb.resp$Total_Chill_portions) 
+
+bb.resp$resp <- as.numeric(bb.resp$response.time)
+
+bb.noNA <- subset(bb.resp, is.na(force)==FALSE & is.na(photo)==FALSE &
+                    is.na(chill)==FALSE & is.na(resp)==FALSE)
+dim(bb.noNA)
+
+
+# Fix species... so it plays well with rstan
+bb.noNA$latbi <- paste(bb.noNA$genus, bb.noNA$species)
+bb.noNA$complex <- as.numeric(as.factor(bb.noNA$latbi))
+
+
+# Start looking at the data ...
+ggplot(bb.noNA, aes(chill, resp, colour=latbi)) + geom_point() + facet_grid(datasetID~.)
+ggplot(bb.noNA, aes(force, resp, colour=latbi)) + geom_point() + facet_grid(datasetID~.)
+ggplot(bb.noNA, aes(photo, resp, colour=latbi)) + geom_point() + facet_grid(datasetID~.)
+# Compare it to  the paper ....
+comppaper <- subset(bb.noNA, select=c("datasetID", "latbi", "chill", "force",
+    "photo", "resp", "figure.table..if.applicable."))
+comppaperfig <- subset(bb.noNA, select=c("datasetID", "latbi", "figure.table..if.applicable."))
+comppaperfig[!duplicated(comppaperfig), ]
+
+# Hmm, chilling units seem to vary in malyshev18 figure 2
+
+# rstan
+datalist.bb <- with(bb.noNA, 
+                    list(y = resp, 
+                         chill = chill, 
+                         force = force, 
+                         photo = photo,
+                         sp = complex,
+                         N = nrow(bb.noNA),
+                         n_sp = length(unique(bb.noNA$complex))
+                    ))
+
+modhere = stan('bb_analysis/stan/nointer_2level.stan', data = datalist.bb,
+    iter = 4000, warmup=3000)
+
+library(shinystan)
+launch_shinystan(modhere) # please go to: Explore -> Click on 'bivariate' on right and make sure y-axis is log-posterior
+
+# At this point I would try a different model formulation! Which ones would you try?
+
+# Some plotting (if you have a good model!)
+bb.stan <- bb.noNA
+post.mohere <- extract(modhere)
+sumer.here <- summary(modhere)$summary
+whichmodel <- sumer.here
+spp <- sort(unique(bb.noNA$complex))
+n <- length(spp)
+qual_col_pals = brewer.pal.info[brewer.pal.info$category == 'qual',]
+colv = unlist(mapply(brewer.pal, qual_col_pals$maxcolors, rownames(qual_col_pals)))
+
+par(mfrow=c(1,3))
+plot(resp~chill, data=bb.stan, type="n")
+for (sp in c(1:length(spp))){
+    subby <- subset(bb.stan, complex==spp[sp])
+    points(resp~chill, data=subby, main=subby$complex.wname[1], col=colv[sp])
+    intercepthere <- whichmodel[grep("a_sp", rownames(whichmodel)),1][spp[sp]+2]
+    slopehere <- whichmodel[grep("b_chill", rownames(whichmodel)),1][spp[sp]+2]
+    abline(intercepthere, slopehere, col=colv[sp])
+    }
+abline(whichmodel[grep("mu_a_sp", rownames(whichmodel)),1],
+    whichmodel[grep("mu_b_chill_sp", rownames(whichmodel)),1], col="black", lwd=3)
+
+plot(resp~force, data=bb.stan, type="n")
+for (sp in c(1:length(spp))){
+    subby <- subset(bb.stan, complex==spp[sp])
+    points(resp~force, data=subby, main=subby$complex.wname[1], col=colv[sp])
+    intercepthere <- whichmodel[grep("a_sp", rownames(whichmodel)),1][spp[sp]+2]
+    slopehere <- whichmodel[grep("b_force", rownames(whichmodel)),1][spp[sp]+2]
+    abline(intercepthere, slopehere, col=colv[sp])
+    }
+abline(whichmodel[grep("mu_a_sp", rownames(whichmodel)),1],
+    whichmodel[grep("mu_b_force_sp", rownames(whichmodel)),1], col="black", lwd=3)
+plot(resp~photo, data=bb.stan, type="n")
+for (sp in c(1:length(spp))){
+    subby <- subset(bb.stan, complex==spp[sp])
+    points(resp~photo, data=subby, main=subby$complex.wname[1], col=colv[sp])
+    intercepthere <- whichmodel[grep("a_sp", rownames(whichmodel)),1][spp[sp]+2]
+    slopehere <- whichmodel[grep("b_photo", rownames(whichmodel)),1][spp[sp]+2]
+    abline(intercepthere, slopehere, col=colv[sp])
+    }
+abline(whichmodel[grep("mu_a_sp", rownames(whichmodel)),1],
+           whichmodel[grep("mu_b_photo_sp", rownames(whichmodel)),1], col="black", lwd=3)
