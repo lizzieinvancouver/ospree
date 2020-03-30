1e111eb9bf7295bb204872a92ea20d7a82426443
dbuona <dbuonaiuto@g.harvard.edu>
2020-03-30 14:20:05 -0400

    tweaks to data checking

--- analyses/misc/datacheck/datacheckup_db_fj.R
+++ analyses/misc/datacheck/datacheckup_db_fj.R
@@ -10,40 +10,69 @@ library(RColorBrewer)
 library(ggplot2)
 
 # Setting working directory. Add in your own path in an if statement for your file structure
-if(length(grep("ailene", getwd()))>0) { 
-  setwd("~/Documents/GitHub/ospree/analyses/")
-} else setwd("~/Documents/git/projects/treegarden/budreview/ospree/analyses/")
-if(length(grep("Ignacio", getwd()))>0) { 
-  setwd("~/GitHub/ospree/analyses") 
-} else setwd("~/Documents/git/projects/treegarden/budreview/ospree/analyses/")
+
+if(length(grep("lizzie", getwd())>0)) { 
+  setwd("~/Documents/git/treegarden/budreview/ospree/bb_analysis") 
+} else if (length(grep("ailene", getwd()))>0) {setwd("/Users/aileneettinger/Documents/GitHub/ospree/analyses/bb_analysis")
+}else if(length(grep("Ignacio", getwd()))>0) { 
+  setwd("~/GitHub/ospree/analyses/bb_analysis") 
+} else if(length(grep("catchamberlain", getwd()))>0) { 
+  setwd("~/Documents/git/ospree/analyses/bb_analysis") 
+} else if(length(grep("danielbuonaiuto", getwd()))>0) { 
+  setwd("~/Documents/git/ospree/analyses/bb_analysis") 
+}else setwd("~/Documents/git/projects/treegarden/budreview/ospree/analyses/bb_analysis")
+
 
 
 # Grab the data, all of it
-dall <- read.csv("output/ospree_clean.csv", header=TRUE)
-dbb <- read.csv("output/ospree_clean_withchill_BB.csv", header=TRUE)
+dall <- read.csv("..//output/ospree_clean.csv", header=TRUE)
+dbb <- read.csv("..//output/ospree_clean_withchill_BB.csv", header=TRUE)
 
 # Get your subset ... 
 sort(unique(dbb$datasetID))
-medata <- c("malyshev18", "nanninga17")
+
+medata <- c("malyshev18", "flynn18","ramos17")
 
 mall <- dall[which(dall$datasetID %in% medata),]
 mdbb <- dbb[which(dbb$datasetID %in% medata),]
+unique(mall$datasetID)
+unique(mdbb$datasetID) ### ramos17 dissapears in cleaing note: the paper is ramos 18 on google drive
 
 # check if you lose data between all and bb (if you do, find out what is lost!)
-dim(mall)
-dim(mdbb) 
+dim(mall) #1332
+dim(mdbb) #1006
+table(mall$datasetID)
+1006+326 ####see row differences between the two dataframes result from the loss of ramos.
+
+#########################################################################################
+############# task 1 Find out why ramos17 goes away.##################################
+#1. Run chillmergeall.R after every step in cleaning type table(d$datasetID=="ramos17")
+#hmmmm it still appear to be in the dataset at the end of chillmergeall.R
+#2. run bb_cleanmergall.R
+## We lose half of ramos17 (163 rows in cleaning/multirespr) (perceptbudburst reproductive)
+### we lose the rest in clean_bbperctodays.R. We checked this before, and Lizzie was going to follow up
+#see double check clean_bbperctodays.R  issue #310
+
 
 ## bb stuff... this code poached from:
 # models_stan.R, models_stan_plotting.R, bbstanleadin.R, and bbdataplease.R 
 ## just the bb data ...
 d <- mdbb
 respvars.thatwewant <- c("daystobudburst", "percentbudburst")
-bb.resp <- d[which(d$respvar.simple %in% respvars.thatwewant),]
+
+table(d$respvar.simple)
+bb.resp <- d[which(d$respvar.simple %in% respvars.thatwewant),] ### removes 110 rows of flowering
 bb.resp <- subset(bb.resp, respvar != "thermaltime") # doesn't remove anything
 
 ## make a bunch of things numeric (eek!)
+
+# values before check making everythig numberic
+unique(bb.resp$forcetemp)
 bb.resp$forceday <- as.numeric(bb.resp$forcetemp)
+unique(bb.resp$forceday)
+
 bb.resp$forcenight <- as.numeric(bb.resp$forcetemp_night)
+
 bb.resp$photonight <- as.numeric(bb.resp$photoperiod_night)
 
 # Correct forcing for day/night differences
@@ -59,6 +88,7 @@ bb.resp$force[is.na(bb.resp$forcenight)==FALSE & is.na(bb.resp$photo)==FALSE &
     is.na(bb.resp$photonight)==FALSE]*
     bb.resp$photonight[is.na(bb.resp$forcenight)==FALSE & is.na(bb.resp$photo)==FALSE &
     is.na(bb.resp$photonight)==FALSE])/24
+####do to thermoperiodicity, this calculation makes it so there are 4 forcing treamtnets instead of 2 for
 
 bb.resp$chill <- as.numeric(bb.resp$Total_Utah_Model) 
 bb.resp$chill.hrs <- as.numeric(bb.resp$Total_Chilling_Hours)
@@ -68,7 +98,7 @@ bb.resp$resp <- as.numeric(bb.resp$response.time)
 
 bb.noNA <- subset(bb.resp, is.na(force)==FALSE & is.na(photo)==FALSE &
                     is.na(chill)==FALSE & is.na(resp)==FALSE)
-dim(bb.noNA)
+dim(bb.noNA) ### nothing is lost by making things numeric. Yay!
 
 
 # Fix species... so it plays well with rstan
@@ -86,25 +116,31 @@ comppaper <- subset(bb.noNA, select=c("datasetID", "latbi", "chill", "force",
 comppaperfig <- subset(bb.noNA, select=c("datasetID", "latbi", "figure.table..if.applicable."))
 comppaperfig[!duplicated(comppaperfig), ]
 
-# Hmm, chilling units seem to vary in malyshev18 figure 2
+# Hmm, chilling units seem to vary in malyshev18 figure 2 agreed
 
+### st up z.score
+bb.noNA$chill.z<-(bb.noNA$chill-mean(bb.noNA$chill))/sd(bb.noNA$chill)
+bb.noNA$force.z<-(bb.noNA$force-mean(bb.noNA$force))/sd(bb.noNA$force)
+bb.noNA$photo.z<-(bb.noNA$photo-mean(bb.noNA$photo))/sd(bb.noNA$photo)
 # rstan
 datalist.bb <- with(bb.noNA, 
                     list(y = resp, 
-                         chill = chill, 
-                         force = force, 
-                         photo = photo,
+                         chill = chill.z, 
+                         force = force.z, 
+                         photo = photo.z,
                          sp = complex,
                          N = nrow(bb.noNA),
                          n_sp = length(unique(bb.noNA$complex))
                     ))
 
-modhere = stan('bb_analysis/stan/nointer_2level.stan', data = datalist.bb,
-    iter = 4000, warmup=3000)
+modhere = stan('stan/nointer_2level.stan', data = datalist.bb,
+    iter = 6000, warmup=5000,control=list(adapt_delta=.99))
+###7 diverget transitions with z-scoring but still bad.
+
 
 library(shinystan)
 launch_shinystan(modhere) # please go to: Explore -> Click on 'bivariate' on right and make sure y-axis is log-posterior
-
+##signa force is the worst, but so is sigma chill, photo not great either
 # At this point I would try a different model formulation! Which ones would you try?
 
 # Some plotting (if you have a good model!)
