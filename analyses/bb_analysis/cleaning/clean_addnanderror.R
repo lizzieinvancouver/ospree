dea783619a45c249c649b129e03965cf087a60f2
AileneKane <ailene.ettinger@gmail.com>
2020-03-30 17:16:06 -0700

    updates to nerror check from AKE

--- analyses/bb_analysis/cleaning/clean_addnanderror.R
+++ analyses/bb_analysis/cleaning/clean_addnanderror.R
@@ -8,6 +8,12 @@ if(is.data.frame(d)){
 ##caffara11a (Ailene)  
   d$n[which(d$datasetID=="caffarra11a" & d$study=="exp3")]<-8#
   d$n[which(d$datasetID=="caffarra11a" & d$study=="exp2")]<-10#
+##myking97 (Ailene)  
+  d$n[which(d$datasetID=="myking97")]<-13#
+##worrall67 (Ailene)  
+  d$n[which(d$datasetID=="worrall67")]<-5#
+  ##laube14b (Ailene)  
+  d$n[which(d$datasetID=="laube14b")]<-10#
   
 } else {
   print("Error: d not a data.frame")
