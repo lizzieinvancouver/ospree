e8ed61fc95014e7911c3f3e1c215f4b65e997d2b
FaithJones <faithann.jones2@gmail.com>
2020-04-23 12:53:40 -0700

    FJ checking malashev cleaning

--- analyses/cleaning/merge_update2019.R
+++ analyses/cleaning/merge_update2019.R
@@ -16,6 +16,7 @@ if(length(grep("Lizzie", getwd())>0)) { setwd("~/Documents/git/projects/treegard
 (length(grep("ailene", getwd()))>0) {setwd("~/Documents/GitHub/ospree/analyses")
 } else 
 setwd("~/Documents/git/ospree/analyses")
+setwd("~/Documents/github/ospree/analyses")
 
 # Get the data
 cjc <- read.csv("input/update2019/ospree_2019update_cjc.csv")
@@ -24,8 +25,8 @@ dmb <- read.csv("input/update2019/ospree_2019update_dmb.csv")
 dss <- read.csv("input/update2019/ospree_2019update_DSS.csv")
 ks <- read.csv("input/update2019/ospree_2019update_ks.csv")
 my <- read.csv("input/update2019/ospree_2019update_my.csv")
-maly18add <- read.csv("input/update2019/ospree_2019update_DSSaddmaly18.csv")
-
+maly18add <- read.csv("input/update2019/ospree_2019update_DSSaddmaly18.csv") #fj couldnt fine thsi file. I found it ina different place though 
+maly18add <-read.csv("/home/faith/Documents/github/ospree/data/update2019/ospree_2019update_DSSaddmaly18.csv")
 
 # See how the data looks and do a little clean-up
 dim(cjc)
@@ -41,23 +42,37 @@ head(dss[,54:75])
 dss[,54:75] <- NULL
 dim(dss)
 
+#Faith looking at the data
+head(dss[dss$datasetID=="malyshev18",])
+tail(dss[dss$datasetID=="malyshev18",])
+malData <- dss[dss$datasetID=="malyshev18",]
+malData$fieldsample.date # the problem at the moment is that all the dates say 31st October, when actually there shoudl eb different feild sample dates
+#this causes problems for calculating chill
+head(maly18add)
+
 # fix malyshev18 issues: field sampel date was originally incorrect; DSS scraped missing x axis from Fig 2 in Apr 2020 to help
 # We're going to do something semi-dangerous here (note to Lizzie: you did this); we're going to paste in the new data with lots of checks for safety.. 
-nrow(dss[which(dss$datasetID=="malyshev18"),])==nrow(maly18add) # good
-identical(paste(dss$genus[which(dss$datasetID=="malyshev18")],
+nrow(dss[which(dss$datasetID=="malyshev18"),])==nrow(maly18add) # good - there are the same no. rows in the original data
+#and the new data with correct dates, so suggests there are the same number of data points (181)
+#is 181 teh correct number of samples though? It can be tricky to see teh exact number of data points from the grafs 
+#because black point can overlap hollow points. FJ had a quick count and also came up with 181 data points, but there was some 
+#ambiguity  
+identical(paste(dss$genus[which(dss$datasetID=="malyshev18")], #correct species names - they all match 
     dss$species[which(dss$datasetID=="malyshev18")]), (paste(maly18add$genus, maly18add$species))) # good
 # det to field sample date
-maly18add$dayssince_dec1stadj <- round(maly18add$dayssince_dec1st)
+maly18add$dayssince_dec1stadj <- round(maly18add$dayssince_dec1st) #rounding up dates because day 13.11 after Dec 1st is silly 
 getdec1 <- format(as.Date("1-12-2013", format="%d-%m-%Y"), "%j")
-maly18add$dayssince_dec1stadjmore <- maly18add$dayssince_dec1stadj+as.numeric(getdec1)
-maly18add$fieldsampledate <- as.Date(NA)
-maly18add$fieldsampledate[which(maly18add$dayssince_dec1stadjmore<=365)] <-
-    as.Date(paste(maly18add$dayssince_dec1stadjmore[which(maly18add$dayssince_dec1stadjmore<=365)],
+maly18add$dayssince_dec1stadjmore <- maly18add$dayssince_dec1stadj+as.numeric(getdec1) #add day of year for 1st Dec to each day to get real day of year 
+maly18add$fieldsampledate <- as.Date(NA)#tell R this new column will hold a date 
+maly18add$fieldsampledate[which(maly18add$dayssince_dec1stadjmore<=365)] <- # for days of year that are 365 or less
+    as.Date(paste(maly18add$dayssince_dec1stadjmore[which(maly18add$dayssince_dec1stadjmore<=365)], # add the year (2013) to the column, and convert to proper dates
     rep("2013", length(maly18add$dayssince_dec1stadjmore[which(maly18add$dayssince_dec1stadjmore<=365)]))), "%j %Y")
-maly18add$fieldsampledate[which(maly18add$dayssince_dec1stadjmore>365)] <-
+maly18add$fieldsampledate[which(maly18add$dayssince_dec1stadjmore>365)] <- # for days of year more than 365, this means they are actually in 2014.
     as.Date(paste(maly18add$dayssince_dec1stadjmore[which(maly18add$dayssince_dec1stadjmore>365)]-365,
     rep("2014", length(maly18add$dayssince_dec1stadjmore[which(maly18add$dayssince_dec1stadjmore>365)]))), "%j %Y")
-dss$fieldsample.date[which(dss$datasetID=="malyshev18")] <- format(maly18add$fieldsampledate, format="%Y-%b-%d")
+
+dss$fieldsample.date[which(dss$datasetID=="malyshev18")] <- format(maly18add$fieldsampledate, format="%Y-%b-%d")#replace dates in malyshev18.
+#this code assumes the data ae in the exact same row order. 
 
 if(FALSE){
 # Check by plotting and compare to paper
@@ -74,6 +89,10 @@ if(FALSE){
 
 # next steps  ...
 # (1) Figure out why the plot from malycheck does not look like Figure 2 (or another check) ... maybe date is not plotting properly?
+# fj - I dont understand what teh problem is with this figure. Too me it looks the same, exept that species are in a different order
+#and there is only the top pannel of Figure 2. This is how it shoud lbe though because we didnt want to scrape the lower pannel data (chilling hours)
+#because we estimate that from feild sample date?
+
 # (2) If there is no clear fix, I think the only good option is to re-scrape the data ... unless someone sees another way
 # (3) For all of dss, chang ex1 or ex2 etc. to exp1, tidy up anything else you seein dss
  
