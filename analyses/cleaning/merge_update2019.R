06381893dc53503234e9ad5909ced8d3def8f0a9
FaithJones <faithann.jones2@gmail.com>
2020-04-28 15:42:33 -0700

    check malashev clean

--- analyses/cleaning/merge_update2019.R
+++ analyses/cleaning/merge_update2019.R
@@ -64,6 +64,10 @@ maly18add$fieldsampledate[which(maly18add$dayssince_dec1stadjmore>365)] <- # for
 dss$fieldsample.date[which(dss$datasetID=="malyshev18")] <- format(maly18add$fieldsampledate, format="%Y-%b-%d")# replace dates in malyshev18.
 # this code assumes the data ae in the exact same row order (that's dangerous bit, see commented out code below where we check it)
 
+#FJ checking dss clean (properly in clean_misc.R) works fine. It seems to solve the problem of a miss sentered data (fj)
+dss$response.time[which(dss$datasetID=="malyshev18" & dss$genus=="Acer" & dss$species=="pseudolatauns" & dss$fieldsample.date=="2014-Mar-31" & dss$photoperiod_day=="16")]  <- 5.574 #  incorrect original entry of data (was 68.837, way too high).
+
+
 if(FALSE){
 # Check by plotting and compare to paper
   malycheck <- dss[which(dss$datasetID=="malyshev18"),]
