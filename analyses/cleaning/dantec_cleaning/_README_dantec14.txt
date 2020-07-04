3b5fbf67dd9bd78dc9057772391b9a07e071b81b
Mira Garner <mira.garner@gmail.com>
2020-07-03 17:51:25 -0700

    dantec 14 cleaning folder

---
+++ analyses/cleaning/dantec_cleaning/_README_dantec14.txt
@@ -0,0 +1,21 @@
+Readme for Dantec14 cleaning. Started 30 June 2020 by Mira Garner.
+
+Work until 30 Jun 2020: Data for Figure 3a (percent budburst) was scrapped by Dan Flynn. Mira Garner and Faith Jones scrapped GDD data for Figures 3b (MDG) and 3c (FJ) during the 2019 ospree retreat. The xlsx sheets for the new data was added to ospree/data/Dantec14add but was never added to the main ospree dataset.
+
+New cleaning steps:
+- Created the ospree/analyses/cleaning/dantec_cleaning folder, modeled after the okie_cleaning folder
+- Removed pdf of article from Dantec14add folder
+- converted MG and FJ xlsx files into csv files. Copied the csv files to the dantec_cleaning folder
+
+- fieldsample.date was changed from "15-Oct-10" to "unclear 15-Oct-10 to 15-Apr-11" in all datasets. The paper lists the sample schedule as about every two weeks mid-Oct 2010 to mid-Apr 2011 without giving specific start or end dates and without
+
+- added letters to figure numbers (FJ = 3a, MDG = 3b, DF = 3c). Previously all said "fig 3)"
+
+- fixed entry labeled "dantec15" in MDG dataset
+- fixed population.altitude.m entries for MDG F. sylvatica (first high elevation entry labeled as low elevation and first low elevation entry labeled as high elevation)
+
+- Chill days from paper were entered in field.chill.units for FJ dataset. This information was moved to chill.days.
+
+# add readme about what we did, why we scraped, and data problems
+# chill units = chilling calc, 5degthreshold, see paper
+# Is the data from Faith and Mira the same just using different threshold for calculation - if so do we keep both?
---
+++ analyses/cleaning/dantec_cleaning/dantec14_cleaning.R
@@ -0,0 +1,41 @@
+######## cleaning for dantec14. Mira Garner 22 April, 2020 ########
+
+rm(list = ls())
+options(stringsAsFactors = FALSE)
+setwd("~/Documents/git/ospree/analyses/")
+
+#subset data already in ospree from DF
+d <- read.csv("output/ospree_clean.csv", header=TRUE)
+
+df <- d[which(d$datasetID == "dantec14"), ]
+head(df)
+
+# read in MDG and FJ datasets
+mdg <- read.csv("cleaning/dantec_cleaning/Dantec14_MG.csv", header = TRUE)
+fj <- read.csv("cleaning/dantec_cleaning/Dantec14_fj.csv", header = TRUE)
+
+# merge datasets
+dan <- rbind(mdg, fj)
+
+# correct datasetID for entry 39 MDG dataset from "dantec15"
+dan$datasetID[which(dan$datasetID == "dantec15")] <- "dantec14"
+
+# change sample date of "15-Oct-10" to "unclear 15-Oct-10 to 15-Apr-11"
+dan$fieldsample.date[which(dan$datasetID == "dantec14")] <- "unclear Oct-10 to Apr-11"
+df$fieldsample.date[which(df$datasetID == "dantec14")] <- "unclear Oct-10 to Apr-11"
+
+# add figure numbers for DF data (3c), MDG (3b) data and FJ (3a) data.
+df$figure.table..if.applicable.[which(df$datasetID == "dantec14" & df$Entered.By == "DF")] <- "3c"
+dan$figure.table..if.applicable.[which(dan$datasetID == "dantec14" & dan$Entered.By == "MDG")] <- "3b"
+dan$figure.table..if.applicable.[which(dan$datasetID == "dantec14" & dan$Entered.By == "FJ")] <- "3a"
+
+# for F. sylvatica, MDG switched the first low elevation and high elevation entries for population.altitude.m (high elevation entry labeled as low elevation but at top of high ele)
+dan$population.altitude.m[which(dan$Entered.By == "MDG" & dan$chilldays == "1.084")] <- "1604"
+dan$population.altitude.m[which(dan$Entered.By == "MDG" & dan$chilldays == "0")] <- "131"
+
+# FJ entered chill.days in field.chill.units. Move this info to chill.days and delete from field.chill.units
+dan$chilldays[which(dan$Entered.By == "FJ")] <- dan$field.chill.units[which(dan$Entered.By == "FJ")]
+dan$field.chill.units[which(dan$Entered.By == "FJ")] <- c() ##
+
+# remove empty row
+dan2 <- dan[which(dan$datasetID != ""),]
