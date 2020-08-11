3499de9f4a2c52b48c659174cc658a5a409df46e
Mira Garner <mira.garner@gmail.com>
2020-08-10 17:20:17 -0700

    dantec14 new data cleaning

--- analyses/cleaning/dantec_cleaning/dantec14_cleaning.R
+++ analyses/cleaning/dantec_cleaning/dantec14_cleaning.R
@@ -2,32 +2,21 @@
 
 rm(list = ls())
 options(stringsAsFactors = FALSE)
-setwd("~/Documents/git/ospree/analyses/")
+setwd("~/Documents/git/ospree/analyses/cleaning/dantec_cleaning/")
 
-#subset data already in ospree from DF
-d <- read.csv("output/ospree_clean.csv", header=TRUE)
-
-df <- d[which(d$datasetID == "dantec14"), ]
-head(df)
-
-# read in MDG and FJ datasets
+#-----------------------------------------------------------------------#
+# read in MDG and FJ datasets for cleaning and merge
 mdg <- read.csv("cleaning/dantec_cleaning/Dantec14_MG.csv", header = TRUE)
 fj <- read.csv("cleaning/dantec_cleaning/Dantec14_fj.csv", header = TRUE)
-
-# merge datasets
 dan <- rbind(mdg, fj)
-
-### dan has more columns than new data - why???
 
 # correct datasetID for entry 39 MDG dataset from "dantec15"
 dan$datasetID[which(dan$datasetID == "dantec15")] <- "dantec14"
 
 # change sample date of "15-Oct-10" to "unclear 15-Oct-10 to 15-Apr-11"
 dan$fieldsample.date[which(dan$datasetID == "dantec14")] <- "unclear Oct-10 to Apr-11"
-df$fieldsample.date[which(df$datasetID == "dantec14")] <- "unclear Oct-10 to Apr-11"
 
-# add figure numbers for DF data (3c), MDG (3b) data and FJ (3a) data.
-df$figure.table..if.applicable.[which(df$datasetID == "dantec14" & df$Entered.By == "DF")] <- "3c"
+# add figure numbers for MDG (3b) data and FJ (3a) data.
 dan$figure.table..if.applicable.[which(dan$datasetID == "dantec14" & dan$Entered.By == "MDG")] <- "3b"
 dan$figure.table..if.applicable.[which(dan$datasetID == "dantec14" & dan$Entered.By == "FJ")] <- "3a"
 
@@ -35,18 +24,20 @@ dan$figure.table..if.applicable.[which(dan$datasetID == "dantec14" & dan$Entered
 dan$population.altitude.m[which(dan$Entered.By == "MDG" & dan$chilldays == "1.084")] <- "1604"
 dan$population.altitude.m[which(dan$Entered.By == "MDG" & dan$chilldays == "0")] <- "131"
 
-# FJ entered chill.days in field.chill.units. Move this info to chill.days and delete from field.chill.units
-dan$chilldays[which(dan$Entered.By == "FJ")] <- dan$field.chill.units[which(dan$Entered.By == "FJ")]
-dan$field.chill.units[which(dan$Entered.By == "FJ")] <- "NA" ## IS IT OKAY TO HAVE NAs
+# MG entered chilling in chill.days not field.chill.units. Delete this info from chill.days and move to field.chill.units
+dan$field.chill.units[which(dan$Entered.By == "MDG")] <- dan$chilldays[which(dan$Entered.By == "MDG")]
+dan$chilldays[which(dan$Entered.By == "MDG")] <- "NA"
 
-# add more info to cu.model
-dan$cu.model[which(dan$cu.model == "day-5")] <- "chillcalc,5degthreshold,see paper"
-dan$cu.model[which(dan$cu.model == "day-10")] <- "chillcalc,10degthreshold,see paper"
+# add more info to cu.model and change FJ threshold to 10 deg
+dan$cu.model[which(dan$cu.model == "days-5")] <- "chillcalc,10degthreshold,see paper"
+dan$cu.model[which(dan$cu.model == "days-10")] <- "chillcalc,10degthreshold,see paper"
 
-# same for DF data
-df$cu.model <- "chillcalc,10degthreshold,see paper"
+# add GDD temperature thresholds to respvar (5 deg for FJ, 10 deg for MDG)
+dan$respvar[which(dan$Entered.By == "MDG")] <- "GDDabove10"
+dan$respvar[which(dan$Entered.By == "FJ")] <- "GDDabove5"
 
 # remove empty row
 dan2 <- dan[which(dan$datasetID != ""),]
+head(dan2)
 
-# merge them together?? MG and FJ data has less columns - not sure where the other columns are coming from in ospree
+write.csv(dan2, file = "../../input/dantec_merge.csv")
