3b39e78ef18adb4503919ea2ca14033d81bad9ee
Elizabeth Wolkovich <wolkovich@biodiversity.edu>
2020-10-07 19:57:43 -0700

    Code to work on new stan models

---
+++ analyses/phylogeny/Phylo_ospree_reanalyses_inprogress.R
@@ -0,0 +1,260 @@
+## Started 7 October 2020 ##
+## By Lizzie, copies Nacho's Phylo_ospree_reanalyses.R code ##
+
+## But edits it to be used for testing new stan models. ##
+
+#### remove objects (activate if needed) ####
+rm(list=ls())
+options(stringsAsFactors = FALSE)
+
+
+# Setting working directory. Add in your own path in an if statement for your file structure
+if(length(grep("Lizzie", getwd())>0)) { 
+  setwd("~/Documents/git/projects/treegarden/budreview/ospree/analyses/phylogeny") 
+} else if (length(grep("ailene", getwd()))>0) {setwd("/Users/aileneettinger/git/ospree/analyses/phylogeny")
+}else if(length(grep("Ignacio", getwd()))>0) { 
+  setwd("~/GitHub/ospree/analyses/phylogeny") 
+} else if(length(grep("catchamberlain", getwd()))>0) { 
+  setwd("~/Documents/git/ospree/analyses/phylogeny") 
+} else if(length(grep("danielbuonaiuto", getwd()))>0) { 
+  setwd("~/Documents/git/ospree/analyses/phylogeny") 
+}else setwd("~/Documents/git/projects/treegarden/budreview/ospree/analyses/phylogeny")
+
+# Loading packages
+library(shinystan)
+library(caper)
+library(pez)
+library(rstan)
+library(phytools)
+library(plyr)
+library(dplyr)
+
+options(mc.cores = parallel::detectCores())
+
+
+#'######################################
+#### get data through bbstanleadin ####
+#'######################################
+
+# Flags to choose for bbstanleadin.R #
+setwd("..//bb_analysis") 
+
+# Master flags! Here you pick if you want the flags for the main model (figure in main text) versus the all spp model (supp)
+use.flags.for.mainmodel <- FALSE
+use.flags.for.allsppmodel <- TRUE
+use.yourown.flagdesign <- FALSE
+nocrops <- TRUE
+agiosponly <- TRUE
+
+if(use.flags.for.mainmodel==TRUE & use.flags.for.allsppmodel | use.flags.for.mainmodel==TRUE & use.yourown.flagdesign |
+   use.yourown.flagdesign  & use.flags.for.allsppmodel | use.flags.for.mainmodel==TRUE & use.flags.for.allsppmodel
+   & use.yourown.flagdesign) print("ALERT! You have set too many master flags to true, you must pick only one!")
+
+if(use.flags.for.mainmodel){
+  use.chillports = FALSE
+  use.zscore = TRUE
+  use.allspp =FALSE # for the main model this is false
+  use.multcuespp = FALSE
+  use.cropspp = FALSE
+  # Default is species complex use  alltypes of designs
+  use.expramptypes.fp = TRUE
+  use.exptypes.fp = FALSE
+  use.expchillonly = FALSE
+}
+
+if(use.flags.for.allsppmodel){
+  use.chillports = FALSE
+  use.zscore = TRUE
+  use.allspp = TRUE
+  use.multcuespp = FALSE
+  use.cropspp = TRUE
+  use.expramptypes.fp = FALSE
+  use.exptypes.fp = FALSE
+  use.expchillonly = FALSE
+}
+
+if(use.yourown.flagdesign){
+  use.chillports = F # change to false for using utah instead of chill portions (most models use chill portions z)
+  use.zscore = TRUE # change to false to use raw predictors
+  
+  # Default is species complex and no crops
+  use.allspp = F
+  use.multcuespp = FALSE
+  use.cropspp = FALSE
+  
+  # Default is species complex use  alltypes of designs
+  use.expramptypes.fp = TRUE
+  use.exptypes.fp = FALSE
+  
+  #Default is all chilling data
+  use.expchillonly = FALSE # change to true for only experimental chilling 
+  #note: with only exp chilling, there is only exp photo and force too.
+  #also: subsetting to exp chill only reduces dataset to 3 species, <9 studies
+}
+
+source("..//bb_analysis/source/bbstanleadin.R")
+
+namesdat <- unique(paste(bb.stan$genus,bb.stan$species,sep="_"))
+bb.stan$spps <- paste(bb.stan$genus,bb.stan$species,sep="_")
+bb.stan$phylo <- paste(bb.stan$genus,bb.stan$species,sep="_")
+
+
+#'###################################
+#### get phylogeny              ####
+#'###################################
+
+setwd("..//phylogeny") 
+source("source/get_phylo_models.R")
+
+## read and pre-process phylogeny
+#phylo <- read.tree("../../data/phylogeny/SBphylo_62complex.tre")
+#phylo <- read.tree("../../data/phylogeny/SBphylo_101sps.tre")
+phylo <- phy.plants.ospree
+
+
+namesphy <- phylo$tip.label
+phylo <- force.ultrametric(phylo, method="extend")
+phylo$node.label <- seq(1,length(phylo$node.label),1)
+is.ultrametric(phylo)
+plot(phylo, cex=0.7)
+VCVPHY <- vcv.phylo(phylo,corr=TRUE)
+
+
+
+## deal with subgrouping
+if(nocrops & agiosponly){
+  gymno <- c("Metasequoia_glyptostroboides",  "Pseudotsuga_menziesii","Larix_laricina",
+             "Larix_gmelinii", "Larix_decidua" ,"Larix_kaempferi",   
+             "Pinus_nigra","Pinus_sylvestris","Pinus_banksiana",  
+             "Pinus_contorta","Pinus_wallichiana","Pinus_strobus", 
+             "Picea_abies"   ,"Picea_mariana" ,"Picea_glauca" ,
+             "Cedrus_libani" ,"Abies_alba"    ,"Abies_homolepis","Ginkgo_biloba")
+  croplist <- read.csv("../../data/croplist/agricultural_species.csv")
+  cropgymno <- c(croplist$Species_name,gymno)
+  bb.stan$crops <- ifelse(bb.stan$spps %in% cropgymno, "cropgymno","nocrop")
+  cropspps <- unique(bb.stan$spps[which(bb.stan$crops=="cropgymno")])
+  bb.stan <- subset(bb.stan, crops == "nocrop")
+  phylo <- drop.tip(phylo, cropspps)
+  VCVPHY<-vcv.phylo(phylo,corr=T)
+} 
+
+if(nocrops & !agiosponly){
+  croplist <- read.csv("../../data/croplist/agricultural_species.csv")
+  bb.stan$crops <- ifelse(bb.stan$spps %in% croplist$Species_name, "crop","nocrop")
+  cropspps <- unique(bb.stan$spps[which(bb.stan$crops=="crop")])
+  bb.stan <- subset(bb.stan, crops == "nocrop")
+  phylo <- drop.tip(phylo, cropspps)
+  VCVPHY<-vcv.phylo(phylo,corr=T)
+} 
+
+
+if(!nocrops & agiosponly){
+  gymno <- c("Metasequoia_glyptostroboides",  "Pseudotsuga_menziesii","Larix_laricina",
+             "Larix_gmelinii", "Larix_decidua" ,"Larix_kaempferi",   
+             "Pinus_nigra","Pinus_sylvestris","Pinus_banksiana",  
+             "Pinus_contorta","Pinus_wallichiana","Pinus_strobus", 
+             "Picea_abies"   ,"Picea_mariana" ,"Picea_glauca" ,
+             "Cedrus_libani" ,"Abies_alba"    ,"Abies_homolepis","Ginkgo_biloba")
+  cropgymno <- c(gymno)
+  bb.stan$crops <- ifelse(bb.stan$spps %in% cropgymno, "cropgymno","nocrop")
+  cropspps <- unique(bb.stan$spps[which(bb.stan$crops=="cropgymno")])
+  bb.stan <- subset(bb.stan, crops == "nocrop")
+  phylo <- drop.tip(phylo, cropspps)
+  VCVPHY<-vcv.phylo(phylo,corr=T)
+} 
+
+
+# Step 1: Get spps and VCVPHY in same order
+# bb.stan$spps[phylo$tip.label]
+phylo$tip.label
+d <- bb.stan[match(phylo$tip.label, bb.stan$spps),] # hmmm, only gives ONE match
+
+phymatch <- data.frame(tip=phylo$tip.label, sppnum=c(1:length(phylo$tip.label)))
+d <- merge(bb.stan, phymatch, by.x="spps", by.y="tip")
+d <- d[order(d$sppnum),]
+# Tilia_cordata versus Tilia_Cordata in phylo
+nspecies <- max(d$sppnum)
+
+
+# Step 2: Run a simple version of the model 
+
+testme <- stan("stan/nointer_2level_force.stan",
+                data=list(N=nrow(d), n_sp=nspecies, sp=d$sppnum,
+                force=d$force.z, y=d$resp,
+                Vphy=vcv(phylo, corr=TRUE)),
+                iter=1000, chains=4, seed=123456)
+summary(testme)$summary
+
+# Step 3: Run some other versions of the model 
+testme2 <- stan("stan/nointer_2levelphyall.stan",
+                data=list(N=nrow(d), n_sp=nspecies, sp=d$sppnum,
+                force=d$force.z, chill=d$chill.z, photo=d$photo.z,
+                y=d$resp,
+                Vphy=vcv(phylo, corr=TRUE)),
+                iter=2000, chains=4, seed=123456)
+summary(testme2)$summary
+
+testme3 <- stan("stan/nointer_2levelphy.stan",
+                data=list(N=nrow(d), n_sp=nspecies, sp=d$sppnum,
+                force=d$force.z, chill=d$chill.z, photo=d$photo.z,
+                y=d$resp,
+                Vphy=vcv(phylo, corr=TRUE)),
+                iter=3000, chains=4, seed=123456)
+summary(testme3)$summary
+# use shinystan to see that this model is struggling on null_interceptsbf and null_interceptsbp
+# save(testme3, file="stan/output/nointer_2levelphy.Rda")
+
+testme4 <- stan("stan/nointer_2levelphy_noppint.stan",
+                data=list(N=nrow(d), n_sp=nspecies, sp=d$sppnum,
+                force=d$force.z, chill=d$chill.z, photo=d$photo.z,
+                y=d$resp,
+                Vphy=vcv(phylo, corr=TRUE)),
+                iter=3000, chains=4, seed=123456)
+summary(testme4)$summary # null_interceptsbf, null_interceptsbp struggling some, but close...
+
+
+# This model uses Will's new method, but it is not running, argh!
+testme5 <- stan("stan/nointer_2levelphynew_force.stan",
+                data=list(N=nrow(d), n_sp=nspecies, sp=d$sppnum,
+                force=d$force.z, 
+                y=d$resp,
+                Vphy=vcv(phylo)),
+                iter=3000, chains=1)
+
+
+source("source/bb_muplotphylo.R")
+modelhere <- testme3
+figpath <- "figures"
+figpathmore <- "testme3"
+library(RColorBrewer)
+cols <- adjustcolor("indianred3", alpha.f = 0.3) 
+my.pal <- rep(brewer.pal(n = 12, name = "Paired"), 4)
+my.pch <- rep(15:18, each=12)
+alphahere = 0.4
+
+muplotfx(modelhere, "", 7, 8, c(0,3), c(-30, 10) , 12, 3.5)
+
+par(mfrow=c(2,3))
+hist(extract(modelhere)[["null_interceptsbf"]], main="force")
+hist(extract(modelhere)[["null_interceptsbc"]], main="chill")
+hist(extract(modelhere)[["null_interceptsbp"]], main="photo")
+
+hist(extract(modelhere)[["lam_interceptsbf"]], main="lambda force")
+hist(extract(modelhere)[["lam_interceptsbc"]], main="lambda chill")
+hist(extract(modelhere)[["lam_interceptsbp"]], main="lambda photo")
+
+
+lamf.int <- mean(extract(modelhere)[["lam_interceptsbf"]])
+nullf.int <- mean(extract(modelhere)[["null_interceptsbf"]])
+lamf.int / (nullf.int + lamf.int)
+
+lamc.int <- mean(extract(modelhere)[["lam_interceptsbc"]])
+nullc.int <- mean(extract(modelhere)[["null_interceptsbc"]])
+lamc.int / (nullc.int + lamc.int)
+
+lamp.int <- mean(extract(modelhere)[["lam_interceptsbp"]])
+nullp.int <- mean(extract(modelhere)[["null_interceptsbp"]])
+lamp.int / (nullp.int + lamp.int)
+
+
+## END TEST
