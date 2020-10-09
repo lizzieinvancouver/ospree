dff242ea9abc512257868b05d8603e9359fea1d0
DeirdreLoughnan <deirdre.loughnan@gmail.com>
2020-10-09 10:45:27 -0700

    examining try data

--- analyses/traits/Rfiles/mergebientry.R
+++ analyses/traits/Rfiles/mergebientry.R
@@ -1,5 +1,16 @@
+# The aim of this code is to clean up the try and bien data and combine it into one useable file! There are many steps that need to be completed for this to happen:
+
+#1. Fix typos try species names
+#2. Remove columns in try that we don't need right now, there is tons of weather data and factors regarding site conditions
+#3. Remove data from experiments where they manipulated factors that might change trails: fert additions, temp manip, chamber studies
+#4. Check that the BIEN data is standardized in its trait units, lat/long
+#5. Remove any dup data
+#6. Subset to spp. with sufficient data, focus on leaf economic traits and SSD (one of the only wood economic traits)
+
 rm(list=ls()) 
 options(stringsAsFactors = FALSE)
+
+require(stringr)
 
 # Set working directory: 
 
@@ -14,82 +25,116 @@ biendat<-read.csv("input/newspp_BIEN_traitdata_Nov11.csv", header=TRUE)
 
 head(biendat)
 length(unique(biendat$scrubbed_species_binomial))
-#94
+#94 species represented in some form
 
-names(biendat)
+trydat<-read.csv("input/TryDataCleaned.csv", header=TRUE)
+head(trydat)
+length(unique(trydat$SpeciesName))
+#122 species represented...but I think some of these were typos
 
+## Fixing species names ############################################################
+trydat$SpeciesName[which(trydat$SpeciesName == "Acer pensilvanicum")] <- "Acer pensylvanicum"
+trydat$SpeciesName[which(trydat$SpeciesName == "Rhamnus catharticus")] <- "Rhamnus cathartica"
+trydat$SpeciesName[which(trydat$SpeciesName == "Quercus velutinam.")] <- "Quercus velutina"
+trydat$SpeciesName[which(trydat$SpeciesName == "Quercus rubra I")] <- "Quercus rubra"
+trydat$SpeciesName[which(trydat$SpeciesName == "Quercus shumardii Buckley")] <- "Quercus shumardii"
+trydat$SpeciesName[which(trydat$SpeciesName == "Quercus ilex ilex")] <- "Quercus ilex"
+trydat$SpeciesName[which(trydat$SpeciesName == "Pseudotsuga menziesii (Mirb.) Franco")] <- "Pseudotsuga menziesii" 
+trydat$SpeciesName[which(trydat$SpeciesName == "Pseudotsuga menziesii var. menziesii")] <- "Pseudotsuga menziesii" 
+trydat$SpeciesName[which(trydat$SpeciesName == "Prunus pensylvanica f.")] <- "Prunus pensylvanica"
+trydat$SpeciesName[which(trydat$SpeciesName == "Quercus bicolor Willd.")] <- "Quercus bicolor"
+trydat$SpeciesName[which(trydat$SpeciesName == "Alnus glutinosa (L.) Gaertn.")] <- "Alnus glutinosa"
+trydat$SpeciesName[which(trydat$SpeciesName == "Betula pendula Roth")] <- "Betula pendula"
+trydat$SpeciesName[which(trydat$SpeciesName == "Alnus incana (L.) Moench")] <- "Alnus incana"
+trydat$SpeciesName[which(trydat$SpeciesName == "Facus grandifolia")] <- "Fagus grandifolia"
+trydat$SpeciesName[which(trydat$SpeciesName == "Fagus grandfolia")] <- "Fagus grandifolia"
+trydat$SpeciesName[which(trydat$SpeciesName == "Photinia melanocarpa (Michx.) K.R. Robertson & Phipps")] <- "Photinia melanocarpa"
+trydat$SpeciesName[which(trydat$SpeciesName == "Populus grandidentata Michx.")] <- "Populus grandidentata"
+trydat$SpeciesName[which(trydat$SpeciesName == "Prunus pennsylvanica")] <- "Prunus pensylvanica"
+trydat$SpeciesName[which(trydat$SpeciesName == "Picea abies (L.) Karst.")] <- "Picea abies"
+trydat$SpeciesName[which(trydat$SpeciesName == "Picea abies/obovata")] <- "Picea abies"
+trydat$SpeciesName[which(trydat$SpeciesName == "Picea abies.x.obovata")] <- "Picea abies"
+trydat$SpeciesName[which(trydat$SpeciesName == "Picea glauca (Moench) Voss")] <- "Picea glauca"
+trydat$SpeciesName[which(trydat$SpeciesName == "Pieris japonica (Thunb.) D. Don ex G. Don")] <- "Pieris japonica"
+trydat$SpeciesName[which(trydat$SpeciesName == "Pinus nigra Arnold")] <- "Pinus nigra"
+trydat$SpeciesName<-str_replace(trydat$SpeciesName," (L.)","") 
+trydat$SpeciesName<-str_replace(trydat$SpeciesName," L.","") 
+trydat$SpeciesName<-str_replace(trydat$SpeciesName," Jacq.","") 
+trydat$SpeciesName<-str_replace(trydat$SpeciesName," Ehrh.","") 
+trydat$SpeciesName<-str_replace(trydat$SpeciesName," Marsh.","") 
+trydat$SpeciesName<-str_replace(trydat$SpeciesName," Hook.","") 
+trydat$SpeciesName<-str_replace(trydat$SpeciesName," Maxim.","") 
+length(unique(trydat$SpeciesName))
+# Once species names are fixed, try dataset only has 79 species
+
+##################################################################################
+names(trydat) # so many of these columns are not useful right now
+
+trydat<-trydat[,c("SpeciesName","Dataset","Reference","Latitude","Longitude","Traits","TraitValue", "OrigUnitStr.y","UnitName","std_Latitude","std_Longitude","Std_Traits","TraitValue_std", "Exposition","Exposition.temperature","Leaf.exposition" )]
+
+# Excluding experiment data
+unique(trydat$Exposition) # I think we should subset out all experiments
+# "Open Top" "open-top chamber"  "open-sided growth chamber" "forest fertilization" "Climate Chamber"
+
+unique(trydat$Exposition.temperature) # Several studies manipulate temp
+unique(trydat$Treatment..exposition.to.measurement.temperature.before.measurement) # All NA
+unique(trydat$Treatment..period.of.darkness.before.respiration.measurement) # All NA
+
+unique(trydat$Exposition..position.of.plant.in.the.canopy) # Variable, some numbers (5,6,7), bottom, middle etc., coments about understory
+
+unique(trydat$Treatment.water.supply) # All NA #Intermediate, high, low
+unique(trydat$Treatment.ozon) # high, low
+unique(trydat$Treatment.conditions) # descriptions of experiments, 17 different comments, but includes an n/a and "no treatment"
+unique(trydat$Treatment.CO2) # Various values, as well as "ambient (about 360ppm)" "actual" 
+unique(trydat$Leaf.exposition) # not clear what these numbers represent
+unique(trydat$Treatment.relative.humidity..Relative.humidity..) # All NA
+unique(trydat$Treatment.plant.growth.temperature.during.night) # All NA
+unique(trydat$Treatment.daylength) # All NA
+unique(trydat$Treatment.plant.growth.temperature.during.day) # All NA
+unique(trydat$Treatment.nutrient.solution.per.week) # a range of comments, including "Natural conditions", "none","None", "natural", and details of how much and when fertilizers were applied
+unique(trydat$Treatment.K..potassium..supply) # All NA
+unique(trydat$Treatment.P..phosphorus..supply) # All NA
+unique(trydat$Treatment.nutrient.supply) # intermediate, low, high
+unique(trydat$Treatment.light) # 2,1,0 no indication of what this represents
+unique(trydat$Treatment.growth.medium...substrat...soil) # All NA
+
+##################################################################################
 #Changing names in BIEN to better match those in try
-colnames(biendat)[colnames(biendat)=="scrubbed_species_binomial"] <- "speciesname"
-colnames(biendat)[colnames(biendat)=="unit"] <- "unitname"
-colnames(biendat)[colnames(biendat)=="maximum.whole.plant.height"] <- "maximum.plant.height"
-colnames(biendat)[colnames(biendat)=="leaf.area.per.leaf.dry.mass"] <- "ldmc"
-colnames(biendat)[colnames(biendat)=="leaf.carbon.content.per.leaf.nitrogen.content"] <- "leaf carbon/nitrogen (c/n) ratio"
-colnames(biendat)[colnames(biendat)=="leaf.dry.mass.per.leaf.fresh.mass"] <-"leaf.dry.matter.content.per.leaf.water-saturated.mass.(ldmc)"
-colnames(biendat)[colnames(biendat)=="trait_value"] <-"value"
-colnames(biendat)[colnames(biendat)=="trait_name"] <-"trait"
+colnames(biendat)[colnames(biendat)=="scrubbed_species_binomial"] <- "SpeciesName"
+colnames(biendat)[colnames(biendat)=="trait_name"] <-"Traits"
+colnames(biendat)[colnames(biendat)=="trait_value"] <-"TraitValue" # Are the bien trait values standardized, something to check!! 
+colnames(biendat)[colnames(biendat)=="unit"] <- "UnitName"
+colnames(biendat)[colnames(biendat)=="longitude"] <- "Longitdue"
+colnames(biendat)[colnames(biendat)=="latitude"] <- "Latitude"
 
-names(biendat)
+##################################################################################
+# But is the BIEN data standardized, ie lat long and trait units?
+unique(biendat$unit)
 
-write.csv(biendat,'~/Desktop/ospree_trait_analysis/bienwide.csv', row.names=FALSE)
-#Need to widen trydata so traits are columns
-#First could remove rows with no value
+# There are some none-standard units: 
+gcm3<-subset(biendat, unit == "g.cm-3") # this study has stem wood density in cm
+temp1<-subset(trydat, Traits == "Stem_specific_density") # this study has stem wood density in cm
+# There are no units for the SSD or SWD from try, but the values are similar
 
-# trydat<-read.csv("input/try_cleaning_dl/try_cleanlong_dl_allcoln.csv", header=TRUE)
+m2kg<-subset(biendat, unit == "m2.kg-1") # this study has leaf area per day mass in m and kg! ie SLA
+temp2<-subset(trydat, Traits == "Specific_leaf_area") 
+
+mgg<-subset(biendat, unit == "mg.g-1") # this study has leaf dry mass per fresh mass in m and mg! ie LDMC
+temp3<-subset(trydat, Traits == "Leaf_dry_matter_content") #standardized is g per g
+
+# What about lat/long
+unique(biendat$latitude)
+unique(biendat$longitude)
+
+unique(trydat$std_Latitude) 
+unique(trydat$std_Longitude) 
+
+# Are there any duplicates in the dataset
+
 # 
-# trydatfull<-subset(trydat, value!="NA") #has 32075 rows
-# head(trydatfull)
-# 
-# require(tidyr)
-# trywide<-spread(trydatfull, 
-#              Trait, 
-#              value)
-# 
-# head(trywide)
-# 
-# names(biendat)
-# names(trywide)            
-# 
-# trywide$UnitName
-# 
-# #to do:
-# #merge try pi first and last name
-# trywide$project_pi<-paste(trywide$FirstName, trywide$LastName)
-# 
-# #make lowercase so easier to merge with bien
-# names(trywide)<- tolower(names(trywide))
-# 
-# #change column names, which are the same and which are different?
-# require(stringr)
-# #First adding "." instead of spaces
-# names(trywide)<-str_replace_all(names(trywide), c(" " = "." , ";" = ".",":"="" ))
-# names(trywide)
-# 
-#                 
-# colnames(trywide)[colnames(trywide)=="plant.developmental.status...plant.age...maturity...plant.life.stage"] <-"whole.plant.growth.form"
-# colnames(trywide)[colnames(trywide)=="wood density; stem specific density; wood specific gravity"] <-"stem.wood.density"
-# colnames(trywide)[colnames(trywide)=="leaf.lifespan..longevity"] <-"leaf.life.span"
-# 
-# #the new dataset should simply be a combination of both
-# 2177892+13929
-# #2191821 columns
-# 
-# require(gtools)
-# test<-smartbind(biendat, trywide, fill=NA)
-# #get the right number of rows, but get a warning message that many of the column classes have been changed - longitude, leaf.life.span, ldmc, leaf.dry.matter.content.per.leaf.water-saturated.mass.(ldmc)
-# 
-# #For example longitude
-# unique(test$leaf.life.span) #does have "LL)" 
-# unique(test$longitude)
-# unique(test$ldmc)
-# unique(test$`leaf.dry.matter.content.per.leaf.water-saturated.mass.(ldmc)`)
-# str(test)
-# 
-# sort(names(trywide))
-# unique(trywide$Bedrock...geological.substrate)
-# 
-# require(dplyr)
-# trywide %>% 
-#   summarise_all((funs(sum(is.na(.))))) 
-# 
-# unique(trywide$Plant.developmental.status...plant.age...maturity...plant.life.stage)
-# 
+
+
+
+
+
+#write.csv(biendat,'~/Desktop/ospree_trait_analysis/bienwide.csv', row.names=FALSE)
