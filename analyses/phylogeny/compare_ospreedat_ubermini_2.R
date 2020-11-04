27ae04ef47718a3bc516b5b8997dbef4a19a8e06
AileneKane <aettinger@fasmail.harvard.edu>
2020-11-04 11:57:03 -0800

    comparing ospree data to test data for phylogeny model

--- analyses/phylogeny/compare_ospreedat_ubermini_2.R
+++ analyses/phylogeny/compare_ospreedat_ubermini_2.R
@@ -20,7 +20,7 @@ if(length(grep("Lizzie", getwd())>0)) {
 }else setwd("~/Documents/git/projects/treegarden/budreview/ospree/analyses")
 
 
-#compare test data to ospree data
+#We want to Compare test data to ospree data
 
 #to do this, first get the ospree data in (code taken from Phylo_ospree_reanalyses_inprogress.R)
 
@@ -41,7 +41,7 @@ options(mc.cores = parallel::detectCores())
 
 
 #'######################################
-#### get data through bbstanleadin ####
+#### get osdata through bbstanleadin ####
 #'######################################
 
 # Master flags! Here you pick if you want the flags for the main model (figure in main text) versus the all spp model (supp)
@@ -98,20 +98,14 @@ if(use.yourown.flagdesign){
 }
 
 source("../bb_analysis/source/bbstanleadin.R")
-#note: i get an error "use.rangespp" not foun- has this been added to bbstanleadin.R>
+#note: i get an error "use.rangespp" not found- has this been added to bbstanleadin.R>
 
 namesdat <- unique(paste(bb.stan$genus,bb.stan$species,sep="_"))
 bb.stan$spps <- paste(bb.stan$genus,bb.stan$species,sep="_")
 bb.stan$phylo <- paste(bb.stan$genus,bb.stan$species,sep="_")
 
-## now add in Lizzie's test data code from git repo pmm/analyses
 
-
-## Started 29 May 2020 ##
-## Generate test data for my phylogeny Stan models ##
-
-## Someday we want:
-# y ~ a[sp] + b_force[phylo]*x1 + b_chill[phylo]*x2 + b_photo[phylo]*x3 + error
+##Now, Lizzie's code to prepare the test data
 
 ## Uber simple to start ...
 ## skip an intercept and just estimate
@@ -153,103 +147,32 @@ dfhere$spnum <- as.numeric(gsub('s', '', dfhere$sp))
     
 dfhere$yerr <- dfhere$y + rnorm(nrow(dfhere), 0, sigy)
 
-testme <- stan("stan/ubermini_2.stan", # Note: changed to a new model
-                data=list(N=nrow(dfhere), n_sp=nspecies, sp=dfhere$spnum,
-                x=dfhere$x, y=dfhere$yerr,
-                Vphy=vcv(spetree)), # Note: dropped the corr=TRUE
-                iter=2000, chains=2, seed=123456)
-summary(testme)$summary
-summary(testme)$summary[c("lam_interceptsb","sigma_interceptsb","b_z"),"50%"]
-summary(testme)$summary[c("lam_interceptsb","sigma_interceptsb","b_z"),"25%"]
-summary(testme)$summary[c("lam_interceptsb","sigma_interceptsb","b_z"),"75%"]
+#Compare test data to ospree data
+#test data are in red, ospree in blue- not meant to be political! just whats on my brain today...
+pdf("../phylogeny/figures/phylotestospcomp.pdf", height = 6, width = 10)
 
-fitContinuous(spetree, slopez, model="lambda")
+par(mfrow=c(2,2))
+#first, compare sinmply the number of species
+nspecies.osp = length(unique(bb.stan$spps))
+hist(nspecies.osp,main = "Num sp", col = "light blue", xlim = c(0,nspecies.osp))
+hist(nspecies,col = "salmon", add = TRUE)
+nind.osp = unique(table(bb.stan$spps))
 
-# Compare true slopes to estimated slopes
-sumer <- summary(testme)$summary
-# sumer[grep("b_force", rownames(sumer)), "mean"]
-# slopez
+#Now look at evenness- number of rows or individuals per species
+hist(table(bb.stan$spps), main = "Num rows/ind per sp", col = "light blue")
+abline(v = nind, col = "salmon", lwd = 3)
 
-plot(slopez, sumer[grep("b_force", rownames(sumer)), "mean"])
+#Now look at resp
+hist(bb.stan$resp, col = "light blue", xlim = c(0,max(bb.stan$resp)), main = "Y (Resp)")
+hist(dfhere$y, col = "salmon", add = TRUE)#compare to resp from ospree
 
+#Now look at x (force.z)
+hist(bb.stan$force.z, col = "light blue", xlim = range(c(bb.stan$force.z, dfhere$x)), main = "X (Force.z)")
+hist(dfhere$x, col = "salmon", add = TRUE)
+dev.off()
 
-#'###################################
-#### get phylogeny              ####
-#'###################################
-
-setwd("..//phylogeny") 
-source("source/get_phylo_models.R")
-
-## read and pre-process phylogeny
-#phylo <- read.tree("../../data/phylogeny/SBphylo_62complex.tre")
-#phylo <- read.tree("../../data/phylogeny/SBphylo_101sps.tre")
-phylo <- phy.plants.ospree
-
-
-namesphy <- phylo$tip.label
-phylo <- force.ultrametric(phylo, method="extend")
-phylo$node.label <- seq(1,length(phylo$node.label),1)
-is.ultrametric(phylo)
-plot(phylo, cex=0.7)
-VCVPHY <- vcv.phylo(phylo,corr=TRUE)
-
-
-
-## deal with subgrouping
-if(nocrops & agiosponly){
-  gymno <- c("Metasequoia_glyptostroboides",  "Pseudotsuga_menziesii","Larix_laricina",
-             "Larix_gmelinii", "Larix_decidua" ,"Larix_kaempferi",   
-             "Pinus_nigra","Pinus_sylvestris","Pinus_banksiana",  
-             "Pinus_contorta","Pinus_wallichiana","Pinus_strobus", 
-             "Picea_abies"   ,"Picea_mariana" ,"Picea_glauca" ,
-             "Cedrus_libani" ,"Abies_alba"    ,"Abies_homolepis","Ginkgo_biloba")
-  croplist <- read.csv("../../data/croplist/agricultural_species.csv")
-  cropgymno <- c(croplist$Species_name,gymno)
-  bb.stan$crops <- ifelse(bb.stan$spps %in% cropgymno, "cropgymno","nocrop")
-  cropspps <- unique(bb.stan$spps[which(bb.stan$crops=="cropgymno")])
-  bb.stan <- subset(bb.stan, crops == "nocrop")
-  phylo <- drop.tip(phylo, cropspps)
-  VCVPHY<-vcv.phylo(phylo,corr=T)
-} 
-
-if(nocrops & !agiosponly){
-  croplist <- read.csv("../../data/croplist/agricultural_species.csv")
-  bb.stan$crops <- ifelse(bb.stan$spps %in% croplist$Species_name, "crop","nocrop")
-  cropspps <- unique(bb.stan$spps[which(bb.stan$crops=="crop")])
-  bb.stan <- subset(bb.stan, crops == "nocrop")
-  phylo <- drop.tip(phylo, cropspps)
-  VCVPHY<-vcv.phylo(phylo,corr=T)
-} 
-
-
-if(!nocrops & agiosponly){
-  gymno <- c("Metasequoia_glyptostroboides",  "Pseudotsuga_menziesii","Larix_laricina",
-             "Larix_gmelinii", "Larix_decidua" ,"Larix_kaempferi",   
-             "Pinus_nigra","Pinus_sylvestris","Pinus_banksiana",  
-             "Pinus_contorta","Pinus_wallichiana","Pinus_strobus", 
-             "Picea_abies"   ,"Picea_mariana" ,"Picea_glauca" ,
-             "Cedrus_libani" ,"Abies_alba"    ,"Abies_homolepis","Ginkgo_biloba")
-  cropgymno <- c(gymno)
-  bb.stan$crops <- ifelse(bb.stan$spps %in% cropgymno, "cropgymno","nocrop")
-  cropspps <- unique(bb.stan$spps[which(bb.stan$crops=="cropgymno")])
-  bb.stan <- subset(bb.stan, crops == "nocrop")
-  phylo <- drop.tip(phylo, cropspps)
-  VCVPHY<-vcv.phylo(phylo,corr=T)
-} 
-
-
-# Step 1: Get spps and VCVPHY in same order
-# bb.stan$spps[phylo$tip.label]
-phylo$tip.label
-d <- bb.stan[match(phylo$tip.label, bb.stan$spps),] # hmmm, only gives ONE match
-
-phymatch <- data.frame(tip=phylo$tip.label, sppnum=c(1:length(phylo$tip.label)))
-d <- merge(bb.stan, phymatch, by.x="spps", by.y="tip")
-d <- d[order(d$sppnum),]
-# Tilia_cordata versus Tilia_Cordata in phylo
-nspecies <- max(d$sppnum)
-
-
-hist(dfhere$y)
-hist(dfhere$x)
-
+#Other things to look at
+range(bb.stan$force.z)
+range(bb.stan$force)
+#Question: simulated slopes are almost all positive but ospree forcing slopes usally negative- does this matter?
+hist(slopez)
