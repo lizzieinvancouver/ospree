7b74eca5eda063892c0557656483033dc53b56db
AileneKane <aettinger@fasmail.harvard.edu>
2021-02-09 14:23:15 -0800

    playing with the code a bit

--- analyses/limitingcues/nonlinearities_more.R
+++ analyses/limitingcues/nonlinearities_more.R
@@ -21,6 +21,7 @@ if(length(grep("Lizzie", getwd())>0)) {
 ########################
 
 library("viridis")
+library(plotfunctions)
 colz <- viridis(4, alpha = 0.1)
 interceptbb <- 120
 
@@ -210,3 +211,122 @@ lines(bbforcechillhinge~temperature, data=df, col="red")
 lines(bbforcechillhingeint~temperature, data=df, col="red", lty=2)
 lines(bbforcechillhingeint2~temperature, data=df, col="red", lty=2)
 dev.off()
+
+#########################################################
+####### Slightly modified code to make one figure #######
+######## with both photoperiod & chilling effects #######
+############# Started by Ailene, Feb 9, 2021 ############
+#########################################################
+interceptbb <- 120
+
+
+howlong <- 100
+plineff <- -4.5/4
+phingeff <- (-4.5/4)*0.8
+whenhingep <- 14
+interceptphinge <- -(phingeff*whenhingep)
+whenlinearp <- 24
+interceptplinear <- -(plineff*whenlinearp)
+
+photcoef <- -0.1
+
+df <- data.frame("temperature"=seq(from=5, to=20, length.out=howlong), "photo"=seq(from=6, to=24, length.out=howlong))
+df$photolinear <- interceptphinge + df$photo*plineff
+df$photohinge <- interceptphinge + df$photo*phingeff
+df$photohinge[which(df$photo>whenhingep)] <- 0
+df$force <- df$temperature
+
+# Now estimate bb with no intxns and with intxns
+
+df$bbforce <- interceptbb + feff*df$force
+df$bbforcephoto <- interceptbb + feff*df$force + photcoef*df$photolinear
+df$bbforcephotohinge <- interceptbb + feff*df$force + photcoef*df$photohinge
+df$bbforcephotoint <- interceptbb + feff*df$force + photcoef*df$photolinear + fpeff*(df$force*df$photolinear)
+df$bbforcephotohingeint <- interceptbb + feff*df$force + photcoef*df$photohinge + fpeff*(df$force*df$photohinge)
+df$bbforcephotoint2 <- interceptbb + feff*df$force + photcoef*df$photolinear + fpeff2*(df$force*df$photolinear)
+df$bbforcephotohingeint2 <- interceptbb + feff*df$force + photcoef*df$photohinge + fpeff2*(df$force*df$photohinge)
+
+
+# ... and PLOT!
+linecol<-"blue4"
+threshcol="red4"
+photocol="turquoise3"
+pdf("figures/intxnsims2021photoaltwithchill.pdf", width=8, height=6)
+par(mfrow=c(2,3))
+plot(photolinear~photo, data=df, type="l", xlim=c(0,24), xlab="photoperiod", ylab="Photoperiod effect", col=linecol, cex.lab = 1.2)
+lines(photohinge~photo, data=df, lty=2, col=threshcol)
+rect(0, -15, 6, 10, col=alpha(photocol, f=.5), border="NA") # xleft, ybottom, xright, ytop
+mtext ("No budburst", side = 3, line = 1, adj = 0, cex = 0.9)
+mtext ("<6 hours", side = 3, line = 0, adj = 0, cex = 0.9)
+text (whenhingep+5,0+.6,"threshold response", col = threshcol)
+text (whenhingep+5,0-4,"linear response", col = linecol)
+
+
+plot(bbforce~temperature, data=df, type="l", ylab="Budburst day", cex.lab = 1.2)
+lines(bbforcephoto~temperature, data=df, col=linecol)
+lines(bbforcephotoint~temperature, data=df, col=linecol, lty=2)
+lines(bbforcephotoint2~temperature, data=df, col=linecol, lty=2)
+
+plot(bbforce~temperature, data=df, type="l", ylab="Budburst day", cex.lab = 1.2)
+lines(bbforcephotohinge~temperature, data=df, col=threshcol)
+lines(bbforcephotohingeint~temperature, data=df, col=threshcol, lty=2)
+lines(bbforcephotohingeint2~temperature, data=df, col=threshcol, lty=2)
+
+#now add chilling plots
+
+feff <- -8.8/5
+clineff <- (-15.8/1248)*0.8 
+chingeff <- -15.8/1248
+fceff <- 9.1/200
+fceff2 <- fceff/10
+chillcoef <- -0.2
+
+whenhingec <- 2000
+intercepcthinge <- -(chingeff*whenhingec)
+whenclinear <- 3000
+interceptclinear <- -(clineff*whenclinear)
+
+howlong <- 100
+
+make.chilldf<-function(howlong,whenhingec,intercepcthinge,whenclinear,interceptclinear){
+  df <- data.frame("temperature"=seq(from=5, to=20, length.out=howlong), "chill"=seq(from=100, to=3000, length.out=howlong))
+  df$chilllinear <- intercepcthinge + clineff*df$chill # replace intercepcthinge with interceptclinear if you want it to end at 0 (but I liked this version better)
+  df$chillhinge <- intercepcthinge + chingeff*df$chill
+  df$chillhinge[which(df$chill>2000)] <- 0
+  df$force <- df$temperature
+  # Now estimate bb with no intxns and with intxns
+  # Build ... temperature-only, then with chill, then with two different sized intxn terms
+  df$bbforce <- interceptbb + feff*df$force
+  df$bbforcechill <- interceptbb + feff*df$force + chillcoef*df$chilllinear
+  df$bbforcechillhinge <- interceptbb + feff*df$force + chillcoef*df$chillhinge
+  df$bbforcechillint <- interceptbb + feff*df$force + chillcoef*df$chilllinear + fceff*(df$force*df$chilllinear)
+  df$bbforcechillhingeint <- interceptbb + feff*df$force + chillcoef*df$chillhinge + fceff*(df$force*df$chillhinge)
+  df$bbforcechillint2 <- interceptbb + feff*df$force + chillcoef*df$chilllinear + fceff2*(df$force*df$chilllinear)
+  df$bbforcechillhingeint2 <- interceptbb + feff*df$force + chillcoef*df$chillhinge + fceff2*(df$force*df$chillhinge)
+  return(df)
+}
+
+chilldf<-make.chilldf(howlong,whenhingec,intercepcthinge,whenclinear,interceptclinear)
+
+plot(chilllinear~chill, data=chilldf, type="l", xlab="Chilling", ylab="Chill effect", col="blue")
+lines(chillhinge~chill, data=chilldf, lty=2, col="red")
+text (whenhingec -500,0-1,"threshold response", col = threshcol)
+text (whenhingec,0+10,"linear response", col = linecol)
+mtext ("When chilling decreases with warming:", side = 3, line = 1, adj = 0, cex = 0.9)
+
+plot(bbforce~temperature, data=chilldf, type="l", ylab="Budburst day")
+lines(bbforcechill~temperature, data=chilldf, col=linecol)
+lines(bbforcechillint~temperature, data=chilldf, col=linecol, lty=2)
+lines(bbforcechillint2~temperature, data=chilldf, col=linecol, lty=2)
+
+plot(bbforce~temperature, data=chilldf, type="l", ylab="Budburst day")
+lines(bbforcechillhinge~temperature, data=chilldf, col=threshcol)
+lines(bbforcechillhingeint~temperature, data=chilldf, col=threshcol, lty=2)
+lines(bbforcechillhingeint2~temperature, data=chilldf, col=threshcol, lty=2)
+
+
+dev.off()
+
+
+
+
