de054e1df2693efb35912ad8f7b639fe8c3e5bcb
DeirdreLoughnan <deirdre.loughnan@gmail.com>
2021-03-31 21:28:20 -0700

    tried fixing issue with study

--- analyses/traits/Rfiles/fakedata_3parm_1trait.R
+++ analyses/traits/Rfiles/fakedata_3parm_1trait.R
@@ -7,6 +7,8 @@ if(length(grep("deirdreloughnan", getwd()) > 0)) {
   setwd("/home/faith/Documents/github/ospree/ospree/analyses/jointmodel/") 
 }
 
+library(rstan)
+
 rm(list=ls()) 
 options(stringsAsFactors = FALSE)
 
@@ -14,26 +16,112 @@ Nstudy <- 10 # number of studies w/ traits
 Nspp <- 5 # number of species with traits
 
 # First making a data frame for the test trait data
-ntrt <- 10 # rep per trait
-N <- Nspp * ntrt * Nstudy
+trtrep <- 10 # rep per trait
+Ntrt <- Nspp * trtrep * Nstudy
 
+#make a dataframe for height
+trt.dat <- data.frame(matrix(NA, Ntrt, 3))
+names(trt.dat) <- c("species","study", "rep")
+trt.dat$rep <- c(1:Ntrt)
+trt.dat$species <- rep(c(1:Nspp), each = trtrep)
+trt.dat$study <- rep(c(1:Nstudy), each = trtrep)
+
+mu.trt <- 20
+sigma.trt <- 10
+ht <- rnorm(Nspp, mu.trt, sigma.trt)
+
+#adding ht data for ea. sp
+trt.dat$trt <- rep(ht, each = trtrep)
+
+#########################################################
 # Next, making a data frame for the pheno data
 
-nphen <- 4 # rep per pheno event 
-Nph.sp <- Nspp * nphen
+nphen <- 10 # rep per pheno event 
+Nph <- Nspp * nphen
+
+pheno.dat <- data.frame(matrix(NA, Nph, 2))
+names(pheno.dat) <- c("species", "rep")
+pheno.dat$rep <- c(1:Nph)
+pheno.dat$species <- rep(c(1:Nspp), each = nphen)
 
 
-traitstanpheno <- list(
-  yTraiti = trait.observe, 
-  N = length(trait.observe), # sample size for trait data is the same as phenology data 
-  n_spec = nsp,  # number of species is the same for traits and phenology data
-  species = dat2df$speciesID, 
-  study = dat2df$studyID, 
-  n_study = nstudy, 
-  yPhenoi = dat2df$phen.response, 
-  Nph = length(trait.observe), # sample size for trait data is the same as phenology data  
-  forcingi = dat2df$forcing,
-  photoi = data2df$photo,
-  chillingi = dat2df$chilling)  
+# Generating data for the cues:
+mu.force <- 20
+sigma.force <- 5
+forcingi <- rnorm(Nph, mu.force, sigma.force)
+pheno.dat$forcingi <- forcingi
 
+mu.chill <- 20
+sigma.chill <- 5
+chillingi <- rnorm(Nph, mu.chill, sigma.chill)
+pheno.dat$chillingi <- chillingi
 
+mu.photo <- 20
+sigma.photo <- 5
+photoi <- rnorm(Nph, mu.photo, sigma.photo)
+pheno.dat$photoi <- photoi
+
+# adding the species level differences
+mu.pheno.sp <- 150
+sigma.pheno.sp <- 2
+
+alpha.pheno.sp <- rnorm(Nspp, mu.pheno.sp, sigma.pheno.sp)
+pheno.dat$alpha.pheno.sp <- rep(alpha.pheno.sp, each = nphen)
+
+# Adding species variation in cue use:
+mu.force.sp <- -1 # negative bc warmer means earlier
+sigma.force.sp <- 0.1
+alpha.force.sp <- rnorm(Nspp, mu.force.sp, sigma.force.sp)
+
+mu.chill.sp <- -2 
+sigma.chill.sp <- 0.1
+alpha.chill.sp <- rnorm(Nspp, mu.chill.sp, sigma.chill.sp)
+
+mu.photo.sp <- -2 
+sigma.photo.sp <- 0.1
+alpha.photo.sp <- rnorm(Nspp, mu.photo.sp, sigma.photo.sp)
+
+#interaction between trait and phenology?
+betaTraitxchill <- -.8 
+betaTraitxphoto<- -.8 
+betaTraitxforce <- .8 
+
+#combine teh effects of forcing and species trait differences into a slope
+beta.forcing.sp1 <- alpha.force.sp + ht*betaTraitxforce
+beta.forcing.sp <- rep(beta.forcing.sp1, )
+pheno.dat$beta.forcing.sp <- rep(beta.forcing.sp, each = nphen)
+
+beta.chilling.sp1 <- alpha.chill.sp + ht*betaTraitxchill
+beta.chilling.sp <- rep(beta.chilling.sp1, )
+pheno.dat$beta.chilling.sp <- rep(beta.chilling.sp, each = nphen)
+
+beta.photo.sp1 <- alpha.photo.sp + ht*betaTraitxphoto
+beta.photo.sp <- rep(beta.photo.sp1, )
+pheno.dat$beta.photo.sp <- rep(beta.photo.sp, each = nphen)
+
+#general variance
+sigma.gen <- 2
+gen.var <- rnorm(Nph, 0, sigma.gen) 
+pheno.dat$gen.var <- gen.var
+
+#"run" the full model to simulate data 
+pheno.dat$doy.i <- pheno.dat$alpha.pheno.sp + pheno.dat$beta.forcing.sp * pheno.dat$forcingi +
+  pheno.dat$beta.chilling.sp * pheno.dat$chillingi + pheno.dat$beta.photo.sp * pheno.dat$photoi + pheno.dat$gen.var
+
+hist(pheno.dat$doy.i)
+
+trt.pheno <- list(
+  yTraiti = trt.dat$trt, 
+  N = length(trt.dat), # sample size for trait data is the same as phenology data 
+  n_spec = Nspp,  # number of species is the same for traits and phenology data
+  species = trt.dat$species, 
+  study = trt.dat$study, 
+  n_study = Nstudy, 
+  yPhenoi = pheno.dat$doy.i, 
+  Nph = length(pheno.dat), # sample size for trait data is the same as phenology data  
+  forcingi = pheno.dat$forcingi,
+  photoi = pheno.dat$photoi,
+  chillingi = pheno.dat$chillingi)  
+
+test.dat <- stan('stan/stan_joint_traitors.stan', data = trt.pheno,
+                 iter = 1000, warmup=1000)
