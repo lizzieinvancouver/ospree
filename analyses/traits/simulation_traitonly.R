bab6c632730877ed9720097ad4a6a3196f42e6c7
Geoffrey Legault <thegeoffrey@gmail.com>
2021-09-09 14:10:39 -0700

    update traitonly and add unbalanced traitonly test

--- analyses/traits/simulation_traitonly.R
+++ analyses/traits/simulation_traitonly.R
@@ -13,8 +13,8 @@ require(shinystan)
 options(mc.cores = 4)
 
 Nrep <- 50 # rep per trait
-Nstudy <- 12 # number of studies w/ traits (10 seems a little low for early simulation code; remember that you are estimating a distribution of this the same as for species)
-Nspp <- 12 # number of species with traits (making this 20 just for speed for now)
+Nstudy <- 13 # number of studies w/ traits (10 seems a little low for early simulation code; remember that you are estimating a distribution of this the same as for species)
+Nspp <- 13 # number of species with traits (making this 20 just for speed for now)
 
 # First making a data frame for the test trait data
 Ntrt <- Nspp * Nstudy * Nrep # total number of traits observations
@@ -28,15 +28,15 @@ trt.dat$study <- rep(c(1:Nstudy), each = Nspp)
 trt.dat$species <- rep(1:Nspp, Nstudy)
 
 # now generating the species trait data, here it is for height
-mu.grand <- 10 # the grand mean of the height model
-sigma.species <- 3 # we want to keep the variaiton across spp. high
+mu.grand <- 1.3 # the grand mean of the height model
+sigma.species <- 2 # we want to keep the variaiton across spp. high
 
 #the alphaTraitSp in Faiths original code:
 mu.trtsp <- rnorm(Nspp, 0, sigma.species)
 trt.dat$mu.trtsp <- rep(mu.trtsp, Nstudy) #adding ht data for ea. sp
 
 #now generating the effects of study
-sigma.study <- 5
+sigma.study <- 3
 mu.study <- rnorm(Nstudy, 0, sigma.study) #intercept for each study
 trt.dat$mu.study <- rep(mu.study, each = Nspp) # generate data for ea study
 
@@ -50,18 +50,18 @@ trt.dat$yTraiti <- mu.grand + trt.dat$mu.trtsp + trt.dat$mu.study + trt.dat$trt.
 ## Trait only stan model ###########################################################
 trait_data <- list(y = trt.dat$yTraiti, 
                    N = Ntrt, 
-                   n_variety = Nspp, 
+                   n_variety = nrow(trt.dat), 
                    variety = trt.dat$species, 
                    company = trt.dat$study, 
                    n_company = Nstudy,
-                   prior_a_grand_mu = 10,
-                   prior_a_grand_sigma = 3,
-                   prior_sigma_a_variety_mu = 3,
-                   prior_sigma_a_variety_sigma = 1,
-                   prior_sigma_a_company_mu = 5,
-                   prior_sigma_a_company_sigma = 2,
+                   prior_a_grand_mu = 1.3,
+                   prior_a_grand_sigma = 0.3,
+                   prior_sigma_a_variety_mu = 2,
+                   prior_sigma_a_variety_sigma = .75,
+                   prior_sigma_a_company_mu = 3,
+                   prior_sigma_a_company_sigma = 1.5,
                    prior_sigma_y_mu = 1,
-                   prior_sigma_y_sigma = .3) 
+                   prior_sigma_y_sigma = .25) 
 
 mdl.traitonly <- stan("stan/int_variety_company.stan",
                       data = trait_data,
@@ -69,6 +69,10 @@ mdl.traitonly <- stan("stan/int_variety_company.stan",
                       warmup = 2000,
                       chains = 4,
                       include = FALSE,
-                      pars = "mu_y")
+                      pars = "mu_y",
+                      seed = 202199)## ,
+                      ## control = list(max_treedepth = 15))
 
 summary(mdl.traitonly, pars = c("a_grand", "sigma_a_company", "sigma_a_variety", "sigma_y"))$summary
+
+summary(mdl.traitonly, pars = c("a_variety", "a_company"))$summary
