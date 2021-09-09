9b537f58eaecbc3dd0bbe07e33d937bd2b7c7083
AileneKane <ailene.ettinger@gmail.com>
2021-09-09 11:14:05 -0700

    Update ranges_simclimphen.R

--- analyses/ranges/ranges_simclimphen.R
+++ analyses/ranges/ranges_simclimphen.R
@@ -27,23 +27,24 @@ library(ggplot2)
 
 frostdamage <- 0
 fstar <- 200
-yearz <- 50 # 
-sitez <- 45 # sites, basically useful as *replicates*
-simsnum <- 10
-
+yearz <- 20 # 
+sitez <- 20 # sites, basically useful as *replicates*
+simsnum <- 5#
 # START inter/intra-annual variation parameter edits here
 dayswinter <- 120
 daysspring <- 90
-wintertemp <- -1
-springtemp <- 1
+
+wintemp_mean <- 1
+#sprtemp_mean <- 2#not used in this version- instead spring temp is 1 degree more than winter temp
 springtempinc <- 0.1
-sigma <- 4 # let's treat this as intra-annual ...
-sigmainter <- seq(0, 10, length.out=simsnum) # inter-annual, let's start by varying this ...
+sigma <- seq(0, 4, length.out=simsnum) # let's treat this as intra-annual (i.e., daily variation)- 
+sigmainter <- seq(0, 10, length.out=simsnum) # inter-annual, let's start by varying this ... 
+
 # END inter/intra-annual variation parameter edits here
 
 if(FALSE){ # not using just now ... as it's chilling
-cstar <- 0
-fstaradjforchill <- 3 # how much more GDD to you need based on diff from cstar at end of daystostart
+   cstar <- 0
+   fstaradjforchill <- 3 # how much more GDD to you need based on diff from cstar at end of daystostart
 }
 
 ## Step 2: Now I put together the seasonal temps, varying fstar (increases when chill is low) and calculate the sensitivities    
@@ -56,41 +57,59 @@ df <- data.frame(intervar=numeric(),
                  year=numeric(),
                  fstar=numeric(),
                  frostevents=numeric()) 
-    
-for (i in sigmainter){
-   for (j in 1:sitez){
-       # TO DO ... inter-annual variation ... and then intra-annual within it
-       # If you're working on that edit here ... START within looop edits
-       daily_temp <- sapply(rep(NA, yearz), function(x) c(rnorm(dayswinter, wintertemp + i, sigma),
-           (rnorm(daysspring, springtemp + i , sigma) + c(1:daysspring)*springtempinc)))
-       # END within loop edits here for inter/intra-annual variation
-       
-       gdd <- daily_temp
-       gdd[(gdd)<0] <- 0
-       leafout_date <- c()
-       frostevents <- c()
-       for (k in 1:ncol(daily_temp)){
-           gddsum <- sapply(1:ncol(gdd), function(x) (cumsum(gdd[dayswinter:nrow(gdd),x])))
-           leafout_date[k] <- min(which(gddsum[,k] > fstar)) # leafout_date unit of days *after* dayswinter
-           colddays <- which(daily_temp[,k] < frostdamage) # Cat, please check all my edits!
-           frostz <- which(colddays > leafout_date[k]) 
-           frostevents[k] <- length(frostz)
-           }
-           yearly_temp <- colMeans(daily_temp)
-           dfadd <- data.frame(intervar=rep(i, yearz),
-                               intravar=NA,
-                               meanwinter=rep(wintertemp, yearz),
-                               meanspring=rep(springtemp, yearz),
-                               meantemp=yearly_temp,
-                               rep=rep(j, yearz),
-                               year=c(1:yearz),
-                               fstar=rep(fstar),
-                               frostevents=frostevents)
-           df <- rbind(df, dfadd)     
-       }
-   }
 
+for (i in sigmainter){
+   
+   for (j in 1:sitez){
+      wintertemp <- rnorm(yearz, wintemp_mean,i)
+      springtemp <- wintertemp+1
+      
+      for (l in sigma){
+         
+         # TO DO ... inter-annual variation ... and then intra-annual within it
+         # If you're working on that edit here ... START within looop edits
+         daily_temp <- sapply(rep(NA, yearz), function(x) c(rnorm(dayswinter, wintertemp, l),
+                                                            (rnorm(daysspring, springtemp, l) + c(1:daysspring)*springtempinc)))
+         # END within loop edits here for inter/intra-annual variation
+         
+         gdd <- daily_temp
+         gdd[(gdd)<0] <- 0
+         leafout_date <- c()
+         frostevents <- c()
+         for (k in 1:ncol(daily_temp)){
+            gddsum <- sapply(1:ncol(gdd), function(x) (cumsum(gdd[dayswinter:nrow(gdd),x])))
+            leafout_date[k] <- min(which(gddsum[,k] > fstar)) # leafout_date unit of days *after* dayswinter
+            colddays <- which(daily_temp[,k] < frostdamage) # Cat, please check all my edits!
+            frostz <- which(colddays > leafout_date[k]) 
+            frostevents[k] <- length(frostz)
+         }
+         yearly_temp <- colMeans(daily_temp)
+         dfadd <- data.frame(intervar=rep(i, yearz),
+                             intravar=rep(l, yearz),
+                             meanwinter=rep(wintertemp, yearz),
+                             meanspring=rep(springtemp, yearz),
+                             meantemp=yearly_temp,
+                             rep=rep(j, yearz),
+                             year=c(1:yearz),
+                             fstar=rep(fstar),
+                             frostevents=frostevents)
+         df <- rbind(df, dfadd)     
+      }
+   }
+}
 # has some warnings sometimes? Last run I did worked... 
+
+#Make some simple plots to see if the code is working
+plot(df$intervar,df$intravar)#seems to be fitting every combination of intra and inter
+par(mfrow=c(2,4))
+boxplot(meanwinter~intervar,data=df)
+boxplot(meanspring~intervar,data=df)
+boxplot(meantemp~intervar,data=df)
+boxplot(frostevents~intervar,data=df)
+boxplot(meanwinter~intravar,data=df)
+boxplot(meanspring~intravar,data=df)
+boxplot(meantemp~intravar,data=df)
+boxplot(frostevents~intravar,data=df)
 
 # code chunks we may want ...
 
