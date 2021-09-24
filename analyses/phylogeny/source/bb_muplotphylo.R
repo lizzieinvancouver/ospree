63bc3f57055cf7ed07fec8f32ee09ef8a0d96cda
Ignacio Morales-Castilla <ignacio.moralesc@uah.es>
2021-09-24 09:28:32 +0200

    updated phylo figures

--- analyses/phylogeny/source/bb_muplotphylo.R
+++ analyses/phylogeny/source/bb_muplotphylo.R
@@ -92,3 +92,112 @@ muplotfx_phylo<-function(modelhere, nameforfig, width, height, ylim, xlim, leg1,
     #dev.off()
     
 }
+
+
+
+## aux function from https://github.com/joelnitta/jntools/blob/master/R/plots.R
+get_tips_in_ape_plot_order <- function (tree) {
+    assertthat::assert_that(inherits(tree, "phylo"))
+    # First filter out internal nodes
+    # from the the second column of the edge matrix
+    is_tip <- tree$edge[,2] <= length(tree$tip.label)
+    ordered_tips <- tree$edge[is_tip, 2]
+    # Use this vector to extract the tips in the right order
+    tree$tip.label[ordered_tips]
+}
+
+
+## modified version of the above function, tweaked to plot results from
+## phylogenetic models once a fit object has been renamed to be used by the
+## stan utility
+muplotfx_phylo_contmap<-function(modelhere, nameforfig, width, height, 
+                         ylim, xlim, leg1, leg2, posspsindata,cue){
+    #width = 7 
+    #height = 8 
+    #ylim =  c(0,194) 
+    #xlim = c(-20, 5)  
+    #leg1 = 12 
+    #leg2 = 3.5
+    spnum <- length(unique(bb.stan$spps))
+    par(xpd=FALSE)
+    
+    
+    slopeshere <- c("b_zf","b_zc","b_zp")
+    
+    
+    for(i in cue){#cue=1;i=cue
+        
+        ## forcing
+        cueresponse = summary(modelhere)$summary[posspsindata[[i]],"mean"]
+        phylo=multi2di(phylo)
+        obj.cue<-contMap(phylo,cueresponse,plot=FALSE)
+        obj.cue<-setMap(obj.cue,invert=TRUE)
+        obj.cue<-setMap(obj.cue,colors=c("yellow","darkcyan","purple"))
+       
+        dev.off()
+        par(mfrow=c(1,2),mar=c(0.5,0.5,1,0.1))
+        
+        plot.contMap(obj.cue,type = "phylogram",legend = 0.6*max(nodeHeights(phylo)),
+                     fsize = c(0.45, 0.7),ftype="off", 
+                     xlim=c(-0.1,1)*max(nodeHeights(phylo)),
+                     outline=FALSE,lwd=2,mar = c(1,1,2,0))
+        
+        
+        par(mar=c(5,6.5,2,1))
+        
+        plot(x=NULL,y=NULL, xlim=xlim, yaxt='n', ylim=ylim,
+             xlab="Model estimate change in days to budburst", ylab="", main="")
+        #axis(2, at=1.1, labels=c("Forcing", "Chilling", "Photoperiod")[cue], las=1)
+        abline(v=0, lty=2, col="darkgrey")
+        
+        #pos.y<-1
+        #pos.x<-mean(summary(modelhere)$summary[which(grepl(slopeshere[i] ,rownames(summary(modelhere)$summary),fixed=TRUE),)])
+        # need to add uncertainty intervals
+        #points(pos.x,pos.y,cex=1.5,pch=19,col="darkblue")
+        tips.order = get_tips_in_ape_plot_order(phylo)
+        par(xpd=TRUE) # so I can plot legend outside
+        
+        for(spsi in 1:spnum){#spsi=1
+           
+            spsiname = phylo$tip.label[spsi]
+            
+            pos.sps.i <- posspsindata[[i]][spsi]
+            pos.y.sps.i <- which(tips.order==spsiname)
+            pos.x.sps.i <- summary(modelhere)$summary[pos.sps.i,"mean"]
+            
+            jj<-which(obj.cue$tree$tip.label==spsiname)
+            kk<-which(obj.cue$tree$edge[,2]==jj)
+            col.sps.i <- setNames(obj.cue$cols[names(obj.cue$tree$maps[[kk]])[length(obj.cue$tree$maps[[kk]])]],
+                         NULL)
+            
+             
+            lines(summary(modelhere)$summary[pos.sps.i,c("25%","75%")],rep(pos.y.sps.i,2),
+                  col=col.sps.i)
+            points(pos.x.sps.i,pos.y.sps.i,cex=0.8, pch=my.pch[spsi], 
+                   col=col.sps.i)
+            
+            
+        text(xlim[1]-1,pos.y.sps.i,tips.order[pos.y.sps.i],cex=0.5,las=1,pos=2)
+            #axis(2,at=1:194,labels=,las=1,cex=0.5)
+
+                    }  #plot(obj.force,fsize=c(0.5,1),outline=FALSE,lwd=c(3,7),leg.txt="forcing")
+        par(xpd=F) # so I can plot legend outside
+        
+       
+       
+    }
+    
+    if(agiosponly){cexleg = 0.4}
+    if(gymnosonly){cexleg = 0.8}
+    
+    legend(leg1, leg2, sort(unique(gsub("_", " ", bb.stan$spps))), pch=my.pch[1:spnum],
+           col=alpha(my.pal[1:spnum], alphahere),
+           cex=cexleg, bty="n", text.font=3)
+    
+    
+    
+    
+    #dev.off()
+    
+}
+
