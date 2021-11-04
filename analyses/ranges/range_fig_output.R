695a73d011b0ff32db51cf211f92a88b5794651b
dbuona <dbuonaiuto@g.harvard.edu>
2021-11-04 12:35:48 -0400

    some more range tables and figures

--- analyses/ranges/range_fig_output.R
+++ analyses/ranges/range_fig_output.R
@@ -117,7 +117,7 @@ scrapegrandies<-function(x){
   photoyb$cue<-"photo"
   
   cuey<-rbind(photoyb,chillyb,forcyb)
-cuey %>% dplyr::group_by(cue) %>% dplyr::summarise(mu=mean(mu),trait_beta=mean(trait_beta))
+cuey %>% dplyr::group_by(cue) %>% dplyr::summarise(mu=mean(mu),trait_beta_sd=sd(trait_beta),trait_beta=mean(trait_beta))
 }  
 
 betas<-scrapebetas(threeparam_jnt.gdd)
@@ -219,3 +219,110 @@ two<-ggpubr::ggarrange(namplot,europlot,namplotstv,europlotstv,nrow=1,ncol=4,lab
 jpeg("./figures/mock1.jpeg",width = 10,height=8, units = "in",res = 300)
 ggpubr::ggarrange(one,two,nrow=2,ncol=1,heights=c(6,5))
 dev.off()
+###table of results
+
+cp_summary <- summary(threeparam_jnt.cp, pars = c("betaTraitxForcing","betaTraitxPhoto","betaTraitxChill"), probs = c(0.1, 0.9))$summary
+gdd_summary <- summary(threeparam_jnt.meangdd, pars = c("betaTraitxForcing","betaTraitxPhoto","betaTraitxChill"), probs = c(0.1, 0.9))$summary
+stv_summary <- summary(threeparam_jnt.stv, pars = c("betaTraitxForcing","betaTraitxPhoto","betaTraitxChill"), probs = c(0.1, 0.9))$summary
+gddlf_summary <- summary(threeparam_jnt.gdd, pars = c("betaTraitxForcing","betaTraitxPhoto","betaTraitxChill"), probs = c(0.1, 0.9))$summary
+
+cp_summary<-as.data.frame(cp_summary)
+cp_summary$climate<-"Mean Chill Portions"
+
+gdd_summary<-as.data.frame(gdd_summary)
+gdd_summary$climate<- "Mean GDDs"
+
+stv_summary<-as.data.frame(stv_summary)
+stv_summary$climate<-"STV"
+
+gddlf_summary<-as.data.frame(gddlf_summary)
+gddlf_summary$climate<-"Var. GGD to last frost"
+
+sums<-rbind(cp_summary,gdd_summary,stv_summary,gddlf_summary)
+sums$paremeter<-rownames(cp_summary)
+
+sums<-dplyr::select(sums,mean,`10%`,`90%`,paremeter,climate)
+goot<-pivot_wider(data = sums, id_cols = climate, names_from =paremeter, values_from = c("mean","10%", "90%"))
+colnames(goot)
+
+###use this for table
+goot2<- goot[, c(1, 2,5,8,3,6,9,4,7,10 )]
+
+cp_summary_nam <- summary(cp_jnt.nam, pars = c("betaTraitxForcing","betaTraitxPhoto","betaTraitxChill"), probs = c(0.1, 0.9))$summary
+gdd_summary_nam <- summary(gdd_jnt.nam, pars = c("betaTraitxForcing","betaTraitxPhoto","betaTraitxChill"), probs = c(0.1, 0.9))$summary
+stv_summary_nam <- summary(stv_jnt.nam, pars = c("betaTraitxForcing","betaTraitxPhoto","betaTraitxChill"), probs = c(0.1, 0.9))$summary
+gddlf_summary_nam <- summary(gddlf_jnt.nam, pars = c("betaTraitxForcing","betaTraitxPhoto","betaTraitxChill"), probs = c(0.1, 0.9))$summary
+
+cp_summary_nam<-as.data.frame(cp_summary_nam)
+cp_summary_nam$climate<-"Mean Chill Portions"
+
+gdd_summary_nam<-as.data.frame(gdd_summary_nam)
+gdd_summary_nam$climate<- "Mean GDDs"
+
+stv_summary_nam<-as.data.frame(stv_summary_nam)
+stv_summary_nam$climate<-"STV"
+
+gddlf_summary_nam<-as.data.frame(gddlf_summary_nam)
+gddlf_summary_nam$climate<-"Var. GGD to last frost"
+
+sums_nam<-rbind(cp_summary_nam,gdd_summary_nam,stv_summary_nam,gddlf_summary_nam)
+sums_nam$paremeter<-rownames(cp_summary_nam)
+
+sums_nam<-dplyr::select(sums_nam,mean,`10%`,`90%`,paremeter,climate)
+goot_nam<-pivot_wider(data = sums_nam, id_cols = climate, names_from =paremeter, values_from = c("mean","10%", "90%"))
+
+
+###use this for table
+goot2_nam<- goot_nam[, c(1, 2,5,8,3,6,9,4,7,10 )]
+
+
+cp_summary_eu <- summary(cp_jnt.eu, pars = c("betaTraitxForcing","betaTraitxPhoto","betaTraitxChill"), probs = c(0.1, 0.9))$summary
+gdd_summary_eu <- summary(gdd_jnt.eu, pars = c("betaTraitxForcing","betaTraitxPhoto","betaTraitxChill"), probs = c(0.1, 0.9))$summary
+stv_summary_eu <- summary(stv_jnt.eu, pars = c("betaTraitxForcing","betaTraitxPhoto","betaTraitxChill"), probs = c(0.1, 0.9))$summary
+gddlf_summary_eu <- summary(gddlf_jnt.eu, pars = c("betaTraitxForcing","betaTraitxPhoto","betaTraitxChill"), probs = c(0.1, 0.9))$summary
+
+cp_summary_eu<-as.data.frame(cp_summary_eu)
+cp_summary_eu$climate<-"Mean Chill Portions"
+
+gdd_summary_eu<-as.data.frame(gdd_summary_eu)
+gdd_summary_eu$climate<- "Mean GDDs"
+
+stv_summary_eu<-as.data.frame(stv_summary_eu)
+stv_summary_eu$climate<-"STV"
+
+gddlf_summary_eu<-as.data.frame(gddlf_summary_eu)
+gddlf_summary_eu$climate<-"Var. GGD to last frost"
+
+sums_eu<-rbind(cp_summary_eu,gdd_summary_eu,stv_summary_eu,gddlf_summary_eu)
+sums_eu$paremeter<-rownames(cp_summary_eu)
+
+sums_eu<-dplyr::select(sums_eu,mean,`10%`,`90%`,paremeter,climate)
+goot_eu<-pivot_wider(data = sums_eu, id_cols = climate, names_from =paremeter, values_from = c("mean","10%", "90%"))
+
+
+###use this for table
+goot2_eu<- goot_eu[, c(1, 2,5,8,3,6,9,4,7,10 )]
+
+goot2_eu$continent<-"Europe"
+goot2_nam$continent<-"N. America"
+
+contsums<-rbind(goot2_eu,goot2_nam)
+contsums<-contsums[, c(11,1,2,3,4,5,6,7,8,9,10 )]
+library(xtable)
+xtable(goot2)
+xtable(contsums)
+
+
+beetas_summary_eu <- summary(gddlf_jnt.eu, pars = c("muChillSp","muPhotoSp","muForceSp"), probs = c(.1,0.25, 0.75,.9))$summary
+beetas_summary_nam <- summary(gddlf_jnt.nam, pars = c("muChillSp","muPhotoSp","muForceSp"), probs = c(.1,0.25, 0.75,.9))$summary
+
+beetas_summary_eu<-as.data.frame(beetas_summary_eu)
+beetas_summary_nam<-as.data.frame(beetas_summary_nam)
+
+beetas_summary_eu$continent<-"Eu"
+beetas_summary_nam$continent<-"N.A"
+
+beetas<-rbind(beetas_summary_eu,beetas_summary_nam)
+beetas<-select(beetas, mean,`10%`,`25%`,`75%`,`90%`, continent)
+
+xtable(beetas)
