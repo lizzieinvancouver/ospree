29eeb202cec27a8f46a640ab7953195151271f0b
DeirdreLoughnan <deirdre.loughnan@gmail.com>
2022-01-14 16:37:12 -0800

    worked on traitors methods

--- analyses/traits/Rfiles/heightresample.R
+++ analyses/traits/Rfiles/heightresample.R
@@ -4,127 +4,74 @@
 ## To make the height data more managable, we are subsampling the data to limit the number of height measurements per species to 5000
 
 #housekeeping
-rm(list=ls()) 
+rm(list=ls())
 options(stringsAsFactors = FALSE)
 
 require(dplyr)
+library(stringr)
+library(plyr)
 
-# Set working directory: 
+# Set working directory:
 # Anyone else working with this code should add their info/path here
 if(length(grep("deirdreloughnan", getwd())>0)) {  setwd("~/Documents/github/ospree/analyses/traits")
 } else if
-(length(grep("Lizzie", getwd())>0)) {   setwd("~/Documents/git/projects/treegarden/budreview/ospree/analyses/traits") 
-} 
+(length(grep("Lizzie", getwd())>0)) {   setwd("~/Documents/git/projects/treegarden/budreview/ospree/analyses/traits")
+}
 
 # Get the data
-dat1 <- read.csv("input/try_bien_nodups_1.csv") 
-dat2 <- read.csv("input/try_bien_nodups_2.csv") 
+dat1 <- read.csv("input/try_bien_nodups_1.csv")
+dat2 <- read.csv("input/try_bien_nodups_2.csv")
 
-dat <- rbind(dat1, dat2)
-names(dat)
+ dat <- rbind(dat1, dat2)
+# names(dat)
 # dat <- dat[,c("SpeciesName", "TraitName", "TraitValue", "UnitName", "Latitude",
 #     "Longitude", "project_pi", "database", "DatasetID")]
 # names(dat) <- c("latbi", "traitname", "traitvalue", "unit", "lat", "long", "project_pi",
 #     "database", "DatasetID")
 
-heighter <- subset(dat, traitname == "Plant_height_vegetative")
-
 # Percentage of data that is height?
-nrow(heighter)/nrow(dat)
+#nrow(heighter)/nrow(dat)
+traitors.sp <- c("Acer_pensylvanicum", "Acer_pseudoplatanus", "Acer_saccharum", "Aesculus_hippocastanum", "Alnus_glutinosa", "Alnus_incana", "Betula_pendula", "Betula_populifolia", "Corylus_avellana", "Fagus_grandifolia","Fagus_sylvatica", "Fraxinus_excelsior", "Juglans_regia", "Populus_tremula", "Prunus_padus", "Prunus_serotina", "Quercus_alba", "Quercus_coccifera", "Quercus_ilex", "Quercus_petraea", "Quercus_robur", "Quercus_rubra", "Quercus_velutina", "Rhamnus_cathartica", "Sorbus_aucuparia", "Ulmus_pumila")
+#
+# # Subset data to traitors species list
+ tdat <- subset(dat, dat$speciesname %in% traitors.sp)
 
-# Eeek! Definitely some name cleaning still needed
-sort(unique(heighter$speciesname)) # 78 species
+ height <- subset(tdat, traitname == "Plant_height_vegetative")
 
-# Just do one example species for now... 
-testsp <- subset(heighter, speciesname == "Sorbus_aria")
-nrow(testsp)
+# For this trait we are really only interested in adults - selecting only values greater than 1.42 m, the height at which you can measure DBH
+small <- subset(height, traitvalue < 1.42) # this is 2.1% of the data 
 
-howmanydf <- data.frame(resample.n = numeric(), mean = numeric(), sd = numeric())
-for(i in 1:15000){
-    mysample <- sample(testsp$traitvalue, i)
-    dfadd <- data.frame(resample.n = i, mean = mean(mysample), sd = sd(mysample))
-    howmanydf <- rbind(howmanydf, dfadd)
-}
+adult <- subset(height, traitvalue > 1.42)
 
-# the central limit theorem!
-plot(mean~resample.n, data=howmanydf)
-plot(sd~resample.n, data=howmanydf)
+#other.trt <- subset(dat, traitname != "Plant_height_vegetative")
 
-# need to repeat with other species and across studyID? ... and,
-# come up with methods to write in paper that we like
+# Remove duplicated height values:
+adult$dup <- duplicated(adult[, c("traitname", "traitvalue","unitname","latitude","longitude","piname","speciesname","database" )])
 
-# DL edits: 
-# Based on Lizzie's code above on line 40-49, I think we could have as few as 5000? 
+temp <- subset(adult, dup == "TRUE") # 3456
+adultNoDup<- subset(adult, dup != "TRUE") #629094
 
-# But how many species have more than 5000 rows of data? 
-ht.sp <- heighter %>%
-  group_by(speciesname) %>%
-  summarise(no_obs = length(speciesname))
-
-# howmanydf <- data.frame(resample.n = numeric(), mean = numeric(), sd = numeric())
-# species <- ht.sp$speciesname
-# obs <- ht.sp$no_obs
-# 
-# for (sp in 1 : length(species)){
-#   htdat <- subset(heighter, speciesname == species[4])
-#   obs <- nrow(htdat)
-#  
-# if(obs > 5000){
-#       for (i in 1:5000){
-#     mysample <- sample(htdat$traitvalue, i)
-#     dfadd <- data.frame(resample.n = i, mean = mean(mysample), sd = sd(mysample))
-#     } else {
-#       dfadd <- subst(heighter, speciesname == i)
-#     }
-#     howmanydf <- rbind(howmanydf, htdat)
-#   }
-#}
-
-# I can't figure out how to get it all working in one loop, so here is my more round about approach:
+## sampling height: start by separating out the species with few obs
 a.lot.ht <- c("Quercus_ellipsoidalis", "Alnus_rubra", "Fraxinus_nigra", "Populus_grandidentata", "Betula_lenta", "Betula_alleghaniensis", "Betula_papyrifera", "Fagus_grandifolia", "Quercus_velutina", "Prunus_serotina", "Quercus_rubra", "Acer_saccharum", "Quercus_alba")
 
-sm <- heighter[!heighter$speciesname %in% a.lot.ht, ]
-sm.sub <- sm[, c("traitname", "traitvalue", "speciesname")]
-lg <- heighter[heighter$speciesname %in% a.lot.ht, ]
+few <- adultNoDup[!adultNoDup$speciesname %in% a.lot.ht, ] 
 
-#resampling for sp with a large amount of height data
-howmanydf <- data.frame(resample.n = numeric(), mean = numeric(), sd = numeric())
-species <- unique(lg$speciesname)
+alot <- adultNoDup[adultNoDup$speciesname %in% a.lot.ht, ] #769505
 
-sp.sub <- species[1:3]
-for (sp in 1: length(species)){
-  testsp <- subset(lg, speciesname == species[sp])
-for(i in 1:5000){
-  mysample <- sample(testsp$traitvalue, i)
-  dfadd <- data.frame(resample.n = i, mean = mean(mysample), sd = sd(mysample))
-  dfadd$speciesname <- species[sp]
-  howmanydf <- rbind(howmanydf, dfadd)
-}
-}
-
-# the central limit theorem!
-plot(mean~resample.n, data=howmanydf)
-plot(sd~resample.n, data=howmanydf)
-
-
-# There are 12 species that have over 5000 observations, also of note, there are 31 of th 72 species that have less than 10 obs. 
-
-
-#resampling for sp with a large amount of height data
+# now sampling for species with a lot of data
 ht <- data.frame(speciesname = character(), height = numeric())
-species <- unique(lg$speciesname)
-
+species <- unique(alot$speciesname)
 
 for (sp in 1: length(species)){
-  testsp <- subset(lg, speciesname == species[sp])
-    mysample <- sample(testsp$traitvalue, 5000)
-      dfadd <- data.frame(speciesname = species[sp], traitvalue = mysample)
-    # mysample$speciesname <- species[sp]
-    ht <- rbind(ht, dfadd)
+  testsp <- subset(alot, speciesname == species[sp])
+  mysample <- sample_n(testsp, 3000)
+  #dfadd <- data.frame(speciesname = species[sp], traitvalue = mysample)
+  # mysample$speciesname <- species[sp]
+  ht <- rbind(ht, mysample)
 }
 
-ht$traitname <- "Plant_height_vegetative"
+heightData <- rbind(few, ht)
 
-ht.sample <- rbind(sm.sub, ht)
-
-unique(ht.sample$speciesname)
+# head(ht.sample)
+# hist(ht.sample$traitvalue)
+# length(unique(ht.sample$speciesname))
