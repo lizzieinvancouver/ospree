6327f29954eb47cdc44e3d81a970d7605d7bad08
DeirdreLoughnan <deirdre.loughnan@gmail.com>
2022-02-05 21:15:19 -0800

    relabeled traitors figures

--- analyses/traits/results_Height_plot.R
+++ analyses/traits/results_Height_plot.R
@@ -93,11 +93,11 @@ betaTraitPhotoeff.26 <- mean(posteriorOld$betaTraitxPhoto) #-0.2202021
 
 ## Species to plot and other plotting parameters
 plot.sp <- c("Corylus_avellana", "Acer_pseudoplatanus") 
-col.sp <- c(rgb(72 / 255, 38 / 255, 119 / 255, alpha = 0.8), rgb(149 / 255, 216 / 255, 64 / 255, alpha = 0.9))
-col1.sp <- c(rgb(72 / 255, 38 / 255, 119 / 255, alpha = 0.14), rgb(149 / 255, 216 / 255, 64 / 255, alpha = 0.2))
-col2.sp <- c(rgb(72 / 255, 38 / 255, 119 / 255, alpha = 0.4), rgb(149 / 255, 216 / 255, 64 / 255, alpha = 0.5))
+col.sp <- c( rgb(149 / 255, 216 / 255, 64 / 255, alpha = 0.9), rgb(72 / 255, 38 / 255, 119 / 255, alpha = 0.8))
+col1.sp <- c( rgb(149 / 255, 216 / 255, 64 / 255, alpha = 0.2), rgb(72 / 255, 38 / 255, 119 / 255, alpha = 0.14))
+col2.sp <- c( rgb(149 / 255, 216 / 255, 64 / 255, alpha = 0.5), rgb(72 / 255, 38 / 255, 119 / 255, alpha = 0.4))
 
-pdf(file = "figures/results_height_forcing_37spp.pdf", width = 7, height = 6)
+pdf(file = "figures/results_height_forcing_37spp_ac.pdf", width = 7, height = 6)
 ## Plotting
 ### Forcing
 par(mar = c(5, 5, 2, 2))
@@ -133,15 +133,15 @@ for(i in 1:length(plot.sp)){
     }
     points(forceadj1 ~ jitter(force.z, factor = 0.75), data = ospree.temp, pch = 21, col = "black", bg = col.sp[i], cex = 1)
 }
-legend("topright", legend = c(expression(paste("Low trait  (", italic("Corylus avellana"), ")")),
-                              expression(paste("High trait  (", italic("Acer pseudoplatanus"), ")")),
+legend("topright", legend = c(expression(paste("Acquisitive  (", italic("Corylus avellana"), ")")),
+                              expression(paste("Conservative  (", italic("Acer pseudoplatanus"), ")")),
                               expression(paste("Trait effect", " = 0", "  (50% interval)", sep = "")),
                               expression(paste("Full model", "  (50% interval)"))),
        col = c("black", "black", rgb(0, 0, 0, alpha = 0.18), rgb(0, 0, 0, alpha = 0.85)), pt.bg = c(col.sp, NA, NA),
        inset = 0.02, pch = c(21, 21, 15, 15), cex = 0.85, bty = "n")
 dev.off()
 
-pdf(file = "figures/results_height_chilling_37spp.pdf", width = 7, height = 6)
+pdf(file = "figures/results_height_chilling_37spp_ac.pdf", width = 7, height = 6)
 ## Plotting
 ### Chilling
 par(mar = c(5, 5, 2, 2))
@@ -177,8 +177,8 @@ for(i in 1:length(plot.sp)){
     }
     points(chilladj1 ~ jitter(chill.z, factor = 0.75), data = ospree.temp, pch = 21, col = "black", bg = col.sp[i], cex = 1)
 }
-legend("topright", legend = c(expression(paste("Low trait  (", italic("Corylus avellana"), ")")),
-                              expression(paste("High trait  (", italic("Acer pseudoplatanus"), ")")),
+legend("topright", legend = c(expression(paste("Acquisitive  (", italic("Corylus avellana"), ")")),
+                              expression(paste("Conservative  (", italic("Acer pseudoplatanus"), ")")),
                               expression(paste("Trait effect", " = 0", "  (50% interval)", sep = "")),
                               expression(paste("Full model", "  (50% interval)"))),
        col = c("black", "black", rgb(0, 0, 0, alpha = 0.18), rgb(0, 0, 0, alpha = 0.85)), pt.bg = c(col.sp, NA, NA),
@@ -186,7 +186,7 @@ legend("topright", legend = c(expression(paste("Low trait  (", italic("Corylus a
 dev.off()
 
 
-pdf(file = "figures/results_height_photoperiod_37spp.pdf", width = 7, height = 6)
+pdf(file = "figures/results_height_photoperiod_37spp_ac.pdf", width = 7, height = 6)
 ## Plotting
 ### Photoperiod
 par(mar = c(5, 5, 2, 2))
@@ -222,8 +222,8 @@ for(i in 1:length(plot.sp)){
     }
     points(photoadj1 ~ jitter(photo.z, factor = 0.75), data = ospree.temp, pch = 21, col = "black", bg = col.sp[i], cex = 1)
 }
-legend("topright", legend = c(expression(paste("Low trait  (", italic("Corylus avellana"), ")")),
-                              expression(paste("High trait  (", italic("Acer pseudoplatanus"), ")")),
+legend("topright", legend = c(expression(paste("Acquisitive  (", italic("Corylus avellana"), ")")),
+                              expression(paste("Conservative  (", italic("Acer pseudoplatanus"), ")")),
                               expression(paste("Trait effect", " = 0", "  (50% interval)", sep = "")),
                               expression(paste("Full model", "  (50% interval)"))),
        col = c("black", "black", rgb(0, 0, 0, alpha = 0.18), rgb(0, 0, 0, alpha = 0.85)), pt.bg = c(col.sp, NA, NA),
