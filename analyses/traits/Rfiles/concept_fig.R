f529406392c4df1b4d75f6ca011a6c718984da9f
DeirdreLoughnan <deirdre.loughnan@gmail.com>
2022-11-26 17:37:46 -0800

    working on concept fig

--- analyses/traits/Rfiles/concept_fig.R
+++ analyses/traits/Rfiles/concept_fig.R
@@ -1,6 +1,6 @@
 # started Jan 31, 2022 by Deirdre
-rm(list=ls())
-options(stringsAsFactors = FALSE)
+# rm(list=ls())
+# options(stringsAsFactors = FALSE)
 
 # aim of this code is to make a conceptual figure for the traitors ms:
 
@@ -10,49 +10,71 @@ col.sp <- c(rgb(72 / 255, 38 / 255, 119 / 255, alpha = 0.8), rgb(149 / 255, 216 
 col1.sp <- c(rgb(72 / 255, 38 / 255, 119 / 255, alpha = 0.14), rgb(149 / 255, 216 / 255, 64 / 255, alpha = 0.2))
 col2.sp <- c(rgb(72 / 255, 38 / 255, 119 / 255, alpha = 0.4), rgb(149 / 255, 216 / 255, 64 / 255, alpha = 0.5))
 
+put.fig.letter <- function(label, location="topleft", x=NULL, y=NULL, 
+                           offset=c(0, 0), ...) {
+  if(length(label) > 1) {
+    warning("length(label) > 1, using label[1]")
+  }
+  if(is.null(x) | is.null(y)) {
+    coords <- switch(location,
+                     topleft = c(0.05,0.98),
+                     topcenter = c(0.5525,0.98),
+                     topright = c(0.985, 0.98),
+                     bottomleft = c(0.015, 0.02), 
+                     bottomcenter = c(0.5525, 0.02), 
+                     bottomright = c(0.985, 0.02),
+                     c(0.015, 0.98) )
+  } else {
+    coords <- c(x,y)
+  }
+  this.x <- grconvertX(coords[1] + offset[1], from="nfc", to="user")
+  this.y <- grconvertY(coords[2] + offset[2], from="nfc", to="user")
+  text(labels=label[1], x=this.x, y=this.y, xpd=T, cex = 2, ...)
+}
+
 # pdf("figures/conceptFig.pdf", width = 18, height = 5)
 # par(bg = "pink", mfrow = c(1,3))
-plot(0, type="n", ylab="Day of budburst", xlab = "Cue", xlim =c(-2,2), ylim =c(0, 150), cex =2, cex.axis = 1.5, cex.lab = 1.2)
-segments(x0 = -1.85, x1 = 1.85, y0 = 130, y1 = 35,lwd =5, lty =2, col = col1.sp[1])
-segments(x0 = -1.85, x1 = 1.85, y0 = 75, y1 = 5,lwd =5, lty =2, col = col2.sp[2])
-segments(x0 = -1.65, x1 = 1.65, y0 = 150, y1 = 17, lwd =5, lty = 1, col = col.sp[1])
-segments(x0 = -1.85, x1 = 1.45, y0 = 85, y1 = 0,lwd =5, lty = 1, col = col.sp[2])
-
-my.label <- paste("a", ".", sep="")
-put.fig.letter(label=my.label, location= "topleft", font=2)
-
-plot(0, type="n", ylab="Day of budburst", xlab = "Cue", xlim =c(-2,2), ylim =c(0, 150), cex =2, cex.axis = 1.5, cex.lab = 1.2)
-segments(x0 = -1.85, x1 = 1.85, y0 = 130, y1 = 45,lwd =5, lty =2, col = col1.sp[1])
-segments(x0 = -1.85, x1 = 1.85, y0 = 100, y1 = 15,lwd =5, lty =2, col = col2.sp[2])
-segments(x0 = -1.85, x1 = 1.85, y0 = 134, y1 = 49, lwd =5, lty = 1, col = col.sp[1])
-segments(x0 = -1.85, x1 = 1.85, y0 = 104, y1 = 19,lwd =5, lty = 1, col = col.sp[2])
-
-my.label <- paste("b", ".", sep="")
-put.fig.letter(label=my.label, location= "topleft", font=2)
-
-plot(0, type="n", ylab="Day of budburst", xlab = "Cue", xlim =c(-2,2), ylim =c(0, 150), cex =2, cex.axis = 1.5, cex.lab = 1.2)
-segments(x0 = -1.60, x1 = 1.75, y0 = 147, y1 = 30, lwd =5, lty = 2, col = col2.sp[2])
-segments(x0 = -1.85, x1 = 1.45, y0 = 90, y1 = 10,lwd =5, lty = 2, col = col2.sp[1])
-segments(x0 = -1.85, x1 = 1.85, y0 = 135, y1 = 45,lwd =5, lty = 1, col = col.sp[2])
-segments(x0 = -1.85, x1 = 1.85, y0 = 80, y1 = 13,lwd =5, lty = 1, col = col.sp[1])
-
-legend("topright", legend = c(expression("Conservative"),
-                              expression("Acquisitive"),
-                              expression(paste("Trait effect", " = 0", sep = "")),
-                              expression(paste("Full model"))),
-       col = c(col.sp[1], col.sp[2],col2.sp[1], col2.sp[2]), pt.bg = c(col.sp, NA, NA),
-       inset = 0.02, lty = c(1,1,3,3), lwd = c(5,5,5,5), cex = 1, bty = "n")
-
-my.label <- paste("c", ".", sep="")
-put.fig.letter(label=my.label, location= "topleft", font=2)
-
-# legend("topright", legend = c(expression(paste("Low trait")),
-#                               expression(paste("High trait")),
+# plot(0, type="n", ylab="Day of budburst", xlab = "Cue", xlim =c(-2,2), ylim =c(0, 150), cex =2, cex.axis = 1.5, cex.lab = 1.2)
+# segments(x0 = -1.85, x1 = 1.85, y0 = 130, y1 = 35,lwd =5, lty =2, col = col1.sp[1])
+# segments(x0 = -1.85, x1 = 1.85, y0 = 75, y1 = 5,lwd =5, lty =2, col = col2.sp[2])
+# segments(x0 = -1.65, x1 = 1.65, y0 = 150, y1 = 17, lwd =5, lty = 1, col = col.sp[1])
+# segments(x0 = -1.85, x1 = 1.45, y0 = 85, y1 = 0,lwd =5, lty = 1, col = col.sp[2])
+# 
+# my.label <- paste("a", ".", sep="")
+# put.fig.letter(label=my.label, location= "topleft", font=2)
+# 
+# plot(0, type="n", ylab="Day of budburst", xlab = "Cue", xlim =c(-2,2), ylim =c(0, 150), cex =2, cex.axis = 1.5, cex.lab = 1.2)
+# segments(x0 = -1.85, x1 = 1.85, y0 = 130, y1 = 45,lwd =5, lty =2, col = col1.sp[1])
+# segments(x0 = -1.85, x1 = 1.85, y0 = 100, y1 = 15,lwd =5, lty =2, col = col2.sp[2])
+# segments(x0 = -1.85, x1 = 1.85, y0 = 134, y1 = 49, lwd =5, lty = 1, col = col.sp[1])
+# segments(x0 = -1.85, x1 = 1.85, y0 = 104, y1 = 19,lwd =5, lty = 1, col = col.sp[2])
+# 
+# my.label <- paste("b", ".", sep="")
+# put.fig.letter(label=my.label, location= "topleft", font=2)
+# 
+# plot(0, type="n", ylab="Day of budburst", xlab = "Cue", xlim =c(-2,2), ylim =c(0, 150), cex =2, cex.axis = 1.5, cex.lab = 1.2)
+# segments(x0 = -1.60, x1 = 1.75, y0 = 147, y1 = 30, lwd =5, lty = 2, col = col2.sp[2])
+# segments(x0 = -1.85, x1 = 1.45, y0 = 90, y1 = 10,lwd =5, lty = 2, col = col2.sp[1])
+# segments(x0 = -1.85, x1 = 1.85, y0 = 135, y1 = 45,lwd =5, lty = 1, col = col.sp[2])
+# segments(x0 = -1.85, x1 = 1.85, y0 = 80, y1 = 13,lwd =5, lty = 1, col = col.sp[1])
+# 
+# legend("topright", legend = c(expression("Conservative"),
+#                               expression("Acquisitive"),
 #                               expression(paste("Trait effect", " = 0", sep = "")),
-#                               expression(paste("Full model"))))
-
-#dev.off()
-par(bg = "white")  
+#                               expression(paste("Full model"))),
+#        col = c(col.sp[1], col.sp[2],col2.sp[1], col2.sp[2]), pt.bg = c(col.sp, NA, NA),
+#        inset = 0.02, lty = c(1,1,3,3), lwd = c(5,5,5,5), cex = 1, bty = "n")
+# 
+# my.label <- paste("c", ".", sep="")
+# put.fig.letter(label=my.label, location= "topleft", font=2)
+# 
+# # legend("topright", legend = c(expression(paste("Low trait")),
+# #                               expression(paste("High trait")),
+# #                               expression(paste("Trait effect", " = 0", sep = "")),
+# #                               expression(paste("Full model"))))
+# 
+# #dev.off()
+# par(bg = "white")  
 
 
 # Trying simulations to generate figures:
@@ -80,45 +102,45 @@ par(bg = "white")
 # For traits like height, we would expect tall trees to bb earlier with more chilling, which would require a larger trait effect - ie larger values of betaTraitxChill
 
 # Define constant parameters:
-alphaChillSp <- -0.5
-mu_grand_sp <- 5
-betaTraitxChill <- c(-0.5,-1, 0, 0.5, 1) # Start with a positive value, I also want to try: negative values, larger values, smaller values
+# alphaChillSp <- -0.5
+# mu_grand_sp <- 5
+# betaTraitxChill <- c(-0.5,-1, 0, 0.5, 1) # Start with a positive value, I also want to try: negative values, larger values, smaller values
+# 
+# alphaPhenoSp <- 5
+# betaForceSp <- 5
+# forcei <- 2
+# 
+# betaPhotoSp <- 1
+# photoi <- 3
+# 
+# chilli <- rnorm(2000, 20, 10) #x-axis
+# # parameters to simulate/that vary:
+# 
+# colors <- c("#cc6a70ff", "#7e4e90ff","#55C667FF","#287D8EFF", "cyan4")
+# 
+# plot(chilli,yPhenoi, type = "n", xlim = c(-15,15), ylim =c(-150, 100))
+# 
+# for (b in 1:length(betaTraitxChill)){
+# betaChillSp = alphaChillSp + betaTraitxChill[b] * mu_grand_sp
+# 
+# yPhenoi <- alphaPhenoSp + betaForceSp * forcei + betaPhotoSp * photoi + betaChillSp * chilli
+# 
+# points(chilli,yPhenoi, type = "l", col= colors[b], lwd = 3)
+# }
+# 
+# legend("bottomleft",legend = c(expression("betaTraitxChill = -0.5"),
+#                             expression("betaTraitxChill = -1"),
+#                             expression("betaTraitxChill = 0"),
+#                             expression("betaTraitxChill = 0.5"),
+#                             expression("betaTraitxChill = 1")),
+#        #col = c("black", "black", "black", "black","black"),
+#        #pt.bg = c("#042333ff","#cc6a70ff","#593d9cff","#f9b641ff","#13306dff","#efe350ff","#eb8055ff"),
+#        col = c( "#cc6a70ff", "#7e4e90ff","#55C667FF","#287D8EFF", "cyan4"),
+#        inset = 0.02, pch = c(21, 21, 21, 21,21 ), cex = 0.75, bty = "n")
 
-alphaPhenoSp <- 5
-betaForceSp <- 5
-forcei <- 2
-
-betaPhotoSp <- 1
-photoi <- 3
-
-chilli <- rnorm(2000, 20, 10) #x-axis
-# parameters to simulate/that vary:
-
-colors <- c("#cc6a70ff", "#7e4e90ff","#55C667FF","#287D8EFF", "cyan4")
-
-plot(chilli,yPhenoi, type = "n", xlim = c(-15,15), ylim =c(-150, 100))
-
-for (b in 1:length(betaTraitxChill)){
-betaChillSp = alphaChillSp + betaTraitxChill[b] * mu_grand_sp
-
-yPhenoi <- alphaPhenoSp + betaForceSp * forcei + betaPhotoSp * photoi + betaChillSp * chilli
-
-points(chilli,yPhenoi, type = "l", col= colors[b], lwd = 3)
-}
-
-legend("bottomleft",legend = c(expression("betaTraitxChill = -0.5"),
-                            expression("betaTraitxChill = -1"),
-                            expression("betaTraitxChill = 0"),
-                            expression("betaTraitxChill = 0.5"),
-                            expression("betaTraitxChill = 1")),
-       #col = c("black", "black", "black", "black","black"),
-       #pt.bg = c("#042333ff","#cc6a70ff","#593d9cff","#f9b641ff","#13306dff","#efe350ff","#eb8055ff"),
-       col = c( "#cc6a70ff", "#7e4e90ff","#55C667FF","#287D8EFF", "cyan4"),
-       inset = 0.02, pch = c(21, 21, 21, 21,21 ), cex = 0.75, bty = "n")
-
-par(mfrow = c(1,3))
+#par(mfrow = c(1,3))
 ## Ok, if the above is correct, then yes the intercepts can vary
-pdf("figures/conceptualFigure3.pdf", width = 12, height = 5)
+#pdf("figures/conceptualFigure3.pdf", width = 12, height = 5)
 
 alphaChillSp <- -2
 mu_grand_sp <- c(2,10)
@@ -138,7 +160,7 @@ chilli <- rnorm(2000, 20, 10) #x-axis
 #colors <- c("#cc6a70ff", "#7e4e90ff","#55C667FF","#287D8EFF", "cyan4")
 
 #plot(chilli,yPhenoi, type = "n", xlim = c(-15,15), ylim =c(-150, 100))
-par(mfrow = c(1,3))
+#par(mfrow = c(1,3))
 
 betaTraitxChill <- c(-0.45, -0.85, 0, 0.45, 0.85)
   b <- 1
@@ -160,20 +182,23 @@ betaTraitxChill <- c(-0.45, -0.85, 0, 0.45, 0.85)
   betaChillSpT0Hi = alphaChillSp + betaTraitxChillT0 * mu_grand_sp[gm]
   yPhenoiT0Hi <- alphaPhenoSp[a] + betaForceSp * forcei + betaPhotoSp * photoi + betaChillSpT0 * chilli
   
-  plot(chilli,yPhenoi, type = "n", xlim = c(-11,17), ylim =c(0, 100), ylab = "Day of budburst", xlab = "Standardized cue")
+  plot(chilli,yPhenoi, type = "n", xlim = c(-11,17), ylim =c(0, 100), ylab = "Day of budburst", xlab = "Standardized cue", cex.lab = 1.2)
+  my.label <- paste("a", ".", sep="")
+  put.fig.letter(label=my.label, location= "topleft", font=2)
+  
   points(chilli,yPhenoi, type = "l", col= "purple4", lwd = 3)
   points(chilli,yPhenoiT0, type = "l", col= "mediumpurple1", lwd = 3)
   points(chilli,yPhenoiHi, type = "l", col= "forestgreen", lwd = 3)
   points(chilli,yPhenoiT0Hi, type = "l", col= "palegreen3", lwd = 3)
   
-  legend("topright",legend = c(expression("Conservative (betaTraitxCue = +ve)"),
+  legend("topright",legend = c(expression("Conservative (- betaTraitxCue)"),
                                expression("Conservative (trait effect = 0)"),
-                               expression("Acquisitive (betaTraitxCue = +ve)"),
+                               expression("Acquisitive (- betaTraitxCue)"),
                                expression("Acquisitive (trait effect = 0)")),
          
          #col = c("black", "black", "black", "black"),
          #pt.bg = c("#042333ff","#cc6a70ff","#593d9cff","#f9b641ff","#13306dff","#efe350ff","#eb8055ff"),
-         col = c("forestgreen", "palegreen3", "mediumpurple1", "purple4"),
+         col = c("forestgreen", "palegreen3", "purple4", "mediumpurple1"),
          inset = 0.02, lty = c(1,1,1,1), lwd = c(5,5,5,5), cex = 1, bty = "n")
   
 ## Now the slope is 0
@@ -195,12 +220,15 @@ betaTraitxChill <- c(-0.45, -0.85, 0, 0.45, 0.85)
   betaChillSpT0Hi = alphaChillSp + betaTraitxChillT0 * mu_grand_sp[gm]
   yPhenoiT0Hi <- alphaPhenoSp[a] + betaForceSp * forcei + betaPhotoSp * photoi + betaChillSpT0 * chilli
   
-  plot(chilli,yPhenoi, type = "n", xlim = c(-11,17), ylim =c(0, 100), ylab = "Day of budburst", xlab = "Standardized cue")
-  points(chilli,yPhenoi, type = "l", col= "purple4", lwd = 3)
+  plot(chilli,yPhenoi, type = "n", xlim = c(-11,17), ylim =c(0, 100), ylab = "Day of budburst", xlab = "Standardized cue", cex.lab = 1.2)
+  my.label <- paste("b", ".", sep="")
+  put.fig.letter(label=my.label, location= "topleft", font=2)
+  
   points(chilli,yPhenoiT0, type = "l", col= "mediumpurple1", lwd = 3)
-  points(chilli,yPhenoiHi, type = "l", col= "forestgreen", lwd = 3)
+  points(chilli,yPhenoi, type = "l", col= "purple4", lwd = 3)
   points(chilli,yPhenoiT0Hi, type = "l", col= "palegreen3", lwd = 3)
-
+  points(chilli,yPhenoiHi, type = "l", col= "forestgreen", lwd = 3)
+ 
   legend("topright",legend = c(expression("Conservative (betaTraitxCue = 0)"),
                                expression("Conservative (trait effect = 0)"),
                                expression("Acquisitive (betaTraitxCue = 0)"),
@@ -208,7 +236,7 @@ betaTraitxChill <- c(-0.45, -0.85, 0, 0.45, 0.85)
          
          #col = c("black", "black", "black", "black"),
          #pt.bg = c("#042333ff","#cc6a70ff","#593d9cff","#f9b641ff","#13306dff","#efe350ff","#eb8055ff"),
-         col = c("forestgreen", "palegreen3", "mediumpurple1", "purple4"),
+         col = c("forestgreen", "palegreen3", "purple4", "mediumpurple1"),
          inset = 0.02, lty = c(1,1,1,1), lwd = c(5,5,5,5), cex = 1, bty = "n")
   
   ## Now the slope is +ve
@@ -233,20 +261,23 @@ betaTraitxChill <- c(-0.45, -0.85, 0, 0.45, 0.85)
   betaChillSpT0Hi = alphaChillSp + betaTraitxChillT0 * mu_grand_sp[gm]
   yPhenoiT0Hi <- alphaPhenoSp[a] + betaForceSp * forcei + betaPhotoSp * photoi + betaChillSpT0 * chilli
   
-  plot(chilli,yPhenoi, type = "n", xlim = c(-10,10), ylim =c(0, 100), ylab = "Day of budburst", xlab = "Standardized cue")
+  plot(chilli,yPhenoi, type = "n", xlim = c(-10,10), ylim =c(0, 100), ylab = "Day of budburst", xlab = "Standardized cue", cex.lab = 1.2)
+  my.label <- paste("c", ".", sep="")
+  put.fig.letter(label=my.label, location= "topleft", font=2)
+  
   points(chilli,yPhenoi, type = "l", col= "purple4", lwd = 3, lty = 1)
   points(chilli,yPhenoiT0, type = "l", col= "mediumpurple1", lwd = 3)
   points(chilli,yPhenoiHi, type = "l", col= "forestgreen", lwd = 3)
   points(chilli,yPhenoiT0Hi, type = "l", col= "palegreen3", lwd = 3)
   
-  legend("topright",legend = c(expression("Conservative (betaTraitxCue = -ve)"),
+  legend("topright",legend = c(expression("Conservative (+ betaTraitxCue)"),
                                expression("Conservative (trait effect = 0)"),
-                               expression("Acquisitive (betaTraitxCue = -ve)"),
+                               expression("Acquisitive (+ betaTraitxCue)"),
                                expression("Acquisitive (trait effect = 0)")),
          
          #col = c("black", "black", "black", "black"),
          #pt.bg = c("#042333ff","#cc6a70ff","#593d9cff","#f9b641ff","#13306dff","#efe350ff","#eb8055ff"),
-         col = c("forestgreen", "palegreen3", "mediumpurple1", "purple4"),
+         col = c("forestgreen", "palegreen3", "purple4", "mediumpurple1"),
          inset = 0.02, lty = c(1,1,1,1), lwd = c(5,5,5,5), cex = 1, bty = "n")
-dev.off()
+#dev.off()
   
